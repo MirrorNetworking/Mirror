ddb7ec1c1c38de3f6f2b4815efbdec2fe39d55da
Matthew Coburn <coburn@dandere.net>
2018-07-26 14:00:11 +1000

    Add the HLAPI Batch Installer (may need admin privileges)

---
+++ install-hlapi-community.cmd
@@ -0,0 +1,15 @@
+@echo off
+cd %~dp0
+set /p UNITY_BASE_DIR="Please enter the location of your Unity Installation (do not include the Editor folder suffix): "
+echo Okay, installing into %UNITY_BASE_DIR%. Sit tight - if you get access denied errors, please run this again as Administrator.
+echo Copying base DLLs
+copy /v /y .\Unity-Technologies-networking\Output\UnityEngine.Networking.* "%UNITY_BASE_DIR%\Editor\Data\UnityExtensions\Unity\Networking"
+echo Copying Editor DLLs
+copy /v /y .\Unity-Technologies-networking\Output\Editor\*.* "%UNITY_BASE_DIR%\Editor\Data\UnityExtensions\Unity\Networking\Editor"
+echo Copying Standalone DLLs
+copy /v /y .\Unity-Technologies-networking\Output\Standalone\*.* "%UNITY_BASE_DIR%\Editor\Data\UnityExtensions\Unity\Networking\Standalone"
+echo Copying Weaver DLLs
+copy /v /y .\Unity-Technologies-networking\Output\Weaver\*.* "%UNITY_BASE_DIR%\Editor\Data\Managed"
+echo If there are no errors, installation is complete. Otherwise, please check the base directory you entered.
+pause
+exit
