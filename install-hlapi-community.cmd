d015f740e4689b21e06152461db190f1e4b18e60
Paul Pacheco <paulpach@gmail.com>
2018-07-27 08:11:23 -0500

    Simplify prompting for installation path

--- install-hlapi-community.cmd
+++ install-hlapi-community.cmd
@@ -5,34 +5,26 @@ title Unity HLAPI Community Edition Installer
 :: Admin mode starts off in %windir%\system32 for some reason
 cd %~dp0
 echo Installer started from %~dp0
+
 :: Where oh where are you Unity?
 if exist "C:\Program Files\Unity" (
   :: Unity 32bit on x86 OS, or Unity 64bit on x64 OS.
-  echo Found a native Unity in your Program Files directory. Is this correct?
-  CHOICE /C YN /M "Is this correct?"
-  if errorlevel 2 goto promptForPath
-  if errorlevel 1 goto prepUnityPath3264ForInstall 
-  goto installDLLs
+  set UNITY_BASE_DIR="C:\Program Files\Unity"
 ) else if exist "C:\Program Files (x86)\Unity" (
   :: Unity 32bit on x64 OS.
-  echo Found Unity in your Program Files directory. This seems to be Unity 32-Bit on a x64 Operating System.
-  CHOICE /C YN /M "Is this correct?"
-  if errorlevel 2 goto promptForPath
-  if errorlevel 1 goto prepUnityPath32ForInstall 
-) else (
-  :: Can't find it!
-  goto promptForPath
+  set UNITY_BASE_DIR="C:\Program Files (x86)\Unity"
 )
 
-:prepUnityPath3264ForInstall
-set UNITY_BASE_DIR=C:\Program Files\Unity
-goto installDLLs
+:: ask for unity base dir,  but default to the detected one
+set /p UNITY_BASE_DIR="Enter Unity installation path or press [ENTER] for default [%UNITY_BASE_DIR%]: " 
 
-:prepUnityPath32ForInstall
-set UNITY_BASE_DIR=C:\Program Files (x86)\Unity
-goto installDLLs
+if not exist %UNITY_BASE_DIR% (
+  echo "Could not find unity"
+  pause
+  exit /B 1
+)
 
-:installDLLs
+:: Install files in unity folder
 echo Okay, installing into %UNITY_BASE_DIR%.
 echo Sit tight - if you get access denied errors, please run this again as Administrator.
 echo Copying base DLLs
@@ -46,8 +38,3 @@ copy /v /y %~dp0\Unity-Technologies-networking\Output\Weaver\*.* "%UNITY_BASE_DI
 echo If there are no errors, installation is complete. Otherwise, please check the base directory you entered.
 pause
 exit
-
-:promptForPath
-echo We couldn't find the location of your Unity Install. Please enter the path below, but do not include the Editor folder suffix. A correct path would be "C:\UnityLTS2017.4", a wrong path would be "C:\UnityLTS2017.4\Editor".
-set /p UNITY_BASE_DIR="Installation path? "
-goto installDLLs
