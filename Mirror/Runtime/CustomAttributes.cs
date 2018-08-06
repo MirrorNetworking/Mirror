527fd2417e116a29509101747261db524f966a81
Paul Pacheco <paulpach@gmail.com>
2018-08-06 16:00:38 -0500

    Generate syncOwnerMask in every network behavior

--- Mirror/Runtime/CustomAttributes.cs
+++ Mirror/Runtime/CustomAttributes.cs
@@ -9,10 +9,14 @@ namespace Mirror
         public float sendInterval = 0.1f;
     }
 
+    // SyncTarget enum is cleaner than 'bool onlyToOwner and allows for more options if needed
+    public enum SyncTarget {Observers, Owner};
+
     [AttributeUsage(AttributeTargets.Field)]
     public class SyncVarAttribute : Attribute
     {
         public string hook;
+        public SyncTarget target = SyncTarget.Observers; // for 'only sync to owner' support
     }
 
     [AttributeUsage(AttributeTargets.Method)]
