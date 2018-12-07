f4a708abda9d6960ee27a8871913d333d65e0a06
Paul Pacheco <paulpach@gmail.com>
2018-12-07 11:59:15 -0600

    Read data immediatelly if available

--- Assets/Mirror/Telepathy/NetworkStreamExtensions.cs
+++ Assets/Mirror/Telepathy/NetworkStreamExtensions.cs
@@ -1,34 +1,50 @@
 ﻿using System.IO;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using Telepathy;
 
-public static class NetworkStreamExtensions
+namespace Telepathy
 {
-    // helper function to read EXACTLY 'n' bytes
-    // -> default .Read reads up to 'n' bytes. this function reads exactly 'n'
-    //    bytes
-    // -> either return all the bytes requested or null if end of stream
-    public static async Task<byte[]> ReadExactlyAsync(this NetworkStream stream, int size)
+
+    public static class NetworkStreamExtensions
     {
-        byte[] data = new byte[size];
-
-        int offset = 0;
-
-        // keep reading until we fill up the buffer;
-        while (offset < size)
+        // helper function to read EXACTLY 'n' bytes
+        // -> default .Read reads up to 'n' bytes. this function reads exactly 'n'
+        //    bytes
+        // -> either return all the bytes requested or null if end of stream
+        public static async Task<byte[]> ReadExactlyAsync(this NetworkStream stream, int size)
         {
-            int received = await stream.ReadAsync(data, offset, size - offset);
+            byte[] data = new byte[size];
 
-            // we just got disconnected
-            if (received == 0)
+            int offset = 0;
+
+            // keep reading until we fill up the buffer;
+            while (offset < size)
             {
-                return null;
+                int received = 0;
+
+                if (stream.DataAvailable)
+                {
+                    // read available data immediatelly
+                    received = stream.Read(data, offset, size - offset);
+                }
+                else
+                {
+                    // wait for more data
+                    received = await stream.ReadAsync(data, offset, size - offset);
+                }
+
+                // we just got disconnected
+                if (received == 0)
+                {
+                    return null;
+                }
+
+                offset += received;
             }
 
-            offset += received;
+            return data;
         }
 
-        return data;
     }
-
 }
