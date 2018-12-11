6fc74e7b6633e8326d93d41ed6f0be847af6a46b
Paul Pacheco <paulpach@gmail.com>
2018-12-11 08:29:58 -0600

    Remove extra space.
    Apaciguate vis OCD

--- Assets/Mirror/Editor/Weaver/CompilationFinishedHook.cs
+++ Assets/Mirror/Editor/Weaver/CompilationFinishedHook.cs
@@ -45,10 +45,10 @@ namespace Mirror.Weaver
                 string mirrorRuntimeDll = FindMirrorRuntime();
                 if (!File.Exists(mirrorRuntimeDll))
                 {
-                    // this is normal,  it happens with any assembly that is built before mirror
+                    // this is normal, it happens with any assembly that is built before mirror
                     // such as unity packages or your own assemblies
                     // those don't need to be weaved
-                    // if any assembly depends on mirror,  then it will be built after
+                    // if any assembly depends on mirror, then it will be built after
                     return;
                 }
 
