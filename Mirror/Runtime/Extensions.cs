1b33f7d6338fb7665cae84567fc9f2f9f879bd5b
vis2k <info@noobtuts.com>
2018-12-30 16:24:28 +0100

    HLAPI: GetStableHashCode added too

---
+++ Mirror/Runtime/Extensions.cs
@@ -0,0 +1,20 @@
+namespace Mirror
+{
+
+    public static class Extensions
+    {
+        // string.GetHashCode is not guaranteed to be the same on all machines, but
+        // we need one that is the same on all machines. simple and stupid:
+        // IMPORTANT: needs to be same one as in Weaver
+        public static int GetStableHashCode(this string text)
+        {
+            unchecked
+            {
+                int hash = 23;
+                foreach (char c in text)
+                    hash = hash * 31 + c;
+                return hash;
+            }
+        }
+    }
+}
