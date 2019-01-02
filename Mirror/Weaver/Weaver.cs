7d905626ae463502243ef7523b47421b5c4cfcc2
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2019-01-02 19:26:05 +0100

    Weaver - ?: (#225)
    * ?:
    
    * Pauls suggestion
    
    * Update Weaver.cs

--- Mirror/Weaver/Weaver.cs
+++ Mirror/Weaver/Weaver.cs
@@ -643,14 +643,8 @@ namespace Mirror.Weaver
                     continue;
 
                 // mismatched ldloca/ldloc for struct/class combinations is invalid IL, which causes crash at runtime
-                if (variable.IsValueType)
-                {
-                    worker.Append(worker.Create(OpCodes.Ldloca, 0));
-                }
-                else
-                {
-                    worker.Append(worker.Create(OpCodes.Ldloc, 0));
-                }
+                OpCode opcode = variable.IsValueType ? OpCodes.Ldloca : OpCodes.Ldloc;
+                worker.Append(worker.Create(opcode, 0));
 
                 var readFunc = GetReadFunc(field.FieldType);
                 if (readFunc != null)
