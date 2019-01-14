904fd731228463e81cb0614f7002e09918e8767d
vis2k <info@noobtuts.com>
2019-01-14 14:40:51 +0100

    Custom on(De)serialize test

---
+++ Assets/TestCustom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class TestCustom : NetworkBehaviour
+{
+    // the syncvar
+    [SyncVar] GameObject test;
+
+    // server-side serialization
+    public override bool OnSerialize(NetworkWriter writer, bool initialState)
+    {
+        writer.Write(test);
+        return true;
+    }
+
+    // client-side deserialization
+    public override void OnDeserialize(NetworkReader reader, bool initialState)
+    {
+        test = reader.ReadGameObject();
+    }
+}
