a742ea99481309e805ffa62dd6a121bbbd2a9cf7
vis2k <info@noobtuts.com>
2019-01-14 13:43:44 +0100

    Test file

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Test : NetworkBehaviour
+{
+    // the syncvar
+    [SyncVar] GameObject test;
+    [SyncVar] NetworkIdentity test2;
+
+    // a function that uses it
+    void Update()
+    {
+        // read and write once
+        Debug.Log(test.name);
+        GameObject read = test;
+        test = gameObject;
+
+        // read and write once
+        Debug.Log(test2.name);
+        NetworkIdentity read2 = test2;
+        test2 = netIdentity;
+    }
+}
