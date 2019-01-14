4664b8da45d2bc3ebd0807bcb4ab35a09ee15130
vis2k <info@noobtuts.com>
2019-01-14 19:24:59 +0100

    public gameobject field test

---
+++ Assets/Tests/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    public GameObject petNonSync;
+    [SyncVar] public GameObject petSync;
+
+    void Update()
+    {
+        Debug.Log("Player.petNonSync=" + petNonSync + " petSync=" + petSync);
+    }
+}
