5b187fe0dfddfd1f4e1d27e3b2a39585c4de7614
vis2k <info@noobtuts.com>
2019-01-15 11:03:55 +0100

    Asset Store Examples added

---
+++ Assets/Mirror/Examples/Movement/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Move : NetworkBehaviour
+{
+    public CharacterController controller;
+    public float speed = 5;
+    public float rotationSpeed = 6;
+
+    void Update()
+    {
+        // movement for local player
+        if (!isLocalPlayer) return;
+
+        // rotate
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+
+        // move
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.SimpleMove(forward * Input.GetAxis("Vertical") * speed);
+    }
+}
