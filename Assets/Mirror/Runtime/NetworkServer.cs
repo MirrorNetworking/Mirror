ffced61058b37fc3086174d00d78af74a409f4fe
vis2k <info@noobtuts.com>
2019-01-15 12:24:29 +0100

    NetworkTransform moved into Components folder so it can be weaved. Removed unnecessary messages.

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -78,7 +78,6 @@ namespace Mirror
         {
             RegisterHandler(MsgType.Ready, OnClientReadyMessage);
             RegisterHandler(MsgType.Command, OnCommandMessage);
-            RegisterHandler(MsgType.LocalPlayerTransform, NetworkTransformBase.OnClientToServerSync);
             RegisterHandler(MsgType.RemovePlayer, OnRemovePlayerMessage);
             RegisterHandler(MsgType.Animation, NetworkAnimator.OnAnimationServerMessage);
             RegisterHandler(MsgType.AnimationParameters, NetworkAnimator.OnAnimationParametersServerMessage);
