50bf5861df98f5c148f0cf6368bb1d563c7f0341
Paul Pacheco <paulpach@gmail.com>
2019-01-19 14:41:57 -0600

    Example for weaver issue

---
+++ Assets/Mirror/Examples/WeaveTest/WeaveMe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public partial class LoginMsg : MessageBase
+{
+    public static short MsgId = 1000;
+    public string account;
+    public string password;
+    public string version;
+}
+
+public class Weaveme : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("BREAK");
+
+        // serialize
+        LoginMsg msg = new LoginMsg();
+        msg.version = "42";
+        NetworkWriter writer = new NetworkWriter();
+        msg.Serialize(writer);
+
+        // deserialize
+        LoginMsg other = new LoginMsg();
+        other.Deserialize(new NetworkReader(writer.ToArray()));
+        Debug.Log("version=" + other.version + " expected=" + msg.version);
+
+        //NetworkMessage netmsg = new NetworkMessage();
+        //netmsg.reader =
+    }
+}
