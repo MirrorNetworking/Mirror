eb8687b0c6b4857989c5f19a46300dc414b65d6a
vis2k <info@noobtuts.com>
2019-01-27 10:31:32 +0100

    Transport as events - via UnityEvent

--- Assets/Mirror/Runtime/Transport/Transport.cs
+++ Assets/Mirror/Runtime/Transport/Transport.cs
@@ -1,26 +1,42 @@
 ﻿// abstract transport layer component
 // note: not all transports need a port, so add it to yours if needed.
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Mirror
 {
-    // note: 'address' is ip / websocket url / ...
-    public enum TransportEvent { Connected, Data, Disconnected }
+    // UnityEvent definitions
+    [Serializable] public class UnityEventByteArray : UnityEvent<byte[]> {}
+    [Serializable] public class UnityEventException : UnityEvent<Exception> {}
+    [Serializable] public class UnityEventInt : UnityEvent<int> {}
+    [Serializable] public class UnityEventIntByteArray : UnityEvent<int, byte[]> {}
+    [Serializable] public class UnityEventIntException : UnityEvent<int, Exception> {}
 
     public abstract class Transport : MonoBehaviour
     {
         // client
+        [Header("Client Events")]
+        public UnityEvent OnClientConnected;
+        public UnityEventByteArray OnClientDataReceived;
+        public UnityEventException OnClientError;
+        public UnityEvent OnClientDisconnected;
+
         public abstract bool ClientConnected();
         public abstract void ClientConnect(string address);
         public abstract bool ClientSend(int channelId, byte[] data);
-        public abstract bool ClientGetNextMessage(out TransportEvent transportEvent, out byte[] data);
         public abstract void ClientDisconnect();
 
         // server
+        [Header("Server Events")]
+        public UnityEventInt OnServerConnected;
+        public UnityEventIntByteArray OnServerDataReceived;
+        public UnityEventIntException OnServerError;
+        public UnityEventInt OnServerDisconnected;
+
         public abstract bool ServerActive();
         public abstract void ServerStart();
         public abstract bool ServerSend(int connectionId, int channelId, byte[] data);
-        public abstract bool ServerGetNextMessage(out int connectionId, out TransportEvent transportEvent, out byte[] data);
         public abstract bool ServerDisconnect(int connectionId);
         public abstract bool GetConnectionInfo(int connectionId, out string address);
         public abstract void ServerStop();
