52887c2292362dff26cf49ebce13e21868e9d2fa
Paul Pacheco <paulpach@gmail.com>
2019-01-30 19:10:53 -0600

    Body expressions

--- Assets/Mirror/Runtime/NetworkClient.cs
+++ Assets/Mirror/Runtime/NetworkClient.cs
@@ -90,10 +90,8 @@ namespace Mirror
             connectState = ConnectState.Disconnected;
 
             ClientScene.HandleClientDisconnect(m_Connection);
-            if (m_Connection != null)
-            {
-                m_Connection.InvokeHandlerNoData((short)MsgType.Disconnect);
-            }
+           
+            m_Connection?.InvokeHandlerNoData((short)MsgType.Disconnect);
         }
 
         void OnDataReceived(byte[] data)
