7f5682bc764c2cb4ae554b355506a7d25b95a46a
Chris Langsenkamp <9826063+MrGadget1024@users.noreply.github.com>
2019-02-04 19:59:32 -0500

    Added documentation to the extension

--- Assets/Mirror/Examples/Lobbey/Scripts/NetworkLobbyManagerExt.cs
+++ Assets/Mirror/Examples/Lobbey/Scripts/NetworkLobbyManagerExt.cs
@@ -5,6 +5,14 @@ namespace Mirror.Examples.NetworkLobby
 {
     public class NetworkLobbyManagerExt : NetworkLobbyManager
     {
+        /// <summary>
+        /// Called just after GamePlayer object is instantiated and just before it replaces LobbyPlayer object.
+        /// This is the ideal point to pass any data like player name, credentials, tokens, colors, etc.
+        /// into the GamePlayer object as it is about to enter the Online scene.
+        /// </summary>
+        /// <param name="lobbyPlayer"></param>
+        /// <param name="gamePlayer"></param>
+        /// <returns>true unless some code in here decides it needs to abort the replacement</returns>
         public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer)
         {
             PlayerController player = gamePlayer.GetComponent<PlayerController>();
