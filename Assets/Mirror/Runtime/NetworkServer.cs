7ed1a72c997852501a0a4ffc7e57ef236a9bf545
Chris Langsenkamp <9826063+MrGadget1024@users.noreply.github.com>
2019-02-14 18:44:27 -0500

    Replaced vars with explicit types

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -555,7 +555,7 @@ namespace Mirror
         {
             if (LogFilter.Debug) { Debug.Log("NetworkServer SetupLocalPlayerForConnection netID:" + identity.netId); }
 
-            var localConnection = conn as ULocalConnectionToClient;
+            ULocalConnectionToClient localConnection = conn as ULocalConnectionToClient;
             if (localConnection != null)
             {
                 if (LogFilter.Debug) { Debug.Log("NetworkServer AddPlayer handling ULocalConnectionToClient"); }
@@ -672,7 +672,7 @@ namespace Mirror
 
             conn.isReady = true;
 
-            var localConnection = conn as ULocalConnectionToClient;
+            ULocalConnectionToClient localConnection = conn as ULocalConnectionToClient;
             if (localConnection != null)
             {
                 if (LogFilter.Debug) { Debug.Log("NetworkServer Ready handling ULocalConnectionToClient"); }
