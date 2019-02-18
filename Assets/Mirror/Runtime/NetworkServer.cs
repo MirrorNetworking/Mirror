c70057da63234b550732e76b790edaa59472df25
Paul Pacheco <paulpach@gmail.com>
2019-02-18 05:08:32 -0600

    Convert spawn finished message to new api

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -789,7 +789,7 @@ namespace Mirror
                 }
             }
 
-            conn.Send((short)MsgType.SpawnFinished, new ObjectSpawnFinishedMessage());
+            conn.Send(new ObjectSpawnFinishedMessage());
         }
 
         internal static void ShowForConnection(NetworkIdentity identity, NetworkConnection conn)
