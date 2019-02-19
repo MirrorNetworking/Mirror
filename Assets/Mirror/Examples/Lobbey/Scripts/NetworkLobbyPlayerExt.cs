1e05a9d6d66bf7eb82e3c5413854766948204f08
Chris Langsenkamp <9826063+MrGadget1024@users.noreply.github.com>
2019-02-19 09:50:16 -0500

    wrapped in namespace

--- Assets/Mirror/Examples/Lobbey/Scripts/NetworkLobbyPlayerExt.cs
+++ Assets/Mirror/Examples/Lobbey/Scripts/NetworkLobbyPlayerExt.cs
@@ -1,38 +1,41 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Mirror;
 using UnityEngine.SceneManagement;
 
-public class NetworkLobbyPlayerExt : NetworkLobbyPlayer
+namespace Mirror.Examples.NetworkLobby
 {
-    public string Name;
-    public override void OnStartClient()
+    public class NetworkLobbyPlayerExt : NetworkLobbyPlayer
     {
-        if (LogFilter.Debug) Debug.LogFormat("OnStartClient {0}", SceneManager.GetActiveScene().name);
+        public string Name;
+        public override void OnStartClient()
+        {
+            if (LogFilter.Debug) Debug.LogFormat("OnStartClient {0}", SceneManager.GetActiveScene().name);
 
-        base.OnStartClient();
-        NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
+            base.OnStartClient();
+            NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
 
-        /*
-            This demonstrates how to set the parent of the LobbyPlayerPrefab to an arbitrary scene object
-            A similar technique would be used if a full canvas layout UI existed and we wanted to show
-            something more visual for each player in that layout, such as a name, avatar, etc.
+            /*
+                This demonstrates how to set the parent of the LobbyPlayerPrefab to an arbitrary scene object
+                A similar technique would be used if a full canvas layout UI existed and we wanted to show
+                something more visual for each player in that layout, such as a name, avatar, etc.
 
-            Note: LobbyPlayer prefab will be marked DontDestroyOnLoad and carried forward to the game scene.
-                  Because of this, NetworkLobbyManager must automatically set the parent to null 
-                  in ServerChangeScene and OnClientChangeScene.
-        */
+                Note: LobbyPlayer prefab will be marked DontDestroyOnLoad and carried forward to the game scene.
+                      Because of this, NetworkLobbyManager must automatically set the parent to null 
+                      in ServerChangeScene and OnClientChangeScene.
+            */
 
-        if (lobby && lobby.LobbyScene == SceneManager.GetActiveScene().name)
-            gameObject.transform.SetParent(GameObject.Find("Players").transform);
-    }
+            if (lobby && lobby.LobbyScene == SceneManager.GetActiveScene().name)
+                gameObject.transform.SetParent(GameObject.Find("Players").transform);
+        }
 
-    public override void OnClientEnterLobby()
-    {
-        Debug.LogFormat("OnClientEnterLobby {0}", SceneManager.GetActiveScene().name);
-    }
+        public override void OnClientEnterLobby()
+        {
+            Debug.LogFormat("OnClientEnterLobby {0}", SceneManager.GetActiveScene().name);
+        }
 
-    public override void OnClientExitLobby()
-    {
-        Debug.LogFormat("OnClientExitLobby {0}", SceneManager.GetActiveScene().name);
+        public override void OnClientExitLobby()
+        {
+            Debug.LogFormat("OnClientExitLobby {0}", SceneManager.GetActiveScene().name);
+        }
     }
 }
