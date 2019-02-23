28f38751109266a623a9ab819576bdeb2d62155b
Paul Pacheco <paulpach@gmail.com>
2019-02-22 18:51:09 -0600

    remove redundant initialization

--- Assets/Mirror/Examples/Lobby/Scripts/Spawner.cs
+++ Assets/Mirror/Examples/Lobby/Scripts/Spawner.cs
@@ -6,8 +6,8 @@ namespace Mirror.Examples.NetworkLobby
     {
         public NetworkIdentity prizePrefab;
 
-        float x = 0f;
-        float z = 0f;
+        float x;
+        float z;
         GameObject newPrize;
         Reward reward;
 
