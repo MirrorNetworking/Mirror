b673167a9eca82c373b4208a91270e3563b93367
Paul Pacheco <paulpach@gmail.com>
2019-02-22 18:48:46 -0600

    Fix indentation

--- Assets/Mirror/Examples/Lobby/Scripts/NetworkLobbyManagerExt.cs
+++ Assets/Mirror/Examples/Lobby/Scripts/NetworkLobbyManagerExt.cs
@@ -32,26 +32,26 @@ namespace Mirror.Examples.NetworkLobby
 
         bool showStartButton = false;
 
-    public override void OnLobbyServerPlayersReady()
-    {
-        // calling the base method calls ServerChangeScene as soon as all players are in Ready state.
-        if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null && startOnHeadless)
-            base.OnLobbyServerPlayersReady();
-        else
-            showStartButton = true;
-    }
-
-    public override void OnGUI()
-    {
-        base.OnGUI();
-
-        if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+        public override void OnLobbyServerPlayersReady()
         {
-            // set to false to hide it in the game scene
-            showStartButton = false;
-
-            ServerChangeScene(GameplayScene);
+            // calling the base method calls ServerChangeScene as soon as all players are in Ready state.
+            if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null && startOnHeadless)
+                base.OnLobbyServerPlayersReady();
+            else
+                showStartButton = true;
         }
-    }
+
+        public override void OnGUI()
+        {
+            base.OnGUI();
+
+            if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+            {
+                // set to false to hide it in the game scene
+                showStartButton = false;
+
+                ServerChangeScene(GameplayScene);
+            }
+        }
     }
 }
