dbd5f57e96fa7b564e7dd7184f94d45f47756661
rodolphito <rodol@rivalrebels.com>
2019-02-27 06:11:52 -0800

    Remove List.ForEach on hot path. (#467)
    * Remove LINQ on hot path.
    
    * Fixed erroneous classification of List.ForEach as LINQ.
    
    * Added back using so that it could be removed by #452.

--- Assets/Mirror/Runtime/NetworkBehaviour.cs
+++ Assets/Mirror/Runtime/NetworkBehaviour.cs
@@ -394,7 +394,13 @@ namespace Mirror
             syncVarDirtyBits = 0L;
 
             // flush all unsynchronized changes in syncobjects
-            m_SyncObjects.ForEach(obj => obj.Flush());
+            // note: don't use List.ForEach here, this is a hot path
+            // List.ForEach: 432b/frame
+            // for: 231b/frame
+            for (int i = 0; i < m_SyncObjects.Count; ++i)
+            {
+                m_SyncObjects[i].Flush();
+            }
         }
 
         internal bool AnySyncObjectDirty()
