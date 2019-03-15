a4ec4cc7c97a0fbf01c2129809186b85c73fee2e
vis2k <info@noobtuts.com>
2019-03-15 14:50:39 +0100

    create basic example

---
+++ Assets/Mirror/Examples/ListenServer/Listen.cs
@@ -0,0 +1,48 @@
+﻿// add this component to the NetworkManager
+using UnityEngine;
+
+namespace Mirror.Examples.Listen
+{
+    public struct ServerInfo
+    {
+        public string ip;
+        public ushort port;
+        public string title;
+        public ushort players;
+        public ushort capacity;
+    }
+
+    [RequireComponent(typeof(NetworkManager))]
+    public class Listen : MonoBehaviour
+    {
+        // game server to listen server connection
+
+        // client to listen server connection
+        // (only active while receiving game server lists)
+
+        public Rect window = new Rect(5, 150, 400, 400);
+
+        void Update()
+        {
+            // TODO send server data to listen
+        }
+
+        void OnGUI()
+        {
+            // TODO show listen data on client
+            if (!NetworkManager.IsHeadless() && !NetworkServer.active && !NetworkClient.active)
+            {
+                GUILayout.BeginArea(window);
+                GUILayout.BeginVertical("box");
+
+                // header
+                GUILayout.Label("Gameserver List:");
+
+
+
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+            }
+        }
+    }
+}
