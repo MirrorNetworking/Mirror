7e9b3efa7d3c3513a23e6e3a14f2e73a6f563dcc
vis2k <info@noobtuts.com>
2019-03-17 14:24:15 +0100

    pong: fixed racket spawn position after reconnecting

---
+++ Assets/Mirror/Examples/Pong/Scripts/NetworkManagerPong.cs
@@ -0,0 +1,18 @@
+﻿// custom NetworkManager that simply assigns the correct racket positions when
+// spawning players. the built in RoundRobin spawn method wouldn't work after
+// someone reconnects (both players would be on the same side).
+using UnityEngine;
+using Mirror;
+
+public class NetworkManagerPong : NetworkManager
+{
+    public Transform leftRacketSpawn;
+    public Transform rightRacketSpawn;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage extraMessage)
+    {
+        Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+}
