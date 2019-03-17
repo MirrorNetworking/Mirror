a773df49055718bbbdac6db6de396b9664da24ba
vis2k <info@noobtuts.com>
2019-03-17 21:28:39 +0100

    movement: use animations

--- Assets/Mirror/Examples/Movement/Scripts/Move.cs
+++ Assets/Mirror/Examples/Movement/Scripts/Move.cs
@@ -6,7 +6,9 @@ namespace Mirror.Examples.Movement
     public class Move : NetworkBehaviour
     {
         public NavMeshAgent agent;
+        public Animator animator;
         public float rotationSpeed = 100;
+        public KeyCode shootKey = KeyCode.Space;
 
         void Update()
         {
@@ -19,6 +21,13 @@ namespace Mirror.Examples.Movement
             // move
             Vector3 forward = transform.TransformDirection(Vector3.forward);
             agent.velocity = forward * Input.GetAxis("Vertical") * agent.speed;
+            animator.SetBool("Moving", agent.velocity != Vector3.zero);
+
+            // shoot
+            if (Input.GetKeyDown(shootKey))
+            {
+                animator.SetTrigger("Shoot");
+            }
         }
     }
 }
