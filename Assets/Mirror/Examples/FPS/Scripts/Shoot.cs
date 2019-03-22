53e9fe20f0bc559efc99d3138013629cebe6a8c0
Rodol Phito <rodolvertice@gmail.com>
2019-03-22 04:39:27 -0700

    Added stack of boxes and made shots push them over.

--- Assets/Mirror/Examples/FPS/Scripts/Shoot.cs
+++ Assets/Mirror/Examples/FPS/Scripts/Shoot.cs
@@ -5,6 +5,8 @@ namespace Mirror.Examples.FPS
     public class Shoot : NetworkBehaviour
     {
         public float damagePerShot = 10f;
+        public float range = 100f;
+        public float pushStrength = 1000f;
         void Update()
         {
             if (!isLocalPlayer) return;
@@ -17,9 +19,14 @@ namespace Mirror.Examples.FPS
         [Command]
         void CmdShoot()
         {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 100f))
+            // Find what was shot at
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, range))
             {
+                // Deal damage if it has health
                 hit.collider.GetComponent<Health>()?.DealDamage(damagePerShot);
+
+                // Push it if it has a rigidbody
+                hit.collider.GetComponent<Rigidbody>()?.AddForceAtPosition(transform.forward * pushStrength, hit.point);
             }
         }
     }
