d9a342d5eb05a29f6b69d22be40bf07bab18b935
Rodol Phito <rodolvertice@gmail.com>
2019-03-30 10:01:59 -0700

    Empty braces

--- Assets/Mirror/Runtime/SyncList.cs
+++ Assets/Mirror/Runtime/SyncList.cs
@@ -87,7 +87,7 @@ namespace Mirror
         // so we need to skip them
         int changesAhead = 0;
 
-        protected virtual void SerializeItem(NetworkWriter writer, T item) { }
+        protected virtual void SerializeItem(NetworkWriter writer, T item) {}
         protected virtual T DeserializeItem(NetworkReader reader) => default(T);
 
         public bool IsDirty => changes.Count > 0;
