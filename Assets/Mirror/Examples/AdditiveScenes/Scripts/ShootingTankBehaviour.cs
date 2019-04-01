32d6c19e5f90d79abb31365aeb4ac8298f39b087
Chris Langsenkamp <chris@clevertech.net>
2019-03-31 22:07:37 -0400

    Initial Publish

---
+++ Assets/Mirror/Examples/AdditiveScenes/Scripts/ShootingTankBehaviour.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+namespace Mirror.Examples.Additive
+{
+    // This script demonstrates the NetworkAnimator and how to leverage
+    // the built-in observers system to track players.
+    // Note that all ProximityCheckers should be restricted to the Player layer.
+    public class ShootingTankBehaviour : NetworkBehaviour
+    {
+        [SyncVar]
+        public Quaternion Rotation;
+
+        NetworkAnimator networkAnimator;
+
+        [ServerCallback]
+        void Start()
+        {
+            networkAnimator = GetComponent<NetworkAnimator>();
+        }
+
+        [Range(0, 1)]
+        public float turnSpeed = 0.1f;
+
+        void Update()
+        {
+            if (isServer && netIdentity.observers.Count > 0)
+                ShootNearestPlayer();
+
+            if (isClient)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Rotation, turnSpeed);
+        }
+
+        NetworkConnection[] networkConnections = new NetworkConnection[] { };
+
+        [Server]
+        void ShootNearestPlayer()
+        {
+            GameObject target = null;
+            float distance = 100f;
+
+            // Avoid a foreach by copying the observers to an array for iteration
+            System.Array.Resize(ref networkConnections, netIdentity.observers.Count);
+            netIdentity.observers.Values.CopyTo(networkConnections, 0);
+
+            for (int i = 0; i < networkConnections.Length; i++)
+            {
+                GameObject tempTarget = networkConnections[i].playerController.gameObject;
+                float tempDistance = Vector3.Distance(tempTarget.transform.position, transform.position);
+
+                if (target == null || distance > tempDistance)
+                {
+                    target = tempTarget;
+                    distance = tempDistance;
+                }
+            }
+
+            if (target != null)
+            {
+                transform.LookAt(target.transform.position + Vector3.down);
+                Rotation = transform.rotation;
+                networkAnimator.SetTrigger("Fire");
+            }
+        }
+    }
+}
