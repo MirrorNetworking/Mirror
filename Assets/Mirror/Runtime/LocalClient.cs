1f9bc46ea967b791a5baec0c7f84ccb6cbc0d7fd
MrGadget <chris@clevertech.net>
2019-04-05 14:03:56 -0400

    Add LocalClient.cs Dummy for Asset Store
    This file should ship with 1.7 to the Asset Store, and can be removed when 1.8 is ready to publish to the store in a May 2019.

---
+++ Assets/Mirror/Runtime/LocalClient.cs
@@ -0,0 +1,5 @@
+// This file was removed in Mirror 1.7
+// The prupose of this file is to get the old file overwritten
+// when users update from the asset store to prevent a flood of errors
+// from having the old file still in the project as a straggler.
+// This file will be dropped from the Assert Store 1.8 package in May 2019
