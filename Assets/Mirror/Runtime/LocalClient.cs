d5fe2676ce4f95367e974b79fa898133d08ac547
MrGadget <chris@clevertech.net>
2019-04-05 18:04:04 -0400

    Remove LocalClient Dummy [DO NOT MERGE UNTIL MAY 2019]

--- Assets/Mirror/Runtime/LocalClient.cs
+++
@@ -1,5 +0,0 @@
-// This file was removed in Mirror 1.7
-// The prupose of this file is to get the old file overwritten
-// when users update from the asset store to prevent a flood of errors
-// from having the old file still in the project as a straggler.
-// This file will be dropped from the Asset Store 1.8 package in May 2019
