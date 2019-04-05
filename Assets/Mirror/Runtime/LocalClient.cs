355585f1fcd3daa2cbea9add701494206174f030
MrGadget <chris@clevertech.net>
2019-04-05 14:21:50 -0400

    Fixed Typo

--- Assets/Mirror/Runtime/LocalClient.cs
+++ Assets/Mirror/Runtime/LocalClient.cs
@@ -2,4 +2,4 @@
 // The prupose of this file is to get the old file overwritten
 // when users update from the asset store to prevent a flood of errors
 // from having the old file still in the project as a straggler.
-// This file will be dropped from the Assert Store 1.8 package in May 2019
+// This file will be dropped from the Asset Store 1.8 package in May 2019
