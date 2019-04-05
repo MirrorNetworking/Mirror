cef2acd74df7a15063f42b8731d4b4919ccdcdbf
Rodol Phito <rodolvertice@gmail.com>
2019-04-04 21:13:21 -0700

    We can hide this better by using this attribute

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -315,6 +315,7 @@ namespace Mirror
         //    if no scene is in build settings then Editor and Build have
         //    different indices too (Editor=0, Build=-1)
         // => ONLY USE THIS FROM POSTPROCESSSCENE!
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public void SetSceneIdSceneHashPartInternal()
         {
             // get deterministic scene hash
