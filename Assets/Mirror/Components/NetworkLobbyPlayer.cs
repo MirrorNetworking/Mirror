3846e2f90410f9df5796b534dbfdb56106a05450
MrGadget <chris@clevertech.net>
2019-04-07 15:48:17 -0400

    Fixed Lobby (#765)

--- Assets/Mirror/Components/NetworkLobbyPlayer.cs
+++ Assets/Mirror/Components/NetworkLobbyPlayer.cs
@@ -23,17 +23,10 @@ namespace Mirror
         /// </summary>
         public void Start()
         {
-            if (isClient) SceneManager.sceneLoaded += ClientLoadedScene;
-
             if (NetworkManager.singleton as NetworkLobbyManager)
                 OnClientEnterLobby();
             else
                 Debug.LogError("LobbyPlayer could not find a NetworkLobbyManager. The LobbyPlayer requires a NetworkLobbyManager object to function. Make sure that there is one in the scene.");
-        }
-
-        void OnDisable()
-        {
-            SceneManager.sceneLoaded -= ClientLoadedScene;
         }
 
         #endregion
@@ -46,13 +39,6 @@ namespace Mirror
             ReadyToBegin = ReadyState;
             NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
             lobby?.ReadyStatusChanged();
-        }
-
-        [Command]
-        public void CmdSendLevelLoaded()
-        {
-            NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
-            lobby?.PlayerLoadedScene(GetComponent<NetworkIdentity>().connectionToClient);
         }
 
         #endregion
@@ -73,16 +59,6 @@ namespace Mirror
         public virtual void OnClientExitLobby() {}
 
         public virtual void OnClientReady(bool readyState) {}
-
-        public virtual void ClientLoadedScene(Scene arg0, LoadSceneMode arg1)
-        {
-            NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
-            if (lobby != null && SceneManager.GetActiveScene().name == lobby.LobbyScene)
-                return;
-
-            if (this != null && isLocalPlayer)
-                CmdSendLevelLoaded();
-        }
 
         #endregion
 
