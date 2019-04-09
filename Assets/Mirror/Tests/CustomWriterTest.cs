df475e1b2b5ed92a1c33f36157be74d1ee1f4acb
Paul Pacheco <paulpach@gmail.com>
2019-04-09 04:58:54 -0500

    Fix typo

--- Assets/Mirror/Tests/CustomWriterTest.cs
+++ Assets/Mirror/Tests/CustomWriterTest.cs
@@ -47,7 +47,7 @@ namespace Mirror.Tests
 
             MyMessage unpacked = MessagePacker.Unpack<MyMessage>(data);
 
-            Assert.That(unpacked.myobj.name, Is.EqualTo("Hello World"), "Show be able to use user provider reader/writer");
+            Assert.That(unpacked.myobj.name, Is.EqualTo("Hello World"), "Should be able to use user provider reader/writer");
         }
     }
 }
