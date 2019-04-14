88325096e8805bc69b3e5f861ad3994706c8e1a8
petris <paprikapolishgamer@gmail.com>
2019-04-14 12:58:42 +0200

    Improve SceneID generation

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Serialization;
 #if UNITY_EDITOR
@@ -217,6 +218,25 @@ namespace Mirror
             return true;
         }
 
+        static uint GetRandomSceneID()
+        {
+            // use Crypto RNG to avoid having time based IDs
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] bytes = new byte[4];
+                uint id = 0;
+                // exclude '0' because that's for unassigned sceneIDs
+                // regenerate in case of an existing ID
+                while (id == 0 || sceneIds.ContainsKey(id))
+                {
+                    rng.GetBytes(bytes);
+                    id = BitConverter.ToUInt32(bytes, 0);
+                }
+
+                return id;
+            }
+        }
+
         // persistent sceneId assignment
         // (because scene objects have no persistent unique ID in Unity)
         //
@@ -295,18 +315,8 @@ namespace Mirror
                 Undo.RecordObject(this, "Generated SceneId");
 
                 // generate random sceneId part (0x00000000FFFFFFFF)
-                // -> exclude '0' because that's for unassigned sceneIDs
-                // TODO use 0,uint.max later. Random.Range only has int version.
-                uint randomId = (uint)UnityEngine.Random.Range(1, int.MaxValue);
-
-                // only assign if not a duplicate of an existing scene id
-                // (small chance, but possible)
-                duplicate = sceneIds.TryGetValue(randomId, out existing) && existing != null && existing != this;
-                if (!duplicate)
-                {
-                    m_SceneId = randomId;
-                    Debug.Log(name + " in scene=" + gameObject.scene.name + " sceneId assigned to: " + m_SceneId.ToString("X"));
-                }
+                m_SceneId = GetRandomSceneID();
+                Debug.Log(name + " in scene=" + gameObject.scene.name + " sceneId assigned to: " + m_SceneId.ToString("X"));
             }
 
             // add to sceneIds dict no matter what
