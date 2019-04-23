a4e75667797448eb5977df2c182f9969f141acdd
Zac North <znorth@gmail.com>
2019-04-23 15:07:52 -0400

    Just reorganize directive usage

--- Assets/Mirror/Runtime/Transport/Websocket/WebsocketTransport.cs
+++ Assets/Mirror/Runtime/Transport/Websocket/WebsocketTransport.cs
@@ -25,17 +25,15 @@ namespace Mirror.Websocket
             // dispatch the events from the server
             server.Connected += (connectionId) => OnServerConnected.Invoke(connectionId);
             server.Disconnected += (connectionId) => OnServerDisconnected.Invoke(connectionId);
-            server.ReceivedData += (connectionId, data) => OnServerDataReceivedNonAlloc.Invoke(connectionId, data);
+            server.ReceivedData += OnServerDataReceivedNonAlloc.Invoke;
             server.ReceivedError += (connectionId, error) => OnServerError.Invoke(connectionId, error);
 
             // dispatch events from the client
             client.Connected += () => OnClientConnected.Invoke();
             client.Disconnected += () => OnClientDisconnected.Invoke();
+            client.ReceivedDataNonAlloc += OnClientDataReceivedNonAlloc.Invoke;
 #if UNITY_WEBGL && !UNITY_EDITOR
-            client.ReceivedData += (data) => OnClientDataReceived.Invoke(data);
-            client.ReceivedDataNonAlloc += (data) => OnClientDataReceivedNonAlloc.Invoke(data);
-#else
-            client.ReceivedData += (data) => OnClientDataReceivedNonAlloc.Invoke(data);
+            client.ReceivedData += OnClientDataReceived.Invoke;
 #endif
             client.ReceivedError += (error) => OnClientError.Invoke(error);
 
