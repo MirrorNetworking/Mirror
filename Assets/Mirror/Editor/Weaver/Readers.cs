e5eecbff729f426e0de387f86fed70dc1c28b35a
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2019-06-13 16:03:54 +0200

    feat: support sending and receiving ArraySegment<byte> (#898)
    Add ArraySegment<byte> support to the weaver

--- Assets/Mirror/Editor/Weaver/Readers.cs
+++ Assets/Mirror/Editor/Weaver/Readers.cs
@@ -48,6 +48,7 @@ namespace Mirror.Weaver
                 { Weaver.NetworkIdentityType.FullName, Resolvers.ResolveMethod(networkReaderType, CurrentAssembly, "ReadNetworkIdentity") },
                 { Weaver.transformType.FullName, Resolvers.ResolveMethod(networkReaderType, CurrentAssembly, "ReadTransform") },
                 { "System.Byte[]", Resolvers.ResolveMethod(networkReaderType, CurrentAssembly, "ReadBytesAndSize") },
+                { "System.ArraySegment`1<System.Byte>", Resolvers.ResolveMethod(networkReaderType, CurrentAssembly, "ReadBytesSegment") }
             };
         }
 
