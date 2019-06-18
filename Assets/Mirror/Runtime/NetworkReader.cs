5ae379d6bb918038c196d77ed7a3ed51f154a3b6
vis2k <info@noobtuts.com>
2019-06-18 19:17:32 +0200

    Make ReadBytesAndSize more readable

--- Assets/Mirror/Runtime/NetworkReader.cs
+++ Assets/Mirror/Runtime/NetworkReader.cs
@@ -169,7 +169,14 @@ namespace Mirror
 
         // Use checked() to force it to throw OverflowException if data is invalid
         // null support, see NetworkWriter
-        public byte[] ReadBytesAndSize() => ReadBoolean() ? ReadBytes(checked((int)ReadPackedUInt32())) : null;
+        public byte[] ReadBytesAndSize()
+        {
+            if (ReadBoolean())
+            {
+                return ReadBytes(checked((int)ReadPackedUInt32()));
+            }
+            return null;
+        }
         public ArraySegment<byte> ReadBytesAndSizeSegment() => new ArraySegment<byte>(ReadBytesAndSize());
 
         // zigzag decoding https://gist.github.com/mfuerstenau/ba870a29e16536fdbaba
