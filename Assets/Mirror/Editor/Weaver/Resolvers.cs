c90bb26972f13fd7591e4cdb652613a51f9d9aba
Paul Pacheco <paulpach@gmail.com>
2019-07-03 08:26:16 -0500

    Remove unused method

--- Assets/Mirror/Editor/Weaver/Resolvers.cs
+++ Assets/Mirror/Editor/Weaver/Resolvers.cs
@@ -137,19 +137,5 @@ namespace Mirror.Weaver
             }
             return null;
         }
-
-        public static MethodReference ResolveGenericProperty(TypeReference tr, AssemblyDefinition scriptDef, string name)
-        {
-            foreach (PropertyDefinition pd in tr.Resolve().Properties)
-            {
-                if (pd.Name == name)
-                {
-                    var reference = new GenericInstanceMethod(scriptDef.MainModule.ImportReference(pd.GetMethod, tr));
-                    return scriptDef.MainModule.ImportReference(reference);
-
-                }
-            }
-            return null;
-        }
     }
 }
