8082df84d93ea0889333ed595a9f3d94f969ac5f
Chris Langsenkamp <chris@clevertech.net>
2019-07-27 07:46:36 -0400

    Moved comments and fixed typos

--- Assets/Mirror/Examples/Pong/Scripts/NetworkManagerPong.cs
+++ Assets/Mirror/Examples/Pong/Scripts/NetworkManagerPong.cs
@@ -1,9 +1,9 @@
-// custom NetworkManager that simply assigns the correct racket positions when
-// spawning players. the built in RoundRobin spawn method wouldn't work after
-// someone reconnects (both players would be on the same side).
 using UnityEngine;
 using Mirror;
 
+// Custom NetworkManager that simply assigns the correct racket positions when
+// spawning players. The built in RoundRobin spawn method wouldn't work after
+// someone reconnects (both players would be on the same side).
 public class NetworkManagerPong : NetworkManager
 {
     public Transform leftRacketSpawn;
