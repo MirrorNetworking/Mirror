35b9919d91a9b942b133426eed0d45733f48cd6b
Paul Pacheco <paulpach@gmail.com>
2019-07-29 08:07:06 -0500

    Revert "New Basic Example (#985)"
    This reverts commit 66f76938557fc43e203c45f22834fbbb37fa3394.

--- Assets/Mirror/Examples/Basic/Scripts/Player.cs
+++ Assets/Mirror/Examples/Basic/Scripts/Player.cs
@@ -1,56 +1,34 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine;
 
-namespace Mirror.Examples.Basic2
+namespace Mirror.Examples.Basic
 {
+
     public class Player : NetworkBehaviour
     {
-        [Header("Player Components")]
-        public RectTransform rectTransform;
-        public Image image;
-
-        [Header("Child Text Objects")]
-        public Text playerNameText;
-        public Text playerDataText;
 
         [SyncVar]
-        int playerNo;
-        [SyncVar]
-        int playerData;
-        [SyncVar]
-        Color playerColor;
+        public int data;
+
+        public TextMesh text;
+
 
         public override void OnStartServer()
         {
             base.OnStartServer();
-            playerNo = connectionToClient.connectionId;
-            playerColor = Random.ColorHSV(0f, 1f, 0.9f, 0.9f, 1f, 1f);
             InvokeRepeating(nameof(UpdateData), 1, 1);
         }
 
-        void UpdateData()
+        public void UpdateData()
         {
-            playerData = Random.Range(100, 1000);
+            data = Random.Range(0, 10);
         }
 
-        void Start()
+        public void Update()
         {
-            transform.SetParent(GameObject.Find("PlayersPanel").transform);
-
-            int x = 100 + ((playerNo % 4) * 150);
-            int y = -170 - ((playerNo / 4) * 80);
-            rectTransform.anchoredPosition = new Vector2(x, y);
-        }
-
-        void Update()
-        {
-            // shade the panel background for the local player
             if (isLocalPlayer)
-                image.color = new Color(1f, 1f, 1f, 0.1f);
+                text.color = Color.red;
 
-            playerNameText.color = playerColor;
-            playerNameText.text = string.Format("Player {0:00}", playerNo);
-            playerDataText.text = string.Format("Data: {0:000}", playerData);
+            text.text = $"Player {netId}\ndata={data}";
         }
     }
 }
