cd88c69df9c2d09d3470cf0155e3adbca1695b88
MrGadget <chris@clevertech.net>
2019-08-11 13:18:16 -0400

    Update NetworkManager.cs
    Added missing XML comment

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -969,9 +969,9 @@ namespace Mirror
         public virtual void OnServerChangeScene(string newSceneName) { }
 
         /// <summary>
-        /// 
+        /// Called on the server when a scene is completed loaded, when the scene load was initiated by the server with ServerChangeScene().
         /// </summary>
-        /// <param name="sceneName"></param>
+        /// <param name="sceneName">The name of the new scene.</param>
         public virtual void OnServerSceneChanged(string sceneName) { }
 
         #endregion
