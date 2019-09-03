13ca6b6542e539de70fbb1dada9976bf31df01d5
Paul Pacheco <paulpach@gmail.com>
2019-09-03 18:01:17 -0500

    Remove unnecesary using

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Serialization;
