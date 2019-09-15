fcac2d8536388d6b4918e18ebce52d3d775b394e
Chris Langsenkamp <chris@clevertech.net>
2019-09-15 14:36:51 -0400

    Add built-in timeout feature

--- Assets/Mirror/Runtime/NetworkAuthenticator.cs
+++ Assets/Mirror/Runtime/NetworkAuthenticator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -16,6 +17,11 @@ namespace Mirror
     [HelpURL("https://mirror-networking.com/xmldocs/articles/Concepts/Authentication.html")]
     public class NetworkAuthenticator : MonoBehaviour
     {
+        [Header("Configuration")]
+
+        [Range(0, 255), Tooltip("Timeout to auto-disconnect in seconds. Set to 0 for no timeout.")]
+        public byte timeout;
+
         [Header("Event Listeners (optional)")]
 
         /// <summary>
@@ -45,13 +51,16 @@ namespace Mirror
         // This will get more code in the near future
         internal void OnServerAuthenticateInternal(NetworkConnection conn)
         {
+            // Start the countdown for Authentication
+            if (timeout > 0) StartCoroutine(AuthenticationTimer(conn, true));
+
             OnServerAuthenticate(conn);
         }
 
         /// <summary>
         /// Called on server from OnServerAuthenticateInternal when a client needs to authenticate
         /// </summary>
-        /// <param name="conn">Connection from client.</param>
+        /// <param name="conn">Connection to client.</param>
         public virtual void OnServerAuthenticate(NetworkConnection conn)
         {
             // setting NetworkConnection.isAuthenticated = true is Required
@@ -61,16 +70,28 @@ namespace Mirror
             OnServerAuthenticated.Invoke(conn);
         }
 
+        /// <summary>
+        /// Called on client when the timeout expires without being authenticated
+        /// </summary>
+        /// <param name="conn">Connection to client.</param>
+        public virtual void OnServerAuthenticationTimeout(NetworkConnection conn)
+        {
+            conn.Disconnect();
+        }
+
         // This will get more code in the near future
         internal void OnClientAuthenticateInternal(NetworkConnection conn)
         {
+            // Start the countdown for Authentication
+            if (timeout > 0) StartCoroutine(AuthenticationTimer(conn, false));
+
             OnClientAuthenticate(conn);
         }
 
         /// <summary>
         /// Called on client from OnClientAuthenticateInternal when a client needs to authenticate
         /// </summary>
-        /// <param name="conn">Connection from client.</param>
+        /// <param name="conn">Connection of the client.</param>
         public virtual void OnClientAuthenticate(NetworkConnection conn)
         {
             // setting NetworkConnection.isAuthenticated = true is Required
@@ -78,6 +99,36 @@ namespace Mirror
 
             // invoking the event is Required
             OnClientAuthenticated.Invoke(conn);
+        }
+
+        /// <summary>
+        /// Called on client when the timeout expires without being authenticated
+        /// </summary>
+        /// <param name="conn">Connection of the client.</param>
+        public virtual void OnClientAuthenticationTimeout(NetworkConnection conn)
+        {
+            conn.Disconnect();
+        }
+
+        /// <summary>
+        /// This is called on both client and server if timeout > 0
+        /// </summary>
+        /// <param name="conn">Connection of the client.</param>
+        public IEnumerator AuthenticationTimer(NetworkConnection conn, bool isServer)
+        {
+            if (LogFilter.Debug) Debug.LogFormat("Authentication countdown started {0} {1}", conn.connectionId, timeout);
+
+            yield return new WaitForSecondsRealtime(timeout);
+
+            if (conn != null && !conn.isAuthenticated)
+            {
+                if (LogFilter.Debug) Debug.LogFormat("Authentication Timeout {0}", conn.connectionId);
+
+                if (isServer)
+                    OnServerAuthenticationTimeout(conn);
+                else
+                    OnClientAuthenticationTimeout(conn);
+            }
         }
     }
 }
