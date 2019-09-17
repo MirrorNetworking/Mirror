8a99ab16e815dd87ed0d54babc6e738e0af35ae0
MrGadget <chris@clevertech.net>
2019-09-16 23:14:02 -0400

    Remove add component from deprecated components (#1087)

--- Assets/Mirror/Components/NetworkLobbyPlayer.cs
+++ Assets/Mirror/Components/NetworkLobbyPlayer.cs
@@ -9,7 +9,6 @@ namespace Mirror
     /// <para>The LobbyPrefab object of the NetworkLobbyManager must have this component on it. This component holds basic lobby player data required for the lobby to function. Game specific data for lobby players can be put in other components on the LobbyPrefab or in scripts derived from NetworkLobbyPlayer.</para>
     /// </summary>
     [DisallowMultipleComponent]
-    [AddComponentMenu("Network/NetworkLobbyPlayer")]
     [HelpURL("https://mirror-networking.com/xmldocs/articles/Components/NetworkRoomPlayer.html")]
     [Obsolete("Use / inherit from NetworkRoomPlayer instead")]
     public class NetworkLobbyPlayer : NetworkRoomPlayer { }
