c289a43cb254ff6d88b5e825d925f8e26995a76d
Paul Pacheco <paulpach@gmail.com>
2019-09-16 22:09:28 -0500

    Provide documentation for the profiler data

--- Assets/Mirror/Runtime/ProfilerData.cs
+++ Assets/Mirror/Runtime/ProfilerData.cs
@@ -12,11 +12,28 @@ namespace Mirror
     public static class ProfilerData
     {
         #region Out messages
+
+        /// <summary>
+        /// Describes an outgoing message
+        /// </summary>
         public readonly struct OutMessage
         {
+            /// <summary>
+            /// The message being sent
+            /// </summary>
             public readonly IMessageBase message;
+            /// <summary>
+            /// channel through which the message was sent
+            /// </summary>
             public readonly int channel;
+            /// <summary>
+            /// how big was the message (does not include transport headers)
+            /// </summary>
             public readonly int bytes;
+            /// <summary>
+            /// How many connections was the message sent to
+            /// If an object has a lot of observers this count could be high
+            /// </summary>
             public readonly int count;
 
             internal OutMessage(IMessageBase message, int channel, int bytes, int count)
@@ -30,6 +47,10 @@ namespace Mirror
 
         public class OutMessageUnityEvent : UnityEvent<OutMessage> { };
 
+        /// <summary>
+        /// Event that gets raised when Mirror sends a message
+        /// Subscribe to this if you want to profile the network
+        /// </summary>
         public static OutMessageUnityEvent OutMessageEvent = new OutMessageUnityEvent();
 
         [Conditional("ENABLE_PROFILER")]
@@ -42,10 +63,22 @@ namespace Mirror
 
         #region In messages
 
+        /// <summary>
+        /// Describe a message received by mirror
+        /// </summary>
         public readonly struct InMessage
         {
+            /// <summary>
+            /// The message received
+            /// </summary>
             public readonly IMessageBase message;
+            /// <summary>
+            /// The channel through which the message was received
+            /// </summary>
             public readonly int channel;
+            /// <summary>
+            /// How big was the message (not including transport headers)
+            /// </summary>
             public readonly int bytes;
 
             internal InMessage(IMessageBase message, int channel, int bytes)
@@ -58,6 +91,10 @@ namespace Mirror
 
         public class InMessageUnityEvent : UnityEvent<InMessage> { };
 
+        /// <summary>
+        /// Event that gets raised when Mirror receives a message
+        /// Subscribe to this if you want to profile the network
+        /// </summary>
         public static InMessageUnityEvent InMessageEvent = new InMessageUnityEvent();
         
         [Conditional("ENABLE_PROFILER")]
