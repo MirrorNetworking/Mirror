77ec19c0dd37e8276145cd57bbaaa8eb950d719f
Paul Pacheco <paulpach@gmail.com>
2019-10-01 11:56:43 -0500

    docs: update links to new add player documentation

--- Assets/Mirror/Runtime/Messages.cs
+++ Assets/Mirror/Runtime/Messages.cs
@@ -159,24 +159,24 @@ namespace Mirror
     public struct AddPlayerMessage : IMessageBase
     {
         /// <summary>
-        /// Obsolete: Create your own message instead. See <a href="../Guides/CustomPlayer.md">Custom Players</a>
+        /// Obsolete: Create your own message instead. See <a href="../Guides/GameObjects/SpawnPlayerCustom.md">Custom Players</a>
         /// </summary>
-        [Obsolete("Create your own message instead. See Guides / Custom Players")]
+        [Obsolete("Create your own message instead. See https://mirror-networking.com/docs/Guides/GameObjects/SpawnPlayerCustom.html")]
         public byte[] value;
 
         /// <summary>
-        /// Obsolete: Create your own message instead. See <a href="../Guides/CustomPlayer.md">Custom Players</a>
+        /// Obsolete: Create your own message instead. See <a href="../Guides/GameObjects/SpawnPlayerCustom.md">Custom Players</a>
         /// </summary>
-        [Obsolete("Create your own message instead. See Guides / Custom Players")]
+        [Obsolete("Create your own message instead. See https://mirror-networking.com/docs/Guides/GameObjects/SpawnPlayerCustom.html")]
         public void Deserialize(NetworkReader reader)
         {
             value = reader.ReadBytesAndSize();
         }
 
         /// <summary>
-        /// Obsolete: Create your own message instead. See <a href="../Guides/CustomPlayer.md">Custom Players</a>
+        /// Obsolete: Create your own message instead. See <a href="../Guides/Guides/GameObjects/SpawnPlayerCustom.md">Custom Players</a>
         /// </summary>
-        [Obsolete("Create your own message instead. See Guides / Custom Players")]
+        [Obsolete("Create your own message instead. See https://mirror-networking.com/docs/Guides/GameObjects/SpawnPlayerCustom.html")]
         public void Serialize(NetworkWriter writer)
         {
             writer.WriteBytesAndSize(value);
