a41d18cd5686272cd16443d6b277a34c7e219cf5
Paul Pacheco <paulpach@gmail.com>
2019-10-25 23:50:27 -0500

    How did this get here?

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -850,9 +850,6 @@ namespace Mirror
                 NetworkClient.AddLocalPlayer(identity);
                 identity.SetClientOwner(conn);
 
-                // Trigger OnAuthority
-                identity.ForceAuthority(true);
-
                 // Trigger OnStartLocalPlayer
                 identity.SetLocalPlayer();
                 return true;
