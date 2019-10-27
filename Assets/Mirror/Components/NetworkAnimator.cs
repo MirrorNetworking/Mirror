a96681cc31c4161aba7a865ae41bd2a2471e1f41
Paul Pacheco <paulpach@gmail.com>
2019-10-27 14:06:31 -0500

    breaking: Remove localPlayerAuthority (#1188)
    * breaking: Remove localPlayerAuthority
    
    This flag does not do anything useful in anything but NetworkAnimator
    so NetworkAnimator can keep it's own flag.
    
    * Add tooltip with explanation

--- Assets/Mirror/Components/NetworkAnimator.cs
+++ Assets/Mirror/Components/NetworkAnimator.cs
@@ -34,17 +34,19 @@ namespace Mirror
         int[] transitionHash;
         float sendTimer;
 
+        [Tooltip("Set to true if animations come from owner client,  set to false if animations always come from server")]
+        public bool clientAuthority;
+
         bool sendMessagesAllowed
         {
             get
             {
                 if (isServer)
                 {
-                    if (!localPlayerAuthority)
+                    if (!clientAuthority)
                         return true;
 
-                    // This is a special case where we have localPlayerAuthority set
-                    // on a NetworkIdentity but we have not assigned the client who has
+                    // This is a special case where we have client authority but we have not assigned the client who has
                     // authority over it, no animator data will be sent over the network by the server.
                     //
                     // So we check here for a clientAuthorityOwner and if it is null we will
@@ -360,7 +362,7 @@ namespace Mirror
         /// <param name="hash">Hash id of trigger (from the Animator).</param>
         public void SetTrigger(int hash)
         {
-            if (hasAuthority && localPlayerAuthority)
+            if (hasAuthority && clientAuthority)
             {
                 if (ClientScene.readyConnection != null)
                 {
@@ -369,7 +371,7 @@ namespace Mirror
                 return;
             }
 
-            if (isServer && !localPlayerAuthority)
+            if (isServer && !clientAuthority)
             {
                 RpcOnAnimationTriggerClientMessage(hash);
             }
