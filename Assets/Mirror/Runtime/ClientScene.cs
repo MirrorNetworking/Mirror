03576a632e56e6f0721e1759581d7ce140ae67c5
Paul Pacheco <paulpach@gmail.com>
2019-11-14 10:49:44 -0600

    Code style

--- Assets/Mirror/Runtime/ClientScene.cs
+++ Assets/Mirror/Runtime/ClientScene.cs
@@ -111,7 +111,7 @@ namespace Mirror
 
             if (LogFilter.Debug) Debug.Log("ClientScene.AddPlayer() called with connection [" + readyConnection + "]");
 
-            AddPlayerMessage message = new AddPlayerMessage()
+            AddPlayerMessage message = new AddPlayerMessage
             {
 #pragma warning disable CS0618 // Type or member is obsolete
                 value = extraData
