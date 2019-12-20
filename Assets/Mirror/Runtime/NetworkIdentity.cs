22b7a5c1f93f9315d8ebd437e4bcbfc84f44dd0a
Chris Langsenkamp <chris@clevertech.net>
2019-12-20 11:38:12 -0500

    Added FormerlySerializedAs

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -94,12 +94,13 @@ namespace Mirror
         /// <para>This is used for spawning scene objects on clients.</para>
         /// </summary>
         // persistent scene id <sceneHash/32,sceneId/32> (see AssignSceneID comments)
+        [FormerlySerializedAs("m_SceneId")]
         public ulong sceneId;
 
-    /// <summary>
-    /// Flag to make this object only exist when the game is running as a server (or host).
-    /// </summary>
-    [FormerlySerializedAs("m_ServerOnly")]
+        /// <summary>
+        /// Flag to make this object only exist when the game is running as a server (or host).
+        /// </summary>
+        [FormerlySerializedAs("m_ServerOnly")]
         public bool serverOnly;
 
         /// <summary>
