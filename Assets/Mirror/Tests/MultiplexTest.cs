a132a9c47a5be0bb68a9d08cd0ad27fa8dc7d82b
Paul Pacheco <paulpach@gmail.com>
2019-12-23 16:26:25 -0600

    Test available method

---
+++ Assets/Mirror/Tests/MultiplexTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.Tests
+{
+    public class MultiplexTest
+    {
+
+        Transport transport1;
+        Transport transport2;
+        MultiplexTransport transport;
+
+        [SetUp]
+        public void SetupMultipex()
+        {
+            transport1 = Substitute.For<Transport>();
+            transport2 = Substitute.For<Transport>();
+
+            GameObject gameObject = new GameObject();
+
+            transport = gameObject.AddComponent<MultiplexTransport>();
+            transport.transports = new []{ transport1, transport2 };
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestAvailable()
+        {
+            transport1.Available().Returns(true);
+            transport2.Available().Returns(false);
+            Assert.That(transport.Available());
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestNotAvailable()
+        {
+            transport1.Available().Returns(false);
+            transport2.Available().Returns(false);
+            Assert.That(transport.Available(), Is.False);
+        }
+
+    }
+}
