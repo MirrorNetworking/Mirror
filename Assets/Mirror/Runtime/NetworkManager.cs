91fa51dbb24096e1ca77dab428f0bc85c41df59f
Paul Pacheco <paulpach@gmail.com>
2019-12-31 07:58:52 -0600

    refactor: use standard inspector for NM (#7)

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -125,7 +125,7 @@ namespace Mirror
         /// List of prefabs that will be registered with the spawning system.
         /// <para>For each of these prefabs, ClientManager.RegisterPrefab() will be automatically invoke.</para>
         /// </summary>
-        [FormerlySerializedAs("m_SpawnPrefabs"), HideInInspector]
+        [FormerlySerializedAs("m_SpawnPrefabs")]
         public List<GameObject> spawnPrefabs = new List<GameObject>();
 
         /// <summary>
