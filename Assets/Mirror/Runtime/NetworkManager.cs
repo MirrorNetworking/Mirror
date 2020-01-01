11586a4177e3c12e84dfbd6ca85fbeb3904a0286
vis2k <info@noobtuts.com>
2020-01-01 12:18:07 +0100

    explain StartHost

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -435,6 +435,24 @@ namespace Mirror
         /// </summary>
         public virtual void StartHost()
         {
+            // StartHost is inherently ASYNCHRONOUS (=doesn't finish immediately)
+            //
+            // Here is what it does:
+            //   Listen
+            //   ConnectHost         <= this should ideally happen after finishing scene load
+            //   if onlineScene:
+            //       LoadSceneAsync
+            //       ...
+            //       FinishLoadScene
+            //           SpawnObjects
+            //   else:
+            //       SpawnObjects
+            //   StartHostClient      <= not guaranteed to happen after SpawnObjects if onlineScene is set!
+            //
+            // there is NO WAY to make it synchronous because both LoadSceneAsync
+            // and LoadScene do not finish loading immediately. as long as we
+            // have the onlineScene feature, it will be asynchronous!
+
             // setup server first
             SetupServer();
 
