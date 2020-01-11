8f42af0a3992cfa549eb404ad9f9693101895ce9
Paul Pacheco <paulpach@gmail.com>
2020-01-11 17:19:25 -0600

    fix: remove Tests from UPM (#33)

--- publish_upm.sh
+++ publish_upm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git subtree split --prefix=Assets/Mirror -b upm 
-git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
+git subtree split --prefix=Assets/Mirror -b upm
+#git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
+git filter-repo --invert-paths --path Tests --refs upm
 git tag $1 upm
 git push -u origin upm --tags
