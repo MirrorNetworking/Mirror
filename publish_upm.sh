8d8ea0f10743044e4a9a3d6c5b9f9973cf48e28b
Paul Pacheco <paulpach@gmail.com>
2020-01-12 11:17:50 -0600

    fix: remove Tests from upm package (#34)
    Remove Tests from upm package
    Move Samples~ back to Examples on main branch,  but rename it in upm branch

--- publish_upm.sh
+++ publish_upm.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-git subtree split --prefix=Assets/Mirror -b upm
+#!/bin/sh -e
+
+BRANCH=$1
+TAG=$2
+git subtree split --prefix=Assets/Mirror -b $BRANCH
 #git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
-git filter-repo --invert-paths --path Tests --refs upm
-git tag $1 upm
-git push -u origin upm --tags
+git gc
+git filter-repo --force --invert-paths --path Tests --path Tests.meta --path Examples.meta --path-rename "Examples:Samples~" --refs $BRANCH
+git tag $TAG $BRANCH
+git push origin $BRANCH --tags
