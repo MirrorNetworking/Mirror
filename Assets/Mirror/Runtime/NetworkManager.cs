c4113358c928ec2a3ad26293fb82eeb63436672c
Chris Langsenkamp <chris@clevertech.net>
2020-01-14 12:28:52 -0500

    Fixed typo

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -583,7 +583,7 @@ namespace Mirror
             // doesn't think we need initialize anything.
             mode = NetworkManagerMode.Offline;
 
-            // if this is a headless server, changing to offline would put it into a locked state
+            // If this is a headless server, changing to offline scene would put it into a locked state
             // unable to receive connections, but still running. If StopServer is called from
             // OnApplicationQuit the server is about to exit anyway, so no scene change needed.
             if (!string.IsNullOrEmpty(offlineScene) && !isHeadless)
