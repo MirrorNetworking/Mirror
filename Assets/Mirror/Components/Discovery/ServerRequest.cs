7a7b0c11f2aac0097a96d2a62e260c0b5b9a4426
Paul Pacheco <paulpach@gmail.com>
2020-01-20 23:51:11 -0600

    Fix indentation

--- Assets/Mirror/Components/Discovery/ServerRequest.cs
+++ Assets/Mirror/Components/Discovery/ServerRequest.cs
@@ -4,13 +4,13 @@ using System.Net;
 namespace Mirror.Discovery
 {
 
-public class ServerRequest : MessageBase
-{
+    public class ServerRequest : MessageBase
+    {
         public long secretHandshake;
 
         // The server that sent this
         public IPEndPoint EndPoint { get; set; }
 
-}
+    }
 
 }
