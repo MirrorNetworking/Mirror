c147caf30c103a4edafb87ec9e925701d191b36c
MrGadget <chris@clevertech.net>
2020-01-21 17:01:34 -0500

    Added ErrorMessage Test (#1448)

---
+++ Assets/Mirror/Tests/MessageTests.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace Mirror.Tests
+{
+    [TestFixture]
+    public class MessageTests : MonoBehaviour
+    {
+        [Test]
+        public void ErrorMessageTest()
+        {
+            // try setting value with constructor
+            ErrorMessage errorMessage = new ErrorMessage(123);
+            Assert.That(errorMessage.value, Is.EqualTo(123));
+
+            // try deserialize
+            byte[] data = { 123 };
+            errorMessage.Deserialize(new NetworkReader(data));
+            Assert.That(errorMessage.value, Is.EqualTo(123));
+
+            // try serialize
+            NetworkWriter writer = new NetworkWriter();
+            errorMessage.Serialize(writer);
+            Assert.That(writer.ToArray()[0], Is.EqualTo(123));
+
+
+            //ErrorMessage errorMessage = new ErrorMessage();
+            //errorMessage.value = 123;
+            //NetworkWriter writer = new NetworkWriter();
+            //errorMessage.Serialize(writer);
+            //errorMessage.Deserialize(new NetworkReader(writer.ToArray()));
+            //Assert.That(errorMessage.value, Is.EqualTo(123));
+        }
+    }
+}
