5736dd8d057ef4d8ba4d0f73776ef0a5a0321375
vis2k <info@noobtuts.com>
2020-01-22 09:30:09 +0100

    MessagePackerTest: cover FormatException case

--- Assets/Mirror/Tests/MessagePackerTest.cs
+++ Assets/Mirror/Tests/MessagePackerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 namespace Mirror.Tests
 {
@@ -19,6 +20,36 @@ namespace Mirror.Tests
 
             Assert.That(unpacked.sceneName, Is.EqualTo("Hello world"));
             Assert.That(unpacked.sceneOperation, Is.EqualTo(SceneOperation.LoadAdditive));
+        }
+
+        [Test]
+        public void TestUnpackIdMismatch()
+        {
+            // Unpack<T> has a id != msgType case that throws a FormatException.
+            // let's try to trigger it.
+
+            SceneMessage message = new SceneMessage()
+            {
+                sceneName = "Hello world",
+                sceneOperation = SceneOperation.LoadAdditive
+            };
+
+            byte[] data = MessagePacker.Pack(message);
+
+            // overwrite the id
+            data[0] = 0x01;
+            data[1] = 0x02;
+
+            try
+            {
+                SceneMessage unpacked = MessagePacker.Unpack<SceneMessage>(data);
+                // BAD: IF WE GET HERE THEN NO EXCEPTION WAS THROWN
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+                // GOOD
+            }
         }
     }
 }
