69939618572e991204a933d07708577a8c42b8ae
Chris Langsenkamp <chris@clevertech.net>
2020-01-29 17:11:27 -0500

    Broken Tests

--- Assets/Mirror/Tests/Editor/BasicAuthenticatorTest.cs
+++ Assets/Mirror/Tests/Editor/BasicAuthenticatorTest.cs
@@ -65,5 +65,89 @@ namespace Mirror.Tests
             Assert.That(fresh.code, Is.EqualTo(123));
             Assert.That(fresh.message, Is.EqualTo("abc"));
         }
+
+        ULocalConnectionToClient connectionToClient;
+        ULocalConnectionToServer connectionToServer;
+
+        [SetUp]
+        public void SetUpConnections()
+        {
+            connectionToServer = new ULocalConnectionToServer();
+            connectionToClient = new ULocalConnectionToClient();
+
+            connectionToClient.connectionToServer = connectionToServer;
+            connectionToServer.connectionToClient = connectionToClient;
+        }
+
+        [TearDown]
+        public void Disconnect()
+        {
+            connectionToServer.Disconnect();
+        }
+
+        public delegate void AuthRequestMessageDelegate(AuthRequestMessage netMsg);
+        public delegate void AuthResponseMessageDelegate(AuthResponseMessage netMsg);
+
+        [Test]
+        public void OnClientAuthenticateTest()
+        {
+            AuthRequestMessage authRequestMessage = new AuthRequestMessage
+            {
+                authUsername = "abc",
+                authPassword = "123"
+            };
+
+            bool invoked = false;
+
+            void handler(AuthRequestMessage msg)
+            {
+                Assert.That(msg.authUsername, Is.EqualTo("abc"));
+                Assert.That(msg.authPassword, Is.EqualTo("123"));
+                invoked = true;
+            }
+
+            //Dictionary<int, AuthRequestMessageDelegate> handlers = new Dictionary<int, AuthRequestMessageDelegate>();
+            //handlers.Add(MessagePacker.GetId<AuthRequestMessage>(), handler);
+            //connectionToClient.SetHandlers(handlers);
+
+            // This is wrong...what should be done here?
+            NetworkServer.RegisterHandler<AuthRequestMessage>(handler);
+            connectionToClient.Send(authRequestMessage);
+
+            connectionToServer.Update();
+
+            Assert.True(invoked, "handler should have been invoked");
+        }
+
+        [Test]
+        public void OnAuthRequestMessage()
+        {
+            AuthResponseMessage authResponseMessage = new AuthResponseMessage
+            {
+                code = 123,
+                message = "abc"
+            };
+
+            bool invoked = false;
+
+            void handler(AuthResponseMessage msg)
+            {
+                Assert.That(msg.code, Is.EqualTo(123));
+                Assert.That(msg.message, Is.EqualTo("abc"));
+                invoked = true;
+            }
+
+            //Dictionary<int, AuthResponseMessageDelegate> handlers = new Dictionary<int, AuthResponseMessageDelegate>();
+            //handlers.Add(MessagePacker.GetId<AuthResponseMessage>(), handler);
+            //connectionToClient.SetHandlers(handlers);
+
+            // This is wrong...what should be done here?
+            NetworkClient.RegisterHandler<AuthResponseMessage>(handler);
+            connectionToServer.Send(authResponseMessage);
+
+            connectionToServer.Update();
+
+            Assert.True(invoked, "handler should have been invoked");
+        }
     }
 }
