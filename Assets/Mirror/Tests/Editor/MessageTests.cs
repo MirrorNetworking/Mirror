deea5e5d01dff1371ad3440c50438b60e1731f83
Chris Langsenkamp <chris@clevertech.net>
2020-01-29 18:21:04 -0500

    Added UpdateVarsMessageTest

--- Assets/Mirror/Tests/Editor/MessageTests.cs
+++ Assets/Mirror/Tests/Editor/MessageTests.cs
@@ -107,5 +107,30 @@ namespace Mirror.Tests
                 Assert.That(fresh.payload.Array[fresh.payload.Offset + i],
                     Is.EqualTo(message.payload.Array[message.payload.Offset + i]));
         }
+
+        [Test]
+        public void UpdateVarsMessageTest()
+        {
+            // try setting value with constructor
+            UpdateVarsMessage message = new UpdateVarsMessage
+            {
+                netId = 42,
+                payload = new ArraySegment<byte>(new byte[] { 0x01, 0x02 })
+            };
+
+            // serialize
+            NetworkWriter writer = new NetworkWriter();
+            message.Serialize(writer);
+            byte[] writerData = writer.ToArray();
+
+            // deserialize the same data - do we get the same result?
+            UpdateVarsMessage fresh = new UpdateVarsMessage();
+            fresh.Deserialize(new NetworkReader(writerData));
+            Assert.That(fresh.netId, Is.EqualTo(message.netId));
+            Assert.That(fresh.payload.Count, Is.EqualTo(message.payload.Count));
+            for (int i = 0; i < fresh.payload.Count; ++i)
+                Assert.That(fresh.payload.Array[fresh.payload.Offset + i],
+                    Is.EqualTo(message.payload.Array[message.payload.Offset + i]));
+        }
     }
 }
