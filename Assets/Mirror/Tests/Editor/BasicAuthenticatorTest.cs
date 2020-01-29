e44ca7f3344a947d6731250633a370fbea6f918a
MrGadget <chris@clevertech.net>
2020-01-29 14:44:30 -0500

    BasicAuthenticatorTest (#1459)
    * First Draft
    
    * Second Draft
    
    * Third Draft
    
    * First 2 tests that work

---
+++ Assets/Mirror/Tests/Editor/BasicAuthenticatorTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Mirror.Tests
+{
+    [TestFixture]
+    public class BasicAuthenticatorTest
+    {
+        public class AuthRequestMessage : MessageBase
+        {
+            public string authUsername;
+            public string authPassword;
+        }
+
+        public class AuthResponseMessage : MessageBase
+        {
+            public byte code;
+            public string message;
+        }
+
+        [Test]
+        public void AuthRequestMessageTest()
+        {
+            // try setting value with constructor
+            AuthRequestMessage message = new AuthRequestMessage
+            {
+                authUsername = "abc",
+                authPassword = "123"
+            };
+            Assert.That(message.authUsername, Is.EqualTo("abc"));
+            Assert.That(message.authPassword, Is.EqualTo("123"));
+
+            // serialize
+            NetworkWriter writer = new NetworkWriter();
+            message.Serialize(writer);
+            byte[] writerData = writer.ToArray();
+
+            // try deserialize
+            AuthRequestMessage fresh = new AuthRequestMessage();
+            fresh.Deserialize(new NetworkReader(writerData));
+            Assert.That(fresh.authUsername, Is.EqualTo("abc"));
+            Assert.That(fresh.authPassword, Is.EqualTo("123"));
+        }
+
+        [Test]
+        public void AuthResponseMessageTest()
+        {
+            // try setting value with constructor
+            AuthResponseMessage message = new AuthResponseMessage
+            {
+                code = 123,
+                message = "abc"
+            };
+            Assert.That(message.code, Is.EqualTo(123));
+            Assert.That(message.message, Is.EqualTo("abc"));
+
+            // serialize
+            NetworkWriter writer = new NetworkWriter();
+            message.Serialize(writer);
+            byte[] writerData = writer.ToArray();
+
+            // try deserialize
+            AuthResponseMessage fresh = new AuthResponseMessage();
+            fresh.Deserialize(new NetworkReader(writerData));
+            Assert.That(fresh.code, Is.EqualTo(123));
+            Assert.That(fresh.message, Is.EqualTo("abc"));
+        }
+    }
+}
