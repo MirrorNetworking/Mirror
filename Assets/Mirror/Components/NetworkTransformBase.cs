eee969201d69df1e1ee1f1623b55a78f6003fbb1
MrGadget <chris@clevertech.net>
2020-02-02 14:35:07 -0500

    Renamed localEulerAnglesSensitivity (#1474)
    BREAKING CHANGE:  localEulerAnglesSensitivity renamed to localRotationSensitivity

--- Assets/Mirror/Components/NetworkTransformBase.cs
+++ Assets/Mirror/Components/NetworkTransformBase.cs
@@ -37,7 +37,7 @@ namespace Mirror
         [Tooltip("Changes to the transform must exceed these values to be transmitted on the network.")]
         public float localPositionSensitivity = .01f;
         [Tooltip("Changes to the transform must exceed these values to be transmitted on the network.")]
-        public float localEulerAnglesSensitivity = .01f;
+        public float localRotationSensitivity = .01f;
         [Tooltip("Changes to the transform must exceed these values to be transmitted on the network.")]
         public float localScaleSensitivity = .01f;
 
@@ -341,7 +341,7 @@ namespace Mirror
             // moved or rotated or scaled?
             // local position/rotation/scale for VR support
             bool moved = Vector3.Distance(lastPosition, targetComponent.transform.localPosition) > localPositionSensitivity;
-            bool rotated = Vector3.Distance(lastRotation.eulerAngles, targetComponent.transform.localRotation.eulerAngles) > localEulerAnglesSensitivity;
+            bool rotated = Vector3.Distance(lastRotation.eulerAngles, targetComponent.transform.localRotation.eulerAngles) > localRotationSensitivity;
             bool scaled = Vector3.Distance(lastScale, targetComponent.transform.localScale) > localScaleSensitivity;
 
             // save last for next frame to compare
