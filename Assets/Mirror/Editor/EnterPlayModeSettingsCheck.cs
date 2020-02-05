ffbf5a52171da79029b51c8477f314cf5452bae5
Chris Langsenkamp <chris@clevertech.net>
2020-02-05 10:29:49 -0500

    Formatted comment and error message

--- Assets/Mirror/Editor/EnterPlayModeSettingsCheck.cs
+++ Assets/Mirror/Editor/EnterPlayModeSettingsCheck.cs
@@ -1,4 +1,4 @@
-﻿// Unity 2019.3 has an experimental 'disable domain reload on play'
+// Unity 2019.3 has an experimental 'disable domain reload on play'
 // feature. keeping any global state between sessions will break
 // Mirror and most of our user's projects. don't allow it for now.
 // https://blogs.unity3d.com/2019/11/05/enter-play-mode-faster-in-unity-2019-3/
