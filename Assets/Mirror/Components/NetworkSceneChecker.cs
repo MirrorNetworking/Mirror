7bb80e3b796f2c69d0958519cf1b4a9f4373268b
MrGadget <chris@clevertech.net>
2020-02-15 02:32:34 -0500

    feat: NetworkSceneChecker use Scene instead of string name (#1496)
    * NetworkSceneChecker use Scene instead of string name
    
    * Added comment

--- Assets/Mirror/Components/NetworkSceneChecker.cs
+++ Assets/Mirror/Components/NetworkSceneChecker.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 namespace Mirror
 {
@@ -20,14 +21,15 @@ namespace Mirror
         [Tooltip("Enable to force this object to be hidden from all observers.")]
         public bool forceHidden;
 
-        public static readonly Dictionary<string, HashSet<NetworkIdentity>> sceneCheckerObjects = new Dictionary<string, HashSet<NetworkIdentity>>();
+        // Use Scene instead of string scene.name because when additively loading multiples of a subscene the name won't be unique
+        static readonly Dictionary<Scene, HashSet<NetworkIdentity>> sceneCheckerObjects = new Dictionary<Scene, HashSet<NetworkIdentity>>();
 
-        string currentScene;
+        Scene currentScene;
 
         [ServerCallback]
         void OnEnable()
         {
-            currentScene = gameObject.scene.name;
+            currentScene = gameObject.scene;
             if (LogFilter.Debug) Debug.Log($"NetworkSceneChecker.OnEnable currentScene: {currentScene}");
         }
 
@@ -42,7 +44,7 @@ namespace Mirror
         [ServerCallback]
         void Update()
         {
-            if (currentScene == gameObject.scene.name)
+            if (currentScene == gameObject.scene)
                 return;
 
             // This object is in a new scene so observers in the prior scene
@@ -55,7 +57,7 @@ namespace Mirror
             RebuildSceneObservers();
 
             // Set this to the new scene this object just entered
-            currentScene = gameObject.scene.name;
+            currentScene = gameObject.scene;
 
             // Make sure this new scene is in the dictionary
             if (!sceneCheckerObjects.ContainsKey(currentScene))
