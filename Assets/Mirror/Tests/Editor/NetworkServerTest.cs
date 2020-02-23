b1ff0757eef9f542be0965c83794e90c8346aaf2
vis2k <info@noobtuts.com>
2020-02-23 10:58:07 +0100

    NetworkServerTest: MaxConnectionsTest

--- Assets/Mirror/Tests/Editor/NetworkServerTest.cs
+++ Assets/Mirror/Tests/Editor/NetworkServerTest.cs
@@ -29,5 +29,29 @@ namespace Mirror.Tests
             NetworkServer.Shutdown();
             Assert.That(NetworkServer.active, Is.False);
         }
+
+        [Test]
+        public void MaxConnectionsTest()
+        {
+            // message handlers
+            NetworkServer.RegisterHandler<ConnectMessage>((conn, msg) => {}, false);
+            NetworkServer.RegisterHandler<DisconnectMessage>((conn, msg) => {}, false);
+            NetworkServer.RegisterHandler<ErrorMessage>((conn, msg) => {}, false);
+
+            // listen with maxconnections=1
+            NetworkServer.Listen(1);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(0));
+
+            // connect first: should work
+            Transport.activeTransport.OnServerConnected.Invoke(42);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(1));
+
+            // connect second: should fail
+            Transport.activeTransport.OnServerConnected.Invoke(43);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(1));
+
+            // shutdown
+            NetworkServer.Shutdown();
+        }
     }
 }
