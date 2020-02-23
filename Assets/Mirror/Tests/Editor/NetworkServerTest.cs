f750d77a8c8030be6e0ffe86b3ab1d595311bb13
vis2k <info@noobtuts.com>
2020-02-23 10:19:38 +0100

    NetworkServerTests: IsActiveTest

---
+++ Assets/Mirror/Tests/Editor/NetworkServerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirror.Tests
+{
+    [TestFixture]
+    public class NetworkServerTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Transport.activeTransport = Substitute.For<Transport>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Transport.activeTransport = null;
+        }
+
+        [Test]
+        public void IsActiveTest()
+        {
+            Assert.That(NetworkServer.active, Is.False);
+            NetworkServer.Listen(1);
+            Assert.That(NetworkServer.active, Is.True);
+            NetworkServer.Shutdown();
+            Assert.That(NetworkServer.active, Is.False);
+        }
+    }
+}
