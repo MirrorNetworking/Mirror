e87c1d6a260f0c63dda5b0ab546725e4835d6dc1
vis2k <info@noobtuts.com>
2020-02-27 10:08:36 +0100

    add comments

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -584,6 +584,9 @@ namespace Mirror
         {
             foreach (NetworkBehaviour comp in NetworkBehaviours)
             {
+                // an exception in OnStartAuthority should be caught, so that one
+                // component's exception doesn't stop all other components from
+                // being initialized
                 try
                 {
                     comp.OnStartAuthority();
@@ -599,6 +602,9 @@ namespace Mirror
         {
             foreach (NetworkBehaviour comp in NetworkBehaviours)
             {
+                // an exception in OnStopAuthority should be caught, so that one
+                // component's exception doesn't stop all other components from
+                // being initialized
                 try
                 {
                     comp.OnStopAuthority();
