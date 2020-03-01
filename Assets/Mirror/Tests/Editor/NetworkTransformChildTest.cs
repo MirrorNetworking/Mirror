e85d3071c50ae576a61f646bf107949ae21b91e2
uwee <uweenukr@gmail.com>
2020-03-01 14:01:16 -0500

    Test coverage for NetworkTransformChild

---
+++ Assets/Mirror/Tests/Editor/NetworkTransformChildTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirror.Tests
+{
+    [TestFixture]
+    public class NetworkTransformChildTest
+    {
+        NetworkTransformChild  networkTransformChild;
+
+        [Test]
+        public void TargetComponentTest()
+        {
+            GameObject gameObject = new GameObject();
+            networkTransformChild = gameObject.AddComponent<NetworkTransformChild>();
+
+            Assert.That(networkTransformChild.target == null);
+
+            networkTransformChild.target = gameObject.transform;
+
+            Assert.That(networkTransformChild.target == networkTransformChild.transform);
+        }
+    }
+}
