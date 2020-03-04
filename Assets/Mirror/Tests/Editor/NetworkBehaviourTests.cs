15915f573e9774c6952bc138d958ff2dca1a3e00
vis2k <info@noobtuts.com>
2020-03-04 23:54:22 +0100

    NetworkBehaviourTests preparations

---
+++ Assets/Mirror/Tests/Editor/NetworkBehaviourTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.Tests
+{
+    public class NetworkBehaviourTests
+    {
+        GameObject gameObject;
+        NetworkIdentity identity;
+
+        [SetUp]
+        public void SetUp()
+        {
+            gameObject = new GameObject();
+            identity = gameObject.AddComponent<NetworkIdentity>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GameObject.DestroyImmediate(gameObject);
+        }
+    }
+}
