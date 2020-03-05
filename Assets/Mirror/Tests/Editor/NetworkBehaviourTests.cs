8f9064a923b8ce8293f7f24f62d2d2093829e0cd
vis2k <info@noobtuts.com>
2020-03-05 10:09:08 +0100

    NetworkBehaviourTests: IsServerOnly

--- Assets/Mirror/Tests/Editor/NetworkBehaviourTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkBehaviourTests.cs
@@ -9,6 +9,10 @@ using UnityEngine.TestTools;
 
 namespace Mirror.Tests
 {
+    class EmptyBehaviour : NetworkBehaviour
+    {
+    }
+
     public class NetworkBehaviourTests
     {
         GameObject gameObject;
@@ -25,6 +29,27 @@ namespace Mirror.Tests
         public void TearDown()
         {
             GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void IsServerOnly()
+        {
+            // add a behaviour
+            EmptyBehaviour comp = gameObject.AddComponent<EmptyBehaviour>();
+
+            // start server and assign netId so that isServer is true
+            Transport.activeTransport = Substitute.For<Transport>();
+            NetworkServer.Listen(1);
+            identity.netId = 42;
+
+            // isServerOnly should be true when isServer = true && isClient = false
+            Assert.That(comp.isServer, Is.True);
+            Assert.That(comp.isClient, Is.False);
+            Assert.That(comp.isServerOnly, Is.True);
+
+            // clean up
+            NetworkServer.Shutdown();
+            Transport.activeTransport = null;
         }
     }
 }
