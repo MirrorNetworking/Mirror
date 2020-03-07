a3ae46f8f47613fe702a62cbb04831b4ff019268
vis2k <info@noobtuts.com>
2020-03-07 14:45:00 +0100

    NetworkBehaviourTests: SyncVarGameObjectEqualValidGOWithDifferentNetId

--- Assets/Mirror/Tests/Editor/NetworkBehaviourTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkBehaviourTests.cs
@@ -728,6 +728,24 @@ namespace Mirror.Tests
             // clean up
             GameObject.DestroyImmediate(go);
         }
+
+        // NOTE: SyncVarGameObjectEqual should be static later
+        [Test]
+        public void SyncVarGameObjectEqualValidGOWithDifferentNetId()
+        {
+            // our identity should have a netid for comparing
+            identity.netId = 42;
+
+            // gameobject with valid networkidentity and netid that is different
+            GameObject go = new GameObject();
+            NetworkIdentity ni = go.AddComponent<NetworkIdentity>();
+            ni.netId = 43;
+            bool result = emptyBehaviour.SyncVarGameObjectEqual(go, identity.netId);
+            Assert.That(result, Is.False);
+
+            // clean up
+            GameObject.DestroyImmediate(go);
+        }
     }
 
     // we need to inherit from networkbehaviour to test protected functions
