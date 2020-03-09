f9b8914e6d242e7fc9eece4ec788460a9a143082
vis2k <info@noobtuts.com>
2020-03-09 10:47:37 +0100

    NetworkClientTests: serverIp

---
+++ Assets/Mirror/Tests/Editor/NetworkClientTests.cs
@@ -0,0 +1,41 @@
+﻿using Mirror;
+using Mirror.Tests;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace  Mirror.Tests
+{
+    public class NetworkClientTests
+    {
+        GameObject transportGO;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // client.connect sets transport.enabled, which only works if
+            // Transport is on a GameObject
+            transportGO = new GameObject();
+            Transport.activeTransport = transportGO.AddComponent<MemoryTransport>();
+
+            // we need a server to connect to
+            NetworkServer.Listen(10);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            NetworkServer.Shutdown();
+            NetworkClient.Shutdown();
+            GameObject.DestroyImmediate(transportGO);
+            Transport.activeTransport = null;
+        }
+
+        [Test]
+        public void serverIp()
+        {
+            NetworkClient.ConnectHost();
+            Assert.That(NetworkClient.serverIp, Is.EqualTo("localhost"));
+        }
+    }
+}
