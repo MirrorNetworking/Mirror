25aae832ede2d95ed920f145f63c529f935e0f77
Paul Pacheco <paulpach@gmail.com>
2020-03-16 08:15:26 -0500

    simplify tests

--- Assets/Mirror/Tests/Editor/MessageTests.cs
+++ Assets/Mirror/Tests/Editor/MessageTests.cs
@@ -10,7 +10,7 @@ namespace Mirror.Tests
         public void CommandMessageTest()
         {
             // try setting value with constructor
-            CommandMessage message = new CommandMessage
+            var message = new CommandMessage
             {
                 netId = 42,
                 componentIndex = 4,
@@ -19,254 +19,129 @@ namespace Mirror.Tests
             };
 
             // serialize
-            NetworkWriter writer = new NetworkWriter();
+            var writer = new NetworkWriter();
             message.Serialize(writer);
             byte[] writerData = writer.ToArray();
 
             // deserialize the same data - do we get the same result?
-            CommandMessage fresh = new CommandMessage();
+            var fresh = new CommandMessage();
             fresh.Deserialize(new NetworkReader(writerData));
             Assert.That(fresh.netId, Is.EqualTo(message.netId));
             Assert.That(fresh.componentIndex, Is.EqualTo(message.componentIndex));
             Assert.That(fresh.functionHash, Is.EqualTo(message.functionHash));
-            Assert.That(fresh.payload.Count, Is.EqualTo(message.payload.Count));
+            Assert.That(fresh.payload, Has.Count.EqualTo(message.payload.Count));
             for (int i = 0; i < fresh.payload.Count; ++i)
                 Assert.That(fresh.payload.Array[fresh.payload.Offset + i],
                     Is.EqualTo(message.payload.Array[message.payload.Offset + i]));
         }
 
-        [Test]
-        public void ConnectMessageTest()
+        private void TestSerializeDeserialize<T>(T message) where T: IMessageBase, new ()
         {
-            // try setting value with constructor
-            ConnectMessage message = new ConnectMessage();
-
             // serialize
-            NetworkWriter writer = new NetworkWriter();
+            var writer = new NetworkWriter();
             message.Serialize(writer);
             byte[] writerData = writer.ToArray();
 
             // deserialize the same data - do we get the same result?
-            ConnectMessage fresh = new ConnectMessage();
+            var fresh = new T();
             fresh.Deserialize(new NetworkReader(writerData));
+            Assert.That(fresh, Is.EqualTo(message));
+        }
+
+        [Test]
+        public void ConnectMessageTest()
+        {
+            TestSerializeDeserialize(new ConnectMessage());
         }
 
         [Test]
         public void DisconnectMessageTest()
         {
-            // try setting value with constructor
-            DisconnectMessage message = new DisconnectMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            DisconnectMessage fresh = new DisconnectMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new DisconnectMessage());
         }
 
         [Test]
         public void ErrorMessageTest()
         {
-            // try setting value with constructor
-            ErrorMessage message = new ErrorMessage(42);
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ErrorMessage fresh = new ErrorMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
-            Assert.That(fresh.value, Is.EqualTo(message.value));
+            TestSerializeDeserialize(new ErrorMessage(42));
         }
 
         [Test]
         public void NetworkPingMessageTest()
         {
-            // try setting value with constructor
-            NetworkPingMessage message = new NetworkPingMessage(DateTime.Now.ToOADate());
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            NetworkPingMessage fresh = new NetworkPingMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
-            Assert.That(fresh.clientTime, Is.EqualTo(message.clientTime));
+            TestSerializeDeserialize(new NetworkPingMessage(DateTime.Now.ToOADate()));
         }
 
         [Test]
         public void NetworkPongMessageTest()
         {
-            // try setting value with constructor
-            NetworkPongMessage message = new NetworkPongMessage
+            TestSerializeDeserialize(new NetworkPongMessage
             {
                 clientTime = DateTime.Now.ToOADate(),
                 serverTime = DateTime.Now.ToOADate(),
-            };
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            NetworkPongMessage fresh = new NetworkPongMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
-            Assert.That(fresh.clientTime, Is.EqualTo(message.clientTime));
-            Assert.That(fresh.serverTime, Is.EqualTo(message.serverTime));
+            });
         }
 
         [Test]
         public void NotReadyMessageTest()
         {
-            // try setting value with constructor
-            NotReadyMessage message = new NotReadyMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            NotReadyMessage fresh = new NotReadyMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new NotReadyMessage());
         }
 
         [Test]
         public void ObjectDestroyMessageTest()
         {
-            // try setting value with constructor
-            ObjectDestroyMessage message = new ObjectDestroyMessage
+            TestSerializeDeserialize(new ObjectDestroyMessage
             {
                 netId = 42,
-            };
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ObjectDestroyMessage fresh = new ObjectDestroyMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
-            Assert.That(fresh.netId, Is.EqualTo(message.netId));
+            });
         }
 
         [Test]
         public void ObjectHideMessageTest()
         {
-            // try setting value with constructor
-            ObjectHideMessage message = new ObjectHideMessage
+            TestSerializeDeserialize(new ObjectHideMessage
             {
                 netId = 42,
-            };
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ObjectHideMessage fresh = new ObjectHideMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
-            Assert.That(fresh.netId, Is.EqualTo(message.netId));
+            });
         }
 
         [Test]
         public void ObjectSpawnFinishedMessageTest()
         {
-            // try setting value with constructor
-            ObjectSpawnFinishedMessage message = new ObjectSpawnFinishedMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ObjectSpawnFinishedMessage fresh = new ObjectSpawnFinishedMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new ObjectSpawnFinishedMessage());
         }
 
         [Test]
         public void ObjectSpawnStartedMessageTest()
         {
             // try setting value with constructor
-            ObjectSpawnStartedMessage message = new ObjectSpawnStartedMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ObjectSpawnStartedMessage fresh = new ObjectSpawnStartedMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new ObjectSpawnStartedMessage());
         }
 
         [Test]
         public void ReadyMessageTest()
         {
-            // try setting value with constructor
-            ReadyMessage message = new ReadyMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            ReadyMessage fresh = new ReadyMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new ReadyMessage());
         }
 
-        [Obsolete("The field we are testing will go away")]
         [Test]
         public void AddPlayerMessageTest()
         {
-            // try setting value with constructor
-            AddPlayerMessage message = new AddPlayerMessage
-            {
-            };
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            AddPlayerMessage fresh = new AddPlayerMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new AddPlayerMessage());
         }
 
         [Test]
         public void RemovePlayerMessageTest()
         {
-            // try setting value with constructor
-            RemovePlayerMessage message = new RemovePlayerMessage();
-
-            // serialize
-            NetworkWriter writer = new NetworkWriter();
-            message.Serialize(writer);
-            byte[] writerData = writer.ToArray();
-
-            // deserialize the same data - do we get the same result?
-            RemovePlayerMessage fresh = new RemovePlayerMessage();
-            fresh.Deserialize(new NetworkReader(writerData));
+            TestSerializeDeserialize(new RemovePlayerMessage());
         }
 
         [Test]
         public void RpcMessageTest()
         {
             // try setting value with constructor
-            RpcMessage message = new RpcMessage
+            var message = new RpcMessage
             {
                 netId = 42,
                 componentIndex = 4,
@@ -275,12 +150,12 @@ namespace Mirror.Tests
             };
 
             // serialize
-            NetworkWriter writer = new NetworkWriter();
+            var writer = new NetworkWriter();
             message.Serialize(writer);
             byte[] writerData = writer.ToArray();
 
             // deserialize the same data - do we get the same result?
-            RpcMessage fresh = new RpcMessage();
+            var fresh = new RpcMessage();
             fresh.Deserialize(new NetworkReader(writerData));
             Assert.That(fresh.netId, Is.EqualTo(message.netId));
             Assert.That(fresh.componentIndex, Is.EqualTo(message.componentIndex));
@@ -300,7 +175,7 @@ namespace Mirror.Tests
             void DoTest(ulong testSceneId)
             {
                 // try setting value with constructor
-                SpawnMessage message = new SpawnMessage
+                var message = new SpawnMessage
                 {
                     netId = 42,
                     isLocalPlayer = true,
@@ -316,12 +191,12 @@ namespace Mirror.Tests
                 UnityEngine.Debug.Log($"sceneId:{message.sceneId} | assetId:{message.assetId}");
 
                 // serialize
-                NetworkWriter writer = new NetworkWriter();
+                var writer = new NetworkWriter();
                 message.Serialize(writer);
                 byte[] writerData = writer.ToArray();
 
                 // deserialize the same data - do we get the same result?
-                SpawnMessage fresh = new SpawnMessage();
+                var fresh = new SpawnMessage();
                 fresh.Deserialize(new NetworkReader(writerData));
                 Assert.That(fresh.netId, Is.EqualTo(message.netId));
                 Assert.That(fresh.isLocalPlayer, Is.EqualTo(message.isLocalPlayer));
@@ -343,7 +218,7 @@ namespace Mirror.Tests
         public void SyncEventMessageTest()
         {
             // try setting value with constructor
-            SyncEventMessage message = new SyncEventMessage
+            var message = new SyncEventMessage
             {
                 netId = 42,
                 componentIndex = 4,
@@ -352,12 +227,12 @@ namespace Mirror.Tests
             };
 
             // serialize
-            NetworkWriter writer = new NetworkWriter();
+            var writer = new NetworkWriter();
             message.Serialize(writer);
             byte[] writerData = writer.ToArray();
 
             // deserialize the same data - do we get the same result?
-            SyncEventMessage fresh = new SyncEventMessage();
+            var fresh = new SyncEventMessage();
             fresh.Deserialize(new NetworkReader(writerData));
             Assert.That(fresh.netId, Is.EqualTo(message.netId));
             Assert.That(fresh.componentIndex, Is.EqualTo(message.componentIndex));
@@ -372,19 +247,19 @@ namespace Mirror.Tests
         public void UpdateVarsMessageTest()
         {
             // try setting value with constructor
-            UpdateVarsMessage message = new UpdateVarsMessage
+            var message = new UpdateVarsMessage
             {
                 netId = 42,
                 payload = new ArraySegment<byte>(new byte[] { 0x01, 0x02 })
             };
 
             // serialize
-            NetworkWriter writer = new NetworkWriter();
+            var writer = new NetworkWriter();
             message.Serialize(writer);
             byte[] writerData = writer.ToArray();
 
             // deserialize the same data - do we get the same result?
-            UpdateVarsMessage fresh = new UpdateVarsMessage();
+            var fresh = new UpdateVarsMessage();
             fresh.Deserialize(new NetworkReader(writerData));
             Assert.That(fresh.netId, Is.EqualTo(message.netId));
             Assert.That(fresh.payload.Count, Is.EqualTo(message.payload.Count));
