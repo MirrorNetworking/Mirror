ae507cf164a377753653f8401e6387e6a14b6776
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2020-03-20 21:03:11 +0000

    Adding check to make sure weaver adds code

--- Assets/Mirror/Tests/Editor/WeaverTest.cs
+++ Assets/Mirror/Tests/Editor/WeaverTest.cs
@@ -1,8 +1,11 @@
 //#define LOG_WEAVER_OUTPUTS
 
 using System.Collections.Generic;
-using NUnit.Framework;
+using System.Linq;
 using Mirror.Weaver;
+using Mono.CecilX;
+using Mono.CecilX.Cil;
+using NUnit.Framework;
 
 namespace Mirror.Tests
 {
@@ -646,6 +649,43 @@ namespace Mirror.Tests
         {
             Assert.That(CompilationFinishedHook.WeaveFailed, Is.True);
             Assert.That(weaverErrors, Contains.Item("Mirror.Weaver error: Cannot generate writer for interface MirrorTest.SuperCoolInterface. Use a concrete type or provide a custom writer"));
+        }
+        #endregion
+
+        #region Server Client Attribute Tests 
+        void CheckAddedCodeServer()
+        {
+            const string networkServerGetActive = "System.Boolean Mirror.NetworkServer::get_active()";
+            CheckAddedCode(networkServerGetActive, "ServerOnlyMethod");
+        }
+
+        void CheckAddedCodeClient()
+        {
+            const string networkClientGetActive = "System.Boolean Mirror.NetworkClient::get_active()";
+            CheckAddedCode(networkClientGetActive, "ClientOnlyMethod");
+        }
+
+        /// <summary>
+        /// Checks that first Instructions in MethodBody is addedString
+        /// </summary>
+        /// <param name="addedString"></param>
+        /// <param name="methodName"></param>
+        void CheckAddedCode(string addedString, string methodName)
+        {
+            string className = "MirrorTest.MirrorTestPlayer";
+            
+            string assemblyName = WeaverAssembler.OutputDirectory + WeaverAssembler.OutputFile;
+            using (AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(assemblyName))
+            {
+                TypeDefinition type = assembly.MainModule.GetType(className);
+                MethodDefinition method = type.Methods.First(m => m.Name == methodName);
+                MethodBody body = method.Body;
+
+                Instruction top = body.Instructions[0];
+
+                Assert.AreEqual(top.OpCode, OpCodes.Call);
+                Assert.AreEqual(top.Operand.ToString(), addedString);
+            }
         }
         #endregion
 
