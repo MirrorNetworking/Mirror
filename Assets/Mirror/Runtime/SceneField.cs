200316d908b236010862a1212ed0736ef97dca09
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2020-03-21 17:40:37 +0000

    adding c# equals checks

--- Assets/Mirror/Runtime/SceneField.cs
+++ Assets/Mirror/Runtime/SceneField.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror
@@ -6,9 +8,34 @@ namespace Mirror
     /// Used for Scene property in the inspector
     /// </summary>
     [System.Serializable]
-    public struct SceneField
+    public struct SceneField : IEquatable<SceneField>
     {
         public string path;
         [SerializeField] string assetGuid;
+
+        public override bool Equals(object obj)
+        {
+            return obj is SceneField field && Equals(field);
+        }
+
+        public bool Equals(SceneField other)
+        {
+            return assetGuid == other.assetGuid;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1488015982 + EqualityComparer<string>.Default.GetHashCode(assetGuid);
+        }
+
+        public static bool operator ==(SceneField left, SceneField right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(SceneField left, SceneField right)
+        {
+            return !(left == right);
+        }
     }
 }
