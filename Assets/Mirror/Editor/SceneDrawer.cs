c6f10ac78e36e224b833bc5066fe6ac2a8e717d4
Paul Pacheco <paulpach@gmail.com>
2020-03-21 08:30:23 -0500

    Automatically fix properties that were using name
    If you open a NetworkManager or other gameobject that uses a scene name
    it now gets converted to scene path by the SceneDrawer

--- Assets/Mirror/Editor/SceneDrawer.cs
+++ Assets/Mirror/Editor/SceneDrawer.cs
@@ -11,6 +11,12 @@ namespace Mirror
             if (property.propertyType == SerializedPropertyType.String)
             {
                 SceneAsset sceneObject = AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue);
+
+                if (sceneObject == null)
+                { 
+                    // try to load it from the build settings for legacy compatibility
+                    sceneObject = GetBuildSettingsSceneObject(property);
+                }
                 SceneAsset scene = (SceneAsset)EditorGUI.ObjectField(position, label, sceneObject, typeof(SceneAsset), true);
                 property.stringValue = AssetDatabase.GetAssetPath(scene);
             }
@@ -18,6 +24,27 @@ namespace Mirror
             {
                 EditorGUI.LabelField(position, label.text, "Use [Scene] with strings.");
             }
+        }
+
+        protected SceneAsset GetBuildSettingsSceneObject(SerializedProperty property)
+        {
+            string sceneObjectName = property.stringValue;
+
+            if (string.IsNullOrEmpty(sceneObjectName))
+            {
+                return null;
+            }
+
+            foreach (EditorBuildSettingsScene editorScene in EditorBuildSettings.scenes)
+            {
+                // hack,  but retains backwards compatibility
+                if (editorScene.path.IndexOf(sceneObjectName) != -1)
+                {
+                    return AssetDatabase.LoadAssetAtPath<SceneAsset>(editorScene.path);
+                }
+            }
+            Debug.LogError($"Scene {sceneObjectName} not found in {property.propertyPath}");
+            return null;
         }
     }
 }
