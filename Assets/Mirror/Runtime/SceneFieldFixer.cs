daa64eaa5688fc2984ec5415e59cb830b2ee16c3
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2020-03-24 23:24:58 +0000

    scenefield fixer for arrays

--- Assets/Mirror/Runtime/SceneFieldFixer.cs
+++ Assets/Mirror/Runtime/SceneFieldFixer.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Mirror
@@ -8,49 +9,99 @@ namespace Mirror
     /// </summary>
     public static class SceneFieldFixer
     {
+        /// <summary>
+        /// works on anything unity serialized as an array, eg arrays/lists
+        /// </summary>
         [System.Diagnostics.Conditional("UNITY_EDITOR")]
-        public static void FixField(UnityEngine.Object target, string oldFieldName, string newFieldName)
+        public static void FixArrayField(Object target, string oldFieldName, string newFieldName)
         {
-            UnityEditor.SerializedObject serializedObject = new UnityEditor.SerializedObject(target);
-            UnityEditor.SerializedProperty oldProp = serializedObject.FindProperty(oldFieldName);
+#if UNITY_EDITOR
+            SerializedObject serializedObject = new SerializedObject(target);
+
+            SerializedProperty oldPropArray = serializedObject.FindProperty(oldFieldName);
+            int arraySize = oldPropArray.arraySize;
+            if (arraySize == 0)
+                return;
+
+            SerializedProperty newPropArray = serializedObject.FindProperty(newFieldName);
+
+            newPropArray.arraySize = arraySize;
+            for (int i = 0; i < arraySize; i++)
+            {
+                SerializedProperty oldEle = oldPropArray.GetArrayElementAtIndex(i);
+                SerializedProperty newEle = newPropArray.GetArrayElementAtIndex(i);
+
+                bool success = FindAndSetScene(oldEle, newEle);
+                if (success)
+                {
+                    Debug.LogWarning($"Could not find Scene with name '{oldEle.stringValue}' in Build settings. Field {newEle.propertyPath} in {target.name} needs manually updating", target);
+                }
+            }
+
+            oldPropArray.arraySize = 0;
+
+            serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(target);
+#endif
+        }
+
+
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void FixField(Object target, string oldFieldName, string newFieldName)
+        {
+#if UNITY_EDITOR
+            SerializedObject serializedObject = new SerializedObject(target);
+            SerializedProperty oldProp = serializedObject.FindProperty(oldFieldName);
 
             if (string.IsNullOrEmpty(oldProp.stringValue))
                 return;
 
-            UnityEditor.SerializedProperty newProp = serializedObject.FindProperty(newFieldName);
-            UnityEditor.SerializedProperty newPropPath = newProp.FindPropertyRelative("path");
-            UnityEditor.SerializedProperty newPropGuid = newProp.FindPropertyRelative("assetGuid");
-
-
-            // only set new prop if is it empty
-            if (string.IsNullOrEmpty(newPropGuid.stringValue))
+            SerializedProperty newProp = serializedObject.FindProperty(newFieldName);
+            bool success = FindAndSetScene(oldProp, newProp);
+            if (success)
             {
-                string path = findScene(oldProp);
-
-                if (!string.IsNullOrEmpty(path))
-                {
-                    newPropPath.stringValue = path;
-                    newPropGuid.stringValue = UnityEditor.AssetDatabase.AssetPathToGUID(path);
-                }
-                else
-                {
-                    Debug.LogWarning($"Could not find Scene with name '{oldProp.stringValue}' in Build settings. Field {newFieldName} in {target.name} needs manually updating", target);
-
-                }
+                Debug.LogWarning($"Could not find Scene with name '{oldProp.stringValue}' in Build settings. Field {newProp.propertyPath} in {target.name} needs manually updating", target);
             }
 
             oldProp.stringValue = "";
 
             serializedObject.ApplyModifiedProperties();
-            UnityEditor.EditorUtility.SetDirty(target);
+            EditorUtility.SetDirty(target);
+#endif
         }
+
 #if UNITY_EDITOR
-        private static string findScene(UnityEditor.SerializedProperty oldProp)
+        static bool FindAndSetScene(SerializedProperty oldProp, SerializedProperty newProp)
         {
-            UnityEditor.EditorBuildSettingsScene[] buildScenes = UnityEditor.EditorBuildSettings.scenes;
-            foreach (UnityEditor.EditorBuildSettingsScene buildScene in buildScenes)
+            SerializedProperty newPropPath = newProp.FindPropertyRelative("path");
+            SerializedProperty newPropGuid = newProp.FindPropertyRelative("assetGuid");
+
+
+            // only set new prop if is it empty
+            if (string.IsNullOrEmpty(newPropGuid.stringValue))
             {
-                UnityEditor.SceneAsset sceneAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEditor.SceneAsset>(buildScene.path);
+                string path = FindScene(oldProp);
+
+                if (!string.IsNullOrEmpty(path))
+                {
+                    newPropPath.stringValue = path;
+                    newPropGuid.stringValue = AssetDatabase.AssetPathToGUID(path);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string FindScene(SerializedProperty oldProp)
+        {
+            EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+            foreach (EditorBuildSettingsScene buildScene in buildScenes)
+            {
+                SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(buildScene.path);
                 if (sceneAsset.name == oldProp.stringValue)
                 {
                    return buildScene.path;
