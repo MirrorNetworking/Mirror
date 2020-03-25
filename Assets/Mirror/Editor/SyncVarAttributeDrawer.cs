f4bc7ca6913c57eb1014258da4375a5924dee209
Paul Pacheco <paulpach@gmail.com>
2020-03-24 20:01:00 -0500

    use same parameter name in override

--- Assets/Mirror/Editor/SyncVarAttributeDrawer.cs
+++ Assets/Mirror/Editor/SyncVarAttributeDrawer.cs
@@ -8,13 +8,13 @@ namespace Mirror
     {
         static readonly GUIContent syncVarIndicatorContent = new GUIContent("SyncVar", "This variable has been marked with the [SyncVar] attribute.");
 
-        public override void OnGUI(Rect propRect, SerializedProperty property, GUIContent label)
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
         {
             Vector2 syncVarIndicatorRect = EditorStyles.miniLabel.CalcSize(syncVarIndicatorContent);
-            float valueWidth = propRect.width - syncVarIndicatorRect.x;
+            float valueWidth = rect.width - syncVarIndicatorRect.x;
 
-            Rect valueRect = new Rect(propRect.x, propRect.y, valueWidth, propRect.height);
-            Rect labelRect = new Rect(propRect.x + valueWidth, propRect.y, syncVarIndicatorRect.x, propRect.height);
+            Rect valueRect = new Rect(rect.x, rect.y, valueWidth, rect.height);
+            Rect labelRect = new Rect(rect.x + valueWidth, rect.y, syncVarIndicatorRect.x, rect.height);
 
             EditorGUI.PropertyField(valueRect, property, true);
             GUI.Label(labelRect, syncVarIndicatorContent, EditorStyles.miniLabel);
