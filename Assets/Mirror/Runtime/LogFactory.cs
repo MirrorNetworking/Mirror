d66cd6781ebc9769baff1dd09b14eb8ef13b9e59
Paul Pacheco <paulpach@gmail.com>
2020-03-27 10:20:49 -0500

    feat: logging api
    Provide a ILogger (from unity framework) per class
    it can track loggers per class and use  Debug.unityLogger by default

---
+++ Assets/Mirror/Runtime/LogFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror
+{
+    // hold a reference to the class logger
+    internal static class Loggers<T>
+    {
+        // note that Loggers<X>.Logger
+        // would have a separate value than Loggers<Y>.Logger
+        // this way we can always get teh same logger back and change it's log level
+        public static ILogger Logger = new Logger(Debug.unityLogger)
+        {
+            // by default, log warnings and up
+            filterLogType = LogType.Warning
+        };
+    }
+
+    public static class LogFactory
+    {
+        public static ILogger GetLogger<T>()
+        {
+            return Loggers<T>.Logger;
+        }
+    }
+}
---
+++ Assets/Mirror/Tests/Editor/LogFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.Tests
+{
+
+    public class LogFactoryTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SameClassSameLogger()
+        {
+            ILogger logger1 = LogFactory.GetLogger<LogFactoryTests>();
+            ILogger logger2 = LogFactory.GetLogger<LogFactoryTests>();
+            Assert.That(logger1, Is.SameAs(logger2));
+        }
+
+        [Test]
+        public void DifferentClassDifferentLogger()
+        {
+            ILogger logger1 = LogFactory.GetLogger<LogFactoryTests>();
+            ILogger logger2 = LogFactory.GetLogger<NetworkManager>();
+            Assert.That(logger1, Is.Not.SameAs(logger2));
+        }
+
+        [Test]
+        public void LogDebugIgnore()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Warning;
+
+            logger.Log("This message should not be logged");
+            LogAssert.NoUnexpectedReceived();
+        }
+
+        [Test]
+        public void LogWarnOnly()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Warning;
+
+            LogAssert.Expect(LogType.Warning, "LogFactoryTests: This message should be logged");
+            logger.LogWarning(nameof(LogFactoryTests), "This message should be logged");
+            LogAssert.NoUnexpectedReceived();
+        }
+
+    }
+}
