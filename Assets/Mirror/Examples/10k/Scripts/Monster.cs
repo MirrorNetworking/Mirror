8787d004728ddb21d2d285ba95d55186cd844d40
vis2k <info@noobtuts.com>
2020-03-28 12:04:57 +0100

    Monster script

---
+++ Assets/Mirror/Examples/10k/Scripts/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.Examples
+{
+    public class Monster : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
