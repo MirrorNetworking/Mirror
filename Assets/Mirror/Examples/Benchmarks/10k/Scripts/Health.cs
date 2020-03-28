ead58d54471803b40368f3e203ea7ba76a3ca87b
vis2k <info@noobtuts.com>
2020-03-28 12:26:49 +0100

    health update

--- Assets/Mirror/Examples/Benchmarks/10k/Scripts/Health.cs
+++ Assets/Mirror/Examples/Benchmarks/10k/Scripts/Health.cs
@@ -5,5 +5,11 @@ namespace Mirror.Examples
     public class Health : NetworkBehaviour
     {
         [SyncVar] public int health = 10;
+
+        [ServerCallback]
+        public void Update()
+        {
+            health = Random.Range(1, 10);
+        }
     }
 }
