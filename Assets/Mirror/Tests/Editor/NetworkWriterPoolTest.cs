3264b794c38eb4b267f9c5e99b171c189eb2b58c
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2020-03-29 15:25:32 +0100

    adding tests for  NetworkWriterPool

--- Assets/Mirror/Tests/Editor/NetworkWriterPoolTest.cs
+++ Assets/Mirror/Tests/Editor/NetworkWriterPoolTest.cs
@@ -1,10 +1,10 @@
-﻿using NUnit.Framework;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Mirror.Tests
 {
     public class NetworkWriterPoolTest
     {
-        // A Test behaves as an ordinary method
         [Test]
         public void TestPoolRecycling()
         {
@@ -19,6 +19,72 @@ namespace Mirror.Tests
             {
                 Assert.That(writer, Is.SameAs(firstWriter), "Pool should reuse the writer");
             }
+        }
+
+
+        [Test]
+        public void PoolCanGetMoreWritersThanPoolSize()
+        {
+            NetworkWriterPool.Capacity = 5;
+
+            const int testWriterCount = 10;
+            PooledNetworkWriter[] writers = new PooledNetworkWriter[testWriterCount];
+
+            for (int i = 0; i < testWriterCount; i++)
+            {
+                writers[i] = NetworkWriterPool.GetWriter();
+            }
+
+            // Make sure all writers are different
+            Assert.That(writers.Distinct().Count(), Is.EqualTo(testWriterCount));
+
+            NetworkWriterPool.ResetCapacity();
+        }
+
+        [Test]
+        [Ignore("WIP")]
+        public void PoolReUsesWritersUpToSizeLimit()
+        {
+            //NetworkWriterPool.ResizePool(5);
+
+            //const int testWriterCount = 5;
+            //PooledNetworkWriter[] writers1 = new PooledNetworkWriter[testWriterCount];
+            //PooledNetworkWriter[] writers2 = new PooledNetworkWriter[testWriterCount];
+            //PooledNetworkWriter extra1;
+            //PooledNetworkWriter extra2;
+
+            //// simulate first update
+            //for (int i = 0; i < testWriterCount; i++)
+            //{
+            //    writers1[i] = NetworkWriterPool.GetWriter();
+            //}
+            //extra1 = NetworkWriterPool.GetWriter();
+
+            //for (int i = 0; i < testWriterCount; i++)
+            //{
+            //    writers1[i].Dispose();
+            //}
+            //extra1.Dispose();
+
+
+            //// simulate second update
+            //for (int i = 0; i < testWriterCount; i++)
+            //{
+            //    writers2[i] = NetworkWriterPool.GetWriter();
+            //}
+            //extra2 = NetworkWriterPool.GetWriter();
+
+            //for (int i = 0; i < testWriterCount; i++)
+            //{
+            //    writers2[i].Dispose();
+            //}
+            //extra2.Dispose();
+
+
+            //Assert
+
+
+            //NetworkWriterPool.ResetPoolSize();
         }
     }
 }
