ea4e9efbe847ae01963e66897ffa09c3c75a4aa2
Paul Pacheco <paulpach@gmail.com>
2020-03-31 17:20:20 -0500

    in memory loopback transport useful for testing

---
+++ Assets/Mirror/Tests/Common/LoopbackTransport.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Mirror.Tests
+{
+
+    public class LoopbackTransport : AsyncTransport
+    {
+        public TaskCompletionSource<IConnection> AcceptCompletionSource ;
+
+        public override Task<IConnection> AcceptAsync()
+        {
+            AcceptCompletionSource = new TaskCompletionSource<IConnection>();
+            return AcceptCompletionSource.Task;
+        }
+
+        public TaskCompletionSource<IConnection> ConnectCompletionSource;
+
+        public override Task<IConnection> ConnectAsync(Uri uri)
+        {
+            (IConnection c1, IConnection c2) = PipeConnection.CreatePipe();
+            if (AcceptCompletionSource == null)
+                return Task.FromException<IConnection>(new SocketException((int)SocketError.ConnectionRefused));
+            else
+            {
+                AcceptCompletionSource?.SetResult(c2);
+                return Task.FromResult(c1);
+            }
+        }
+
+        public override void Disconnect()
+        {
+            AcceptCompletionSource.TrySetResult(null);
+        }
+
+        public override Task ListenAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Uri ServerUri()
+        {
+            return new Uri("tcp4://localhost");
+        }
+    }
+}
