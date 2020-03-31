3375e3ee53464e722fd166a4d11a68c0bbd75609
Paul Pacheco <paulpach@gmail.com>
2020-03-31 17:19:02 -0500

    Mock transport useful for testing

---
+++ Assets/Mirror/Tests/Common/MockTransport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mirror.Tests
+{
+
+    public class MockTransport : AsyncTransport
+    {
+        public TaskCompletionSource<IConnection> AcceptCompletionSource ;
+
+        public override Task<IConnection> AcceptAsync()
+        {
+            AcceptCompletionSource = new TaskCompletionSource<IConnection>();
+            return AcceptCompletionSource.Task;
+        }
+
+        public TaskCompletionSource<IConnection> ConnectCompletionSource;
+
+        public override Task<IConnection> ConnectAsync(Uri uri)
+        {
+            ConnectCompletionSource = new TaskCompletionSource<IConnection>();
+            return ConnectCompletionSource.Task;
+        }
+
+        public override void Disconnect()
+        {
+            AcceptCompletionSource.TrySetResult(null);
+        }
+
+        public override Task ListenAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Uri ServerUri()
+        {
+            return new Uri("tcp4://localhost");
+        }
+    }
+}
