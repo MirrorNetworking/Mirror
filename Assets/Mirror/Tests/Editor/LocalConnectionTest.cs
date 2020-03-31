4fb743b3dad50e73039903118c2b43a2204480e9
Paul Pacheco <paulpach@gmail.com>
2020-03-31 10:39:40 -0500

    fix bad merge

--- Assets/Mirror/Tests/Editor/LocalConnectionTest.cs
+++ Assets/Mirror/Tests/Editor/LocalConnectionTest.cs
@@ -41,83 +41,11 @@ namespace Mirror.Tests
         {
             Assert.That(connectionToServer.Address, Is.EqualTo(new IPEndPoint(IPAddress.Loopback, 0)));
         }
-    }
 
         [Test]
         public void ClientToServerFailTest()
         {
-            Assert.Throws<InvalidMessageException>( () => connectionToServer.Send(new ArraySegment<byte>(new byte[0])));
+            Assert.Throws<InvalidMessageException>(() => connectionToServer.Send(new ArraySegment<byte>(new byte[0])));
         }
-
-        /*[Test]
-        public void ServerToClientTest()
-        {
-            Assert.That(connectionToClient.address, Is.EqualTo("localhost"));
-
-            var myMessage = new MyMessage
-            {
-                id = 3,
-                name = "hello"
-            };
-
-            bool invoked = false;
-
-            void handler(NetworkConnection conn, NetworkReader reader, int channelId)
-            {
-                var received = new MyMessage();
-                received.Deserialize(reader);
-                Assert.That(received.id, Is.EqualTo(3));
-                Assert.That(received.name, Is.EqualTo("hello"));
-                invoked = true;
-            }
-
-            var handlers = new Dictionary<int, NetworkMessageDelegate>
-            {
-                { MessagePacker.GetId<MyMessage>(), handler }
-            };
-
-            connectionToClient.SetHandlers(handlers);
-            connectionToServer.Send(myMessage);
-
-            connectionToServer.Update();
-
-            Assert.True(invoked, "handler should have been invoked");
-        }*/
-
-        /*[Test]
-        public void ClientToServerTest()
-        {
-            Assert.That(connectionToServer.address, Is.EqualTo("localhost"));
-
-            var myMessage = new MyMessage
-            {
-                id = 3,
-                name = "hello"
-            };
-
-            bool invoked = false;
-
-            void handler(NetworkConnection conn, NetworkReader reader, int channelId)
-            {
-                var received = new MyMessage();
-                received.Deserialize(reader);
-                Assert.That(received.id, Is.EqualTo(3));
-                Assert.That(received.name, Is.EqualTo("hello"));
-                invoked = true;
-            }
-
-            var handlers = new Dictionary<int, NetworkMessageDelegate>
-            {
-                { MessagePacker.GetId<MyMessage>(), handler }
-            };
-
-            connectionToServer.SetHandlers(handlers);
-            connectionToClient.Send(myMessage);
-
-            connectionToServer.Update();
-
-            Assert.True(invoked, "handler should have been invoked");
-        }*/
-
     }
 }
