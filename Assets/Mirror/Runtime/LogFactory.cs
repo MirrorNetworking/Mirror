419fd79eb5fd3ea4ead924e3b882ce79c8ff3b86
James Frowen <jamesfrowendev@gmail.com>
2020-04-01 12:00:20 +0100

    MultiLogHandler

--- Assets/Mirror/Runtime/LogFactory.cs
+++ Assets/Mirror/Runtime/LogFactory.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror
@@ -14,12 +14,13 @@ namespace Mirror
 
         public static ILogger GetLogger(string loggerName)
         {
-            if (loggers.TryGetValue(loggerName, out ILogger logger ))
+            if (loggers.TryGetValue(loggerName, out ILogger logger))
             {
                 return logger;
             }
 
-            logger = new Logger(Debug.unityLogger)
+            ILogHandler handler = new MultiLogHandler(Debug.unityLogger);
+            logger = new Logger(handler)
             {
                 // by default, log warnings and up
                 filterLogType = LogType.Warning
@@ -27,6 +28,52 @@ namespace Mirror
 
             loggers[loggerName] = logger;
             return logger;
+        }
+
+        public static void AddLogHandler(ILogHandler handler)
+        {
+            foreach (ILogger logger in loggers.Values)
+            {
+                if (logger.logHandler is MultiLogHandler multiLog)
+                {
+                    multiLog.handlers.Add(handler);
+                }
+            }
+        }
+        public static void RemoveLogHandler(ILogHandler handler)
+        {
+            foreach (ILogger logger in loggers.Values)
+            {
+                if (logger.logHandler is MultiLogHandler multiLog)
+                {
+                    multiLog.handlers.Remove(handler);
+                }
+            }
+        }
+    }
+
+    internal class MultiLogHandler : ILogHandler
+    {
+        public readonly List<ILogHandler> handlers;
+
+        public MultiLogHandler(params ILogHandler[] loggers)
+        {
+            handlers = new List<ILogHandler>(loggers);
+        }
+
+        public void LogFormat(LogType logType, Object context, string format, params object[] args)
+        {
+            for (int i = 0; i < handlers.Count; i++)
+            {
+                handlers[i].LogFormat(logType, context, format, args);
+            }
+        }
+        public void LogException(System.Exception exception, Object context)
+        {
+            for (int i = 0; i < handlers.Count; i++)
+            {
+                handlers[i].LogException(exception, context);
+            }
         }
     }
 }
