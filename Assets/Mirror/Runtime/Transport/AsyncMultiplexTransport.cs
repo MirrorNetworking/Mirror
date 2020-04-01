c0e7e9280931a5996f595e41aa516bef20208b6f
Paul Pacheco <paulpach@gmail.com>
2020-03-31 22:57:41 -0500

    feat: async multiplex transport (#145)
    * feat: async multiplex transport
    
    * reset accepters when we start listening
    
    * Reset accepters once we start listening

---
+++ Assets/Mirror/Runtime/Transport/AsyncMultiplexTransport.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mirror
+{
+    public class AsyncMultiplexTransport : AsyncTransport
+    {
+
+        public AsyncTransport[] transports;
+
+        private Dictionary<Task<IConnection>, AsyncTransport> Accepters;
+
+        public override async Task<IConnection> AcceptAsync()
+        {
+            if (Accepters == null)
+            {
+                Accepters = new Dictionary<Task<IConnection>, AsyncTransport>();
+
+                foreach (AsyncTransport transport in transports)
+                {
+                    Task<IConnection> transportAccepter = transport.AcceptAsync();
+                    Accepters[transportAccepter] = transport;
+                }
+            }
+
+            // that's it nobody is left to accept
+            if (Accepters.Count == 0)
+                return null;
+
+            // wait for any one of them to accept
+            var task = await Task.WhenAny(Accepters.Keys);
+
+            AsyncTransport acceptedTransport = Accepters[task];
+            Accepters.Remove(task);
+
+            IConnection value = await task;
+
+            if (value == null)
+            {
+                // this transport closed. Get the next one
+                return await AcceptAsync();
+            }
+            else
+            {
+                // transport may accept more connections
+                task = acceptedTransport.AcceptAsync();
+                Accepters[task] = acceptedTransport;
+
+                return value;
+            }
+        }
+
+        public override async Task<IConnection> ConnectAsync(Uri uri)
+        {
+            foreach (AsyncTransport transport in transports)
+            {
+                try
+                {
+                    return await transport.ConnectAsync(uri);
+                }
+                catch (ArgumentException)
+                {
+                    // try the next transport
+                }
+            }
+            throw new ArgumentException($"No transport was able to connect to {uri}");
+        }
+
+        public override void Disconnect()
+        {
+            foreach (AsyncTransport transport in transports)
+                transport.Disconnect();
+
+        }
+
+        public override async Task ListenAsync()
+        {
+            IEnumerable<Task> tasks = from t in transports select t.ListenAsync();
+            await Task.WhenAll(tasks);
+            Accepters = null;
+        }
+
+        public override Uri ServerUri()
+        {
+            return transports[0].ServerUri();
+        }
+    }
+}
---
+++ Assets/Mirror/Tests/Editor/Transport/AsyncMultiplexTransportTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using static Mirror.Tests.AsyncUtil;
+using Object = UnityEngine.Object;
+
+namespace Mirror.Tests
+{
+    public class AsyncMultiplexTransportTest
+    {
+        #region SetUp
+
+        private GameObject transportObj;
+        private AsyncMultiplexTransport transport;
+
+        private AsyncTransport transport1;
+        private AsyncTransport transport2;
+
+        IConnection conn1;
+        IConnection conn2;
+
+        [SetUp]
+        public void Setup()
+        {
+            transportObj = new GameObject();
+
+            transport = transportObj.AddComponent<AsyncMultiplexTransport>();
+
+            // this gives warnings,  it is ok
+            transport1 = Substitute.For<AsyncTransport>();
+            transport2 = Substitute.For<AsyncTransport>();
+
+            transport.transports = new[] { transport1, transport2 };
+            conn1 = Substitute.For<IConnection>();
+            conn2 = Substitute.For<IConnection>();
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        { 
+            Object.DestroyImmediate(transportObj);
+        }
+        #endregion
+
+        [UnityTest]
+        public IEnumerator AcceptTransport1() => RunAsync(async () =>
+        {
+            transport1.AcceptAsync().Returns(Task.FromResult(conn1));
+
+            Assert.That(await transport.AcceptAsync(), Is.SameAs(conn1));
+        });
+
+        [UnityTest]
+        public IEnumerator AcceptTransport2() => RunAsync(async () =>
+        {
+            transport2.AcceptAsync().Returns(Task.FromResult(conn1));
+            // transport1 task never ends
+            transport1.AcceptAsync().Returns(new TaskCompletionSource<IConnection>().Task);
+            Assert.That(await transport.AcceptAsync(), Is.SameAs(conn1));
+        });
+
+        [UnityTest]
+        public IEnumerator AcceptMultiple() => RunAsync(async () =>
+        {
+            transport1.AcceptAsync().Returns(Task.FromResult(conn1), Task.FromResult(conn2));
+            // transport2 task never ends
+            transport2.AcceptAsync().Returns(new TaskCompletionSource<IConnection>().Task);
+            Assert.That(await transport.AcceptAsync(), Is.SameAs(conn1));
+            Assert.That(await transport.AcceptAsync(), Is.SameAs(conn2));
+        });
+
+        [UnityTest]
+        public IEnumerator AcceptUntilAllGone() => RunAsync(async () =>
+        {
+            transport1.AcceptAsync().Returns(x => Task.FromResult(conn1), x => Task.FromResult<IConnection>(null));
+            // transport2 task never ends
+            transport2.AcceptAsync().Returns(x => Task.FromResult(conn2), x => Task.FromResult<IConnection>(null));
+
+            var accepted1 = await transport.AcceptAsync();
+            var accepted2 = await transport.AcceptAsync();
+
+            Assert.That(new[] { accepted1, accepted2 }, Is.EquivalentTo( new [] { conn1, conn2}));
+
+            Assert.That(await transport.AcceptAsync(), Is.Null);
+        });
+
+        [UnityTest]
+        public IEnumerator Listen() => RunAsync(async () =>
+        {
+            transport1.ListenAsync().Returns(Task.CompletedTask);
+            transport2.ListenAsync().Returns(Task.CompletedTask);
+            await transport.ListenAsync();
+
+            _ = transport1.Received().ListenAsync();
+            _ = transport1.Received().ListenAsync();
+
+        });
+
+        [Test]
+        public void Disconnect()
+        {
+            transport.Disconnect();
+
+            transport1.Received().Disconnect();
+            transport2.Received().Disconnect();
+        }
+
+        [Test]
+        public void ServerUri()
+        {
+            transport1.ServerUri().Returns(new Uri("tcp4://myserver"));
+
+            Assert.That(transport.ServerUri(), Is.EqualTo(new Uri("tcp4://myserver")));
+        }
+
+        [UnityTest]
+        public IEnumerator Connect() => RunAsync(async () =>
+        {
+            transport1.ConnectAsync(Arg.Any<Uri>())
+                .Returns(Task.FromException<IConnection>(new ArgumentException("Invalid protocol")));
+
+            // transport2 gives a connection
+            transport2.ConnectAsync(Arg.Any<Uri>())
+                .Returns(Task.FromResult(conn2));
+
+            var accepted1 = await transport.ConnectAsync(new Uri("tcp4://localhost"));
+
+            Assert.That(accepted1, Is.SameAs(conn2));
+        });
+    }
+}
