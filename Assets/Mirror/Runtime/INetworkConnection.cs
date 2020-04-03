ce076de5cbd2da00e0e9de5b8de5bc8c0110969b
uwee <uweenukr@gmail.com>
2020-04-03 17:57:17 -0400

    populate INetworkConnection (#149)

--- Assets/Mirror/Runtime/INetworkConnection.cs
+++ Assets/Mirror/Runtime/INetworkConnection.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks;
 
 namespace Mirror
 {
     public interface INetworkConnection : IDisposable
     {
+        void Disconnect();
 
+        void RegisterHandler<C, T>(Action<C, T> handler, bool requireAuthentication = true)
+                where T : IMessageBase, new()
+                where C : NetworkConnection;
+
+        void RegisterHandler<T>(Action<T> handler, bool requireAuthentication = true) where T : IMessageBase, new();
+
+        void UnregisterHandler<T>() where T : IMessageBase;
+
+        void ClearHandlers();
+
+        void Send<T>(T msg, int channelId = Channels.DefaultReliable) where T : IMessageBase;
+
+        Task SendAsync<T>(T msg, int channelId = Channels.DefaultReliable) where T : IMessageBase;
+
+        string ToString();
+
+        bool InvokeHandler<T>(T msg, int channelId) where T : IMessageBase;
     }
 }
