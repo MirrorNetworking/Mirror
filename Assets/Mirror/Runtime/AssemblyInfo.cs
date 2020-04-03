fc9e53c7ad16ad20bf4352dba03dfa3b017c6fef
James Frowen <jamesfrowendev@gmail.com>
2020-04-03 01:18:46 +0100

    adding performance test asmdef

--- Assets/Mirror/Runtime/AssemblyInfo.cs
+++ Assets/Mirror/Runtime/AssemblyInfo.cs
@@ -2,3 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Mirror.Tests")]
 [assembly: InternalsVisibleTo("Mirror.Tests.Runtime")]
+[assembly: InternalsVisibleTo("Mirror.Tests.Performance")]
+[assembly: InternalsVisibleTo("Mirror.Tests.Performance.Runtime")]
