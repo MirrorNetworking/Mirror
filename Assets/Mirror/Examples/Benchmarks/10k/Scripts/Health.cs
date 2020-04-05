13647d19a53e60727d565134de07f286cab9c40b
James Frowen <jamesfrowendev@gmail.com>
2020-04-05 13:43:57 +0100

    adding bool to stop health updating

--- Assets/Mirror/Examples/Benchmarks/10k/Scripts/Health.cs
+++ Assets/Mirror/Examples/Benchmarks/10k/Scripts/Health.cs
@@ -1,12 +1,19 @@
-﻿namespace Mirror.Examples
+namespace Mirror.Examples
 {
     public class Health : NetworkBehaviour
     {
+        /// <summary>
+        /// When false health updates every second causes it to be synced over network
+        /// </summary>
+        public static bool idle = false;
+
         [SyncVar] public int health = 10;
 
         [ServerCallback]
         public void Update()
         {
+            if (idle) { return; }
+
             health = (health + 1) % 10;
         }
     }
