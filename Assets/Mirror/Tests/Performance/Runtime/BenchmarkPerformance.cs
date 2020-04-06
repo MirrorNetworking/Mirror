af8d79028fc6e7cfb76b36a8cc70cc793004cba0
James Frowen <jamesfrowendev@gmail.com>
2020-04-06 01:55:29 +0100

    using hooks to measure time of Lateupdate
    moving code to setup and teardown

--- Assets/Mirror/Tests/Performance/Runtime/BenchmarkPerformance.cs
+++ Assets/Mirror/Tests/Performance/Runtime/BenchmarkPerformance.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Diagnostics;
 using Mirror;
+using Mirror.Examples;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
 using UnityEditor.SceneManagement;
@@ -17,14 +19,33 @@ namespace Tests
         const float Warmup = 1f;
         const int MeasureCount = 120;
 
-        [UnityTest]
-#if UNITY_2019_2_OR_NEWER
-        [Performance]
-#else
-        [PerformanceUnityTest]
-#endif
-        public IEnumerator Benchmark10k()
+        readonly SampleGroupDefinition NetworkManagerSample = new SampleGroupDefinition("NetworkManagerLateUpdate", SampleUnit.Millisecond, AggregationType.Average);
+        readonly Stopwatch stopwatch = new Stopwatch();
+        bool captureMeasurement;
+        void BeforeLateUpdate()
         {
+            if (!captureMeasurement)
+                return;
+
+            stopwatch.Start();
+        }
+        void AfterLateUpdate()
+        {
+            if (!captureMeasurement)
+                return;
+
+            stopwatch.Stop();
+            double time = stopwatch.Elapsed.TotalMilliseconds;
+            Measure.Custom(NetworkManagerSample, time);
+            stopwatch.Reset();
+        }
+
+
+
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            captureMeasurement = false;
             // load scene
             yield return EditorSceneManager.LoadSceneAsyncInPlayMode(ScenePath, new LoadSceneParameters { loadSceneMode = LoadSceneMode.Additive });
             Scene scene = SceneManager.GetSceneByPath(ScenePath);
@@ -35,14 +56,55 @@ namespace Tests
             // load host
             NetworkManager.singleton.StartHost();
 
-            // warmup
-            yield return new WaitForSecondsRealtime(Warmup);
+            BenchmarkNetworkManager benchmarker = NetworkManager.singleton as BenchmarkNetworkManager;
+            if (benchmarker == null)
+            {
+                Assert.Fail("Could not find Benchmarker");
+                yield break;
+            }
 
+
+            benchmarker.BeforeLateUpdate = BeforeLateUpdate;
+            benchmarker.AfterLateUpdate = AfterLateUpdate;
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
             // run benchmark
             yield return Measure.Frames().MeasurementCount(MeasureCount).Run();
 
-            // unload scenne
+            // shutdown
+            GameObject go = NetworkManager.singleton.gameObject;
+            NetworkManager.Shutdown();
+            // unload scene
+            Scene scene = SceneManager.GetSceneByPath(ScenePath);
             yield return SceneManager.UnloadSceneAsync(scene);
+            if (go != null)
+            {
+                UnityEngine.Object.Destroy(go);
+            }
+        }
+
+        [UnityTest]
+#if UNITY_2019_2_OR_NEWER
+        [Performance]
+#else
+        [PerformanceUnityTest]
+#endif
+        public IEnumerator Benchmark10k()
+        {
+            // warmup
+            yield return new WaitForSecondsRealtime(Warmup);
+
+            captureMeasurement = true;
+
+            for (int i = 0; i < MeasureCount; i++)
+            {
+                yield return null;
+            }
+
+            captureMeasurement = false;
         }
     }
 }
