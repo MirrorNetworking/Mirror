9fd3de461f1d258bbd03a5433f517b5a4844b847
Chris Langsenkamp <chris@clevertech.net>
2020-04-10 15:32:42 -0400

    Stop using sceneIds at runtime

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -259,33 +259,33 @@ namespace Mirror
             observers?.Remove(conn.connectionId);
         }
 
-        void Awake()
-        {
-            // detect runtime sceneId duplicates, e.g. if a user tries to
-            // Instantiate a sceneId object at runtime. if we don't detect it,
-            // then the client won't know which of the two objects to use for a
-            // SpawnSceneObject message, and it's likely going to be the wrong
-            // object.
-            //
-            // This might happen if for example we have a Dungeon GameObject
-            // which contains a Skeleton monster as child, and when a player
-            // runs into the Dungeon we create a Dungeon Instance of that
-            // Dungeon, which would duplicate a scene object.
-            //
-            // see also: https://github.com/vis2k/Mirror/issues/384
-            if (Application.isPlaying && sceneId != 0)
-            {
-                if (sceneIds.TryGetValue(sceneId, out NetworkIdentity existing) && existing != this)
-                {
-                    Debug.LogError(name + "'s sceneId: " + sceneId.ToString("X") + " is already taken by: " + existing.name + ". Don't call Instantiate for NetworkIdentities that were in the scene since the beginning (aka scene objects). Otherwise the client won't know which object to use for a SpawnSceneObject message.");
-                    Destroy(gameObject);
-                }
-                else
-                {
-                    sceneIds[sceneId] = this;
-                }
-            }
-        }
+        //void Awake()
+        //{
+        //    // detect runtime sceneId duplicates, e.g. if a user tries to
+        //    // Instantiate a sceneId object at runtime. if we don't detect it,
+        //    // then the client won't know which of the two objects to use for a
+        //    // SpawnSceneObject message, and it's likely going to be the wrong
+        //    // object.
+        //    //
+        //    // This might happen if for example we have a Dungeon GameObject
+        //    // which contains a Skeleton monster as child, and when a player
+        //    // runs into the Dungeon we create a Dungeon Instance of that
+        //    // Dungeon, which would duplicate a scene object.
+        //    //
+        //    // see also: https://github.com/vis2k/Mirror/issues/384
+        //    if (Application.isPlaying && sceneId != 0)
+        //    {
+        //        if (sceneIds.TryGetValue(sceneId, out NetworkIdentity existing) && existing != this)
+        //        {
+        //            Debug.LogError(name + "'s sceneId: " + sceneId.ToString("X") + " is already taken by: " + existing.name + ". Don't call Instantiate for NetworkIdentities that were in the scene since the beginning (aka scene objects). Otherwise the client won't know which object to use for a SpawnSceneObject message.");
+        //            Destroy(gameObject);
+        //        }
+        //        else
+        //        {
+        //            sceneIds[sceneId] = this;
+        //        }
+        //    }
+        //}
 
         void OnValidate()
         {
@@ -509,8 +509,8 @@ namespace Mirror
             // remove from sceneIds
             // -> remove with (0xFFFFFFFFFFFFFFFF) and without (0x00000000FFFFFFFF)
             //    sceneHash to be 100% safe.
-            sceneIds.Remove(sceneId);
-            sceneIds.Remove(sceneId & 0x00000000FFFFFFFF);
+            //sceneIds.Remove(sceneId);
+            //sceneIds.Remove(sceneId & 0x00000000FFFFFFFF);
 
             // Only call NetworkServer.Destroy on server and only if reset is false
             // reset will be false from incorrect use of Destroy instead of NetworkServer.Destroy
