cab639990969eca5384576f6a83ef6e88c203ce9
Chris Langsenkamp <chris@clevertech.net>
2020-04-10 19:24:23 -0400

    Multiple Additive Physics Scenes Example

---
+++ Assets/Mirror/Examples/MultipleAdditiveScenes/Scripts/PhysicsSimulator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Mirror.Examples.MultipleAdditiveScenes
+{
+    public class PhysicsSimulator : MonoBehaviour
+    {
+        public PhysicsScene physicsScene;
+        private float timer;
+
+        private void Awake()
+        {
+            enabled = (NetworkServer.active);
+            physicsScene = gameObject.scene.GetPhysicsScene();
+        }
+
+        void Update()
+        {
+            if (!physicsScene.IsValid())
+                return; // do nothing if the physics Scene is not valid.
+
+            timer += Time.deltaTime;
+
+            // Catch up with the game time.
+            // Advance the physics simulation in portions of Time.fixedDeltaTime
+            // Note that generally, we don't want to pass variable delta to Simulate as that leads to unstable results.
+            while (timer >= Time.fixedDeltaTime)
+            {
+                timer -= Time.fixedDeltaTime;
+                physicsScene.Simulate(Time.fixedDeltaTime);
+            }
+        }
+    }
+}
