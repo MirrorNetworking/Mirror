40513398a864bd8b7b39eaa519ee5fd7cd03106e
vis2k <info@noobtuts.com>
2020-04-10 10:06:32 +0200

    backup

---
+++ Assets/Mirror/Runtime/NetworkProximityCheck.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Mirror
+{
+    // unfortunately the name NetworkProximityChecker is already used by our
+    // component that does spherecasts.
+    // maybe rename it later.
+    //
+    // note: we inherit from NetworkBehaviour so we can reuse .netIdentity, etc.
+    public abstract class NetworkProximityCheck : NetworkBehaviour
+    {
+    }
+}
