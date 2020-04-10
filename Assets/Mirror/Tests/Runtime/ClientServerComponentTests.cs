c14cc10a00941d0b403ab64d86348835b80683af
Paul Pacheco <paulpach@gmail.com>
2020-04-10 17:27:34 -0500

    Add tests for client/server mode

---
+++ Assets/Mirror/Tests/Runtime/ClientServerComponentTests.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.Tests
+{
+    public class ClientServerComponentTests : ClientServerSetup<MockComponent>
+    {
+        /*
+        [Test]
+        public void CommandWithoutAuthority()
+        {
+            var gameObject2 = new GameObject();
+            MockComponent rpcComponent2 = gameObject2.AddComponent<MockComponent>();
+
+            // spawn it without client authority
+            server.Spawn(gameObject2);
+
+            // process spawn message from server
+            client.Update();
+
+            // only authorized clients can call command
+            Assert.Throws<UnauthorizedAccessException>(() =>
+           {
+               rpcComponent2.CmdTest(1, "hello");
+           });
+
+        }
+        */
+
+
+
+        [Test]
+        public void CheckNotHost()
+        {
+            Assert.That(serverPlayerGO, Is.Not.SameAs(clientPlayerGO));
+
+            Assert.That(serverPlayerGO, Is.Not.Null);
+            Assert.That(clientPlayerGO, Is.Not.Null);
+        }
+
+
+        [UnityTest]
+        public IEnumerator Command()
+        {
+            clientComponent.CmdTest(1, "hello");
+            yield return null;
+
+            Assert.That(serverComponent.cmdArg1, Is.EqualTo(1));
+            Assert.That(serverComponent.cmdArg2, Is.EqualTo("hello"));
+        }
+
+        
+        [UnityTest]
+        public IEnumerator CommandWithNetworkIdentity()
+        {
+            clientComponent.CmdNetworkIdentity(clientIdentity);
+
+            yield return null;
+
+            Assert.That(serverComponent.cmdNi, Is.SameAs(serverIdentity));
+        }
+
+        [UnityTest]
+        public IEnumerator ClientRpc()
+        {
+            serverComponent.RpcTest(1, "hello");
+            // process spawn message from server
+            yield return null;
+
+            Assert.That(clientComponent.rpcArg1, Is.EqualTo(1));
+            Assert.That(clientComponent.rpcArg2, Is.EqualTo("hello"));
+        }
+
+        [UnityTest]
+        public IEnumerator TargetRpc()
+        {
+            serverComponent.TargetRpcTest(connectionToClient, 1, "hello");
+            // process spawn message from server
+            yield return null;
+
+            Assert.That(clientComponent.targetRpcConn, Is.SameAs(connectionToServer));
+            Assert.That(clientComponent.targetRpcArg1, Is.EqualTo(1));
+            Assert.That(clientComponent.targetRpcArg2, Is.EqualTo("hello"));
+        }
+
+        /*
+        [UnityTest]
+        public IEnumerator DisconnectHostTest()
+        {
+            // set local connection
+            Assert.That(server.LocalClientActive, Is.True);
+            Assert.That(server.connections, Has.Count.EqualTo(1));
+
+            server.Disconnect();
+
+            // wait for messages to get dispatched
+            yield return null;
+
+            // state cleared?
+            Assert.That(server.connections, Is.Empty);
+            Assert.That(server.Active, Is.False);
+            Assert.That(server.LocalConnection, Is.Null);
+            Assert.That(server.LocalClientActive, Is.False);
+        }
+        */
+
+    }
+}
---
+++ Assets/Mirror/Tests/Runtime/ClientServerSetup.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using static Mirror.Tests.AsyncUtil;
+using Object = UnityEngine.Object;
+
+namespace Mirror.Tests
+{
+
+    // set's up a host
+
+    public class ClientServerSetup<T> where T : NetworkBehaviour
+    {
+
+        #region Setup
+        protected GameObject networkManagerGo;
+        protected NetworkManager manager;
+        protected NetworkServer server;
+        protected NetworkClient client;
+
+        protected GameObject serverPlayerGO;
+        protected NetworkIdentity serverIdentity;
+        protected T serverComponent;
+
+        protected GameObject clientPlayerGO;
+        protected NetworkIdentity clientIdentity;
+        protected T clientComponent;
+
+        private GameObject playerPrefab;
+        protected INetworkConnection connectionToServer;
+        protected INetworkConnection connectionToClient;
+
+        [UnitySetUp]
+        public IEnumerator Setup() => RunAsync(async () =>
+        {
+            networkManagerGo = new GameObject("NetworkManager", typeof(LoopbackTransport), typeof(NetworkClient), typeof(NetworkServer), typeof(NetworkManager));
+
+            manager = networkManagerGo.GetComponent<NetworkManager>();
+            manager.client = networkManagerGo.GetComponent<NetworkClient>();
+            manager.server = networkManagerGo.GetComponent<NetworkServer>();
+
+            server = manager.server;
+            client = manager.client;
+            manager.startOnHeadless = false;
+
+            // create and register a prefab
+            playerPrefab = new GameObject("serverPlayer", typeof(NetworkIdentity), typeof(T));
+            playerPrefab.GetComponent<NetworkIdentity>().AssetId = Guid.NewGuid();
+            client.RegisterPrefab(playerPrefab);
+
+
+            // wait for client and server to initialize themselves
+            await Task.Delay(1);
+
+            // start the server
+            await manager.StartServer();
+
+            // now start the client
+            await manager.StartClient("localhost");
+
+            // get the connections so that we can spawn players
+            connectionToServer = client.Connection;
+            connectionToClient = server.connections.First();
+
+
+            // create a player object in the server
+            serverPlayerGO = GameObject.Instantiate(playerPrefab);
+            serverIdentity = serverPlayerGO.GetComponent<NetworkIdentity>();
+            serverComponent = serverPlayerGO.GetComponent<T>();
+            server.AddPlayerForConnection(connectionToClient, serverPlayerGO);
+
+            // wait for client to spawn it
+            await Task.Delay(1);
+
+            clientPlayerGO = connectionToServer.Identity.gameObject;
+            clientIdentity = clientPlayerGO.GetComponent<NetworkIdentity>();
+            clientComponent = clientPlayerGO.GetComponent<T>();
+
+
+        });
+
+        [UnityTearDown]
+        public IEnumerator ShutdownHost()
+        {
+            manager.StopClient();
+            manager.StopServer();
+
+            yield return null;
+
+            Object.DestroyImmediate(playerPrefab);
+            Object.DestroyImmediate(networkManagerGo);
+        }
+
+        #endregion
+    }
+}
