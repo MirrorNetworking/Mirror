9f5f0b27f8857bf55bf4f5ffbd436247f99cf390
Paul Pacheco <paulpach@gmail.com>
2020-04-13 21:55:22 -0500

    feat: Button to register all prefabs in NetworkClient (#179)
    * feat: Button to register all prefabs in NetworkClient
    
    * Fix smells
    
    * Method can be private
    
    * Make sure user prefabs are still there
    
    * fix tests

---
+++ Assets/Mirror/Editor/NetworkClientInspector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Mirror
+{
+    [CustomEditor(typeof(NetworkClient), true)]
+    [CanEditMultipleObjects]
+    public class NetworkClientInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Register All Prefabs"))
+            {
+                RegisterPrefabs((NetworkClient)target);
+                Undo.RecordObject(target, "Changed Area Of Effect");
+            }
+        }
+
+        public void RegisterPrefabs(NetworkClient gameObject)
+        {
+            ISet<GameObject> prefabs = LoadPrefabsContaining<NetworkIdentity>("Assets");
+
+            foreach (var existing in gameObject.spawnPrefabs)
+            {
+                prefabs.Add(existing);
+            }
+            gameObject.spawnPrefabs.Clear();
+            gameObject.spawnPrefabs.AddRange(prefabs);
+        }
+
+        private static ISet<GameObject> LoadPrefabsContaining<T>(string path) where T : UnityEngine.Component
+        {
+            var result = new HashSet<GameObject>();
+
+            var guids = AssetDatabase.FindAssets("t:Object", new[] { path });
+
+            foreach (var guid in guids)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+
+                T obj = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+
+                if (obj != null)
+                    result.Add(obj.gameObject);
+            }
+            return result;
+        }
+    }
+}
---
+++ Assets/Mirror/Tests/Editor/NetworkClientInspectorTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirror.Tests
+{
+    [TestFixture(Category = "NetworkClient")]
+    public class NetworkClientInspectorTest
+    {
+
+        [Test]
+        public void RegisterPrefabs()
+        {
+            var gameObject = new GameObject("NetworkClient", typeof(NetworkClient));
+
+            NetworkClient client = gameObject.GetComponent<NetworkClient>();
+
+            NetworkClientInspector inspector = ScriptableObject.CreateInstance<NetworkClientInspector>();
+            inspector.RegisterPrefabs(client);
+
+            Assert.That(client.spawnPrefabs, Has.Count.GreaterThan(13));
+
+            foreach (var prefab in client.spawnPrefabs)
+            {
+                Assert.That(prefab.GetComponent<NetworkIdentity>(), Is.Not.Null);
+            }
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void PreserveExisting()
+        {
+            var preexisting = new GameObject("object", typeof(NetworkIdentity));
+
+            var gameObject = new GameObject("NetworkClient", typeof(NetworkClient));
+            NetworkClient client = gameObject.GetComponent<NetworkClient>();
+            client.spawnPrefabs.Add(preexisting);
+
+            NetworkClientInspector inspector = ScriptableObject.CreateInstance<NetworkClientInspector>();
+
+            inspector.RegisterPrefabs(client);
+
+            Assert.That(client.spawnPrefabs, Contains.Item(preexisting));
+
+            GameObject.DestroyImmediate(gameObject);
+            GameObject.DestroyImmediate(preexisting);
+        }
+    }
+}
