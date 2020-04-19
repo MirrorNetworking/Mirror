8e3c4693c927e9a5ede1bb10ab62265c97d18120
Chris Langsenkamp <chris@clevertech.net>
2020-04-19 08:45:46 -0400

    Changed to Play test, still not working

--- Assets/Mirror/Tests/Runtime/NetworkServerTest.cs
+++ Assets/Mirror/Tests/Runtime/NetworkServerTest.cs
@@ -27,7 +27,6 @@ namespace Mirror.Tests
             Object.Destroy(transportGameObject);
         }
 
-
         [UnityTest]
         public IEnumerator DestroyPlayerForConnectionTest()
         {
@@ -44,6 +43,45 @@ namespace Mirror.Tests
             yield return null;
 
             Assert.That(player == null, "Player should be destroyed with DestroyPlayerForConnection");
+        }
+
+        [UnityTest]
+        public IEnumerator DisconnectTimeoutTest()
+        {
+            // message handlers
+            NetworkServer.RegisterHandler<ConnectMessage>((conn, msg) => { }, false);
+            NetworkServer.RegisterHandler<DisconnectMessage>((conn, msg) => { }, false);
+            NetworkServer.RegisterHandler<ErrorMessage>((conn, msg) => { }, false);
+
+            // Set high ping frequency so no NetworkPingMessage is generated
+            NetworkTime.PingFrequency = 20f;
+
+            // listen
+            NetworkServer.Listen(2);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(0));
+
+            NetworkClient.ConnectHost();
+            ULocalConnectionToClient localConnection = NetworkServer.localConnection as ULocalConnectionToClient;
+            Assert.That(NetworkServer.localConnection, Is.Not.Null);
+            localConnection.serverIdleTimeout = 5f;
+
+            //GameObject HostPlayer = new GameObject("HostPlayer", typeof(NetworkIdentity));
+            //NetworkServer.AddPlayerForConnection(localConnection, HostPlayer);
+
+            GameObject RemotePlayer = new GameObject("RemotePlayer", typeof(NetworkIdentity));
+            NetworkConnectionToClient remoteConnection = new NetworkConnectionToClient(1);
+            remoteConnection.serverIdleTimeout = 5f;
+            NetworkServer.OnConnected(remoteConnection);
+            NetworkServer.AddPlayerForConnection(remoteConnection, RemotePlayer);
+
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(1));
+
+            // wait 10 seconds for conn43 to timeout as idle
+            yield return new WaitForSeconds(10f);
+
+            // host client connection should still be alive
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(0));
+            Assert.That(NetworkServer.localConnection, Is.EqualTo(localConnection));
         }
     }
 }
