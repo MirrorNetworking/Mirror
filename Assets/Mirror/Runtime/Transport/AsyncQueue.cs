673e3168416074cde65f8bbaf0e3e87e9b8ef3db
Paul Pacheco <paulpach@gmail.com>
2020-04-18 23:29:48 -0500

    Simple asynchronous queue that event based transports can use

---
+++ Assets/Mirror/Runtime/Transport/AsyncQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Mirror
+{
+    public class AsyncQueue<T>
+    {
+        private readonly Queue<T> queue = new Queue<T>();
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(0);
+
+
+        internal void Equeue(T v)
+        {
+            queue.Enqueue(v);
+            semaphore.Release();
+        }
+
+        internal async Task<T> DequeueAsync()
+        {
+            await semaphore.WaitAsync();
+            return queue.Dequeue();
+        }
+    }
+}
---
+++ Assets/Mirror/Tests/Editor/Transport/AsyncQueueTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using static Mirror.Tests.AsyncUtil;
+
+namespace Mirror.Tests
+{
+    public class AsyncQueueTests
+    {
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator QueueDequeu() => RunAsync(async () =>
+        {
+            var asyncQueue = new AsyncQueue<int>();
+
+            asyncQueue.Equeue(3);
+
+            int result = await asyncQueue.DequeueAsync();
+
+            Assert.That(result, Is.EqualTo(3));
+        });
+
+        [UnityTest]
+        public IEnumerator EnqueueShouldWait() => RunAsync(async () =>
+        {
+            var asyncQueue = new AsyncQueue<int>();
+
+            Task<int> task1 = asyncQueue.DequeueAsync();
+
+            Assert.That(task1.IsCompleted, Is.False);
+
+            asyncQueue.Equeue(1);
+
+            Assert.That(await task1, Is.EqualTo(1));
+        });
+
+        [UnityTest]
+        public IEnumerator FirstInFirstOut() => RunAsync(async () =>
+        {
+            var asyncQueue = new AsyncQueue<int>();
+
+            asyncQueue.Equeue(1);
+            asyncQueue.Equeue(2);
+
+            Assert.That(await asyncQueue.DequeueAsync(), Is.EqualTo(1));
+            Assert.That(await asyncQueue.DequeueAsync(), Is.EqualTo(2));
+        });
+    }
+}
