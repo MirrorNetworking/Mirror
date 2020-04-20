87a91220c1d3a3db22c57fbd4ccdb2ad7629306e
James Frowen <jamesfrowendev@gmail.com>
2020-04-20 14:33:32 +0100

    adding message if there are no log levels

--- Assets/Mirror/Editor/Logging/LogSettingsGUI.cs
+++ Assets/Mirror/Editor/Logging/LogSettingsGUI.cs
@@ -19,14 +19,21 @@ namespace Mirror.Logging
         {
             using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope())
             {
-                foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
+                if (LogFactory.loggers.Count == 0)
                 {
-                    DrawLoggerField(item);
+                    EditorGUILayout.LabelField("No Keys found in LogFactory.loggers\nPlay the game for default log values to be added to LogFactory", EditorStyles.wordWrappedLabel);
                 }
-
-                if (scope.changed)
+                else
                 {
-                    settings.SaveFromDictionary(LogFactory.loggers);
+                    foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
+                    {
+                        DrawLoggerField(item);
+                    }
+
+                    if (scope.changed)
+                    {
+                        settings.SaveFromDictionary(LogFactory.loggers);
+                    }
                 }
             }
         }
