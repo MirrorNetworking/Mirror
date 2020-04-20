500d9ff3a120ea9460dcfce095221a748d31c92a
James Frowen <jamesfrowendev@gmail.com>
2020-04-20 14:34:45 +0100

    adding CurrentScriptField

--- Assets/Mirror/Editor/Logging/LogSettingsEditor.cs
+++ Assets/Mirror/Editor/Logging/LogSettingsEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+using UnityEditor;
+using UnityEngine;
 
 namespace Mirror.Logging
 {
@@ -7,7 +8,16 @@ namespace Mirror.Logging
     {
         public override void OnInspectorGUI()
         {
+            CurrentScriptField();
+
             LogSettingsGUI.DrawLogFactoryDictionary(target as LogSettings);
+        }
+
+        public void CurrentScriptField()
+        {
+            GUI.enabled = false;
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
+            GUI.enabled = true;
         }
     }
 }
