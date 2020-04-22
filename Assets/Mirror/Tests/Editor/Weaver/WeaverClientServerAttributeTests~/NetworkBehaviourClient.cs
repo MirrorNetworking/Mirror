740d3b3901489ddcb26e260e27d9d1597bfa7594
Paul Pacheco <paulpach@gmail.com>
2020-04-22 17:40:32 -0500

    test class name matches file name

--- Assets/Mirror/Tests/Editor/Weaver/WeaverClientServerAttributeTests~/NetworkBehaviourClient.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverClientServerAttributeTests~/NetworkBehaviourClient.cs
@@ -5,7 +5,7 @@ using Mirror;
 
 namespace MirrorTest
 {
-    class MirrorTestPlayer : NetworkBehaviour
+    class NetworkBehaviourClient : NetworkBehaviour
     {
         [Client]
         void ClientOnlyMethod() 
--- Assets/Mirror/Tests/Editor/Weaver/WeaverClientServerAttributeTests~/NetworkBehaviourServer.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverClientServerAttributeTests~/NetworkBehaviourServer.cs
@@ -5,7 +5,7 @@ using Mirror;
 
 namespace MirrorTest
 {
-    class MirrorTestPlayer : NetworkBehaviour
+    class NetworkBehaviourServer : NetworkBehaviour
     {
         [Server]
         void ServerOnlyMethod() 
