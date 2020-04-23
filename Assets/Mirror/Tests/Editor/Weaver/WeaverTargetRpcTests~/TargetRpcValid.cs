fb489c67e8b3de88aec5669ab56771eefb2caee4
Paul Pacheco <paulpach@gmail.com>
2020-04-22 20:58:22 -0500

    test class name matches file name

--- Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcValid.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcValid.cs
@@ -5,7 +5,7 @@ using Mirror;
 
 namespace MirrorTest
 {
-    class MirrorTestPlayer : NetworkBehaviour
+    class TargetRpcValid : NetworkBehaviour
     {
         [TargetRpc]
         void TargetThatIsTotallyValid(NetworkConnection nc) {}
