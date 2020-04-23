84aa3529a9a828a8e4c2f64d60b7c7d6c00ef7c4
Paul Pacheco <paulpach@gmail.com>
2020-04-22 20:58:22 -0500

    test class name matches file name

--- Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcCantBeStatic.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcCantBeStatic.cs
@@ -5,7 +5,7 @@ using Mirror;
 
 namespace MirrorTest
 {
-    class MirrorTestPlayer : NetworkBehaviour
+    class TargetRpcCantBeStatic : NetworkBehaviour
     {
         [TargetRpc]
         static void TargetCantBeStatic(NetworkConnection nc) {}
--- Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcNetworkConnectionMissing.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverTargetRpcTests~/TargetRpcNetworkConnectionMissing.cs
@@ -5,7 +5,7 @@ using Mirror;
 
 namespace MirrorTest
 {
-    class MirrorTestPlayer : NetworkBehaviour
+    class TargetRpcNetworkConnectionMissing : NetworkBehaviour
     {
         [TargetRpc]
         void TargetRpcMethod() {}
