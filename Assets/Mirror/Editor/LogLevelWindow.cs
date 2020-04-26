1cf74b918eb22af5a689dd1a6f87b0bd46ae5c18
Paul Pacheco <paulpach@gmail.com>
2020-04-26 16:28:34 -0500

    Log window is now tied to a component

--- Assets/Mirror/Editor/LogLevelWindow.cs
+++ Assets/Mirror/Editor/LogLevelWindow.cs
@@ -1,12 +1,30 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace Mirror
 {
-    public class LogLevelWindow : EditorWindow
+    [CustomEditor(typeof(LogSettings), true)]
+    public class LogSettingEditor : Editor
     {
-        void OnGUI()
+        void OnEnable()
+        {
+            // Setup the SerializedProperties.
+            EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+        }
+
+        private void EditorApplication_playModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
+                SaveLevels();
+            }
+        }
+
+        #region GUI
+        public override void OnInspectorGUI()
         {
             EditorGUILayout.BeginVertical();
             EditorGUILayout.Space();
@@ -15,29 +33,117 @@ namespace Mirror
             EditorGUILayout.Space();
             EditorGUILayout.EndVertical();
 
+            EditorGUI.BeginChangeCheck();
+
             EditorGUILayout.BeginVertical(EditorStyles.inspectorDefaultMargins);
+            LoadSavedLevels();
+
             foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
             {
-                DrawLoggerField(item);
+                DrawLoggerField(item.Key, item.Value);
             }
             EditorGUILayout.EndVertical();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                SaveLevels();
+            }
         }
 
-        static void DrawLoggerField(KeyValuePair<string, ILogger> item)
+        static void DrawLoggerField(string loggerName, ILogger logger)
         {
-            ILogger logger = item.Value;
-            string name = item.Key;
-
-            logger.filterLogType = (LogType)EditorGUILayout.EnumPopup(new GUIContent(name), logger.filterLogType);
+            logger.filterLogType = (LogType)EditorGUILayout.EnumPopup(new GUIContent(loggerName), logger.filterLogType);
         }
 
+        #endregion
 
-        [MenuItem("Window/Analysis/Mirror Log Levels", priority = 20002)]
-        public static void ShowWindow()
+        #region Log settings persistence
+
+        private void SetLoggers(IEnumerable<LogSettings.Level> savedLevels)
         {
-            LogLevelWindow window = GetWindow<LogLevelWindow>();
-            window.titleContent = new GUIContent("Mirror Log levels");
-            window.Show();
+            foreach (var setting in savedLevels)
+            {
+                if (setting.Name != null)
+                {
+                    ILogger logger = LogFactory.GetLogger(setting.Name);
+                    logger.filterLogType = setting.level;
+                }
+            }
         }
+
+        private void LoadSavedLevels()
+        {
+            string levelsJson = EditorPrefs.GetString("LogLevels");
+            var settings = FromJson<LogSettings.Level>(levelsJson);
+            SetLoggers(settings ?? new LogSettings.Level[] { });
+        }
+
+        private void SaveLevels()
+        {
+            // save in EditorPrefs
+            SaveInEditorPrefs();
+            SaveInGameObject();
+        }
+
+        private void SaveInGameObject()
+        {
+            LogSettings setting = target as LogSettings;
+
+            if (target == null)
+                return;
+
+            setting.Levels = new List<LogSettings.Level>();
+
+            foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
+            {
+                setting.Levels.Add(new LogSettings.Level
+                {
+                    Name = item.Key,
+                    level = item.Value.filterLogType
+                });
+            }
+
+            Undo.RecordObject(target, "Update log settings");
+        }
+
+
+        private static void SaveInEditorPrefs()
+        {
+            LogSettings.Level[] settings = new LogSettings.Level[LogFactory.loggers.Count()];
+
+            int i = 0;
+            foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
+            {
+                settings[i++] = new LogSettings.Level
+                {
+                    Name = item.Key,
+                    level = item.Value.filterLogType
+                };
+            }
+
+            string levelsJSon = ToJson(settings);
+            EditorPrefs.SetString("LogLevels", levelsJSon);
+        }
+
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+
+        #endregion
     }
 }
