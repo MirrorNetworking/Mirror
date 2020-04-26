aa36d11377f231759da64e6e8645f0866a2c4973
Paul Pacheco <paulpach@gmail.com>
2020-04-26 15:01:21 -0500

    feat: keep log levels when player starts

--- Assets/Mirror/Editor/LogLevelWindow.cs
+++ Assets/Mirror/Editor/LogLevelWindow.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,6 +8,7 @@ namespace Mirror
 {
     public class LogLevelWindow : EditorWindow
     {
+
         void OnGUI()
         {
             EditorGUILayout.BeginVertical();
@@ -15,22 +18,36 @@ namespace Mirror
             EditorGUILayout.Space();
             EditorGUILayout.EndVertical();
 
+            EditorGUI.BeginChangeCheck();
+
             EditorGUILayout.BeginVertical(EditorStyles.inspectorDefaultMargins);
+            LoadSavedLevels();
+
             foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
             {
-                DrawLoggerField(item);
+                DrawLoggerField(item.Key, item.Value);
             }
             EditorGUILayout.EndVertical();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                SaveLevels();
+            }
         }
 
-        static void DrawLoggerField(KeyValuePair<string, ILogger> item)
+        private void SetLoggers(IEnumerable<LogSetting> savedLevels)
         {
-            ILogger logger = item.Value;
-            string name = item.Key;
-
-            logger.filterLogType = (LogType)EditorGUILayout.EnumPopup(new GUIContent(name), logger.filterLogType);
+            foreach (LogSetting setting in savedLevels)
+            {
+                ILogger logger = LogFactory.GetLogger(setting.logger);
+                logger.filterLogType = setting.logType;
+            }
         }
 
+        static void DrawLoggerField(string loggerName, ILogger logger)
+        {
+            logger.filterLogType = (LogType)EditorGUILayout.EnumPopup(new GUIContent(loggerName), logger.filterLogType);
+        }
 
         [MenuItem("Window/Analysis/Mirror Log Levels", priority = 20002)]
         public static void ShowWindow()
@@ -38,6 +55,36 @@ namespace Mirror
             LogLevelWindow window = GetWindow<LogLevelWindow>();
             window.titleContent = new GUIContent("Mirror Log levels");
             window.Show();
+        }
+
+        public struct LogSetting
+        {
+            public string logger;
+            public LogType logType;
+        }
+
+        private void LoadSavedLevels()
+        {
+            string levelsJson = EditorPrefs.GetString("LogLevels");
+            LogSetting[] settings =  JsonUtility.FromJson<LogSetting[]>(levelsJson);
+            SetLoggers(settings ?? new LogSetting[] { });
+        }
+
+        private void SaveLevels()
+        {
+            List<LogSetting> settings = new List<LogSetting>();
+
+            foreach (KeyValuePair<string, ILogger> item in LogFactory.loggers)
+            {
+                settings.Add(new LogSetting
+                {
+                    logger = item.Key,
+                    logType = item.Value.filterLogType
+                });
+            }
+
+            string levelsJSon = JsonUtility.ToJson(settings);
+            EditorPrefs.SetString("LogLevels", levelsJSon);
         }
     }
 }
