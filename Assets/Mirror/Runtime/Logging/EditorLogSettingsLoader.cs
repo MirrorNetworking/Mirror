b449cb9c34ebc87d754073a6af2028b05f8edbd2
James Frowen <jamesfrowendev@gmail.com>
2020-04-27 13:11:16 +0100

    adding helper to find Existing LogSettings

---
+++ Assets/Mirror/Runtime/Logging/EditorLogSettingsLoader.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Mirror.Logging
+{
+#if UNITY_EDITOR
+    public static class EditorLogSettingsLoader
+    {
+        public static void LoadLogSettingsIntoDictionary()
+        {
+            LogSettings settings = FindLogSettings();
+            if (settings != null)
+            {
+                settings.LoadIntoDictionary(LogFactory.loggers);
+            }
+        }
+
+        static LogSettings cache;
+        public static LogSettings FindLogSettings()
+        {
+            if (cache != null)
+                return cache;
+
+            string[] assetGuids = AssetDatabase.FindAssets("t:" + nameof(LogSettings));
+            if (assetGuids.Length == 0)
+                return null;
+
+            string firstGuid = assetGuids[0];
+
+            string path = AssetDatabase.GUIDToAssetPath(firstGuid);
+            cache = AssetDatabase.LoadAssetAtPath<LogSettings>(path);
+
+            if (assetGuids.Length > 2)
+            {
+                Debug.LogWarning("Found more than one LogSettings, Delete extra settinsg. Using first asset found: " + path);
+            }
+            Debug.Assert(cache != null, "Failed to load asset at: " + path);
+
+            return cache;
+        }
+    }
+#endif
+}
