ec3898e1aa3b5734e6adbac66b1e239f07cadc20
James Frowen <jamesfrowendev@gmail.com>
2020-04-27 17:00:53 +0100

    adding attributes

--- Assets/Mirror/Runtime/Logging/NetworkLogSettings.cs
+++ Assets/Mirror/Runtime/Logging/NetworkLogSettings.cs
@@ -5,6 +5,8 @@ namespace Mirror.Logging
     /// <summary>
     /// Used to load LogSettings in build
     /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Network/NetworkLogSettings")]
     public class NetworkLogSettings : MonoBehaviour
     {
         [SerializeField] internal LogSettings settings;
