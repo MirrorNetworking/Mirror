39a69ed8ef8125fd224ea974379cc6d89c6e621b
Chris Langsenkamp <chris@clevertech.net>
2020-04-29 05:32:13 -0400

    Added derived components

---
+++ Assets/Mirror/Components/Experimental/NetworkTransform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Mirror.Experimental
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Network/Experimental/NetworkTransform")]
+    [HelpURL("https://mirror-networking.com/docs/Components/NetworkTransform.html")]
+    public class NetworkTransform : NetworkTransformBase
+    {
+        protected override Transform targetComponent => transform;
+    }
+}
