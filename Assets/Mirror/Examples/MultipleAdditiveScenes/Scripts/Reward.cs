34cc08d8d76041c13c51a01f4471d6c8da44414e
Chris Langsenkamp <chris@clevertech.net>
2020-04-30 20:06:26 -0400

    Revert "Restored old jump code"
    This reverts commit 6a986fb088885f96a00e2384968c02cd815c08c8.

--- Assets/Mirror/Examples/MultipleAdditiveScenes/Scripts/Reward.cs
+++ Assets/Mirror/Examples/MultipleAdditiveScenes/Scripts/Reward.cs
@@ -7,6 +7,7 @@ namespace Mirror.Examples.MultipleAdditiveScenes
     {
         public bool available = true;
         public Spawner spawner;
+        uint points;
 
         public RandomColor randomColor;
 
@@ -39,7 +40,7 @@ namespace Mirror.Examples.MultipleAdditiveScenes
 
                 // calculate the points from the color ... lighter scores higher as the average approaches 255
                 // UnityEngine.Color RGB values are float fractions of 255
-                uint points = (uint)(((prizeColor.r * 255) + (prizeColor.g * 255) + (prizeColor.b * 255)) / 3);
+                points = (uint)(((prizeColor.r * 255) + (prizeColor.g * 255) + (prizeColor.b * 255)) / 3);
                 if (LogFilter.Debug) Debug.LogFormat("Scored {0} points R:{1} G:{2} B:{3}", points, prizeColor.r, prizeColor.g, prizeColor.b);
 
                 // award the points via SyncVar on the PlayerController
