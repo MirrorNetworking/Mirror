ea75a762ebf272483d83b3258907da8168623fee
James Frowen <jamesfrowendev@gmail.com>
2020-05-04 16:44:34 +0100

    changing namespace

--- Assets/Mirror/Editor/LogLevelWindow.cs
+++ Assets/Mirror/Editor/LogLevelWindow.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace Mirror
+namespace Mirror.EditorScripts.Logging
 {
     public class LogLevelWindow : EditorWindow
     {
