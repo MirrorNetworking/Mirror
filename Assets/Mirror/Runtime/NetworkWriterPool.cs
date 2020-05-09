756dc3d391c295d5100acb59f70f0c11a47a02d9
uwee <uweenukr@gmail.com>
2020-05-09 09:56:22 -0400

    update logger refs in NetworkWriterPool

--- Assets/Mirror/Runtime/NetworkWriterPool.cs
+++ Assets/Mirror/Runtime/NetworkWriterPool.cs
@@ -91,7 +91,7 @@ namespace Mirror
             }
             else
             {
-                if (LogFilter.Debug) { Debug.LogWarning("NetworkWriterPool.Recycle, Pool was full leaving extra writer for GC"); }
+                if (LogFilter.Debug) { logger.LogWarning("NetworkWriterPool.Recycle, Pool was full leaving extra writer for GC"); }
             }
         }
     }
