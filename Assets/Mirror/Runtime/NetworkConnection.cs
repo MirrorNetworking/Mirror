ddd82b22a5b52723011af7ba22b41f84b247dbfe
MrGadget <chris@clevertech.net>
2020-05-08 21:37:42 -0400

    Update Assets/Mirror/Runtime/NetworkConnection.cs

--- Assets/Mirror/Runtime/NetworkConnection.cs
+++ Assets/Mirror/Runtime/NetworkConnection.cs
@@ -230,7 +230,7 @@ namespace Mirror
         /// </summary>
         /// <typeparam name="T">The message type to unregister.</typeparam>
         /// <param name="msg">The message object to process.</param>
-        /// <returns>Returns true is the handler was sucessfully invoked</returns>
+        /// <returns>Returns true if the handler was successfully invoked</returns>
         public bool InvokeHandler<T>(T msg, int channelId) where T : IMessageBase
         {
             // get writer from pool
