ebae8991931471b5d56a6fd0a3ef3ce90a208a17
uwee <uweenukr@gmail.com>
2020-05-09 09:55:21 -0400

    update logger refs in MessagePacker

--- Assets/Mirror/Runtime/MessagePacker.cs
+++ Assets/Mirror/Runtime/MessagePacker.cs
@@ -123,7 +123,7 @@ namespace Mirror
                 if (requireAuthenication && !conn.isAuthenticated)
                 {
                     // message requires authentication, but the connection was not authenticated
-                    Debug.LogWarning($"Closing connection: {conn}. Received message {typeof(T)} that required authentication, but the user has not authenticated yet");
+                    logger.LogWarning($"Closing connection: {conn}. Received message {typeof(T)} that required authentication, but the user has not authenticated yet");
                     conn.Disconnect();
                     return;
                 }
@@ -135,7 +135,7 @@ namespace Mirror
             }
             catch (Exception exception)
             {
-                Debug.LogError("Closed connection: " + conn + ". This can happen if the other side accidentally (or an attacker intentionally) sent invalid data. Reason: " + exception);
+                logger.LogError("Closed connection: " + conn + ". This can happen if the other side accidentally (or an attacker intentionally) sent invalid data. Reason: " + exception);
                 conn.Disconnect();
                 return;
             }
