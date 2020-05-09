9e2cef29fe02f950a6d33fccf924b901cdca5fb8
uwee <uweenukr@gmail.com>
2020-05-09 19:47:09 -0400

    remove unused logger

--- Assets/Mirror/Runtime/NetworkManagerHUD.cs
+++ Assets/Mirror/Runtime/NetworkManagerHUD.cs
@@ -16,8 +16,6 @@ namespace Mirror
     [HelpURL("https://mirror-networking.com/docs/Components/NetworkManagerHUD.html")]
     public class NetworkManagerHUD : MonoBehaviour
     {
-        static readonly ILogger logger = LogFactory.GetLogger<NetworkManagerHUD>();
-
         NetworkManager manager;
 
         /// <summary>
