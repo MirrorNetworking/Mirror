0a4cf9ffab01b0e8ee28cabe8d75613c21ca0b23
uwee <uweenukr@gmail.com>
2020-05-09 09:55:00 -0400

    update logger refs in NetworkRoomPlayerExt

--- Assets/Mirror/Examples/Room/Scripts/NetworkRoomPlayerExt.cs
+++ Assets/Mirror/Examples/Room/Scripts/NetworkRoomPlayerExt.cs
@@ -10,19 +10,19 @@ namespace Mirror.Examples.NetworkRoom
 
         public override void OnStartClient()
         {
-            if (LogFilter.Debug) Debug.LogFormat("OnStartClient {0}", SceneManager.GetActiveScene().path);
+            if (LogFilter.Debug) logger.LogFormat(LogType.Log, "OnStartClient {0}", SceneManager.GetActiveScene().path);
 
             base.OnStartClient();
         }
 
         public override void OnClientEnterRoom()
         {
-            if (LogFilter.Debug) Debug.LogFormat("OnClientEnterRoom {0}", SceneManager.GetActiveScene().path);
+            if (LogFilter.Debug) logger.LogFormat(LogType.Log, "OnClientEnterRoom {0}", SceneManager.GetActiveScene().path);
         }
 
         public override void OnClientExitRoom()
         {
-            if (LogFilter.Debug) Debug.LogFormat("OnClientExitRoom {0}", SceneManager.GetActiveScene().path);
+            if (LogFilter.Debug) logger.LogFormat(LogType.Log, "OnClientExitRoom {0}", SceneManager.GetActiveScene().path);
         }
     }
 }
