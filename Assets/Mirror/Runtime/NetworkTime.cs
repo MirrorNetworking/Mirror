1e5904fcdb592cfee7e662b006221a823de6c0a8
uwee <uweenukr@gmail.com>
2020-05-09 09:56:05 -0400

    update logger refs in NetworkTime

--- Assets/Mirror/Runtime/NetworkTime.cs
+++ Assets/Mirror/Runtime/NetworkTime.cs
@@ -68,7 +68,7 @@ namespace Mirror
         // and time from the server
         internal static void OnServerPing(NetworkConnection conn, NetworkPingMessage msg)
         {
-            if (LogFilter.Debug) Debug.Log("OnPingServerMessage  conn=" + conn);
+            if (LogFilter.Debug) logger.Log("OnPingServerMessage  conn=" + conn);
 
             NetworkPongMessage pongMsg = new NetworkPongMessage
             {
