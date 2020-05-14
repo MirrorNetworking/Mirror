b4bd78c53baf2f599215bff368f8cceb8533cd4a
James Frowen <jamesfrowendev@gmail.com>
2020-05-14 19:14:46 +0100

    adding clear test
    fixing Contains check

--- Assets/Mirror/Tests/Editor/SyncListClassTest.cs
+++ Assets/Mirror/Tests/Editor/SyncListClassTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using NUnit.Framework;
 
 namespace Mirror.Tests
@@ -5,7 +6,7 @@ namespace Mirror.Tests
     public class SyncListClassTest
     {
         [Test]
-        public void RemoveShouldNotFail()
+        public void RemoveShouldRemoveItem()
         {
             SyncListTestObject serverList = new SyncListTestObject();
             SyncListTestObject clientList = new SyncListTestObject();
@@ -21,15 +22,43 @@ namespace Mirror.Tests
             // sync
             SyncListTest.SerializeDeltaTo(serverList, clientList);
 
-
-            // remove some items
+            // clear all items            
             serverList.Remove(item1);
 
             // sync
             SyncListTest.SerializeDeltaTo(serverList, clientList);
 
-            Assert.That(clientList.Contains(item1), Is.False);
-            Assert.That(clientList.Contains(item2), Is.True);
+            Assert.IsFalse(clientList.Any(x => x.id == item1.id));
+            Assert.IsTrue(clientList.Any(x => x.id == item1.id));
+        }
+
+        [Test]
+        public void ClearShouldClearAll()
+        {
+            SyncListTestObject serverList = new SyncListTestObject();
+            SyncListTestObject clientList = new SyncListTestObject();
+
+            SyncListTest.SerializeAllTo(serverList, clientList);
+
+            // add some items
+            TestObject item1 = new TestObject { id = 1, text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ullam aliquid perferendis, aut nihil sunt quod ipsum corporis a. Cupiditate, alias. Commodi, molestiae distinctio repellendus dolor similique delectus inventore eum." };
+            serverList.Add(item1);
+            TestObject item2 = new TestObject { id = 2, text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ullam aliquid perferendis, aut nihil sunt quod ipsum corporis a. Cupiditate, alias. Commodi, molestiae distinctio repellendus dolor similique delectus inventore eum." };
+            serverList.Add(item2);
+
+            // sync
+            SyncListTest.SerializeDeltaTo(serverList, clientList);
+
+            // clear all items            
+            serverList.Clear();
+
+            // sync
+            SyncListTest.SerializeDeltaTo(serverList, clientList);
+
+            Assert.That(clientList.Count, Is.Zero);
+
+            Assert.IsFalse(clientList.Any(x => x.id == item1.id));
+            Assert.IsFalse(clientList.Any(x => x.id == item1.id));
         }
     }
 
@@ -38,6 +67,7 @@ namespace Mirror.Tests
     {
 
     }
+    [System.Serializable]
     public class TestObject
     {
         public int id;
