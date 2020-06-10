d6d67ac749ac9b20816c8facaa0ffde1d50030cb
MrGadget <chris@clevertech.net>
2020-06-10 16:35:28 -0400

    Update Assets/Mirror/Runtime/Transport/Websocket/WebsocketTransport.cs

--- Assets/Mirror/Runtime/Transport/Websocket/WebsocketTransport.cs
+++ Assets/Mirror/Runtime/Transport/Websocket/WebsocketTransport.cs
@@ -108,8 +108,7 @@ namespace Mirror.Websocket
                 server._secure = Secure;
                 server._sslConfig = new Server.SslConfiguration
                 {
-                    Certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(CertificatePath,
-                        CertificatePassword),
+                        Certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(CertificatePath, CertificatePassword),
                     ClientCertificateRequired = false,
                     CheckCertificateRevocation = false,
                     EnabledSslProtocols = System.Security.Authentication.SslProtocols.Default
