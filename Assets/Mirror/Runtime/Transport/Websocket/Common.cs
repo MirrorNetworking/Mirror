b29e4df649e2f4d1fdc4879eb7f9fc8b5da9ad9e
Chris Langsenkamp <chris@clevertech.net>
2020-06-13 08:53:10 -0400

    Added Common class with Queue

---
+++ Assets/Mirror/Runtime/Transport/Websocket/Common.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+
+namespace Mirror.Websocket
+{
+    public abstract class Common
+    {
+        public bool NoDelay = true;
+
+        public bool enabled;
+
+        public bool Connecting { get; set; }
+
+        protected ConcurrentQueue<byte[]> receiveQueue = new ConcurrentQueue<byte[]>();
+     
+        public int ReceiveQueueCount => receiveQueue.Count;
+
+        public bool GetNextMessage(out byte[] message)
+        {
+            return receiveQueue.TryDequeue(out message);
+        }
+    }
+}
