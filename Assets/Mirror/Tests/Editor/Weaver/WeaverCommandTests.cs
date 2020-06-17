ed138fb2cface0333ce2c3d29d2e6c0056b00ed8
James Frowen <jamesfrowendev@gmail.com>
2020-06-17 23:40:15 +0100

    adding test with multiple overrides

--- Assets/Mirror/Tests/Editor/Weaver/WeaverCommandTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverCommandTests.cs
@@ -83,6 +83,12 @@ namespace Mirror.Weaver.Tests
         }
 
         [Test]
+        public void OverrideVirtualCallsBaseCommandWithOverride()
+        {
+            Assert.That(weaverErrors, Is.Empty);
+        }
+
+        [Test]
         public void AbstractCommand()
         {
             Assert.That(weaverErrors, Contains.Item("Abstract Commands are currently not supported, use virtual method instead (at System.Void WeaverCommandTests.AbstractCommand.AbstractCommand::CmdDoSomething())"));
