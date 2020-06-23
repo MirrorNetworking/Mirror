a55363f4dc099667b253f58a89ec872b8dd64e41
James Frowen <jamesfrowendev@gmail.com>
2020-06-23 21:52:33 +0100

    adding doc comment for SpawnMessag

--- Assets/Mirror/Runtime/Messages.cs
+++ Assets/Mirror/Runtime/Messages.cs
@@ -204,16 +204,43 @@ namespace Mirror
     #region Internal System Messages
     public struct SpawnMessage : IMessageBase
     {
+        /// <summary>
+        /// netId of new or existing object
+        /// </summary>
         public uint netId;
+        /// <summary>
+        /// Is the spawning object the local player. Sets ClientScene.localPlayer
+        /// </summary>
         public bool isLocalPlayer;
+        /// <summary>
+        /// Sets hasAuthority on the spawned object
+        /// </summary>
         public bool isOwner;
+        /// <summary>
+        /// The id of the scene object to spawn
+        /// </summary>
         public ulong sceneId;
+        /// <summary>
+        /// The id of the prefab to spawn
+        /// <para>If sceneId != 0 then it is used instead of assetId</para>
+        /// </summary>
         public Guid assetId;
+        /// <summary>
+        /// Local position
+        /// </summary>
         public Vector3 position;
+        /// <summary>
+        /// Local rotation
+        /// </summary>
         public Quaternion rotation;
+        /// <summary>
+        /// Local scale
+        /// </summary>
         public Vector3 scale;
-        // the serialized component data
-        // -> ArraySegment to avoid unnecessary allocations
+        /// <summary>
+        /// The serialized component data
+        /// <remark>ArraySegment to avoid unnecessary allocations</remark>
+        /// </summary>
         public ArraySegment<byte> payload;
 
         public void Deserialize(NetworkReader reader)
