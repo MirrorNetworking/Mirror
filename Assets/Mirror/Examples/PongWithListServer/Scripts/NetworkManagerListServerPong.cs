7409531b0a54e16266ad175b50e617f933e6260b
James Frowen <jamesfrowendev@gmail.com>
2020-06-24 00:50:45 +0100

    fixing pong spawn points

--- Assets/Mirror/Examples/PongWithListServer/Scripts/NetworkManagerListServerPong.cs
+++ Assets/Mirror/Examples/PongWithListServer/Scripts/NetworkManagerListServerPong.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace Mirror.CloudServices.Example
 {
@@ -40,6 +41,19 @@ namespace Mirror.CloudServices.Example
         {
             base.OnServerDisconnect(conn);
             onPlayerListChanged?.Invoke(connectionCount);
+        }
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            Debug.Assert(startPositions.Count == 2, "Pong Scene should have 2 start Poitions");
+            // add player at correct spawn position
+            Transform startPos = numPlayers == 0 ? startPositions[0] : startPositions[1];
+
+            GameObject player = startPos != null
+                ? Instantiate(playerPrefab, startPos.position, startPos.rotation)
+                : Instantiate(playerPrefab);
+
+            NetworkServer.AddPlayerForConnection(conn, player);
         }
 
         public override void OnStartServer()
