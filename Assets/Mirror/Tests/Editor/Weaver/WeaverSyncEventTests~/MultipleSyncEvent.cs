67f13381f9516d3945d9a388927d914a73a879dc
James Frowen <jamesfrowendev@gmail.com>
2020-06-29 16:51:04 +0100

    weaver test for mutlitple events

---
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverSyncEventTests~/MultipleSyncEvent.cs
@@ -0,0 +1,14 @@
+using Mirror;
+
+namespace WeaverSyncEventTests.MultipleSyncEvent
+{
+    class MultipleSyncEvent : NetworkBehaviour
+    {
+        public delegate void MySyncEventDelegate();
+        public delegate void MySyncEventDelegate2(int someNumber);
+
+        [SyncEvent]
+        public event MySyncEventDelegate EventDoCoolThingsWithExcitingPeople;
+        public event MySyncEventDelegate2 EventDoMoreCoolThingsWithExcitingPeople;
+    }
+}
