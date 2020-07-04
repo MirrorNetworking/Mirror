a34faed632dbc3ff81682901e526c7ca0759fccb
Paul Pacheco <paul.pacheco@aa.com>
2020-07-04 15:49:44 -0500

    Just measure frame time

--- Assets/Mirror/Tests/Performance/Runtime/BenchmarkPerformanceLight.cs
+++ Assets/Mirror/Tests/Performance/Runtime/BenchmarkPerformanceLight.cs
@@ -15,38 +15,12 @@ namespace Mirror.Tests.Performance.Runtime
     public class BenchmarkPerformanceLight
     {
         const string ScenePath = "Assets/Mirror/Examples/Benchmarks/10klight/Scenes/Scene.unity";
-        const float Warmup = 1f;
-        const int MeasureCount = 120;
 
-        readonly SampleGroup NetworkManagerSample = new SampleGroup("NetworkManagerLateUpdate", SampleUnit.Millisecond);
-        readonly Stopwatch stopwatch = new Stopwatch();
-
-        bool captureMeasurement;
-        private BenchmarkNetworkManager benchmarker;
-
-        void BeforeLateUpdate()
-        {
-            if (!captureMeasurement)
-                return;
-
-            stopwatch.Start();
-        }
-
-        void AfterLateUpdate()
-        {
-            if (!captureMeasurement)
-                return;
-
-            stopwatch.Stop();
-            double time = stopwatch.Elapsed.TotalMilliseconds;
-            Measure.Custom(NetworkManagerSample, time);
-            stopwatch.Reset();
-        }
+        private NetworkManager benchmarker;
 
         [UnitySetUp]
         public IEnumerator SetUp()
         {
-            captureMeasurement = false;
             // load scene
             yield return EditorSceneManager.LoadSceneAsyncInPlayMode(ScenePath, new LoadSceneParameters { loadSceneMode = LoadSceneMode.Additive });
             Scene scene = SceneManager.GetSceneByPath(ScenePath);
@@ -55,32 +29,16 @@ namespace Mirror.Tests.Performance.Runtime
             // wait for NetworkManager awake
             yield return null;
             // load host
-            benchmarker = Object.FindObjectOfType<BenchmarkNetworkManager>();
+            benchmarker = Object.FindObjectOfType<NetworkManager>();
+
 
             if (benchmarker == null)
             {
                 Assert.Fail("Could not find Benchmarker");
                 yield break;
             }
+            _ = benchmarker.StartHost();
 
-
-            benchmarker.BeforeLateUpdate = BeforeLateUpdate;
-            benchmarker.AfterLateUpdate = AfterLateUpdate;
-        }
-
-        IEnumerator RunBenchmark()
-        {
-            // warmup
-            yield return new WaitForSecondsRealtime(Warmup);
-
-            // run benchmark
-            // capture frames and LateUpdate time
-
-            captureMeasurement = true;
-
-            yield return Measure.Frames().MeasurementCount(MeasureCount).Run();
-
-            captureMeasurement = false;
         }
 
         [UnityTearDown]
@@ -95,32 +53,13 @@ namespace Mirror.Tests.Performance.Runtime
             GameObject.Destroy(benchmarker.gameObject);
         }
 
-        static void EnableHealth(bool value)
-        {
-            Health[] all = Health.FindObjectsOfType<Health>();
-            foreach (Health health in all)
-            {
-                health.enabled = value;
-            }
-        }
-
         [UnityTest]
         [Performance]
-        public IEnumerator Benchmark10k()
-        {
-            EnableHealth(true);
-
-            yield return RunBenchmark();
+        public IEnumerator Benchmark10kLight()
+        { 
+            yield return Measure.Frames().MeasurementCount(320).WarmupCount(60).Run();
         }
 
-        [UnityTest]
-        [Performance]
-        public IEnumerator Benchmark10kIdle()
-        {
-            EnableHealth(false);
-
-            yield return RunBenchmark();
-        }
     }
 }
 
