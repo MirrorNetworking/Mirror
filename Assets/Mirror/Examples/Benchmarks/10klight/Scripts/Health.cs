2b2e71cc007dba2c1d90b565c4983814c1e0b7d1
Paul Pacheco <paul.pacheco@aa.com>
2020-07-04 14:50:56 -0500

    feat: Example with 10k monster that change unfrequently

---
+++ Assets/Mirror/Examples/Benchmarks/10klight/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Mirror.Examples.Light
+{
+    public class Health : NetworkBehaviour
+    {
+        [SyncVar] public int health = 10;
+
+        public void OnStartServer()
+        {
+            StartCoroutine(UpdateHealth());
+        }
+
+        public void OnStopServer()
+        {
+            StopAllCoroutines();
+        }
+
+        internal IEnumerator UpdateHealth()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(3f, 5f));
+                health = (health + 1) % 10;
+            }
+        }
+    }
+}
