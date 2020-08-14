9fd8e6a438124f363e88bb792efd3792de1cdf11
James Frowen <jamesfrowendev@gmail.com>
2020-08-14 23:24:52 +0100

    weaver tests for list read write

---
+++ Assets/Mirror/Tests/Editor/Weaver/GeneratedReaderWriter~/CreatesForList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Mirror;
+
+namespace GeneratedReaderWriter.CreatesForList
+{
+    public class CreatesForList : NetworkBehaviour
+    {
+        [ClientRpc]
+        public void RpcDoSomething(List<int> data)
+        {
+            // empty
+        }
+    }
+}
