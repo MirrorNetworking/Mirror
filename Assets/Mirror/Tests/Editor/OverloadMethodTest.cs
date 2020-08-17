dd8442543ce5b05413cc9369562b159fe87ae942
James Frowen <jamesfrowendev@gmail.com>
2020-08-17 15:52:38 +0100

    weaver error with message overloads

---
+++ Assets/Mirror/Tests/Editor/OverloadMethodTest.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+
+namespace Mirror.Tests.MessageTests
+{
+    class NoArgMethodMessage : IMessageBase
+    {
+        public int someValue;
+
+        // Weaver should ignore these methods because they have no args
+        public void Serialize() { /* method with no arg */ }
+        public void Deserialize() { /* method with no arg */ }
+
+        // Mirror will fill out these empty methods
+        public void Serialize(NetworkWriter writer) { }
+        public void Deserialize(NetworkReader reader) { }
+    }
+
+    class TwoArgMethodMessage : IMessageBase
+    {
+        public int someValue;
+
+        // Weaver should ignore these methods because they have two args
+        public void Serialize(NetworkWriter writer, int AnotherValue) { /* method with 2 args */ }
+        public void Deserialize(NetworkReader reader, int AnotherValue) { /* method with 2 args */ }
+
+
+        // Mirror will fill out these empty methods
+        public void Serialize(NetworkWriter writer) { }
+        public void Deserialize(NetworkReader reader) { }
+    }
+
+    public class OverloadMethodTest
+    {
+        [Test]
+        public void MethodsWithNoArgs()
+        {
+            const int value = 10;
+            NoArgMethodMessage intMessage = new NoArgMethodMessage
+            {
+                someValue = value
+            };
+
+            byte[] data = MessagePacker.Pack(intMessage);
+
+            NoArgMethodMessage unpacked = MessagePacker.Unpack<NoArgMethodMessage>(data);
+
+            Assert.That(unpacked.someValue, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void MethodsWithTwoArgs()
+        {
+            const int value = 10;
+            TwoArgMethodMessage intMessage = new TwoArgMethodMessage
+            {
+                someValue = value
+            };
+
+            byte[] data = MessagePacker.Pack(intMessage);
+
+            TwoArgMethodMessage unpacked = MessagePacker.Unpack<TwoArgMethodMessage>(data);
+
+            Assert.That(unpacked.someValue, Is.EqualTo(value));
+        }
+    }
+}
