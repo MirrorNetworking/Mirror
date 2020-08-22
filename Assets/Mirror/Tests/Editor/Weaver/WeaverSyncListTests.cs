2862a62f4acb6adb1dd4789198cbd0fd53d98485
James Frowen <jamesfrowendev@gmail.com>
2020-08-22 21:02:24 +0100

    removing error from test

--- Assets/Mirror/Tests/Editor/Weaver/WeaverSyncListTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverSyncListTests.cs
@@ -90,8 +90,6 @@ namespace Mirror.Weaver.Tests
             // we need this negative test to make sure that SyncList is being processed 
             HasError("Cannot generate writer for Object. Use a supported type or provide a custom writer",
                 "UnityEngine.Object");
-            HasError("target has unsupported type. Use a type supported by Mirror instead",
-                "UnityEngine.Object WeaverSyncListTests.SyncListNestedInStructWithInvalid.SyncListNestedInStructWithInvalid/SomeData::target");
             HasError("SyncList has sync object generic type SomeData.  Use a type supported by mirror instead",
                 "WeaverSyncListTests.SyncListNestedInStructWithInvalid.SyncListNestedInStructWithInvalid/SomeData/SyncList");
         }
