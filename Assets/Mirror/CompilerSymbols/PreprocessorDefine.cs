24e3ea099c96d497341df717ddfb52e58cbf74b0
James Frowen <jamesfrowendev@gmail.com>
2020-09-03 15:46:15 +0100

    adding version define from v18 breaking change (#2214)

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -31,7 +31,8 @@ namespace Mirror
                 "MIRROR_14_0_OR_NEWER",
                 "MIRROR_15_0_OR_NEWER",
                 "MIRROR_16_0_OR_NEWER",
-                "MIRROR_17_0_OR_NEWER"
+                "MIRROR_17_0_OR_NEWER",
+                "MIRROR_18_0_OR_NEWER"
             };
 
             // only touch PlayerSettings if we actually modified it.
