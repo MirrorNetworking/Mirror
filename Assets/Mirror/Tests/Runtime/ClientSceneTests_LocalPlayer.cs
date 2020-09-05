22e593d36fd206884722ae9ca14519d1922d627b
James Frowen <jamesfrowendev@gmail.com>
2020-09-05 21:14:51 +0100

    adding test for local player destroy

---
+++ Assets/Mirror/Tests/Runtime/ClientSceneTests_LocalPlayer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.Tests.Runtime.ClientSceneTests
+{
+    public class ClientSceneTests_LocalPlayer : ClientSceneTestsBase
+    {
+        [UnityTest]
+        public IEnumerator LocalPlayer_IsSetToNullAfterDestroy()
+        {
+            NetworkIdentity identity = SpawnLocalPlayer();
+
+            GameObject.Destroy(identity);
+
+            // wait a frame for destroy to happen
+            yield return null;
+
+            // use "is null" here to avoid unity == check
+            Assert.IsTrue(ClientScene.localPlayer is null, "local player should be set to c# null");
+        }
+
+        NetworkIdentity SpawnLocalPlayer()
+        {
+            Debug.Assert(ClientScene.localPlayer == null, "LocalPlayer should be null before this test");
+            const uint netId = 1000;
+
+            GameObject go = new GameObject();
+            _createdObjects.Add(go);
+
+            NetworkIdentity identity = go.AddComponent<NetworkIdentity>();
+
+            SpawnMessage msg = new SpawnMessage
+            {
+                netId = netId,
+                isLocalPlayer = true,
+                isOwner = true,
+            };
+
+            PropertyInfo readyConnProperty = typeof(ClientScene).GetProperty(nameof(ClientScene.readyConnection));
+            readyConnProperty.SetValue(null, new FakeNetworkConnection());
+
+            ClientScene.ApplySpawnPayload(identity, msg);
+
+            Assert.That(ClientScene.localPlayer, Is.EqualTo(identity));
+
+            return identity;
+        }
+    }
+}
