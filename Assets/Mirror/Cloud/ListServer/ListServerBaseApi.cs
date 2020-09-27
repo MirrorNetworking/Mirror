99f746ed54c9e41f9e2aff0f55cf04fca67a6f3a
vis2k <info@noobtuts.com>
2020-09-27 21:22:15 +0200

    Remove Cloud

--- Assets/Mirror/Cloud/ListServer/ListServerBaseApi.cs
+++
@@ -1,9 +0,0 @@
-namespace Mirror.Cloud.ListServerService
-{
-    public abstract class ListServerBaseApi : BaseApi
-    {
-        protected ListServerBaseApi(ICoroutineRunner runner, IRequestCreator requestCreator) : base(runner, requestCreator)
-        {
-        }
-    }
-}
--- Assets/Mirror/Examples/Cloud/GUI/README.md
+++
@@ -1,8 +0,0 @@
-# Cloud GUI
-
-This folder contains an example of a List server canvas and scripts that can help set up a GUI.
-
-
-## Other Examples
-
-See `PongWithListServer` or `TanksWithListServer` for playable examples
