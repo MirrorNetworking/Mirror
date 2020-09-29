66be588512575af3ec11a9bc9d7809735847d76d
Paul Pacheco <paul.pacheco@aa.com>
2020-09-28 21:32:16 -0500

    reuse ReadLength (#2292)

--- Assets/Mirror/Editor/Weaver/Readers.cs
+++ Assets/Mirror/Editor/Weaver/Readers.cs
@@ -132,9 +132,7 @@ namespace Mirror.Weaver
             ILProcessor worker = readerFunc.Body.GetILProcessor();
 
             // int length = reader.ReadPackedInt32();
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Call, GetReadFunc(WeaverTypes.Import<int>())));
-            worker.Append(worker.Create(OpCodes.Stloc_0));
+            GenerateReadLength(worker);
 
             // if (length < 0) {
             //    return null
@@ -170,6 +168,13 @@ namespace Mirror.Weaver
             worker.Append(worker.Create(OpCodes.Ldloc_1));
             worker.Append(worker.Create(OpCodes.Ret));
             return readerFunc;
+        }
+
+        private static void GenerateReadLength(ILProcessor worker)
+        {
+            worker.Append(worker.Create(OpCodes.Ldarg_0));
+            worker.Append(worker.Create(OpCodes.Call, GetReadFunc(WeaverTypes.Import<int>())));
+            worker.Append(worker.Create(OpCodes.Stloc_0));
         }
 
         static MethodDefinition GenerateEnumReadFunc(TypeReference variable)
@@ -212,9 +217,7 @@ namespace Mirror.Weaver
             ILProcessor worker = readerFunc.Body.GetILProcessor();
 
             // int length = reader.ReadPackedInt32();
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Call, GetReadFunc(WeaverTypes.Import<int>())));
-            worker.Append(worker.Create(OpCodes.Stloc_0));
+            GenerateReadLength(worker);
 
             // T[] array = new int[length]
             worker.Append(worker.Create(OpCodes.Ldloc_0));
@@ -312,10 +315,8 @@ namespace Mirror.Weaver
 
             ILProcessor worker = readerFunc.Body.GetILProcessor();
 
-            // int count = reader.ReadPackedInt32();
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Call, GetReadFunc(WeaverTypes.Import<int>())));
-            worker.Append(worker.Create(OpCodes.Stloc_0));
+            // int length = reader.ReadPackedInt32();
+            GenerateReadLength(worker);
 
             // -1 is null list, so if count is less than 0 return null
             // if (count < 0) {
