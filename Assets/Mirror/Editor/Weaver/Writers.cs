51996990f8848885af206f1a7d9f97e3e7f1b6ab
Paul Pacheco <paul.pacheco@aa.com>
2020-09-28 21:28:23 -0500

    Refactor writer for loops

--- Assets/Mirror/Editor/Weaver/Writers.cs
+++ Assets/Mirror/Editor/Weaver/Writers.cs
@@ -279,40 +279,19 @@ namespace Mirror.Weaver
             worker.Append(worker.Create(OpCodes.Ldlen));
             worker.Append(worker.Create(OpCodes.Stloc_0));
 
-            // writer.WritePackedInt32(length);
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Ldloc_0));
-            worker.Append(worker.Create(OpCodes.Call, intWriterFunc));
 
-            // for (int i=0; i< value.length; i++) {
-            worker.Append(worker.Create(OpCodes.Ldc_I4_0));
-            worker.Append(worker.Create(OpCodes.Stloc_1));
-            Instruction labelHead = worker.Create(OpCodes.Nop);
-            worker.Append(worker.Create(OpCodes.Br, labelHead));
-
-            // loop body
-            Instruction labelBody = worker.Create(OpCodes.Nop);
-            worker.Append(labelBody);
-            // writer.Write(value[i]);
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Ldarg_1));
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldelema, elementType));
-            worker.Append(worker.Create(OpCodes.Ldobj, elementType));
-            worker.Append(worker.Create(OpCodes.Call, elementWriteFunc));
-
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldc_I4_1));
-            worker.Append(worker.Create(OpCodes.Add));
-            worker.Append(worker.Create(OpCodes.Stloc_1));
-
-            // end for loop
-            worker.Append(labelHead);
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldarg_1));
-            worker.Append(worker.Create(OpCodes.Ldlen));
-            worker.Append(worker.Create(OpCodes.Conv_I4));
-            worker.Append(worker.Create(OpCodes.Blt, labelBody));
+            // writer.WritePackedInt32(count);
+            // for (int i=0; i < count; i++)
+            GenerateFor(worker, () =>
+            {
+                // writer.Write(value[i]);
+                worker.Append(worker.Create(OpCodes.Ldarg_0));
+                worker.Append(worker.Create(OpCodes.Ldarg_1));
+                worker.Append(worker.Create(OpCodes.Ldloc_1));
+                worker.Append(worker.Create(OpCodes.Ldelema, elementType));
+                worker.Append(worker.Create(OpCodes.Ldobj, elementType));
+                worker.Append(worker.Create(OpCodes.Call, elementWriteFunc));
+            });
 
             // return
             worker.Append(worker.Create(OpCodes.Ret));
@@ -347,50 +326,22 @@ namespace Mirror.Weaver
             worker.Append(worker.Create(OpCodes.Call, countref));
             worker.Append(worker.Create(OpCodes.Stloc_0));
 
-
-            // writer.WritePackedInt32(length);
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Ldloc_0));
-            worker.Append(worker.Create(OpCodes.Call, intWriterFunc));
-
-            // Loop through the ArraySegment<T> and call the writer for each element.
-            // generates this:
-            // for (int i=0; i< length; i++)
-            // {
-            //    writer.Write(value.Array[i + value.Offset]);
-            // }
-            worker.Append(worker.Create(OpCodes.Ldc_I4_0));
-            worker.Append(worker.Create(OpCodes.Stloc_1));
-            Instruction labelHead = worker.Create(OpCodes.Nop);
-            worker.Append(worker.Create(OpCodes.Br, labelHead));
-
-            // loop body
-            Instruction labelBody = worker.Create(OpCodes.Nop);
-            worker.Append(labelBody);
-
-            // writer.Write(value.Array[i + value.Offset]);
-            worker.Append(worker.Create(OpCodes.Ldarg_0));
-            worker.Append(worker.Create(OpCodes.Ldarga_S, (byte)1));
-            worker.Append(worker.Create(OpCodes.Call, WeaverTypes.ArraySegmentArrayReference.MakeHostInstanceGeneric(genericInstance)));
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldarga_S, (byte)1));
-            worker.Append(worker.Create(OpCodes.Call, WeaverTypes.ArraySegmentOffsetReference.MakeHostInstanceGeneric(genericInstance)));
-            worker.Append(worker.Create(OpCodes.Add));
-            worker.Append(worker.Create(OpCodes.Ldelema, elementType));
-            worker.Append(worker.Create(OpCodes.Ldobj, elementType));
-            worker.Append(worker.Create(OpCodes.Call, elementWriteFunc));
-
-
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldc_I4_1));
-            worker.Append(worker.Create(OpCodes.Add));
-            worker.Append(worker.Create(OpCodes.Stloc_1));
-
-            // end for loop
-            worker.Append(labelHead);
-            worker.Append(worker.Create(OpCodes.Ldloc_1));
-            worker.Append(worker.Create(OpCodes.Ldloc_0));
-            worker.Append(worker.Create(OpCodes.Blt, labelBody));
+            // writer.WritePackedInt32(count);
+            // for (int i=0; i < count; i++)
+            GenerateFor(worker, () =>
+            {
+                // writer.Write(value.Array[i + value.Offset]);
+                worker.Append(worker.Create(OpCodes.Ldarg_0));
+                worker.Append(worker.Create(OpCodes.Ldarga_S, (byte)1));
+                worker.Append(worker.Create(OpCodes.Call, WeaverTypes.ArraySegmentArrayReference.MakeHostInstanceGeneric(genericInstance)));
+                worker.Append(worker.Create(OpCodes.Ldloc_1));
+                worker.Append(worker.Create(OpCodes.Ldarga_S, (byte)1));
+                worker.Append(worker.Create(OpCodes.Call, WeaverTypes.ArraySegmentOffsetReference.MakeHostInstanceGeneric(genericInstance)));
+                worker.Append(worker.Create(OpCodes.Add));
+                worker.Append(worker.Create(OpCodes.Ldelema, elementType));
+                worker.Append(worker.Create(OpCodes.Ldobj, elementType));
+                worker.Append(worker.Create(OpCodes.Call, elementWriteFunc));
+            });
 
             // return
             worker.Append(worker.Create(OpCodes.Ret));
@@ -445,6 +396,28 @@ namespace Mirror.Weaver
             worker.Append(worker.Create(OpCodes.Stloc_0));
 
             // writer.WritePackedInt32(count);
+            // for (int i=0; i < count; i++)
+            GenerateFor(worker, () => {
+                MethodReference getItem = WeaverTypes.ListGetItemReference.MakeHostInstanceGeneric(genericInstance);
+
+                // writer.Write(value[i]);
+                worker.Append(worker.Create(OpCodes.Ldarg_0)); // writer
+                worker.Append(worker.Create(OpCodes.Ldarg_1)); // value
+                worker.Append(worker.Create(OpCodes.Ldloc_1)); // i
+                worker.Append(worker.Create(OpCodes.Call, getItem)); //get_Item
+                worker.Append(worker.Create(OpCodes.Call, elementWriteFunc)); // Write
+            });
+
+            // return
+            worker.Append(worker.Create(OpCodes.Ret));
+            return writerFunc;
+        }
+
+        private static void GenerateFor(ILProcessor worker, Action body)
+        {
+            MethodReference intWriterFunc = GetWriteFunc(WeaverTypes.Import<int>());
+
+            // writer.WritePackedInt32(count);
             worker.Append(worker.Create(OpCodes.Ldarg_0));
             worker.Append(worker.Create(OpCodes.Ldloc_0));
             worker.Append(worker.Create(OpCodes.Call, intWriterFunc));
@@ -464,18 +437,9 @@ namespace Mirror.Weaver
             Instruction labelBody = worker.Create(OpCodes.Nop);
             worker.Append(labelBody);
 
-            MethodReference getItem = WeaverTypes.ListGetItemReference.MakeHostInstanceGeneric(genericInstance);
-
-            // writer.Write(value[i]);
-            worker.Append(worker.Create(OpCodes.Ldarg_0)); // writer
-            worker.Append(worker.Create(OpCodes.Ldarg_1)); // value
-            worker.Append(worker.Create(OpCodes.Ldloc_1)); // i
-            worker.Append(worker.Create(OpCodes.Call, getItem)); //get_Item
-            worker.Append(worker.Create(OpCodes.Call, elementWriteFunc)); // Write
-
+            body();
 
             // end for loop
-
             // for loop i++ 
             worker.Append(worker.Create(OpCodes.Ldloc_1));
             worker.Append(worker.Create(OpCodes.Ldc_I4_1));
@@ -487,10 +451,6 @@ namespace Mirror.Weaver
             worker.Append(worker.Create(OpCodes.Ldloc_1));
             worker.Append(worker.Create(OpCodes.Ldloc_0));
             worker.Append(worker.Create(OpCodes.Blt, labelBody));
-
-            // return
-            worker.Append(worker.Create(OpCodes.Ret));
-            return writerFunc;
         }
     }
 }
