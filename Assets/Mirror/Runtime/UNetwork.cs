1afdfd91d03fbea2a5efd920614adf311abef29f
vis2k <info@noobtuts.com>
2020-09-30 12:12:46 +0200

    Remove unused

--- Assets/Mirror/Runtime/UNetwork.cs
+++ Assets/Mirror/Runtime/UNetwork.cs
@@ -1,30 +1,15 @@
-using System;
 using System.Runtime.InteropServices;
-using UnityEngine;
 
 namespace Mirror
 {
     // Handles network messages on client and server
     public delegate void NetworkMessageDelegate(NetworkConnection conn, NetworkReader reader, int channelId);
 
-    // Handles requests to spawn objects on the client
-    public delegate GameObject SpawnDelegate(Vector3 position, Guid assetId);
-
-    public delegate GameObject SpawnHandlerDelegate(SpawnMessage msg);
-
-    // Handles requests to unspawn objects on the client
-    public delegate void UnSpawnDelegate(GameObject spawned);
-
     // invoke type for Cmd/Rpc
     public enum MirrorInvokeType
     {
         Command,
         ClientRpc
-    }
-
-    public enum Version
-    {
-        Current = 1
     }
 
     public static class Channels
