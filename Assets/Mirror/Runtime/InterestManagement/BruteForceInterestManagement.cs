e673b0b8a62ded87a9d1549def26a272c33885da
vis2k <info@noobtuts.com>
2020-10-02 14:19:51 +0200

    BruteForceInterestManagementSystem: Tick

--- Assets/Mirror/Runtime/InterestManagement/BruteForceInterestManagement.cs
+++ Assets/Mirror/Runtime/InterestManagement/BruteForceInterestManagement.cs
@@ -1,13 +1,35 @@
 ﻿// straight forward brute force interest management from DOTSNET
+using UnityEngine;
 namespace Mirror
 {
     public class BruteForceInterestManagement : InterestManagement
     {
+        // visibility radius
+        public float visibilityRadius = float.MaxValue;
+
+        // don't update every tick. update every so often.
+        public float updateInterval = 1;
+        double lastUpdateTime;
+
         public override void RebuildAll()
         {
             //RebuildObservers();
             //RemoveOldObservers();
             //AddNewObservers();
+        }
+
+        // update rebuilds every couple of seconds
+        void Update()
+        {
+            // only while server is running
+            if (NetworkServer.active)
+            {
+                if (Time.time >= lastUpdateTime + updateInterval)
+                {
+                    RebuildAll();
+                    lastUpdateTime = Time.time;
+                }
+            }
         }
     }
 }
