017090ff85589c844e477c76799ea6db34fae767
vis2k <info@noobtuts.com>
2020-10-02 14:08:31 +0200

    abstract InterestManagement, BruteForceInterestManagement empty components

---
+++ Assets/Mirror/Runtime/InterestManagement/BruteForceInterestManagement.cs
@@ -0,0 +1,13 @@
+﻿// straight forward brute force interest management from DOTSNET
+namespace Mirror
+{
+    public class BruteForceInterestManagement : InterestManagement
+    {
+        public override void RebuildAll()
+        {
+            //RebuildObservers();
+            //RemoveOldObservers();
+            //AddNewObservers();
+        }
+    }
+}
