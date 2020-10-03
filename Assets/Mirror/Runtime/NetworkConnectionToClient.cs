e0b72040fb98897a2cda8b147938631d1a231cdf
Emre Bugday <47198270+EmreB99@users.noreply.github.com>
2020-10-03 19:29:25 +0300

    Calling OnStopServer on disconnect
    This seems to be calling the OnStopServer but with a little twist add to it. It calls it twice which is unacceptable. I can add a private bool to it to check if it is called before but I believe I can find a better solution than a cheessy hack.
    
    I am sending this commit just incase I forgot what I have done cuz I'm a little fish brained. I forgot shit real quick.

--- Assets/Mirror/Runtime/NetworkConnectionToClient.cs
+++ Assets/Mirror/Runtime/NetworkConnectionToClient.cs
@@ -58,6 +58,7 @@ namespace Mirror
             // set not ready and handle clientscene disconnect in any case
             // (might be client or host mode here)
             isReady = false;
+            identity.OnStopServer();
             Transport.activeTransport.ServerDisconnect(connectionId);
             RemoveObservers();
         }
