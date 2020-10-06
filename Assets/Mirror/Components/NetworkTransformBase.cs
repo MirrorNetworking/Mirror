5ad3fb5807de699e18ee1afe18821c3a5dc4a2dd
James Frowen <jamesfrowendev@gmail.com>
2020-10-06 18:47:16 +0100

    feat: NetworkTranform Teleport (#2320)
    * Server Teleport Enchancement for NetworkTransform
    
    Related to this issue: https://github.com/vis2k/Mirror/issues/2200
    
    * Improvements on NetworkTransformBase
    
    We disable clientAuthority until teleportation is finished and acknowledged. We set the goal and start data points to null before teleportation.
    We set the clientAuthority back to it's initial value registered on server, both on the server and client side; it seems like we don't need to change it to anything on Client side because to my understanding clientAuthortiy is not affecting anything on client side. I may be wrong but still it is a great practice to set it back to it's initial value just in case...
    
    * Added teleportation with rotation
    
    I forgot to add a rotation functionality on my previous commit. It wouldn't be a teleportation without also changing the rotation. If provided a rotation input it will teleport with a rotation, if not it will simply protect the original rotation before the teleportation.
    
    * Removed my debug statement
    
    I forgot to remove the debug log statement lol silly me
    
    * fixing up code
    
    * resetting goal and last pos on server and client
    * renaming variables
    * making sure TeleportFinished can't request authority at any time
    
    * moving teleport block
    
    * removing duplicate lines
    
    Co-authored-by: Emre Bugday <47198270+EmreB99@users.noreply.github.com>

--- Assets/Mirror/Components/NetworkTransformBase.cs
+++ Assets/Mirror/Components/NetworkTransformBase.cs
@@ -27,6 +27,11 @@ namespace Mirror
         [Tooltip("Set to true if moves come from owner client, set to false if moves always come from server")]
         public bool clientAuthority;
 
+        /// <summary>
+        /// We need to store this locally on the server so clients can't request Authority when ever they like
+        /// </summary>
+        bool clientAuthorityBeforeTeleport;
+
         // Is this a client with authority over this transform?
         // This component could be on the player object or any object that has been assigned authority to this client.
         bool IsClientWithAuthority => hasAuthority && clientAuthority;
@@ -382,6 +387,88 @@ namespace Mirror
                 }
             }
         }
+
+        #region Server Teleport (force move player)
+        /// <summary>
+        /// Server side teleportation.
+        /// This method will override this GameObject's current Transform.Position to the Vector3 you have provided
+        /// and send it to all other Clients to override it at their side too.
+        /// </summary>
+        /// <param name="position">Where to teleport this GameObject</param>
+        [Server]
+        public void ServerTeleport(Vector3 position)
+        {
+            Quaternion rotation = transform.rotation;
+            ServerTeleport(position, rotation);
+        }
+
+        /// <summary>
+        /// Server side teleportation.
+        /// This method will override this GameObject's current Transform.Position and Transform.Rotation
+        /// to the Vector3 you have provided
+        /// and send it to all other Clients to override it at their side too.
+        /// </summary>
+        /// <param name="position">Where to teleport this GameObject</param>
+        /// <param name="rotation">Which rotation to set this GameObject</param>
+        [Server]
+        public void ServerTeleport(Vector3 position, Quaternion rotation)
+        {
+            // To prevent applying the position updates received from client (if they have ClientAuth) while being teleported.
+
+            // clientAuthorityBeforeTeleport defaults to false when not teleporting, if it is true then it means hat teleport was previously called but not finished
+            // therefore we should keep it as true so that 2nd teleport call doesn't clear authority
+            clientAuthorityBeforeTeleport = clientAuthority || clientAuthorityBeforeTeleport;
+            clientAuthority = false;
+
+            DoTeleport(position, rotation);
+
+            // tell all clients about new values
+            RpcTeleport(position, rotation, clientAuthorityBeforeTeleport);
+        }
+
+        void DoTeleport(Vector3 newPosition, Quaternion newRotation)
+        {
+            transform.position = newPosition;
+            transform.rotation = newRotation;
+
+            // Since we are overriding the position we don't need a goal and start.
+            // Reset them to null for fresh start
+            goal = null;
+            start = null;
+            lastPosition = newPosition;
+            lastRotation = newRotation;
+        }
+
+        [ClientRpc]
+        void RpcTeleport(Vector3 newPosition, Quaternion newRotation, bool isClientAuthority)
+        {
+            DoTeleport(newPosition, newRotation);
+
+            // only send finished if is owner and is ClientAuthority on server 
+            if (hasAuthority && isClientAuthority)
+                CmdTeleportFinished();
+        }
+
+        /// <summary>
+        /// This RPC will be invoked on server after client finishes overriding the position.
+        /// </summary>
+        /// <param name="initialAuthority"></param>
+        [Command]
+        void CmdTeleportFinished()
+        {
+            if (clientAuthorityBeforeTeleport)
+            {
+                clientAuthority = true;
+
+                // reset value so doesnt effect future calls, see note in ServerTeleport
+                clientAuthorityBeforeTeleport = false;
+            }
+            else
+            {
+                Debug.LogWarning("Client called TeleportFinished when clientAuthority was false on server", this);
+            }
+        }
+        #endregion
 
         static void DrawDataPointGizmo(DataPoint data, Color color)
         {
