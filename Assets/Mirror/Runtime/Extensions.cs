8c51319f5e2d23e430b86716a9a42ff1b841dc81
vis2k <info@noobtuts.com>
2020-10-08 12:07:37 +0200

    DotNetCompatibility and StringHash merged into Extensions

--- Assets/Mirror/Runtime/Extensions.cs
+++ Assets/Mirror/Runtime/Extensions.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Mirror
 {
-    public static class StringHash
+    public static class Extensions
     {
         // string.GetHashCode is not guaranteed to be the same on all machines, but
         // we need one that is the same on all machines. simple and stupid:
@@ -13,6 +15,16 @@ namespace Mirror
                     hash = hash * 31 + c;
                 return hash;
             }
+        }
+
+        // DotNet compatibility
+        internal static string GetMethodName(this Delegate func)
+        {
+#if NETFX_CORE
+            return func.GetMethodInfo().Name;
+#else
+            return func.Method.Name;
+#endif
         }
     }
 }
