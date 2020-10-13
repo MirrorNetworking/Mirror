6c8927f11d32e48610427536fc11ed22be44ad88
vis2k <info@noobtuts.com>
2020-10-13 16:42:16 +0200

    KcpTransport: more

--- Assets/Mirror/Runtime/Transport/KCP/MirrorTransport/KCPTransport.cs
+++ Assets/Mirror/Runtime/Transport/KCP/MirrorTransport/KCPTransport.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
 
@@ -6,6 +7,20 @@ namespace Mirror.KCP
 {
     public class KcpTransport : Transport
     {
+        // common
+        [Header("Transport Configuration")]
+        public ushort Port = 7777;
+        readonly byte[] buffer = new byte[1500];
+
+        // server
+        [SerializeField] string bindAddress = "localhost";
+        Socket serverSocket;
+        EndPoint serverNewClientEP = new IPEndPoint(IPAddress.IPv6Any, 0);
+
+        // client
+        Socket clientSocket;
+        EndPoint clientNewClientEP = new IPEndPoint(IPAddress.IPv6Any, 0);
+
         void Awake()
         {
             Debug.Log("KCPTransport initialized!");
@@ -16,14 +31,24 @@ namespace Mirror.KCP
             Application.platform != RuntimePlatform.WebGLPlayer;
 
         // client
-        public override bool ClientConnected() => throw new NotImplementedException();
-        public override void ClientConnect(string address) => throw new NotImplementedException();
+        public override bool ClientConnected() => clientSocket != null; // TODO
+        public override void ClientConnect(string address)
+        {
+            // TODO connect
+            //var client = new KcpClientConnection();
+            //await client.ConnectAsync(address, Port);
+        }
         public override bool ClientSend(int channelId, ArraySegment<byte> segment)
         {
             throw new NotImplementedException();
         }
 
-        public override void ClientDisconnect() => throw new NotImplementedException();
+        public override void ClientDisconnect()
+        {
+            clientSocket?.Close();
+            clientSocket = null;
+        }
+
 
         // IMPORTANT: set script execution order to >1000 to call Transport's
         //            LateUpdate after all others. Fixes race condition where
@@ -37,11 +62,72 @@ namespace Mirror.KCP
             // (https://github.com/vis2k/Mirror/pull/379)
             if (!enabled)
                 return;
+
+            // update server
+            while (serverSocket != null && serverSocket.Poll(0, SelectMode.SelectRead))
+            {
+                int msgLength = serverSocket.ReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref serverNewClientEP);
+
+                // is this a new connection?
+                Debug.LogWarning($"KCP: server recv {msgLength}");
+                /*if (!connectedClients.TryGetValue(endpoint, out KcpServerConnection connection))
+                {
+                    // add it to a queue
+                    connection = new KcpServerConnection(socket, endpoint);
+                    acceptedConnections.Writer.TryWrite(connection);
+                    connectedClients.Add(endpoint, connection);
+                    connection.Disconnected += () =>
+                    {
+                        connectedClients.Remove(endpoint);
+                    };
+                }
+
+                connection.RawInput(data, msgLength);*/
+            }
+
+            // update client
+            while (clientSocket != null && clientSocket.Poll(0, SelectMode.SelectRead))
+            {
+                int msgLength = clientSocket.ReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref clientNewClientEP);
+                Debug.LogWarning($"KCP: client recv {msgLength}");
+
+                // is this a new connection?
+                /*if (!connectedClients.TryGetValue(endpoint, out KcpServerConnection connection))
+                {
+                    // add it to a queue
+                    connection = new KcpServerConnection(socket, endpoint);
+                    acceptedConnections.Writer.TryWrite(connection);
+                    connectedClients.Add(endpoint, connection);
+                    connection.Disconnected += () =>
+                    {
+                        connectedClients.Remove(endpoint);
+                    };
+                }
+
+                connection.RawInput(data, msgLength);*/
+            }
         }
 
         // server
-        public override bool ServerActive() => throw new NotImplementedException();
-        public override void ServerStart() => throw new NotImplementedException();
+        public override bool ServerActive() => serverSocket != null;
+        public override void ServerStart()
+        {
+            // only start once
+            if (serverSocket != null)
+            {
+                Debug.LogWarning("KCP: server already started!");
+            }
+
+            // listen
+            serverSocket = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.DualMode = true;
+            serverSocket.Bind(new IPEndPoint(IPAddress.IPv6Any, Port));
+
+            // TODO accept
+            //KcpServerConnection connection = await acceptedConnections.Reader.ReadAsync();
+            //await connection.Handshake();
+            //return connection;
+        }
         public override bool ServerSend(int connectionId, int channelId, ArraySegment<byte> segment)
         {
             throw new NotImplementedException();
@@ -51,7 +137,11 @@ namespace Mirror.KCP
         {
             throw new NotImplementedException();
         }
-        public override void ServerStop() => throw new NotImplementedException();
+        public override void ServerStop()
+        {
+            serverSocket?.Close();
+            serverSocket = null;
+        }
 
         // common
         public override void Shutdown()
