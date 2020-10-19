7d756ee16097e9439e5baff6f4528b1440663a27
James Frowen <jamesfrowendev@gmail.com>
2020-10-19 22:32:54 +0100

    Removing IDisposable from NetworkConnection
    * clientOwnedObjects is a hash set which doesn't need to be disposed
    * DestroyOwnedObjects already clears list which is called by networkmanager when connection disconnects

--- Assets/Mirror/Runtime/NetworkClient.cs
+++ Assets/Mirror/Runtime/NetworkClient.cs
@@ -215,7 +215,6 @@ namespace Mirror
                 if (connection != null)
                 {
                     connection.Disconnect();
-                    connection.Dispose();
                     connection = null;
                     RemoveTransportHandlers();
                 }
