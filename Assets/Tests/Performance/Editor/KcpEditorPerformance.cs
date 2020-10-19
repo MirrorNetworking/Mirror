1ffa92e0310bdafdce3995bfbdb710bbd05ca3fa
Lymdun <lymdun@protonmail.com>
2020-10-19 14:41:04 +0200

    add write bytes performance test

---
+++ Assets/Tests/Performance/Editor/KcpEditorPerformance.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using Mirror.KCP;
+
+namespace Mirror.Tests.Performance
+{
+    public class KcpEditorPerformance
+    {
+        [Test]
+        [Performance]
+        public void WriteBytes()
+        {
+            Measure.Method(WBytes)
+                .WarmupCount(10)
+                .MeasurementCount(100)
+                .Run();
+        }
+
+        static void WBytes()
+        {
+            var buffer = new ByteBuffer(1024);
+            byte[] bytes = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
+
+            for (int i = 0; i < 1000; i++)
+            {
+                buffer.WriteBytes(bytes, 3, 3);
+            }
+        }
+    }
+}
