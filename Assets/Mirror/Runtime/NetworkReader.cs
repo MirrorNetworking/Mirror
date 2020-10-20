85c5662a6c9964630ab53ee02651e0fe25daf6d9
Paul Pacheco <paulpach@gmail.com>
2020-10-19 22:18:23 -0500

    simplify serializing collections (#416)
    The code to serialize lists, arrays, and array segments is now in the runtime instead of generated in the weaver.
    
    removes about 300 LOC of weaver witchcraft

--- Assets/Mirror/Runtime/NetworkReader.cs
+++ Assets/Mirror/Runtime/NetworkReader.cs
@@ -7,6 +7,7 @@
 //   now:
 //     0.0% CPU time,  32KB memory, 0.02ms
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -389,6 +390,32 @@ namespace Mirror
 
             if (logger.WarnEnabled()) logger.LogFormat(LogType.Warning, "ReadNetworkIdentity netId:{0} not found in spawned", netId);
             return null;
+        }
+
+        public static List<T> ReadList<T>(this NetworkReader reader)
+        {
+            int length = reader.ReadPackedInt32();
+            if (length < 0)
+                return null;
+            var result = new List<T>(length);
+            for (int i=0; i< length; i++)
+            {
+                result.Add(reader.Read<T>());
+            }
+            return result;
+        }
+
+        public static T[] ReadArray<T>(this NetworkReader reader)
+        {
+            int length = reader.ReadPackedInt32();
+            if (length < 0)
+                return null;
+            var result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = reader.Read<T>();
+            }
+            return result;
         }
 
         public static Uri ReadUri(this NetworkReader reader)
