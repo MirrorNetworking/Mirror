7fdcca2811611703e87e82c0ff564cff8595730e
James Frowen <jamesfrowendev@gmail.com>
2020-10-24 17:54:44 +0100

    using expression bodies

--- Assets/Mirror/Runtime/Transport/MiddlewareTransport.cs
+++ Assets/Mirror/Runtime/Transport/MiddlewareTransport.cs
@@ -8,69 +8,30 @@ namespace Mirror
     /// </summary>
     public abstract class MiddlewareTransport : Transport
     {
+        /// <summary>
+        /// Transport to call to after middleware
+        /// </summary>
         public Transport inner;
 
-        public override bool Available()
-        {
-            return inner.Available();
-        }
-        public override int GetMaxPacketSize(int channelId = 0)
-        {
-            return inner.GetMaxPacketSize(channelId);
-        }
-        public override void Shutdown()
-        {
-            inner.Shutdown();
-        }
+        public override bool Available() => inner.Available();
+        public override int GetMaxPacketSize(int channelId = 0) => inner.GetMaxPacketSize(channelId);
+        public override void Shutdown() => inner.Shutdown();
 
         #region Client
-        public override void ClientConnect(string address)
-        {
-            inner.ClientConnect(address);
-        }
-        public override bool ClientConnected()
-        {
-            return inner.ClientConnected();
-        }
-        public override void ClientDisconnect()
-        {
-            inner.ClientDisconnect();
-        }
-        public override bool ClientSend(int channelId, ArraySegment<byte> segment)
-        {
-            return inner.ClientSend(channelId, segment);
-        }
+        public override void ClientConnect(string address) => inner.ClientConnect(address);
+        public override bool ClientConnected() => inner.ClientConnected();
+        public override void ClientDisconnect() => inner.ClientDisconnect();
+        public override bool ClientSend(int channelId, ArraySegment<byte> segment) => inner.ClientSend(channelId, segment);
         #endregion
 
         #region Server
-        public override bool ServerActive()
-        {
-            return inner.ServerActive();
-        }
-        public override void ServerStart()
-        {
-            inner.ServerStart();
-        }
-        public override void ServerStop()
-        {
-            inner.ServerStop();
-        }
-        public override bool ServerSend(List<int> connectionIds, int channelId, ArraySegment<byte> segment)
-        {
-            return inner.ServerSend(connectionIds, channelId, segment);
-        }
-        public override bool ServerDisconnect(int connectionId)
-        {
-            return inner.ServerDisconnect(connectionId);
-        }
-        public override string ServerGetClientAddress(int connectionId)
-        {
-            return inner.ServerGetClientAddress(connectionId);
-        }
-        public override Uri ServerUri()
-        {
-            return inner.ServerUri();
-        }
+        public override bool ServerActive() => inner.ServerActive();
+        public override void ServerStart() => inner.ServerStart();
+        public override void ServerStop() => inner.ServerStop();
+        public override bool ServerSend(List<int> connectionIds, int channelId, ArraySegment<byte> segment) => inner.ServerSend(connectionIds, channelId, segment);
+        public override bool ServerDisconnect(int connectionId) => inner.ServerDisconnect(connectionId);
+        public override string ServerGetClientAddress(int connectionId) => inner.ServerGetClientAddress(connectionId);
+        public override Uri ServerUri() => inner.ServerUri();
         #endregion
     }
 }
