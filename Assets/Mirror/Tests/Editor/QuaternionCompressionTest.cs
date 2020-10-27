7aa2a5f45bb596caf1ae22a834087fb0f6509496
James Frowen <jamesfrowendev@gmail.com>
2020-10-27 16:00:34 +0000

    cleaning up test file
    * removing left over stuff from old version
    * using yield return in test cases

--- Assets/Mirror/Tests/Editor/QuaternionCompressionTest.cs
+++ Assets/Mirror/Tests/Editor/QuaternionCompressionTest.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -8,37 +8,27 @@ namespace Mirror.Tests
     {
         // worse case where xyzw all equal error in largest is ~1.732 times greater than error in smallest 3
         // High/Low Precision fails when xyzw all equal,
-        // *1.8f is enough to allow extra error in largest,
-        internal const float HighestPrecision = 0.000000169f * 1.8f;
-        internal const float MediumPrecision = 0.00138f;
-        // *1.2f is enough to allow extra error in largest
-        internal const float LowPrecision = 0.0110f * 1.2f;
+        internal const float AllowedPrecision = 0.00138f;
 
         [Test]
-        [TestCaseSource(nameof(GetTestCases))]
+        [TestCaseSource(nameof(QuaternionTestCases))]
         public void QuaternionCompressAtHalfPrecision(Quaternion rotationIn)
         {
-            QuaternionCompressWithinPrecision(rotationIn, MediumPrecision);
-        }
+            uint packed = Compression.CompressQuaternion(rotationIn);
 
-
-        static void QuaternionCompressWithinPrecision(Quaternion rotationIn, float allowedPrecision)
-        {
-            uint packed = QuaternionCompression.Pack(rotationIn);
-
-            Quaternion rotationOut = QuaternionCompression.Unpack(packed);
+            Quaternion rotationOut = Compression.DecompressQuaternion(packed);
 
             Assert.That(rotationOut.x, Is.Not.NaN, "x was NaN");
             Assert.That(rotationOut.y, Is.Not.NaN, "y was NaN");
             Assert.That(rotationOut.z, Is.Not.NaN, "z was NaN");
             Assert.That(rotationOut.w, Is.Not.NaN, "w was NaN");
 
-            AssertPrecision(rotationIn, rotationOut, allowedPrecision);
+            AssertPrecision(rotationIn, rotationOut, AllowedPrecision);
         }
 
         internal static void AssertPrecision(Quaternion inRot, Quaternion outRot, float precision)
         {
-            int largest = QuaternionCompression.FindLargestIndex(inRot);
+            int largest = Compression.FindLargestIndex(inRot);
             float sign = Mathf.Sign(inRot[largest]);
             // flip sign of A if largest is is negative
             // Q == (-Q)
@@ -49,98 +39,88 @@ namespace Mirror.Tests
             Assert.AreEqual(sign * inRot.w, outRot.w, precision, $"w off by {Mathf.Abs(sign * inRot.w - outRot.w)}");
         }
 
-        static object[] GetTestCases()
+        static IEnumerable QuaternionTestCases
         {
-            List<Quaternion> list = new List<Quaternion>
+            get
             {
-                Quaternion.identity,
-                new Quaternion(1, 0, 0, 0),
-                new Quaternion(0, 1, 0, 0),
-                new Quaternion(0, 0, 1, 0),
+                yield return new TestCaseData(Quaternion.identity);
+                yield return new TestCaseData(new Quaternion(1, 0, 0, 0));
+                yield return new TestCaseData(new Quaternion(0, 1, 0, 0));
+                yield return new TestCaseData(new Quaternion(0, 0, 1, 0));
 
-                new Quaternion(1, 1, 0, 0).normalized,
-                new Quaternion(0, 1, 1, 0).normalized,
-                new Quaternion(0, 1, 1, 0).normalized,
-                new Quaternion(0, 0, 1, 1).normalized,
+                yield return new TestCaseData(new Quaternion(1, 1, 0, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, 1, 1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, 1, 1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, 0, 1, 1).normalized);
 
-                new Quaternion(1, 1, 1, 0).normalized,
-                new Quaternion(1, 1, 0, 1).normalized,
-                new Quaternion(1, 0, 1, 1).normalized,
-                new Quaternion(0, 1, 1, 1).normalized,
+                yield return new TestCaseData(new Quaternion(1, 1, 1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(1, 1, 0, 1).normalized);
+                yield return new TestCaseData(new Quaternion(1, 0, 1, 1).normalized);
+                yield return new TestCaseData(new Quaternion(0, 1, 1, 1).normalized);
 
-                new Quaternion(1, 1, 1, 1).normalized,
+                yield return new TestCaseData(new Quaternion(1, 1, 1, 1).normalized);
 
-                new Quaternion(-1, 0, 0, 0),
-                new Quaternion(0, -1, 0, 0),
-                new Quaternion(0, 0, -1, 0),
-                new Quaternion(0, 0, 0, -1),
+                yield return new TestCaseData(new Quaternion(-1, 0, 0, 0));
+                yield return new TestCaseData(new Quaternion(0, -1, 0, 0));
+                yield return new TestCaseData(new Quaternion(0, 0, -1, 0));
+                yield return new TestCaseData(new Quaternion(0, 0, 0, -1));
 
-                new Quaternion(-1, -1, 0, 0).normalized,
-                new Quaternion(0, -1, -1, 0).normalized,
-                new Quaternion(0, -1, -1, 0).normalized,
-                new Quaternion(0, 0, -1, -1).normalized,
+                yield return new TestCaseData(new Quaternion(-1, -1, 0, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, -1, -1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, -1, -1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(0, 0, -1, -1).normalized);
 
-                new Quaternion(-1, -1, -1, 0).normalized,
-                new Quaternion(-1, -1, 0, -1).normalized,
-                new Quaternion(-1, 0, -1, -1).normalized,
-                new Quaternion(0, -1, -1, -1).normalized,
+                yield return new TestCaseData(new Quaternion(-1, -1, -1, 0).normalized);
+                yield return new TestCaseData(new Quaternion(-1, -1, 0, -1).normalized);
+                yield return new TestCaseData(new Quaternion(-1, 0, -1, -1).normalized);
+                yield return new TestCaseData(new Quaternion(0, -1, -1, -1).normalized);
 
-                new Quaternion(-1, -1, -1, -1).normalized,
+                yield return new TestCaseData(new Quaternion(-1, -1, -1, -1).normalized);
 
-                Quaternion.Euler(200, 100, 10),
-                Quaternion.LookRotation(new Vector3(0.3f, 0.4f, 0.5f)),
-                Quaternion.Euler(45f, 56f, Mathf.PI),
-                Quaternion.AngleAxis(30, new Vector3(1, 2, 5)),
-                Quaternion.AngleAxis(5, new Vector3(-1, .01f, 0.44f)),
-                Quaternion.AngleAxis(358, new Vector3(0.5f, 2, 5)),
-                Quaternion.AngleAxis(-54, new Vector3(1, 2, 5)),
-            };
-
-            int count = list.Count;
-            object[] cases = new object[count];
-            for (int i = 0; i < count; i++)
-            {
-                cases[i] = new object[] { list[i] };
+                yield return new TestCaseData(Quaternion.Euler(200, 100, 10));
+                yield return new TestCaseData(Quaternion.LookRotation(new Vector3(0.3f, 0.4f, 0.5f)));
+                yield return new TestCaseData(Quaternion.Euler(45f, 56f, Mathf.PI));
+                yield return new TestCaseData(Quaternion.AngleAxis(30, new Vector3(1, 2, 5)));
+                yield return new TestCaseData(Quaternion.AngleAxis(5, new Vector3(-1, .01f, 0.44f)));
+                yield return new TestCaseData(Quaternion.AngleAxis(358, new Vector3(0.5f, 2, 5)));
+                yield return new TestCaseData(Quaternion.AngleAxis(-54, new Vector3(1, 2, 5)));
             }
-
-            return cases;
         }
 
-
         [Test]
-        [TestCaseSource(nameof(GetLargestIndexTestCases))]
+        [TestCaseSource(nameof(LargestIndexTestCases))]
         public void FindLargestIndexWork(Quaternion quaternion, int expected)
         {
-            int largest = QuaternionCompression.FindLargestIndex(quaternion);
+            int largest = Compression.FindLargestIndex(quaternion);
 
             Assert.That(largest, Is.EqualTo(expected));
         }
-        static object[] GetLargestIndexTestCases()
+
+        static IEnumerable LargestIndexTestCases
         {
-            return new object[]
+            get
             {
                 // args = Quaternion quaternion, int expected
-                new object[] { new Quaternion(1, 0, 0, 0), 0 },
-                new object[] { new Quaternion(0, 1, 0, 0), 1 },
-                new object[] { new Quaternion(0, 0, 1, 0), 2 },
-                new object[] { new Quaternion(0, 0, 0, 1), 3 },
+                yield return new TestCaseData(new Quaternion(1, 0, 0, 0), 0);
+                yield return new TestCaseData(new Quaternion(0, 1, 0, 0), 1);
+                yield return new TestCaseData(new Quaternion(0, 0, 1, 0), 2);
+                yield return new TestCaseData(new Quaternion(0, 0, 0, 1), 3);
 
-                new object[] { new Quaternion(-1, 0, 0, 0), 0 },
-                new object[] { new Quaternion(0, -1, 0, 0), 1 },
-                new object[] { new Quaternion(0, 0, -1, 0), 2 },
-                new object[] { new Quaternion(0, 0, 0, -1), 3 },
+                yield return new TestCaseData(new Quaternion(-1, 0, 0, 0), 0);
+                yield return new TestCaseData(new Quaternion(0, -1, 0, 0), 1);
+                yield return new TestCaseData(new Quaternion(0, 0, -1, 0), 2);
+                yield return new TestCaseData(new Quaternion(0, 0, 0, -1), 3);
 
-                new object[] { new Quaternion(1, 0, 0.5f, 0).normalized, 0 },
-                new object[] { new Quaternion(0, 1, 0.5f, 0).normalized, 1 },
-                new object[] { new Quaternion(0, 0.5f, 1, 0).normalized, 2 },
-                new object[] { new Quaternion(0, 0.5f, 0, 1).normalized, 3 },
+                yield return new TestCaseData(new Quaternion(1, 0, 0.5f, 0).normalized, 0);
+                yield return new TestCaseData(new Quaternion(0, 1, 0.5f, 0).normalized, 1);
+                yield return new TestCaseData(new Quaternion(0, 0.5f, 1, 0).normalized, 2);
+                yield return new TestCaseData(new Quaternion(0, 0.5f, 0, 1).normalized, 3);
 
-                new object[] { new Quaternion(-1, 0.9f, 0.5f, 0).normalized, 0 },
-                new object[] { new Quaternion(0.9f, -1, 0.5f, 0).normalized, 1 },
-                new object[] { new Quaternion(0, 0.5f, -1, 0.9f).normalized, 2 },
-                new object[] { new Quaternion(0, 0.5f, 0.9f, -1).normalized, 3 },
-            };
+                yield return new TestCaseData(new Quaternion(-1, 0.9f, 0.5f, 0).normalized, 0);
+                yield return new TestCaseData(new Quaternion(0.9f, -1, 0.5f, 0).normalized, 1);
+                yield return new TestCaseData(new Quaternion(0, 0.5f, -1, 0.9f).normalized, 2);
+                yield return new TestCaseData(new Quaternion(0, 0.5f, 0.9f, -1).normalized, 3);
+            }
         }
-
     }
 }
