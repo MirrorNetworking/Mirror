a532ea7778ac203e9b3177b3b0825db716daadf6
uwee <uweenukr@gmail.com>
2020-10-29 10:57:44 -0400

    update NetworkMenu for new comps

--- Assets/Mirror/Editor/NetworkMenu.cs
+++ Assets/Mirror/Editor/NetworkMenu.cs
@@ -11,7 +11,7 @@ namespace Mirror
         [MenuItem("GameObject/Network/NetworkManager", priority = 7)]
         public static GameObject CreateNetworkManager()
         {
-            var go = new GameObject("NetworkManager", typeof(KcpTransport), typeof(NetworkSceneManager), typeof(NetworkClient), typeof(NetworkServer), typeof(NetworkManager), typeof(PlayerSpawner), typeof(NetworkManagerHud));
+            var go = new GameObject("NetworkManager", typeof(NetworkManager), typeof(NetworkServer), typeof(NetworkClient), typeof(NetworkSceneManager), typeof(ServerObjectManager), typeof(ClientObjectManager), typeof(PlayerSpawner), typeof(KcpTransport), typeof(LogSettings));
 
             KcpTransport transport = go.GetComponent<KcpTransport>();
             NetworkSceneManager nsm = go.GetComponent<NetworkSceneManager>();
@@ -22,14 +22,27 @@ namespace Mirror
             NetworkServer networkServer = go.GetComponent<NetworkServer>();
             networkServer.transport = transport;
 
+            ServerObjectManager serverObjectManager = go.GetComponent<ServerObjectManager>();
+            serverObjectManager.server = networkServer;
+            serverObjectManager.networkSceneManager = nsm;
+
+            ClientObjectManager clientObjectManager = go.GetComponent<ClientObjectManager>();
+            clientObjectManager.client = networkClient;
+            clientObjectManager.networkSceneManager = nsm;
+
             NetworkManager networkManager = go.GetComponent<NetworkManager>();
             networkManager.client = networkClient;
             networkManager.server = networkServer;
+            networkManager.serverObjectManager = serverObjectManager;
+            networkManager.clientObjectManager = clientObjectManager;
+            networkManager.sceneManager = nsm;
 
             PlayerSpawner playerSpawner = go.GetComponent<PlayerSpawner>();
             playerSpawner.client = networkClient;
             playerSpawner.server = networkServer;
             playerSpawner.sceneManager = nsm;
+            playerSpawner.serverObjectManager = serverObjectManager;
+            playerSpawner.clientObjectManager = clientObjectManager;
 
             nsm.client = networkClient;
             nsm.server = networkServer;
