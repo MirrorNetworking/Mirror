44460e40b50a44ac9b277b510832e0e63e513465
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2020-10-30 08:42:05 -0400

    Update Assets/Mirror/Components/NetworkTransformChild.cs
    Co-authored-by: vis2k <info@noobtuts.com>

--- Assets/Mirror/Components/NetworkTransformChild.cs
+++ Assets/Mirror/Components/NetworkTransformChild.cs
@@ -6,7 +6,7 @@ namespace Mirror
     /// A component to synchronize the position of child transforms of networked objects.
     /// <para>There must be a NetworkTransform on the root object of the hierarchy. There can be multiple NetworkTransformChild components on an object. This does not use physics for synchronization, it simply synchronizes the localPosition and localRotation of the child transform and lerps towards the recieved values.</para>
     /// </summary>
-    [AddComponentMenu("Network/NetworkTransformChildExperimentalExperimental")]
+    [AddComponentMenu("Network/NetworkTransformChild")]
     [HelpURL("https://mirror-networking.com/docs/Components/NetworkTransformChild.html")]
     public class NetworkTransformChild : NetworkTransformBase
     {
