0df5d8f9cd5cef080c7fb45cf69df2b0a458675a
Martin Bozek <mato.bozek@gmail.com>
2020-11-06 19:51:18 +0100

    Update NetworkPingDisplay.cs
    updated according the latest comment

--- Assets/Mirror/Components/NetworkPingDisplay.cs
+++ Assets/Mirror/Components/NetworkPingDisplay.cs
@@ -31,7 +31,8 @@ namespace Mirror
         {
             if (!showPing) { return; }
 
-            string text = string.Format("{0}ms", (int)(NetworkTime.rtt * 1000));
+            string format = "{0}ms";
+            string text = string.Format(format, (int)(NetworkTime.rtt * 1000));
 
             // leave here or create special method to update fontSize and textColor
             style.fontSize = fontSize;
