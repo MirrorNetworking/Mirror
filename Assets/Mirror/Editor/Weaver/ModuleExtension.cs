ed3007a8b3afa287e6681d5f3e74e7e92e3d3f47
Paul Pacheco <paul.pacheco@aa.com>
2020-11-06 15:40:01 -0600

    Module can import methods with linq expressions

---
+++ Assets/Mirror/Editor/Weaver/ModuleExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace Mirror.Weaver
+{
+    public static class ModuleExtension
+    {
+        public static MethodReference ImportReference(this ModuleDefinition module, Expression<Action> expression) => ImportReference(module, (LambdaExpression)expression);
+        public static MethodReference ImportReference<T>(this ModuleDefinition module, Expression<Action<T>> expression) => ImportReference(module, (LambdaExpression)expression);
+
+        public static MethodReference ImportReference(this ModuleDefinition module, LambdaExpression expression)
+        {
+            if (!(expression.Body is MethodCallExpression outermostExpression))
+            {
+                throw new ArgumentException("Invalid Expression. Expression should consist of a Method call only.");
+            }
+
+            MethodInfo methodInfo = outermostExpression.Method;
+
+            return module.ImportReference(methodInfo);
+        }
+    }
+}
