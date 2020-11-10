581dacaaa5e5324ff478e161df0b50542689e1a2
MrGadget1024 <chris@clevertech.net>
2020-11-10 18:26:26 -0500

    Updated PreprocessorDefine & ProjectSettings version to 27 This should have been done in PR #2400

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -34,7 +34,8 @@ namespace Mirror
                 "MIRROR_17_0_OR_NEWER",
                 "MIRROR_18_0_OR_NEWER",
                 "MIRROR_24_0_OR_NEWER",
-                "MIRROR_26_0_OR_NEWER"
+                "MIRROR_26_0_OR_NEWER",
+                "MIRROR_27_0_OR_NEWER"
             };
 
             // only touch PlayerSettings if we actually modified it.
