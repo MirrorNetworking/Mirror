e9286b8259e768532e3b6a7c13011fd10f70e513
MrGadget1024 <chris@clevertech.net>
2020-11-14 00:58:42 -0500

    Added server checks to Spawner

--- Assets/Mirror/Examples/Room/Scripts/Spawner.cs
+++ Assets/Mirror/Examples/Room/Scripts/Spawner.cs
@@ -3,19 +3,23 @@ using UnityEngine.Serialization;
 
 namespace Mirror.Examples.NetworkRoom
 {
-    public class Spawner: MonoBehaviour
+    internal class Spawner: MonoBehaviour
     {
         [FormerlySerializedAs("prizePrefab")]
         internal static GameObject rewardPrefab;
 
         internal static void InitialSpawn()
         {
+            if (!NetworkServer.active) return;
+
             for (int i = 0; i < 10; i++)
                 SpawnReward();
         }
 
         internal static void SpawnReward()
         {
+            if (!NetworkServer.active) return;
+
             Vector3 spawnPosition = new Vector3(Random.Range(-19, 20), 1, Random.Range(-19, 20));
             NetworkServer.Spawn(Instantiate(rewardPrefab, spawnPosition, Quaternion.identity));
         }
