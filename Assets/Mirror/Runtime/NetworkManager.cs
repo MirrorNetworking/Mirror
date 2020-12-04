1f97b86abd592959e21d5ea8d30b55ceafeb73da
James Frowen <jamesfrowendev@gmail.com>
2020-12-04 15:24:24 +0000

    Sorting usings
    Seems like this has come up a few times so here is a commit to sort this out.
    
    We shouldn't have to worry about the order of the using, we should just let plugins automatically sort them. They are sorted into alphabetical order expect for system becase we have `dotnet_sort_system_directives_first = true` enabled in `.editorconfig`

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using kcp2k;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
-using kcp2k;
 
 namespace Mirror
 {
