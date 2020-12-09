7e3eb9aa914ec59fcf68cc153a79b8575c5a3b1f
vis2k <info@noobtuts.com>
2020-12-09 14:29:29 +0800

    Update KcpTransport.cs

--- Assets/Mirror/Runtime/Transport/KCP/MirrorTransport/KcpTransport.cs
+++ Assets/Mirror/Runtime/Transport/KCP/MirrorTransport/KcpTransport.cs
@@ -41,7 +41,10 @@ namespace kcp2k
         void Awake()
         {
             // logging
-            Log.Info = debugLog ? Debug.Log : (_) => {};
+            if (debugLog)
+                Log.Info = Debug.Log;
+            else
+                Log.Info = _ => {};
             Log.Warning = Debug.LogWarning;
             Log.Error = Debug.LogError;
 
