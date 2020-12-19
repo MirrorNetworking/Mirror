80c9f3620561a1a159313358747fc87ce2ee80ea
MrGadget1024 <chris@clevertech.net>
2020-12-19 18:20:05 -0500

    fix: DisallowMultipleComponent on all transports

--- Assets/Mirror/Runtime/Transport/SimpleWebTransport/SimpleWebTransport.cs
+++ Assets/Mirror/Runtime/Transport/SimpleWebTransport/SimpleWebTransport.cs
@@ -6,6 +6,7 @@ using UnityEngine.Serialization;
 
 namespace Mirror.SimpleWeb
 {
+    [DisallowMultipleComponent]
     public class SimpleWebTransport : Transport
     {
         public const string NormalScheme = "ws";
