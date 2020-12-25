d22db330867a6bbd6c27663dc077333b2fb09589
James Frowen <jamesfrowendev@gmail.com>
2020-12-25 08:49:14 +0000

    Moving FindPath to editor scripts (#2512)
    * moving FindPath to editor scripts
    
    this allows this function to be used by other scripts
    
    * Update Assets/Mirror/Editor/EditorHelper.cs
    
    * removing extra using

---
+++ Assets/Mirror/Editor/EditorHelper.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mirror
+{
+    public static class EditorHelper
+    {
+        public static string FindPath<T>()
+        {
+            string typeName = typeof(T).Name;
+
+            string[] guidsFound = AssetDatabase.FindAssets($"t:Script " + typeName);
+            if (guidsFound.Length >= 1 && !string.IsNullOrEmpty(guidsFound[0]))
+            {
+                if (guidsFound.Length > 1)
+                {
+                    Debug.LogWarning($"Found more than one{typeName}");
+                }
+
+                string path = AssetDatabase.GUIDToAssetPath(guidsFound[0]);
+                return Path.GetDirectoryName(path);
+            }
+            else
+            {
+                Debug.LogError($"Could not find path of {typeName}");
+                return string.Empty;
+            }
+        }
+    }
+}
