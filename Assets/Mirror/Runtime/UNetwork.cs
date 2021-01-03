17e03d8b0a62d724c8f237d6ded0b7d67d883983
Matt Coburn <coburn@dandere.net>
2021-01-03 15:47:07 +1000

    Revert "perf: NetworkWriter/Reader Write/ReadBlittable<T> for 4-6x performance improvement! (#2441)"
    This reverts commit 1947f061add00b9a8b02a8778f5bd9bc2b7cd158 and applies some patches to bring it back up to date without the DOTSNET feature.

--- Assets/Mirror/Runtime/UNetwork.cs
+++ Assets/Mirror/Runtime/UNetwork.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace Mirror
@@ -30,5 +31,39 @@ namespace Mirror
     {
         public const int DefaultReliable = 0;
         public const int DefaultUnreliable = 1;
+    }
+
+    // -- helpers for float conversion without allocations --
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct UIntFloat
+    {
+        [FieldOffset(0)]
+        public float floatValue;
+
+        [FieldOffset(0)]
+        public uint intValue;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct UIntDouble
+    {
+        [FieldOffset(0)]
+        public double doubleValue;
+
+        [FieldOffset(0)]
+        public ulong longValue;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct UIntDecimal
+    {
+        [FieldOffset(0)]
+        public ulong longValue1;
+
+        [FieldOffset(8)]
+        public ulong longValue2;
+
+        [FieldOffset(0)]
+        public decimal decimalValue;
     }
 }
