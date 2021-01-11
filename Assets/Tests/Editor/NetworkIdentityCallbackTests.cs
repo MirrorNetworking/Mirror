ef598784a158714b13e31f274930bd93cdc0bdcd
MrGadget1024 <chris@clevertech.net>
2021-01-11 15:00:31 -0500

    style: use Guid.NewGuid()

--- Assets/Tests/Editor/NetworkIdentityCallbackTests.cs
+++ Assets/Tests/Editor/NetworkIdentityCallbackTests.cs
@@ -249,7 +249,7 @@ namespace Mirror.Tests
             identity.AssetId = guid1;
 
             // assign a guid
-            var guid2 = new Guid();
+            var guid2 = Guid.NewGuid();
             ArgumentException exception = Assert.Throws<ArgumentException>(() =>
             {
                 identity.AssetId = guid2;
@@ -264,7 +264,7 @@ namespace Mirror.Tests
         {
             Debug.Assert(identity.AssetId == Guid.Empty, "assetId needs to be empty at the start of this test");
             // assign a guid
-            var guid2 = new Guid();
+            var guid2 = Guid.NewGuid();
             // expect no errors
             identity.AssetId = guid2;
 
