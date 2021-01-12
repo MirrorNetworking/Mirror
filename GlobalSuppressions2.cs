46b867bab11b542c178c5ca54d3b059860a738ac
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2021-01-12 16:25:32 -0500

    style: updated / added suppressions (#530)
    Co-authored-by: MrGadget1024 <chris@clevertech.net>

---
+++ GlobalSuppressions2.cs
@@ -0,0 +1,20 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnAngularDragChanged(System.Single,System.Single)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnAngularVelocityChanged(UnityEngine.Vector3,UnityEngine.Vector3)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnIsKinematicChanged(System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnuDragChanged(System.Single,System.Single)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnUseGravityChanged(System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnVelocityChanged(UnityEngine.Vector3,UnityEngine.Vector3)")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkIdentityPerformance.NetworkIdentityServerUpdateIsDirty")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkIdentityPerformance.NetworkIdentityServerUpdateNotDirty")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkIdentityPerformanceWithMultipleBehaviour.ServerUpdateIsDirty")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkIdentityPerformanceWithMultipleBehaviour.ServerUpdateNotDirty")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkWriterPerformance.WriteGenericInt32")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkWriterPerformance.WriteInt32")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Tests.Performance.NetworkWriterPerformance.WritePackedInt32")]
