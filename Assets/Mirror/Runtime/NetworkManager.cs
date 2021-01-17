872556543c933bb3339cc29f7c7a1238aba6bdfe
MrGadget1024 <chris@clevertech.net>
2021-01-17 15:37:20 -0500

    fix: NetworkManager: changed `print` to `logger.Log`

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -686,14 +686,14 @@ namespace Mirror
             if (NetworkClient.isConnected)
             {
                 StopClient();
-                print("OnApplicationQuit: stopped client");
+                logger.Log("OnApplicationQuit: stopped client");
             }
 
             // stop server after stopping client (for proper host mode stopping)
             if (NetworkServer.active)
             {
                 StopServer();
-                print("OnApplicationQuit: stopped server");
+                logger.Log("OnApplicationQuit: stopped server");
             }
         }
 
