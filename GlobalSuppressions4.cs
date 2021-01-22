4d6aa32abc6232d842c1273d1ee7952e63993b68
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2021-01-21 23:07:22 -0500

    style: added to suppressions files (#543)
    Co-authored-by: MrGadget1024 <chris@clevertech.net>

---
+++ GlobalSuppressions4.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnVelocityChanged(UnityEngine.Vector3,UnityEngine.Vector3)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnAngularVelocityChanged(UnityEngine.Vector3,UnityEngine.Vector3)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnIsKinematicChanged(System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnUseGravityChanged(System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnuDragChanged(System.Single,System.Single)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Experimental.NetworkRigidbody.OnAngularDragChanged(System.Single,System.Single)")]
---
+++ GlobalSuppressions5.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Examples.Additive.RandomColor.SetColor(UnityEngine.Color32,UnityEngine.Color32)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Examples.Basic.Player.OnPlayerDataChanged(System.Int32,System.Int32)")]
+[assembly: SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Examples.MultipleAdditiveScenes.RandomColor.SetColor(UnityEngine.Color32,UnityEngine.Color32)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Examples.Basic.Player.OnPlayerDataChanged(System.Int32,System.Int32)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:Mirror.Examples.Additive.RandomColor.SetColor(UnityEngine.Color32,UnityEngine.Color32)")]
