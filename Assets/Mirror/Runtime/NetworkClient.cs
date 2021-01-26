24ec357a46893a9274706eb222f9514de6f9c167
vis2k <info@noobtuts.com>
2021-01-26 11:46:23 +0800

    add comment

--- Assets/Mirror/Runtime/NetworkClient.cs
+++ Assets/Mirror/Runtime/NetworkClient.cs
@@ -275,6 +275,7 @@ namespace Mirror
                 RegisterHandler<ObjectSpawnStartedMessage>((conn, msg) => { });
                 // host mode doesn't need spawning
                 RegisterHandler<ObjectSpawnFinishedMessage>((conn, msg) => { });
+                // host mode doesn't need state updates
                 RegisterHandler<UpdateVarsMessage>((conn, msg) => { });
             }
             else
