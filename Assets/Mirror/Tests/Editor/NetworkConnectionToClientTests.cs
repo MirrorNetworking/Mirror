f71a92b930a055b1b989a8514c148a447026a2f1
vis2k <info@noobtuts.com>
2021-01-27 12:58:29 +0800

    Test: Send_BatchesUntilUpdate

--- Assets/Mirror/Tests/Editor/NetworkConnectionToClientTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkConnectionToClientTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -6,6 +8,8 @@ namespace Mirror.Tests
     public class NetworkConnectionToClientTests
     {
         GameObject transportGO;
+        MemoryTransport transport;
+        List<byte[]> clientReceived = new List<byte[]>();
 
         [SetUp]
         public void SetUp()
@@ -14,7 +18,16 @@ namespace Mirror.Tests
             // it needs to be on a gameobject because client.connect enables it,
             // which throws a NRE if not on a gameobject
             transportGO = new GameObject();
-            Transport.activeTransport = transportGO.AddComponent<MemoryTransport>();
+            Transport.activeTransport = transport = transportGO.AddComponent<MemoryTransport>();
+            transport.OnClientDataReceived = (message, channelId) => {
+                byte[] array = new byte[message.Count];
+                Buffer.BlockCopy(message.Array, message.Offset, array, 0, message.Count);
+                clientReceived.Add(array);
+            };
+            transport.ServerStart();
+            transport.ClientConnect("localhost");
+            Assert.That(transport.ServerActive, Is.True);
+            Assert.That(transport.ClientConnected, Is.True);
         }
 
         [TearDown]
@@ -24,9 +37,21 @@ namespace Mirror.Tests
         }
 
         [Test]
-        public void Send_BatchInterval_0()
+        public void Send_BatchesUntilUpdate()
         {
+            // create connection and send
             NetworkConnectionToClient connection = new NetworkConnectionToClient(42, 0);
+            byte[] message = {0x01, 0x02};
+            connection.Send(new ArraySegment<byte>(message));
+
+            // Send() should only add to batch, not send anything yet
+            transport.LateUpdate();
+            Assert.That(clientReceived.Count, Is.EqualTo(0));
+
+            // updating the connection should now send
+            connection.Update();
+            transport.LateUpdate();
+            Assert.That(clientReceived.Count, Is.EqualTo(1));
         }
     }
 }
