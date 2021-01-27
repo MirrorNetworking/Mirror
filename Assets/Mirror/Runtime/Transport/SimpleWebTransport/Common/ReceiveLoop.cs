d870351afb55c5efe8d55989a98eca796b4fb797
James Frowen <jamesfrowendev@gmail.com>
2021-01-27 21:31:37 +0000

    fix(SimpleWebTransport): removing unimportant error
    ReadHelperException can be throw when connection disconnects, this is normal and should not be sent to mirror as an error.

--- Assets/Mirror/Runtime/Transport/SimpleWebTransport/Common/ReceiveLoop.cs
+++ Assets/Mirror/Runtime/Transport/SimpleWebTransport/Common/ReceiveLoop.cs
@@ -69,9 +69,8 @@ namespace Mirror.SimpleWeb
             catch (ObjectDisposedException e) { Log.InfoException(e); }
             catch (ReadHelperException e)
             {
-                // this could happen if client sends bad message
+                // log as info only
                 Log.InfoException(e);
-                queue.Enqueue(new Message(conn.connId, e));
             }
             catch (SocketException e)
             {
