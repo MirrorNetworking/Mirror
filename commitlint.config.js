c0637c3e704756a492d1e2b748cd418a560bac31
Paul Pacheco <paulpach@gmail.com>
2021-02-02 17:26:30 -0600

    style: follow standard conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,1 @@
-module.exports = { extends: ["@commitlint/config-conventional"], rules: { "scope-case": [2, "always", "upper-case"] } };
+module.exports = { extends: ["@commitlint/config-conventional"]};
