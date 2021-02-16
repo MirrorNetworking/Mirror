deaf73cc2a84b453dcd548799c4e8b9c96551471
vis2k <info@noobtuts.com>
2021-02-16 19:59:55 +0800

    remove unused imports

--- Assets/Mirror/Examples/Basic/Scripts/Player.cs
+++ Assets/Mirror/Examples/Basic/Scripts/Player.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror.Examples.Basic
--- Assets/Mirror/Examples/Room/Scripts/NetworkRoomPlayerExt.cs
+++ Assets/Mirror/Examples/Room/Scripts/NetworkRoomPlayerExt.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Mirror.Examples.NetworkRoom
 {
