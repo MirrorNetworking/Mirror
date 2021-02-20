15e77507f6fa69236d65927d47e0b0b5b1a71e38
MrGadget1024 <chris@clevertech.net>
2021-02-20 09:11:39 -0500

    NetworkRoomManager: Removed redundant code

--- Assets/Mirror/Examples/Room/Scripts/NetworkRoomManagerExt.cs
+++ Assets/Mirror/Examples/Room/Scripts/NetworkRoomManagerExt.cs
@@ -40,21 +40,11 @@ namespace Mirror.Examples.NetworkRoom
 
         public override void OnRoomStopClient()
         {
-            // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
-            // going to the offline scene to avoid collision with the one that lives there.
-            if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
-                SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
-
             base.OnRoomStopClient();
         }
 
         public override void OnRoomStopServer()
         {
-            // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
-            // going to the offline scene to avoid collision with the one that lives there.
-            if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
-                SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
-
             base.OnRoomStopServer();
         }
 
