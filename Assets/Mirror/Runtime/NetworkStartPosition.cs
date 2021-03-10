efc303ceb87d181d2399b0bdedee63f0d201d337
vis2k <info@noobtuts.com>
2021-03-10 16:09:14 +0800

    NetworkStartPosition cleanup

--- Assets/Mirror/Runtime/NetworkStartPosition.cs
+++ Assets/Mirror/Runtime/NetworkStartPosition.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 namespace Mirror
 {
-    /// <summary>
-    /// This component is used to make a gameObject a starting position for spawning player objects in multiplayer games.
-    /// <para>This object's transform will be automatically registered and unregistered with the NetworkManager as a starting position.</para>
-    /// </summary>
+    /// <summary>Start position for player spawning, automaticallyr egisters itself in the NetworkManager.</summary>
     [DisallowMultipleComponent]
     [AddComponentMenu("Network/NetworkStartPosition")]
     [HelpURL("https://mirror-networking.com/docs/Articles/Components/NetworkStartPosition.html")]
     public class NetworkStartPosition : MonoBehaviour
     {
-        public void Awake()
-        {
-            NetworkManager.RegisterStartPosition(transform);
-        }
-
-        public void OnDestroy()
-        {
-            NetworkManager.UnRegisterStartPosition(transform);
-        }
+        public void Awake() => NetworkManager.RegisterStartPosition(transform);
+        public void OnDestroy() => NetworkManager.UnRegisterStartPosition(transform);
     }
 }
