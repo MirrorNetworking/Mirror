27c6ec09aa09875f9e5b77392f671605140605f8
Mirzam <fanavi666@gmail.com>
2021-03-11 12:40:52 +0700

    use GetCurentPlayerLoop in unity 2019.3 or newer

--- Assets/Mirror/Runtime/NetworkLoop.cs
+++ Assets/Mirror/Runtime/NetworkLoop.cs
@@ -144,7 +144,12 @@ namespace Mirror
             // breaking other custom system's custom loops. Let's use Default
             // for now until we upgrade to 2019 so we have the same behaviour
             // at all times (instead of different loop behavior on 2018/2019)
-            PlayerLoopSystem playerLoop = PlayerLoop.GetDefaultPlayerLoop();
+            PlayerLoopSystem playerLoop =
+            #if UNITY_2019_3_OR_NEWER
+                            PlayerLoop.GetCurrentPlayerLoop();
+            #else
+                            PlayerLoop.GetDefaultPlayerLoop();
+            #endif
 
             // add NetworkEarlyUpdate to the end of EarlyUpdate so it runs after
             // any Unity initializations but before the first Update/FixedUpdate
