ce0d6c1eeee684d585ff448103b7e06b830a01ea
vis2k <info@noobtuts.com>
2021-03-15 10:54:45 +0800

    add comments

--- Assets/Mirror/Components/Experimental/Oumuamua/Oumuamua.cs
+++ Assets/Mirror/Components/Experimental/Oumuamua/Oumuamua.cs
@@ -1,3 +1,9 @@
+// ʻOumuamua's light curve, assuming little systematic error, presents its
+// motion as tumbling, rather than smoothly rotating, and moving sufficiently
+// fast relative to the Sun.
+//
+// A small number of astronomers suggested that ʻOumuamua could be a product of
+// alien technology, but evidence in support of this hypothesis is weak.
 using UnityEngine;
 
 namespace Mirror.Experimental
