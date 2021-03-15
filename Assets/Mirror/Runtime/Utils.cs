0713db8338c0bb73d8b0f5bba1a0311b11ea9d28
vis2k <info@noobtuts.com>
2021-03-15 19:26:18 +0800

    add comment

--- Assets/Mirror/Runtime/Utils.cs
+++ Assets/Mirror/Runtime/Utils.cs
@@ -29,6 +29,13 @@ namespace Mirror
         Current = 1
     }
 
+    // channels are const ints instead of an enum so people can add their own
+    // channels (can't extend an enum otherwise).
+    //
+    // note that Mirror is slowly moving towards quake style networking which
+    // will only require reliable for handshake, and unreliable for the rest.
+    // so eventually we can change this to an Enum and transports shouldn't
+    // add custom channels anymore.
     public static class Channels
     {
         public const int DefaultReliable = 0;
