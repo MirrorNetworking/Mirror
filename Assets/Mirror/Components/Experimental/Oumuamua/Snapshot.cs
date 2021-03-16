2115457c90f84275b6cb96d85d698cb6234e671d
vis2k <info@noobtuts.com>
2021-03-16 13:09:04 +0800

    snapshot struct

---
+++ Assets/Mirror/Components/Experimental/Oumuamua/Snapshot.cs
@@ -0,0 +1,19 @@
+// snapshot for snapshot interpolation
+// position, rotation, scale for compatibility for now.
+using UnityEngine;
+
+namespace Mirror.Experimental
+{
+    internal struct Snapshot
+    {
+        internal Vector3 position;
+        internal Quaternion rotation;
+        internal Vector3 scale;
+        internal Snapshot(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+    }
+}
