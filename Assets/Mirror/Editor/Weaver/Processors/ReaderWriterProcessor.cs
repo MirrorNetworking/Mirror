95a7f830c3868264ada6da82f564441726089b53
vis2k <info@noobtuts.com>
2021-03-26 19:34:37 +0800

    add log for debugging if needed

--- Assets/Mirror/Editor/Weaver/Processors/ReaderWriterProcessor.cs
+++ Assets/Mirror/Editor/Weaver/Processors/ReaderWriterProcessor.cs
@@ -164,6 +164,10 @@ namespace Mirror.Weaver
             // fill function body with reader/writer initializers
             ILProcessor worker = rwInitializer.Body.GetILProcessor();
 
+            // for debugging: add a log to see if initialized on load
+            //worker.Emit(OpCodes.Ldstr, $"[InitReadWriters] called!");
+            //worker.Emit(OpCodes.Call, WeaverTypes.logWarningReference);
+
             Writers.InitializeWriters(worker);
             Readers.InitializeReaders(worker);
 
