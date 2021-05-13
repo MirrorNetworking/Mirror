23cfcb7db7d2b05eb92bc1f7a54126fd6ef0f14d
vis2k <info@noobtuts.com>
2021-05-13 12:47:15 +0800

    Tests: LocalConnectionsTest ClientToServer

--- Assets/Mirror/Tests/Editor/LocalConnectionTest.cs
+++ Assets/Mirror/Tests/Editor/LocalConnectionTest.cs
@@ -48,36 +48,26 @@ namespace Mirror.Tests
             Assert.True(invoked, "handler should have been invoked");
         }
 
-        /*[Test]
+        [Test]
         public void ClientToServerTest()
         {
             Assert.That(connectionToServer.address, Is.EqualTo("localhost"));
 
-            MyMessage myMessage = new MyMessage()
-            {
-                id = 3,
-                name = "hello"
-            };
-
             bool invoked = false;
-
-            void handler(NetworkConnection conn, NetworkReader reader, int channelId)
+            void Handler(NetworkConnection conn, NetworkReader reader, int channelId)
             {
-                MyMessage received = msg.ReadMessage<MyMessage>();
-                Assert.That(received.id, Is.EqualTo(3));
-                Assert.That(received.name, Is.EqualTo("hello"));
                 invoked = true;
             }
 
-            Dictionary<int, NetworkMessageDelegate> handlers = new Dictionary<int, NetworkMessageDelegate>();
-            handlers.Add(MessagePacker.GetId<MyMessage>(), handler);
+            Dictionary<ushort, NetworkMessageDelegate> handlers = new Dictionary<ushort, NetworkMessageDelegate>();
+            handlers.Add(MessagePacking.GetId<TestMessage>(), Handler);
 
             connectionToServer.SetHandlers(handlers);
-            connectionToClient.Send(myMessage);
+            connectionToClient.Send(new TestMessage());
 
             connectionToServer.Update();
 
             Assert.True(invoked, "handler should have been invoked");
-        }*/
+        }
     }
 }
