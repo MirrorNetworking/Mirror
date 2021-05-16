f691d83ccdbd874265b54f5f0aab6628861b25b6
vis2k <info@noobtuts.com>
2021-05-16 18:35:17 +0800

    player

---
+++ Assets/Scenery/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
