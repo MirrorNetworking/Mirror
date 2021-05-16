fdf005a3592fc887f871ced90909dbc5977da0e9
vis2k <info@noobtuts.com>
2021-05-16 15:10:31 +0800

    test scene

---
+++ Assets/Scenery/NetMan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NetMan : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(5, 5, 100, 25), "Start Server"))
+        {
+        }
+
+        if (GUI.Button(new Rect(5, 30, 100, 25), "Start Client"))
+        {
+        }
+    }
+}
---
+++ Assets/Scenery/Tree.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.LogWarning($"Tree [{gameObject.scene.name}] OnTrigger: {col.name}[{col.gameObject.scene.name}]");
+    }
+}
