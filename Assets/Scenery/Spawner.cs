a99399705443eaeb9f9516677f8876c482181c9a
vis2k <info@noobtuts.com>
2021-05-16 18:38:49 +0800

    spawner

---
+++ Assets/Scenery/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 25), "Spawn"))
+        {
+            GameObject spawned = Instantiate(prefab);
+        }
+    }
+}
