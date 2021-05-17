9a92324342c2e14c5d1e1d75f90d5d533a510e59
vis2k <info@noobtuts.com>
2021-05-17 17:57:14 +0800

    breaking: perf: remove NetworkWriter.Length/SetLength/EnsureLength. Position is enough.

--- Assets/Mirror/Tests/Editor/SyncVarTestBase.cs
+++ Assets/Mirror/Tests/Editor/SyncVarTestBase.cs
@@ -60,7 +60,7 @@ namespace Mirror.Tests
         {
             NetworkWriter writer = new NetworkWriter();
             bool written = serverObject.OnSerialize(writer, initialState);
-            writeLength = writer.Length;
+            writeLength = writer.Position;
             data = writer.ToArraySegment();
 
             return written;
