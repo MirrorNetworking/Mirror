dc70cd33d757da1db910a0c5fd75ad2268ad1c08
vis2k <info@noobtuts.com>
2021-05-19 12:37:20 +0800

    Tests: CreateNetworked without NetworkBehaviour

--- Assets/Mirror/Tests/Common/TestUtils.cs
+++ Assets/Mirror/Tests/Common/TestUtils.cs
@@ -7,6 +7,16 @@ namespace Mirror.Tests
 {
     public static class TestUtils
     {
+        // create GameObject + NetworkIdentity
+        // add to tracker list if needed (useful for cleanups afterwards)
+        public static void CreateNetworked(out GameObject go, out NetworkIdentity identity, List<GameObject> tracker = null)
+            where T : NetworkBehaviour
+        {
+            go = new GameObject();
+            identity = go.AddComponent<NetworkIdentity>();
+            tracker?.Add(go);
+        }
+
         // create GameObject + NetworkIdentity + NetworkBehaviour<T>
         // add to tracker list if needed (useful for cleanups afterwards)
         public static void CreateNetworked<T>(out GameObject go, out NetworkIdentity identity, out T component, List<GameObject> tracker = null)
