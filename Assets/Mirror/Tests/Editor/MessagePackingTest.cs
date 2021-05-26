0a6cd15bbdc38453fdfc66b4b4489af0fc55cbf7
vis2k <info@noobtuts.com>
2021-05-26 18:14:55 +0800

    MessagePackingTest: don't rely on UpdateVarsMessage

--- Assets/Mirror/Tests/Editor/MessagePackingTest.cs
+++ Assets/Mirror/Tests/Editor/MessagePackingTest.cs
@@ -60,7 +60,7 @@ namespace Mirror.Tests
 
             Assert.Throws<FormatException>(() =>
             {
-                UpdateVarsMessage unpacked = UnpackFromByteArray<UpdateVarsMessage>(data);
+                ReadyMessage unpacked = UnpackFromByteArray<ReadyMessage>(data);
             });
         }
 
