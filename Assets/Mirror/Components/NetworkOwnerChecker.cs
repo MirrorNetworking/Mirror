49ae0fad122a3c93c2b48ffa7db1ccad5ab0a57d
MrGadget1024 <github@clevertech.net>
2021-05-29 15:17:54 -0400

    Removed unnecessary usings

--- Assets/Mirror/Components/NetworkOwnerChecker.cs
+++ Assets/Mirror/Components/NetworkOwnerChecker.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 namespace Mirror
--- Assets/Mirror/Components/NetworkSceneChecker.cs
+++ Assets/Mirror/Components/NetworkSceneChecker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
