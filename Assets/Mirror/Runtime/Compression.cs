ab4fe99a78bfb5ff43ee834fde52fd4530ed9593
MrGadget1024 <github@clevertech.net>
2021-05-29 21:12:10 -0400

    fix: Replace Quaternion Compression Replacement and Tests from Mirage Included some original tests and a couple tests of values from #2624
    Fixes #2624

--- Assets/Mirror/Runtime/Compression.cs
+++ Assets/Mirror/Runtime/Compression.cs
@@ -1,193 +1,174 @@
-// Quaternion compression from DOTSNET
-
+using System;
 using UnityEngine;
 
 namespace Mirror
 {
+    enum ComponentType : uint
+    {
+        X = 0,
+        Y = 1,
+        Z = 2,
+        W = 3
+    }
+
     /// <summary>Functions to Compress Quaternions and Floats</summary>
+    /// <remarks>
+    ///     Credit to this man for converting gaffer games c code to c#
+    ///     https://gist.github.com/fversnel/0497ad7ab3b81e0dc1dd
+    /// </remarks>
     public static class Compression
     {
-        // quaternion compression //////////////////////////////////////////////
-        // smallest three: https://gafferongames.com/post/snapshot_compression/
-        // compresses 16 bytes quaternion into 4 bytes
+        // note: 1.0f / sqrt(2)
+        private const float Maximum = +1.0f / 1.414214f;
 
-        // helper function to find largest absolute element
-        // returns the index of the largest one
-        public static int LargestAbsoluteComponentIndex(Vector4 value, out float largest, out Vector3 withoutLargest)
+        private const int BitsPerAxis = 10;
+        private const int LargestComponentShift = BitsPerAxis * 3;
+        private const int AShift = BitsPerAxis * 2;
+        private const int BShift = BitsPerAxis * 1;
+        private const int IntScale = (1 << (BitsPerAxis - 1)) - 1;
+        private const int IntMask = (1 << BitsPerAxis) - 1;
+
+        public static uint CompressQuaternion(Quaternion quaternion)
         {
-            // convert to abs
-            Vector4 abs = new Vector4(Mathf.Abs(value.x), Mathf.Abs(value.y), Mathf.Abs(value.z), Mathf.Abs(value.w));
+            float absX = Mathf.Abs(quaternion.x);
+            float absY = Mathf.Abs(quaternion.y);
+            float absZ = Mathf.Abs(quaternion.z);
+            float absW = Mathf.Abs(quaternion.w);
 
-            // set largest to first value (x)
-            largest = value.x;
-            withoutLargest = new Vector3(value.y, value.z, value.w);
-            int index = 0;
+            ComponentType largestComponent = ComponentType.X;
+            float largestAbs = absX;
+            float largest = quaternion.x;
 
-            // compare to the others, starting at second value
-            // performance for 100k calls
-            //   for-loop:       25ms
-            //   manual checks:  22ms
-            if (abs.y > largest)
+            if (absY > largestAbs)
             {
-                index = 1;
-                largest = abs.y;
-                withoutLargest = new Vector3(value.x, value.z, value.w);
+                largestAbs = absY;
+                largestComponent = ComponentType.Y;
+                largest = quaternion.y;
             }
-            if (abs.z > largest)
+            if (absZ > largestAbs)
             {
-                index = 2;
-                largest = abs.z;
-                withoutLargest = new Vector3(value.x, value.y, value.w);
+                largestAbs = absZ;
+                largestComponent = ComponentType.Z;
+                largest = quaternion.z;
             }
-            if (abs.w > largest)
+            if (absW > largestAbs)
             {
-                index = 3;
-                largest = abs.w;
-                withoutLargest = new Vector3(value.x, value.y, value.z);
-            }
-
-            return index;
-        }
-
-        // scale a float within min/max range to an ushort between min/max range
-        // note: can also use this for byte range from byte.MinValue to byte.MaxValue
-        public static ushort ScaleFloatToUShort(float value, float minValue, float maxValue, ushort minTarget, ushort maxTarget)
-        {
-            // note: C# ushort - ushort => int, hence so many casts
-            // max ushort - min ushort only fits into something bigger
-            int targetRange = maxTarget - minTarget;
-            float valueRange = maxValue - minValue;
-            float valueRelative = value - minValue;
-            return (ushort)(minTarget + (ushort)(valueRelative / valueRange * targetRange));
-        }
-
-        // scale an ushort within min/max range to a float between min/max range
-        // note: can also use this for byte range from byte.MinValue to byte.MaxValue
-        public static float ScaleUShortToFloat(ushort value, ushort minValue, ushort maxValue, float minTarget, float maxTarget)
-        {
-            // note: C# ushort - ushort => int, hence so many casts
-            float targetRange = maxTarget - minTarget;
-            ushort valueRange = (ushort)(maxValue - minValue);
-            ushort valueRelative = (ushort)(value - minValue);
-            return minTarget + (valueRelative / (float)valueRange * targetRange);
-        }
-
-        const float QuaternionMinRange = -0.707107f;
-        const float QuaternionMaxRange =  0.707107f;
-        const ushort TenBitsMax = 0x3FF;
-
-        // helper function to access 'nth' component of quaternion
-        static float QuaternionElement(Quaternion q, int element)
-        {
-            switch (element)
-            {
-                case 0: return q.x;
-                case 1: return q.y;
-                case 2: return q.z;
-                case 3: return q.w;
-                default: return 0;
-            }
-        }
-
-        // note: assumes normalized quaternions
-        public static uint CompressQuaternion(Quaternion q)
-        {
-            // note: assuming normalized quaternions is enough. no need to force
-            //       normalize here. we already normalize when decompressing.
-
-            // find the largest component index [0,3] + value
-            int largestIndex = LargestAbsoluteComponentIndex(new Vector4(q.x, q.y, q.z, q.w), out float _, out Vector3 withoutLargest);
-
-            // from here on, we work with the 3 components without largest!
-
-            // "You might think you need to send a sign bit for [largest] in
-            // case it is negative, but you don’t, because you can make
-            // [largest] always positive by negating the entire quaternion if
-            // [largest] is negative. in quaternion space (x,y,z,w) and
-            // (-x,-y,-z,-w) represent the same rotation."
-            if (QuaternionElement(q, largestIndex) < 0)
-                withoutLargest = -withoutLargest;
-
-            // put index & three floats into one integer.
-            // => index is 2 bits (4 values require 2 bits to store them)
-            // => the three floats are between [-0.707107,+0.707107] because:
-            //    "If v is the absolute value of the largest quaternion
-            //     component, the next largest possible component value occurs
-            //     when two components have the same absolute value and the
-            //     other two components are zero. The length of that quaternion
-            //     (v,v,0,0) is 1, therefore v^2 + v^2 = 1, 2v^2 = 1,
-            //     v = 1/sqrt(2). This means you can encode the smallest three
-            //     components in [-0.707107,+0.707107] instead of [-1,+1] giving
-            //     you more precision with the same number of bits."
-            // => the article recommends storing each float in 9 bits
-            // => our uint has 32 bits, so we might as well store in (32-2)/3=10
-            //    10 bits max value: 1023=0x3FF (use OSX calc to flip 10 bits)
-            ushort aScaled = ScaleFloatToUShort(withoutLargest.x, QuaternionMinRange, QuaternionMaxRange, 0, TenBitsMax);
-            ushort bScaled = ScaleFloatToUShort(withoutLargest.y, QuaternionMinRange, QuaternionMaxRange, 0, TenBitsMax);
-            ushort cScaled = ScaleFloatToUShort(withoutLargest.z, QuaternionMinRange, QuaternionMaxRange, 0, TenBitsMax);
-
-            // now we just need to pack them into one integer
-            // -> index is 2 bit and needs to be shifted to 31..32
-            // -> a is 10 bit and needs to be shifted 20..30
-            // -> b is 10 bit and needs to be shifted 10..20
-            // -> c is 10 bit and needs to be at      0..10
-            return (uint)(largestIndex << 30 | aScaled << 20 | bScaled << 10 | cScaled);
-        }
-
-        // Quaternion normalizeSAFE from ECS math.normalizesafe()
-        // => useful to produce valid quaternions even if client sends invalid
-        //    data
-        static Quaternion QuaternionNormalizeSafe(Quaternion value)
-        {
-            // The smallest positive normal number representable in a float.
-            const float FLT_MIN_NORMAL = 1.175494351e-38F;
-
-            Vector4 v = new Vector4(value.x, value.y, value.z, value.w);
-            float length = Vector4.Dot(v, v);
-            return length > FLT_MIN_NORMAL
-                   ? value.normalized
-                   : Quaternion.identity;
-        }
-
-        // note: gives normalized quaternions
-        public static Quaternion DecompressQuaternion(uint data)
-        {
-            // get cScaled which is at 0..10 and ignore the rest
-            ushort cScaled = (ushort)(data & TenBitsMax);
-
-            // get bScaled which is at 10..20 and ignore the rest
-            ushort bScaled = (ushort)((data >> 10) & TenBitsMax);
-
-            // get aScaled which is at 20..30 and ignore the rest
-            ushort aScaled = (ushort)((data >> 20) & TenBitsMax);
-
-            // get 2 bit largest index, which is at 31..32
-            int largestIndex = (int)(data >> 30);
-
-            // scale back to floats
-            float a = ScaleUShortToFloat(aScaled, 0, TenBitsMax, QuaternionMinRange, QuaternionMaxRange);
-            float b = ScaleUShortToFloat(bScaled, 0, TenBitsMax, QuaternionMinRange, QuaternionMaxRange);
-            float c = ScaleUShortToFloat(cScaled, 0, TenBitsMax, QuaternionMinRange, QuaternionMaxRange);
-
-            // calculate the omitted component based on a²+b²+c²+d²=1
-            float d = Mathf.Sqrt(1 - a*a - b*b - c*c);
-
-            // reconstruct based on largest index
-            Vector4 value;
-            switch (largestIndex)
-            {
-                case 0:  value = new Vector4(d, a, b, c); break;
-                case 1:  value = new Vector4(a, d, b, c); break;
-                case 2:  value = new Vector4(a, b, d, c); break;
-                default: value = new Vector4(a, b, c, d); break;
+                largestComponent = ComponentType.W;
+                largest = quaternion.w;
             }
 
-            // ECS Rotation only works with normalized quaternions.
-            // make sure that's always the case here to avoid ECS bugs where
-            // everything stops moving if the quaternion isn't normalized.
-            // => NormalizeSafe returns a normalized quaternion even if we pass
-            //    in NaN from deserializing invalid values!
-            return QuaternionNormalizeSafe(new Quaternion(value.x, value.y, value.z, value.w));
+            float a = 0;
+            float b = 0;
+            float c = 0;
+            switch (largestComponent)
+            {
+                case ComponentType.X:
+                    a = quaternion.y;
+                    b = quaternion.z;
+                    c = quaternion.w;
+                    break;
+                case ComponentType.Y:
+                    a = quaternion.x;
+                    b = quaternion.z;
+                    c = quaternion.w;
+                    break;
+                case ComponentType.Z:
+                    a = quaternion.x;
+                    b = quaternion.y;
+                    c = quaternion.w;
+                    break;
+                case ComponentType.W:
+                    a = quaternion.x;
+                    b = quaternion.y;
+                    c = quaternion.z;
+                    break;
+            }
+
+            if (largest < 0)
+            {
+                a = -a;
+                b = -b;
+                c = -c;
+            }
+
+            uint integerA = ScaleToUint(a);
+            uint integerB = ScaleToUint(b);
+            uint integerC = ScaleToUint(c);
+
+            return (((uint)largestComponent) << LargestComponentShift) | (integerA << AShift) | (integerB << BShift) | integerC;
+        }
+
+        internal static uint ScaleToUint(float v)
+        {
+            float normalized = v / Maximum;
+            return (uint)Mathf.RoundToInt(normalized * IntScale) & IntMask;
+        }
+
+        internal static float ScaleToFloat(uint v)
+        {
+            float unscaled = v * Maximum / IntScale;
+
+            if (unscaled > Maximum)
+                unscaled -= Maximum * 2;
+            return unscaled;
+        }
+
+        public static Quaternion DecompressQuaternion(uint compressed)
+        {
+            var largestComponentType = (ComponentType)(compressed >> LargestComponentShift);
+            uint integerA = (compressed >> AShift) & IntMask;
+            uint integerB = (compressed >> BShift) & IntMask;
+            uint integerC = compressed & IntMask;
+
+            float a = ScaleToFloat(integerA);
+            float b = ScaleToFloat(integerB);
+            float c = ScaleToFloat(integerC);
+
+            Quaternion rotation;
+            switch (largestComponentType)
+            {
+                case ComponentType.X:
+                    // (?) y z w
+                    rotation.y = a;
+                    rotation.z = b;
+                    rotation.w = c;
+                    rotation.x = Mathf.Sqrt(1 - rotation.y * rotation.y
+                                              - rotation.z * rotation.z
+                                              - rotation.w * rotation.w);
+                    break;
+                case ComponentType.Y:
+                    // x (?) z w
+                    rotation.x = a;
+                    rotation.z = b;
+                    rotation.w = c;
+                    rotation.y = Mathf.Sqrt(1 - rotation.x * rotation.x
+                                              - rotation.z * rotation.z
+                                              - rotation.w * rotation.w);
+                    break;
+                case ComponentType.Z:
+                    // x y (?) w
+                    rotation.x = a;
+                    rotation.y = b;
+                    rotation.w = c;
+                    rotation.z = Mathf.Sqrt(1 - rotation.x * rotation.x
+                                              - rotation.y * rotation.y
+                                              - rotation.w * rotation.w);
+                    break;
+                case ComponentType.W:
+                    // x y z (?)
+                    rotation.x = a;
+                    rotation.y = b;
+                    rotation.z = c;
+                    rotation.w = Mathf.Sqrt(1 - rotation.x * rotation.x
+                                              - rotation.y * rotation.y
+                                              - rotation.z * rotation.z);
+                    break;
+                default:
+                    // Should never happen!
+                    throw new ArgumentOutOfRangeException("Unknown rotation component type: " + largestComponentType);
+            }
+
+            return rotation;
         }
     }
 }
--- Assets/Mirror/Tests/Editor/CompressionTests.cs
+++ Assets/Mirror/Tests/Editor/CompressionTests.cs
@@ -6,55 +6,126 @@ namespace Mirror.Tests
     public class CompressionTests
     {
         [Test]
-        public void LargestAbsoluteComponentIndex()
+        [Repeat(1000)]
+        public void QuaternionCompression()
         {
-            // positive value & xyw smallest
-            Vector4 value = new Vector4(1, 3, 4, 2);
-            int index = Compression.LargestAbsoluteComponentIndex(value, out float largest, out Vector3 withoutLargest);
-            Assert.That(index, Is.EqualTo(2));
-            Assert.That(largest, Is.EqualTo(Mathf.Abs(value.z)));
-            Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.x, value.y, value.w)));
+            Quaternion expected = Random.rotation;
 
-            // negative value should use abs & xzw smallest
-            value = new Vector4(1, -5, 4, 0);
-            index = Compression.LargestAbsoluteComponentIndex(value, out largest, out withoutLargest);
-            Assert.That(index, Is.EqualTo(1));
-            Assert.That(largest, Is.EqualTo(Mathf.Abs(value.y)));
-            Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.x, value.z, value.w)));
+            uint compressed = Compression.CompressQuaternion(expected);
 
-            // positive value & yzw smallest
-            value = new Vector4(5, 2, 3, 4);
-            index = Compression.LargestAbsoluteComponentIndex(value, out largest, out withoutLargest);
-            Assert.That(index, Is.EqualTo(0));
-            Assert.That(largest, Is.EqualTo(Mathf.Abs(value.x)));
-            Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.y, value.z, value.w)));
+            Quaternion decompressed = Compression.DecompressQuaternion(compressed);
+
+            float angle = Quaternion.Angle(expected, decompressed);
+            if (angle > 1.2f)
+                Debug.LogWarning($"{expected}\t{angle}");
+
+            Assert.That(angle, Is.LessThan(1.3f));
+
+            // decompressed should be almost the same
+            Assert.That(Mathf.Abs(Quaternion.Dot(expected, decompressed)), Is.GreaterThan(1 - 0.001));
+        }
+
+        // User supplied data that gave bad results
+        // Quaternion.Euler(338.850037f,170.609955f,182.979996f)
+        // Quaternion.Euler(338.850006f,230.809998f,182.979996f)
+
+        [Test]
+        public void QuaternionCompression2()
+        {
+            Quaternion expected = Quaternion.Euler(338.850037f, 170.609955f, 182.979996f);
+
+            uint compressed = Compression.CompressQuaternion(expected);
+
+            Quaternion decompressed = Compression.DecompressQuaternion(compressed);
+
+            float angle = Quaternion.Angle(expected, decompressed);
+            Debug.Log($"{expected}\t{angle}");
+
+            Assert.That(angle, Is.LessThan(1f));
+
+            // decompressed should be almost the same
+            Assert.That(Mathf.Abs(Quaternion.Dot(expected, decompressed)), Is.GreaterThan(1 - 0.001));
         }
 
         [Test]
-        public void LargestAbsoluteComponentIndexBenchmark()
+        public void QuaternionCompression3()
         {
-            Vector4 value = new Vector4(1, 2, 3, 4);
-            for (int i = 0; i < 100000; ++i)
-                Compression.LargestAbsoluteComponentIndex(value, out float _, out Vector3 _);
+            Quaternion expected = Quaternion.Euler(338.850006f, 230.809998f, 182.979996f);
+
+            uint compressed = Compression.CompressQuaternion(expected);
+
+            Quaternion decompressed = Compression.DecompressQuaternion(compressed);
+
+            float angle = Quaternion.Angle(expected, decompressed);
+            Debug.Log($"{expected}\t{angle}");
+
+            Assert.That(angle, Is.LessThan(1f));
+
+            // decompressed should be almost the same
+            Assert.That(Mathf.Abs(Quaternion.Dot(expected, decompressed)), Is.GreaterThan(1 - 0.001));
         }
 
         [Test]
-        public void ScaleFloatToUShort()
+        public void Compress90Degrees()
         {
-            Assert.That(Compression.ScaleFloatToUShort(-1f, -1f, 1f, ushort.MinValue, ushort.MaxValue), Is.EqualTo(0));
-            Assert.That(Compression.ScaleFloatToUShort(0f, -1f, 1f, ushort.MinValue, ushort.MaxValue), Is.EqualTo(32767));
-            Assert.That(Compression.ScaleFloatToUShort(0.5f, -1f, 1f, ushort.MinValue, ushort.MaxValue), Is.EqualTo(49151));
-            Assert.That(Compression.ScaleFloatToUShort(1f, -1f, 1f, ushort.MinValue, ushort.MaxValue), Is.EqualTo(65535));
+            uint compressed = Compression.CompressQuaternion(Quaternion.Euler(0, 90, 0));
+
+            Quaternion decompressed = Compression.DecompressQuaternion(compressed);
+
+            Vector3 euler = decompressed.eulerAngles;
+            Assert.That(euler.y, Is.EqualTo(90).Within(0.005f));
         }
 
-        [Test]
-        public void ScaleUShortToFloat()
-        {
-            Assert.That(Compression.ScaleUShortToFloat(0, ushort.MinValue, ushort.MaxValue, -1, 1), Is.EqualTo(-1).Within(0.0001f));
-            Assert.That(Compression.ScaleUShortToFloat(32767, ushort.MinValue, ushort.MaxValue, -1, 1), Is.EqualTo(-0f).Within(0.0001f));
-            Assert.That(Compression.ScaleUShortToFloat(49151, ushort.MinValue, ushort.MaxValue, -1, 1), Is.EqualTo(0.5f).Within(0.0001f));
-            Assert.That(Compression.ScaleUShortToFloat(65535, ushort.MinValue, ushort.MaxValue, -1, 1), Is.EqualTo(1).Within(0.0001f));
-        }
+        //[Test]
+        //public void LargestAbsoluteComponentIndex()
+        //{
+        //    // positive value & xyw smallest
+        //    Vector4 value = new Vector4(1, 3, 4, 2);
+        //    int index = Compression.LargestAbsoluteComponentIndex(value, out float largest, out Vector3 withoutLargest);
+        //    Assert.That(index, Is.EqualTo(2));
+        //    Assert.That(largest, Is.EqualTo(Mathf.Abs(value.z)));
+        //    Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.x, value.y, value.w)));
+
+        //    // negative value should use abs & xzw smallest
+        //    value = new Vector4(1, -5, 4, 0);
+        //    index = Compression.LargestAbsoluteComponentIndex(value, out largest, out withoutLargest);
+        //    Assert.That(index, Is.EqualTo(1));
+        //    Assert.That(largest, Is.EqualTo(Mathf.Abs(value.y)));
+        //    Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.x, value.z, value.w)));
+
+        //    // positive value & yzw smallest
+        //    value = new Vector4(5, 2, 3, 4);
+        //    index = Compression.LargestAbsoluteComponentIndex(value, out largest, out withoutLargest);
+        //    Assert.That(index, Is.EqualTo(0));
+        //    Assert.That(largest, Is.EqualTo(Mathf.Abs(value.x)));
+        //    Assert.That(withoutLargest, Is.EqualTo(new Vector3(value.y, value.z, value.w)));
+        //}
+
+        //[Test]
+        //public void LargestAbsoluteComponentIndexBenchmark()
+        //{
+        //    Vector4 value = new Vector4(1, 2, 3, 4);
+        //    for (int i = 0; i < 100000; ++i)
+        //        Compression.LargestAbsoluteComponentIndex(value, out float _, out Vector3 _);
+        //}
+
+        //[Test]
+        //public void ScaleToUint()
+        //{
+        //    Assert.That(Compression.ScaleToUint(-1f), Is.EqualTo(0));
+        //    Assert.That(Compression.ScaleToUint(0f), Is.EqualTo(32767));
+        //    Assert.That(Compression.ScaleToUint(0.5f), Is.EqualTo(49151));
+        //    Assert.That(Compression.ScaleToUint(1f), Is.EqualTo(65535));
+        //}
+
+        //[Test]
+        //public void ScaleToFloat()
+        //{
+        //    Assert.That(Compression.ScaleToFloat(0), Is.EqualTo(-1).Within(0.0001f));
+        //    Assert.That(Compression.ScaleToFloat(32767), Is.EqualTo(-0f).Within(0.0001f));
+        //    Assert.That(Compression.ScaleToFloat(49151), Is.EqualTo(0.5f).Within(0.0001f));
+        //    Assert.That(Compression.ScaleToFloat(65535), Is.EqualTo(1).Within(0.0001f));
+        //}
 
         [Test]
         public void CompressAndDecompressQuaternion()
@@ -64,7 +135,7 @@ namespace Mirror.Tests
 
             // compress
             uint data = Compression.CompressQuaternion(value);
-            Assert.That(data, Is.EqualTo(0xA83E2F07));
+            Assert.That(data, Is.EqualTo(0x88463108));
 
             // decompress
             Quaternion decompressed = Compression.DecompressQuaternion(data);
@@ -74,37 +145,37 @@ namespace Mirror.Tests
             Assert.That(decompressed.w, Is.EqualTo(value.w).Within(0.005f));
         }
 
-        // someone mentioned issues with 90 degree euler becoming -90 degree
-        [Test]
-        public void CompressAndDecompressQuaternion_90DegreeEuler()
-        {
-            // we need a normalized value
-            Quaternion value = Quaternion.Euler(0, 90, 0).normalized;
+        //// someone mentioned issues with 90 degree euler becoming -90 degree
+        //[Test]
+        //public void CompressAndDecompressQuaternion_90DegreeEuler()
+        //{
+        //    // we need a normalized value
+        //    Quaternion value = Quaternion.Euler(0, 90, 0).normalized;
 
-            // compress
-            uint data = Compression.CompressQuaternion(value);
+        //    // compress
+        //    uint data = Compression.CompressQuaternion(value);
 
-            // decompress
-            Quaternion decompressed = Compression.DecompressQuaternion(data);
+        //    // decompress
+        //    Quaternion decompressed = Compression.DecompressQuaternion(data);
 
-            // compare them. Quaternion.Angle is easiest to get the angle
-            // between them. using .eulerAngles would give 0, 90, 360 which is
-            // hard to compare.
-            Debug.Log("euler=" + decompressed.eulerAngles);
-            float angle = Quaternion.Angle(value, decompressed);
-            // 1 degree tolerance
-            Assert.That(Mathf.Abs(angle), Is.LessThanOrEqualTo(1));
-        }
+        //    // compare them. Quaternion.Angle is easiest to get the angle
+        //    // between them. using .eulerAngles would give 0, 90, 360 which is
+        //    // hard to compare.
+        //    Debug.Log("euler=" + decompressed.eulerAngles);
+        //    float angle = Quaternion.Angle(value, decompressed);
+        //    // 1 degree tolerance
+        //    Assert.That(Mathf.Abs(angle), Is.LessThanOrEqualTo(1));
+        //}
 
-        // client sending invalid data should still produce valid quaternions to
-        // avoid any possible bugs on server
-        [Test]
-        public void DecompressQuaternionInvalidData()
-        {
-            // decompress
-            // 0xFFFFFFFF will decompress to (0.7, 0.7, 0.7, NaN)
-            Quaternion decompressed = Compression.DecompressQuaternion(0xFFFFFFFF);
-            Assert.That(decompressed, Is.EqualTo(Quaternion.identity));
-        }
+        //// client sending invalid data should still produce valid quaternions to
+        //// avoid any possible bugs on server
+        //[Test]
+        //public void DecompressQuaternionInvalidData()
+        //{
+        //    // decompress
+        //    // 0xFFFFFFFF will decompress to (0.7, 0.7, 0.7, NaN)
+        //    Quaternion decompressed = Compression.DecompressQuaternion(0xFFFFFFFF);
+        //    Assert.That(decompressed, Is.EqualTo(Quaternion.identity));
+        //}
     }
 }
