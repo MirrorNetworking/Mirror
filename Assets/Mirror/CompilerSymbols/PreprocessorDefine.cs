3eefed30fbf780c9b3736f6d76d6135b412efb3c
vis2k <info@noobtuts.com>
2021-06-07 17:41:35 +0800

    cherry picked preprocessor define from asset store release branch

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -30,7 +30,8 @@ namespace Mirror
                 "MIRROR_35_1_OR_NEWER",
                 "MIRROR_37_0_OR_NEWER",
                 "MIRROR_38_0_OR_NEWER",
-                "MIRROR_39_0_OR_NEWER"
+                "MIRROR_39_0_OR_NEWER",
+                "MIRROR_40_0_OR_NEWER"
             };
 
             // only touch PlayerSettings if we actually modified it.
