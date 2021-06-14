7bd65c2d9b678771908605d45c216903917b2496
vis2k <info@noobtuts.com>
2021-06-14 21:19:43 +0800

    syntax

--- Assets/Mirror/Tests/Common/FakeNetworkConnection.cs
+++ Assets/Mirror/Tests/Common/FakeNetworkConnection.cs
@@ -4,19 +4,9 @@ namespace Mirror.Tests
 {
     public class FakeNetworkConnection : NetworkConnectionToClient
     {
-        public FakeNetworkConnection() : base(1, false)
-        {
-        }
-
+        public FakeNetworkConnection() : base(1, false) {}
         public override string address => "Test";
-
-        public override void Disconnect()
-        {
-            // nothing
-        }
-
-        internal override void Send(ArraySegment<byte> segment, int channelId = 0)
-        {
-        }
+        public override void Disconnect() {}
+        internal override void Send(ArraySegment<byte> segment, int channelId = 0) {}
     }
 }
