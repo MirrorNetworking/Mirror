1b134570f3cea09ef210367df0bf0c1b03df6d5d
MrGadget1024 <github@clevertech.net>
2021-06-21 11:47:10 -0400

    fix: Don't call FinishLoadScene when customHandling FinishLoadScene will be called from `UpdateScene`  if `loadingSceneAsync != null && loadingSceneAsync.isDone` and it's the responsibility of the user in their custom handler to assign `loadingSceneAsync` to something to block FinishLoadScene until that completes.

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -800,11 +800,10 @@ namespace Mirror
             OnClientChangeScene(newSceneName, sceneOperation, customHandling);
 
             // scene handling will happen in overrides of OnClientChangeScene and/or OnClientSceneChanged
+            // Do not call FinishLoadScene here. Custom handler will assign loadingSceneAsync and we need
+            // to wait for that to finish. UpdateScene already checks for that to be not null and isDone.
             if (customHandling)
-            {
-                FinishLoadScene();
                 return;
-            }
 
             switch (sceneOperation)
             {
