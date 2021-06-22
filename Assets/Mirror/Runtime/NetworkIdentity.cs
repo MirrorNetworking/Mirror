b309f546ca18d5bed47913d1831d4bc2ce0fbad3
vis2k <info@noobtuts.com>
2021-06-22 19:06:09 +0800

    NetworkIdentity: #if UNITY_2021_2 support when importing SceneManagent to avoid AutoUpgrader modifying NetworkIdentity

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -9,6 +9,9 @@ using UnityEditor;
 #if UNITY_2018_3_OR_NEWER
 using UnityEditor.Experimental.SceneManagement;
 #endif
+#if UNITY_2021_2_OR_NEWER
+using UnityEditor.SceneManagement;
+#endif
 #endif
 
 namespace Mirror
