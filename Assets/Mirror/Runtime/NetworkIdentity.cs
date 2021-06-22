81854247c7e95bfeee33b454e59098556472d28c
MrGadget1024 <github@clevertech.net>
2021-06-22 12:18:33 -0400

    indention

--- Assets/Mirror/Runtime/NetworkIdentity.cs
+++ Assets/Mirror/Runtime/NetworkIdentity.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 #if UNITY_EDITOR
-using UnityEditor;
-#if UNITY_2021_2_OR_NEWER
-using UnityEditor.SceneManagement;
-#elif UNITY_2018_3_OR_NEWER
-using UnityEditor.Experimental.SceneManagement;
-#endif
+    using UnityEditor;
+
+    #if UNITY_2021_2_OR_NEWER
+        using UnityEditor.SceneManagement;
+    #elif UNITY_2018_3_OR_NEWER
+        using UnityEditor.Experimental.SceneManagement;
+    #endif
 #endif
 
 namespace Mirror
