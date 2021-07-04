c65aa790de34971849a5bf5662cd783c49b14fde
vis2k <info@noobtuts.com>
2021-07-04 10:23:26 +0800

    fix test

--- Assets/Mirror/Tests/Editor/SnapshotInterpolationTests.cs
+++ Assets/Mirror/Tests/Editor/SnapshotInterpolationTests.cs
@@ -508,10 +508,10 @@ namespace Mirror.Tests.NetworkTransform2k
             // -> compute will add 0.5 deltaTime
             // -> so we overshoot beyond the second one and move to the next
             //
-            // localTime is 4.5 + delta = 5. third snapshot localTime is at 3.
+            // localTime is 5. third snapshot localTime is at 3.
             // bufferTime is 2.
             // so third is exactly old enough and we should move there.
-            double localTime = 4.5;
+            double localTime = 5;
             double deltaTime = 0.5;
             double interpolationTime = 1;
             float bufferTime = 2;
