fecfcd3378d6e6132fd932ae6741ffae5f3e6cb6
vis2k <info@noobtuts.com>
2021-07-16 10:46:49 +0800

    fix: SceneInterestManagement OnCheckObserver uses identity.gameObject instead of own gameObject

--- Assets/Mirror/Components/InterestManagement/Scene/SceneInterestManagement.cs
+++ Assets/Mirror/Components/InterestManagement/Scene/SceneInterestManagement.cs
@@ -91,7 +91,7 @@ namespace Mirror
 
         public override bool OnCheckObserver(NetworkIdentity identity, NetworkConnection newObserver)
         {
-            return identity.gameObject.scene == gameObject.scene;
+            return identity.gameObject.scene == newObserver.identity.gameObject.scene;
         }
 
         public override void OnRebuildObservers(NetworkIdentity identity, HashSet<NetworkConnection> newObservers,
