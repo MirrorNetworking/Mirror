343d7c71f4b8067a9953d161ffca80d692b01221
vis2k <info@noobtuts.com>
2021-07-18 17:08:48 +0800

    oops

--- Assets/Mirror/Tests/Editor/DeltaCompression/FossilDelta/FossilDeltaCompressionTests.cs
+++ Assets/Mirror/Tests/Editor/DeltaCompression/FossilDelta/FossilDeltaCompressionTests.cs
@@ -12,8 +12,8 @@ namespace Mirror.Tests.DeltaCompression
         public override void ApplyPatch(NetworkWriter from, NetworkWriter delta, NetworkWriter result)
         {
             // TODO avoid .ToArray() copying for benchmark.
-            byte[] delta = Fossil.Delta.Apply(from.ToArray(), delta.ToArray());
-            result.WriteBytes(delta, 0, delta.Length);
+            byte[] patched = Fossil.Delta.Apply(from.ToArray(), delta.ToArray());
+            result.WriteBytes(patched, 0, patched.Length);
         }
     }
 }
