5040b206dd328c512853744febc7ca3d3033bcbf
vis2k <info@noobtuts.com>
2021-07-18 13:51:00 +0800

    works

--- Assets/Mirror/Tests/Editor/DeltaCompression/DeltaCompressionTests.cs
+++ Assets/Mirror/Tests/Editor/DeltaCompression/DeltaCompressionTests.cs
@@ -4,6 +4,62 @@ using UnityEngine;
 
 namespace Mirror.Tests
 {
+    public struct InventorySlot
+    {
+        public int itemId;
+        public int amount;
+    }
+
+    // test monster for compression.
+    // can't be in separate file because Unity complains about it being an Editor
+    // script because it's in the Editor folder.
+    public class CompressionMonster : NetworkBehaviour
+    {
+        // a variable length name field
+        [SyncVar] public string monsterName;
+
+        // a couple of fixed length fields
+        [SyncVar] public int health;
+        [SyncVar] public int mana;
+        [SyncVar] public int level;
+        [SyncVar] public Vector3 position;
+        [SyncVar] public Quaternion rotation;
+
+        // variable length inventory
+        SyncList<InventorySlot> inventory = new SyncList<InventorySlot>();
+
+        // a couple more fixed fields AFTER variable length inventory
+        // to make sure they are still delta compressed decently.
+        [SyncVar] public int strength;
+        [SyncVar] public int intelligence;
+        [SyncVar] public int damage;
+        [SyncVar] public int defense;
+
+        public void Initialize(
+            string monsterName,
+            int health, int mana, int level,
+            Vector3 position, Quaternion rotation,
+            List<InventorySlot> inventory,
+            int strength, int intelligence,
+            int damage, int defense)
+        {
+            this.monsterName = monsterName;
+            this.health = health;
+            this.mana = mana;
+            this.level = level;
+            this.position = position;
+            this.rotation = rotation;
+
+            foreach (InventorySlot slot in inventory)
+                this.inventory.Add(slot);
+
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.damage = damage;
+            this.defense = defense;
+        }
+    }
+
     public class DeltaCompressionTests
     {
         // two snapshots
@@ -60,6 +116,16 @@ namespace Mirror.Tests
                 5000,
                 2000
             );
+        }
+
+        // quick test to write the uncompressed component.
+        // to make sure mirror generates serialization etc.
+        [Test]
+        public void Uncompressed()
+        {
+            NetworkWriter writer = new NetworkWriter();
+            A.OnSerialize(writer, true);
+            Debug.Log($"A uncompressed size: {writer.Position} bytes");
         }
     }
 }
