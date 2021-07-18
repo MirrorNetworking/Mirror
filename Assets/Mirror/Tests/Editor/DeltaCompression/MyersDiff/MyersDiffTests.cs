7ac743bbf38048e6ec3c665b71fdec62d0606298
vis2k <info@noobtuts.com>
2021-07-18 16:51:39 +0800

    Modified & better logging

--- Assets/Mirror/Tests/Editor/DeltaCompression/MyersDiff/MyersDiffTests.cs
+++ Assets/Mirror/Tests/Editor/DeltaCompression/MyersDiff/MyersDiffTests.cs
@@ -1,7 +1,7 @@
 // original paper: http://www.xmailserver.org/diff2.pdf
 // used in diff, git!
-
 using System;
+using System.Collections.Generic;
 using MyersDiff;
 using NUnit.Framework;
 using UnityEngine;
@@ -10,6 +10,65 @@ namespace Mirror.Tests.DeltaCompression
 {
     public class MyersDiffTests : DeltaCompressionTests
     {
+        // like Diff.Item, but with the actual deleted/inserted values
+        public struct Modified
+        {
+            // Start Line number in Data A.
+            public int StartA;
+            // Start Line number in Data B.
+            public int StartB;
+
+            // Number of deletions in Data A.
+            public int deletedA;
+            // actual values inserted into Data B.
+            public List<int> insertedB;
+
+            // tostring for easier debugging / understanding
+            public override string ToString()
+            {
+                string s = "";
+
+                // deleted
+                if (deletedA > 0)
+                    s += $"deleted {deletedA} entries at StartA={StartA} StartB={StartB}; ";
+
+                // inserted
+                foreach (int value in insertedB)
+                    s += $"inserted '{value}' at StartA={StartA} StartB={StartB}; ";
+
+                return s;
+            }
+        }
+
+        // helper function to convert Diff.Item[] to an actual patch
+        public static List<Modified> MakePatch(int[] A, int[] B, Diff.Item[] diffs)
+        {
+            // an item has:
+            //   StartA
+            //   StartB
+            //   DeletedA
+            //   InsertedB
+            //
+            // to make an actual patch, we need to also included the insered values.
+            // (the deleted values are deleted. we don't need to include those.)
+            List<Modified> result = new List<Modified>();
+            foreach (Diff.Item item in diffs)
+            {
+                Modified modified = new Modified();
+                modified.StartA = item.StartA;
+                modified.StartB = item.StartB;
+                modified.deletedA = item.deletedA;
+                // add the inserted values
+                modified.insertedB = new List<int>();
+                for (int i = 0; i < item.insertedB; ++i)
+                {
+                    modified.insertedB.Add(A[item.StartA]);
+                }
+                result.Add(modified);
+            }
+            return result;
+        }
+
         public override void ComputeDelta(NetworkWriter from, NetworkWriter to, NetworkWriter result)
         {
             // algorithm needs int[] for now
@@ -28,7 +87,7 @@ namespace Mirror.Tests.DeltaCompression
 
         public override void ApplyPatch(NetworkWriter from, NetworkWriter patch, NetworkWriter result)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         // simple test for understanding
@@ -37,12 +96,18 @@ namespace Mirror.Tests.DeltaCompression
         {
             int[] A = {0, 1,       2, 3, 4};
             int[] B = {0, 1, 1, 1, 2,    4};
-            Diff.Item[] items = Diff.DiffInt(A, B);
-
             Debug.Log($"A={String.Join(", ", A)}");
             Debug.Log($"B={String.Join(", ", B)}");
+
+            // myers diff
+            Diff.Item[] items = Diff.DiffInt(A, B);
             foreach (Diff.Item item in items)
                 Debug.Log($"item: startA={item.StartA} startB={item.StartB} deleteA={item.deletedA} insertB={item.insertedB}");
+
+            // make patch
+            List<Modified> patch = MakePatch(A, B, items);
+            foreach (Modified modified in patch)
+                Debug.Log("modified: " + modified);
         }
     }
 }
