aa4bbf04b22f887b15d358622404e8f7e657dab0
vis2k <info@noobtuts.com>
2021-07-18 14:19:59 +0800

    FossilDelta dropin

---
+++ Assets/Mirror/Tests/Editor/DeltaCompression/FossilDelta/FossilDelta/Reader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+
+namespace Fossil
+{
+	public class Reader
+	{
+		static readonly int[] zValue = {
+			-1, -1, -1, -1, -1, -1, -1, -1,   -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1,   -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1,   -1, -1, -1, -1, -1, -1, -1, -1,
+			0,  1,  2,  3,  4,  5,  6,  7,    8,  9, -1, -1, -1, -1, -1, -1,
+			-1, 10, 11, 12, 13, 14, 15, 16,   17, 18, 19, 20, 21, 22, 23, 24,
+			25, 26, 27, 28, 29, 30, 31, 32,   33, 34, 35, -1, -1, -1, -1, 36,
+			-1, 37, 38, 39, 40, 41, 42, 43,   44, 45, 46, 47, 48, 49, 50, 51,
+			52, 53, 54, 55, 56, 57, 58, 59,   60, 61, 62, -1, -1, -1, 63, -1
+		};
+			
+		public byte[] a;
+		public uint pos;
+
+		public Reader (byte[] array)
+		{
+			this.a = array;
+			this.pos = 0;
+		}
+
+		public bool HaveBytes () 
+		{
+			return this.pos < this.a.Length;
+		}
+
+		public byte GetByte () 
+		{
+			byte b = this.a[this.pos];
+			this.pos++;
+			if (this.pos > this.a.Length) 
+				throw new IndexOutOfRangeException("out of bounds");
+			return b;
+		}
+
+		public char GetChar() 
+		{
+			//  return String.fromCharCode(this.getByte());
+			return (char) this.GetByte();
+		}
+
+		/**
+		 * Read bytes from *pz and convert them into a positive integer.  When
+		 * finished, leave *pz pointing to the first character past the end of
+		 * the integer.  The *pLen parameter holds the length of the string
+		 * in *pz and is decremented once for each character in the integer.
+		 */
+		public uint GetInt ()
+		{
+			uint v = 0;
+			int c;
+			while(this.HaveBytes() && (c = zValue[0x7f & this.GetByte()]) >= 0) {
+				v = (uint) ((((Int32) v) << 6) + c);
+			}
+			this.pos--;
+			return v;
+		}
+	}
+}
+
---
+++ Assets/Mirror/Tests/Editor/DeltaCompression/FossilDelta/FossilDelta/RollingHash.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Fossil
+{
+	public class RollingHash
+	{
+		private UInt16 a;
+		private UInt16 b;
+		private UInt16 i;
+		private byte[] z;
+		static int ii = 0;
+
+		public RollingHash ()
+		{
+			this.a = 0;
+			this.b = 0;
+			this.i = 0;
+			this.z = new byte[Delta.NHASH];
+		}
+
+		/**
+		 * Initialize the rolling hash using the first NHASH characters of z[]
+		 */
+		public void Init (byte[] z, int pos)
+		{
+			UInt16 a = 0, b = 0, i, x;
+			for(i = 0; i < Delta.NHASH; i++){
+				x = z[pos+i];
+				a = (UInt16) ((a + x) & 0xffff);
+				b = (UInt16) ((b + (Delta.NHASH-i)*x) & 0xffff);
+				this.z[i] = (byte) x;
+			}
+			this.a = (UInt16) (a & 0xffff);
+			this.b = (UInt16) (b & 0xffff);
+			this.i = 0;
+		}
+
+		/**
+		 * Advance the rolling hash by a single character "c"
+		 */
+		public void Next (byte c) {
+			UInt16 old = this.z[this.i];
+			this.z[this.i] = c;
+			this.i = (UInt16) ((this.i+1)&(Delta.NHASH-1));
+			this.a = (UInt16) (this.a - old + c);
+			this.b = (UInt16) (this.b - Delta.NHASH*old + this.a);
+		}
+
+
+		/**
+		 * Return a 32-bit hash value
+		 */
+		public UInt32 Value () {
+			RollingHash.ii ++;
+			return (UInt32) (((UInt32)(this.a & 0xffff)) | (((UInt32)(this.b & 0xffff)) << 16));
+		}
+
+		/*
+		 * Compute a hash on NHASH bytes.
+		 *
+		 * This routine is intended to be equivalent to:
+		 *    hash h;
+		 *    hash_init(&h, zInput);
+		 *    return hash_32bit(&h);
+		 */
+		public static UInt32 Once (byte[] z) {
+			UInt16 a, b, i;
+			a = b = z[0];
+			for(i=1; i<Delta.NHASH; i++){
+				a += z[i];
+				b += a;
+			}
+			return a | (((UInt32)b)<<16);
+		}
+			
+	}
+}
+
