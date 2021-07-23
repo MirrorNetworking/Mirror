ce9b39630ad0c91b73b2e11c9e214e511d8fbe66
vis2k <info@noobtuts.com>
2021-07-23 20:25:34 +0800

    cleaner

--- Assets/Mirror/Runtime/DeltaCompression/MyersDiffXPatching.cs
+++ Assets/Mirror/Runtime/DeltaCompression/MyersDiffXPatching.cs
@@ -37,46 +37,20 @@ namespace Mirror
             }
         }
 
+        // ApplyPatch 'from scratch' version.
+        // * starts with empty writer
+        // * for each change:
+        //   * copies everything until next change
+        //   * applies change (delete=skip, insert=insert)
+        // * copies remainder at the end
+        //
+        // harder to think about, but faster than the 'duplicate & apply' version
+        //
+        // Benchmark for 100k ApplyPatch calls:
+        //   duplicate A & apply:      395ms
+        //   reconstruct from scratch: 146ms (nonalloc!)
         public static void ApplyPatch(NetworkWriter A, NetworkReader delta, NetworkWriter result)
         {
-            // the challenge here is to reconstruct B := A + Delta
-            // AND do that without allocations.
-            //
-            // the easy solution is to duplicate A and apply all changes.
-            // that's too slow though. need RemoveRange/Insert/duplications etc.
-            //
-            // let's try to reconstruct from scratch, directly into the result.
-            //
-            // for reference, here is a simple example:
-            //   Delta(abc, aab) gives:
-            //     item: startA=1 startB=1 deletedA=0 insertedB=1
-            //     item: startA=2 startB=3 deletedA=1 insertedB=0
-            //
-            // applying a patch FORWARD, step by step:
-            //     B := A
-            //     B = abc
-            //     we insert 1 value from A[StartA] at B[StartB]:
-            //     B = aabc
-            //     we delete 1 value that was at A[StartA] in B[StartB]:
-            //     B = aab
-            //
-            // applying a patch FROM SCRATCH, step by step:
-            //     B = ""
-            //     first change is insert 1 value from A[StartA] at B[StartB]:
-            //       copy A until StartA first:
-            //         B = a
-            //       insert the value from A[StartA] now:
-            //         B = aa
-            //     second change is delete 1 value from A[StartA] at B[StartB]:
-            //       copy A until StartA first, from where we left of
-            //         B = aab
-            //       delete the value from A[StartA] now:
-            //         means simply skip them in A
-            //
-            // Benchmark for 100k ApplyPatch calls:
-            //   duplicate A & apply:      395ms
-            //   reconstruct from scratch: 146ms (nonalloc!)
-
             ArraySegment<byte> ASegment = A.ToArraySegment();
             int indexA = 0;
 
@@ -90,11 +64,10 @@ namespace Mirror
                 int deletedA = (int)Compression.DecompressVarInt(delta);
                 int insertedB = (int)Compression.DecompressVarInt(delta);
 
-                // we progressed through 'A' until 'IndexA'.
-                // copy everything until the next change at 'StartB'
-
-                // first of: copy everything from A[indexA] until this change
-                //           EXCLUDING this change/index
+                // we progressed through A until indexA already.
+                // now we have change at StartA.
+                // copy everything from indexA until before that change first.
+                // TODO safety. should be > 0 and within range etc.
                 int copy = StartA - indexA;
                 result.WriteBytes(ASegment.Array, ASegment.Offset + indexA, copy);
                 indexA += copy;
@@ -119,15 +92,19 @@ namespace Mirror
             // copy everything that's left.
             int remainder = ASegment.Count - indexA;
             result.WriteBytes(ASegment.Array, ASegment.Offset + indexA, remainder);
+        }
 
-
-
-            // FOR FUTURE REFERENCE, this is the 'DUPLICATE A & RECONSTRUCT'
-            // algorithm.
-            //
-            // DUPLICATE A needs to always send 'StartB', NOT 'StartA'.
-            // FROM SCRATCH always needs to send 'StartA', NOT 'StartB'.
-            /*
+        /*
+        // ApplyPatch 'duplicate & apply' version:
+        // * duplicates A into B
+        // * applies all 'delta' changes in order
+        //
+        // simple, but slow because:
+        // - we need to duplicate A into a List<byte>
+        // - we need to allocate that list
+        // - we need to Insert & Delete, which is slow in lists
+        public static void ApplyPatch(NetworkWriter A, NetworkReader delta, NetworkWriter result)
+        {
             List<byte> B = new List<byte>();
             ArraySegment<byte> ASegment = A.ToArraySegment();
             for (int i = 0; i < ASegment.Count; ++i)
@@ -163,7 +140,7 @@ namespace Mirror
             // put B into result writer (nonalloc)
             for (int i = 0; i < B.Count; ++i)
                 result.WriteByte(B[i]);
-            */
         }
+        */
     }
 }
