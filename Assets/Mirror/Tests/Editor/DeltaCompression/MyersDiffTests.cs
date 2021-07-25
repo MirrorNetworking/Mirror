d637ae6fb9d88b3e10fc0aeff40dc3e62fc1f4cc
vis2k <info@noobtuts.com>
2021-07-25 19:02:40 +0800

    simple nativecollections test

--- Assets/Mirror/Tests/Editor/DeltaCompression/MyersDiffTests.cs
+++ Assets/Mirror/Tests/Editor/DeltaCompression/MyersDiffTests.cs
@@ -22,6 +22,15 @@ namespace Mirror.Tests.DeltaCompression
 {
     public class MyersDiffTests : DeltaCompressionTests
     {
+        // helper function to asset all contents of 'Item'
+        public static void AssertItem(Item item, int StartA, int StartB, int deletedA, int insertedB)
+        {
+            Assert.That(item.StartA, Is.EqualTo(StartA));
+            Assert.That(item.StartB, Is.EqualTo(StartB));
+            Assert.That(item.deletedA, Is.EqualTo(deletedA));
+            Assert.That(item.insertedB, Is.EqualTo(insertedB));
+        }
+
         public override void ComputeDelta(NetworkWriter from, NetworkWriter to, NetworkWriter result)
         {
             // prepare caches
@@ -183,6 +192,45 @@ namespace Mirror.Tests.DeltaCompression
             // run 1k times
             for (int i = 0; i < 1000; ++i)
                 MyersDiffX.MyersDiffX.DiffNonAlloc(new ArraySegment<byte>(A), new ArraySegment<byte>(B), modifiedA, modifiedB, DownVector, UpVector, result);
+        }
+
+        // NativeArray test to prepare for Burst
+        [Test]
+        public void Diff_NativeCollections()
+        {
+            // prepare a big byte[]
+            NativeArray<byte> A = new NativeArray<byte>(3, Allocator.Persistent);
+            NativeArray<byte> B = new NativeArray<byte>(3, Allocator.Persistent);
+            // change one value in B
+            B[0] = 0xFF;
+
+            // prepare the caches for nonalloc
+            // allocate the lists.
+            // already with expected capacity to avoid resizing.
+            NativeList<Item> result = new NativeList<Item>(1000, Allocator.Persistent);
+            NativeList<byte> modifiedA = new NativeList<byte>(A.Length + 2, Allocator.Persistent);
+            NativeList<byte> modifiedB = new NativeList<byte>(B.Length + 2, Allocator.Persistent);
+
+            // need two vectors of size 2 * MAX + 2
+            int MAX = A.Length + B.Length + 1;
+            // vector for the (0,0) to (x,y) search
+            NativeList<int> DownVector = new NativeList<int>(2 * MAX + 2, Allocator.Persistent);
+            // vector for the (u,v) to (N,M) search
+            NativeList<int> UpVector = new NativeList<int>(2 * MAX + 2, Allocator.Persistent);
+
+            // diff
+            MyersDiffXBurst.DiffNonAlloc(A, B, modifiedA, modifiedB, DownVector, UpVector, result);
+            Assert.That(result.Length, Is.EqualTo(1));
+            AssertItem(result[0], 0, 0, 1, 1);
+
+            // cleanup
+            A.Dispose();
+            B.Dispose();
+            result.Dispose();
+            modifiedA.Dispose();
+            modifiedB.Dispose();
+            DownVector.Dispose();
+            UpVector.Dispose();
         }
 
         // NativeArray test to prepare for Burst
