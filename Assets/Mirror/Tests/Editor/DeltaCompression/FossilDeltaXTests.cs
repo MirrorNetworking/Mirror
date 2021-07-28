0b5e464031b57a06062da036873dbed9c3aa39d4
vis2k <info@noobtuts.com>
2021-07-28 15:48:04 +0800

    nonalloc

--- Assets/Mirror/Tests/Editor/DeltaCompression/FossilDeltaXTests.cs
+++ Assets/Mirror/Tests/Editor/DeltaCompression/FossilDeltaXTests.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace Mirror.Tests.DeltaCompression
 {
     public class FossilDeltaXTests : DeltaCompressionTests
@@ -15,6 +17,27 @@ namespace Mirror.Tests.DeltaCompression
             byte[] bytes = delta.ReadBytes(delta.Length);
             byte[] patched = FossilDeltaX.Delta.Apply(from.ToArray(), bytes);
             result.WriteBytes(patched, 0, patched.Length);
+        }
+
+        // overwrite Delta benchmark for nonalloc version
+        public override void ComputeDeltaBenchmark(NetworkWriter writerA, NetworkWriter writerB, int amount, NetworkWriter result)
+        {
+            ArraySegmentX<byte> ASegment = writerA.ToArraySegment();
+            ArraySegmentX<byte> BSegment = writerB.ToArraySegment();
+            MemoryStream stream = new MemoryStream();
+
+            for (int i = 0; i < amount; ++i)
+            {
+                // reset write each time. don't want to measure resizing.
+                result.Position = 0;
+                stream.Position = 0;
+
+                // nonalloc delta
+                FossilDeltaX.Delta.Create(ASegment, BSegment, stream);
+                // copy to result for fairness. need to do in Mirror later too.
+                ArraySegmentX<byte> streamSegment = new ArraySegmentX<byte>(stream.GetBuffer(), 0, (int)stream.Position);
+                result.WriteBytes(streamSegment.Array, streamSegment.Offset, streamSegment.Count);
+            }
         }
     }
 }
