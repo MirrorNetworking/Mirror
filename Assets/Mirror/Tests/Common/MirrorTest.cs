94f35014a9e1f88846fea3dbfb02ac7c9f728588
vis2k <info@noobtuts.com>
2021-08-09 14:04:58 +0800

    MirrorTest doesn't need to clear spawned anymore. NetworkClient/NetworkServer.Shutdown already do.

--- Assets/Mirror/Tests/Common/MirrorTest.cs
+++ Assets/Mirror/Tests/Common/MirrorTest.cs
@@ -41,8 +41,6 @@ namespace Mirror.Tests
                 if (go != null)
                     GameObject.DestroyImmediate(go);
 
-            NetworkIdentity.spawned.Clear();
-
             GameObject.DestroyImmediate(transport.gameObject);
             Transport.activeTransport = null;
         }
