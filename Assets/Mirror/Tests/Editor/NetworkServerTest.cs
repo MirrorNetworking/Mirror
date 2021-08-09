b340397ad08353d47fa202c337f776b7cb03bae3
vis2k <info@noobtuts.com>
2021-08-09 16:42:48 +0800

    NetworkServerTests: SendCommand test simplified

--- Assets/Mirror/Tests/Editor/NetworkServerTest.cs
+++ Assets/Mirror/Tests/Editor/NetworkServerTest.cs
@@ -762,17 +762,10 @@ namespace Mirror.Tests
             NetworkServer.Listen(1);
             ConnectClientBlockingAuthenticatedAndReady(out NetworkConnectionToClient connectionToClient);
 
-            // add an identity with two networkbehaviour components
-            CreateNetworked(out GameObject _, out NetworkIdentity identity, out CommandTestNetworkBehaviour comp0, out CommandTestNetworkBehaviour comp1);
-            identity.netId = 42;
+            // add an identity with two networkbehaviour components.
+            // spawned, otherwise command handler won't find it in .spawned.
+            CreateNetworkedAndSpawn(out GameObject _, out NetworkIdentity identity, out CommandTestNetworkBehaviour comp0, out CommandTestNetworkBehaviour comp1, connectionToClient);
             identity.isLocalPlayer = true;
-            // for authority check
-            identity.connectionToClient = connectionToClient;
-            connectionToClient.identity = identity;
-
-            // identity needs to be in spawned dict, otherwise command handler
-            // won't find it
-            NetworkIdentity.spawned[identity.netId] = identity;
 
             // call the command
             comp1.TestCommand();
