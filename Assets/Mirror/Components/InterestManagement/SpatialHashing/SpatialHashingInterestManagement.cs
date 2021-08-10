e35c60de4dfec3981c662ac17115399b8d1e663f
PrincessLunaOfficial <88183054+PrincessLunaOfficial@users.noreply.github.com>
2021-08-10 17:21:40 +0300

    Update SpatialHashingInterestManagement.cs

--- Assets/Mirror/Components/InterestManagement/SpatialHashing/SpatialHashingInterestManagement.cs
+++ Assets/Mirror/Components/InterestManagement/SpatialHashing/SpatialHashingInterestManagement.cs
@@ -40,6 +40,7 @@ namespace Mirror
             checkMethod == CheckMethod.XZ_FOR_3D
             ? Vector2Int.RoundToInt(new Vector2(position.x, position.z) / resolution)
             : Vector2Int.RoundToInt(new Vector2(position.x, position.y) / resolution);
+
         public override bool OnCheckObserver(NetworkIdentity identity, NetworkConnection newObserver)
         {
             // calculate projected positions
@@ -68,6 +69,9 @@ namespace Mirror
         {
             // only on server
             if (!NetworkServer.active) return;
+
+            // NOTE: unlike Scene/MatchInterestManagement, this rebuilds ALL
+            //       entities every INTERVAL. consider the other approach later.
 
             // IMPORTANT: refresh grid every update!
             // => newly spawned entities get observers assigned via
@@ -112,10 +116,10 @@ namespace Mirror
             // rebuild all spawned entities' observers every 'interval'
             // this will call OnRebuildObservers which then returns the
             // observers at grid[position] for each entity.
-            if (NetworkTime.time >= lastRebuildTime + rebuildInterval)
+            if (NetworkTime.localTime >= lastRebuildTime + rebuildInterval)
             {
                 RebuildAll();
-                lastRebuildTime = NetworkTime.time;
+                lastRebuildTime = NetworkTime.localTime;
             }
         }
 
