4925f1397af033b278ff57fba3105f8d2e3b774e
vis2k <info@noobtuts.com>
2021-08-10 19:19:14 +0800

    Tests: merge HostSetup into the only class that inherits from it

--- Assets/Mirror/Tests/Runtime/ClientSceneTests_LocalPlayer_AsHost.cs
+++ Assets/Mirror/Tests/Runtime/ClientSceneTests_LocalPlayer_AsHost.cs
@@ -5,8 +5,65 @@ using UnityEngine.TestTools;
 
 namespace Mirror.Tests.Runtime
 {
-    public class ClientSceneTest_LocalPlayer_AsHost : HostSetup
+    public class ClientSceneTest_LocalPlayer_AsHost : MirrorPlayModeTest
     {
+        protected GameObject networkManagerGo;
+        protected NetworkManager manager;
+
+        protected GameObject playerGO;
+        protected NetworkIdentity identity;
+
+        protected virtual bool AutoAddPlayer => true;
+
+        protected virtual void afterStartHost() {}
+        protected virtual void beforeStopHost() {}
+
+        [UnitySetUp]
+        public override IEnumerator UnitySetUp()
+        {
+            base.SetUp();
+
+            networkManagerGo = transport.gameObject;
+            manager = networkManagerGo.AddComponent<NetworkManager>();
+            Transport.activeTransport = transport;
+
+            // create a tracked prefab (not spawned)
+            CreateGameObject(out playerGO);
+            identity = playerGO.AddComponent<NetworkIdentity>();
+            identity.assetId = System.Guid.NewGuid();
+
+            manager.playerPrefab = playerGO;
+            manager.autoStartServerBuild = false;
+            manager.autoCreatePlayer = AutoAddPlayer;
+
+            if (Application.isBatchMode)
+            {
+                Application.targetFrameRate = 60;
+            }
+
+            yield return null;
+
+            manager.StartHost();
+
+            yield return null;
+
+            afterStartHost();
+        }
+
+        [UnityTearDown]
+        public override IEnumerator UnityTearDown()
+        {
+            beforeStopHost();
+
+            yield return null;
+
+            // needed for stophost
+            Transport.activeTransport = transport;
+            manager.StopHost();
+
+            base.TearDown();
+        }
+
         [UnityTest]
         public IEnumerator LocalPlayerIsSetToNullAfterNetworkDestroy()
         {
