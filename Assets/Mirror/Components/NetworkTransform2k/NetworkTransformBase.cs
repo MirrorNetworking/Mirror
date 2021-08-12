1c44d28cfe516267e41d00c67b8fc0c036422f6d
vis2k <info@noobtuts.com>
2021-08-12 12:25:41 +0800

    Update NetworkTransformBase.cs

--- Assets/Mirror/Components/NetworkTransform2k/NetworkTransformBase.cs
+++ Assets/Mirror/Components/NetworkTransform2k/NetworkTransformBase.cs
@@ -538,7 +538,8 @@ namespace Mirror
         protected virtual void OnDrawGizmos()
         {
             // This fires in edit mode but that spams NRE's so check isPlaying
-            if (!showGizmos || !Application.isPlaying) return;
+            if (!Application.isPlaying) return;
+            if (!showGizmos) return;
 
             if (isServer) DrawGizmos(serverBuffer);
             if (isClient) DrawGizmos(clientBuffer);
