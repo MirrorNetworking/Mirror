d64bc8c95956548dbb5df089b7aa788936acf4cd
vis2k <info@noobtuts.com>
2021-08-13 21:02:40 +0800

    ILPostProcessorHook empty

---
+++ Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/ILPostProcessorHook.cs
@@ -0,0 +1,28 @@
+// hook via ILPostProcessor from Unity 2020+
+#if UNITY_2020_1_OR_NEWER
+// Unity.CompilationPipeline reference is only resolved if assembly name is
+// Unity.*.CodeGen:
+// https://forum.unity.com/threads/how-does-unity-do-codegen-and-why-cant-i-do-it-myself.853867/#post-5646937
+using Unity.CompilationPipeline.Common.ILPostProcessing;
+
+namespace Mirror.Weaver
+{
+    public class ILPostProcessorHook : ILPostProcessor
+    {
+        public override ILPostProcessor GetInstance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool WillProcess(ICompiledAssembly compiledAssembly)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ILPostProcessResult Process(ICompiledAssembly compiledAssembly)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+#endif
