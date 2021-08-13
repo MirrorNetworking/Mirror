c3b8bd680887531c973601268e1dc49997a54e02
vis2k <info@noobtuts.com>
2021-08-13 22:13:02 +0800

    logging

--- Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/ILPostProcessorHook.cs
+++ Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/ILPostProcessorHook.cs
@@ -3,6 +3,8 @@
 // Unity.CompilationPipeline reference is only resolved if assembly name is
 // Unity.*.CodeGen:
 // https://forum.unity.com/threads/how-does-unity-do-codegen-and-why-cant-i-do-it-myself.853867/#post-5646937
+using System.Collections.Generic;
+using Unity.CompilationPipeline.Common.Diagnostics;
 using Unity.CompilationPipeline.Common.ILPostProcessing;
 // IMPORTANT: 'using UnityEngine' does not work in here.
 // Unity gives "(0,0): error System.Security.SecurityException: ECall methods must be packaged into a system module."
@@ -15,19 +17,38 @@ namespace Mirror.Weaver
         // from CompilationFinishedHook
         const string MirrorRuntimeAssemblyName = "Mirror";
 
+        // can't Debug.Log in here. need to add to this list.
+        public List<DiagnosticMessage> Logs = new List<DiagnosticMessage>();
+
+        public void Log(string message)
+        {
+            Logs.Add(new DiagnosticMessage
+            {
+                // TODO add file etc. for double click opening later?
+                DiagnosticType = DiagnosticType.Warning, // doesn't have .Log
+                File = null,
+                Line = 0,
+                Column = 0,
+                MessageData = message
+            });
+        }
+
         // ???
         public override ILPostProcessor GetInstance() => this;
 
         // TODO process Mirror, or anything that references Mirror
         public override bool WillProcess(ICompiledAssembly compiledAssembly)
         {
-            // TODO need logging to see what ICompiledAssembly references are
-            return false;
+            Log($"Considering {compiledAssembly.Name}");
+            return true;
         }
 
         public override ILPostProcessResult Process(ICompiledAssembly compiledAssembly)
         {
-            throw new System.NotImplementedException();
+            Log($"Processing {compiledAssembly.Name}");
+            byte[] peData = new byte[0];
+            byte[] pdbData = new byte[0];
+            return new ILPostProcessResult(new InMemoryAssembly(peData, pdbData), Logs);
         }
     }
 }
