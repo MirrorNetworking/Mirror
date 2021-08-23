c8b4e64c69d7d00debca3924c50a0f24cc5efa90
vis2k <info@noobtuts.com>
2021-08-23 19:24:30 +0800

    comments

--- Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/ILPostProcessorReflectionImporter.cs
+++ Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/ILPostProcessorReflectionImporter.cs
@@ -1,5 +1,9 @@
 // based on paul's resolver from
 // https://github.com/MirageNet/Mirage/commit/def64cd1db525398738f057b3d1eb1fe8afc540c?branch=def64cd1db525398738f057b3d1eb1fe8afc540c&diff=split
+//
+// ILPostProcessorAssemblyRESOLVER does not find the .dll file for:
+// "System.Private.CoreLib"
+// we need this custom reflection importer to fix that.
 using System.Linq;
 using System.Reflection;
 using Mono.CecilX;
