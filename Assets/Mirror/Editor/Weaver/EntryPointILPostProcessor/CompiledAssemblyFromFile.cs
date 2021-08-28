e481a3fba507a2f3b5f1f4ab49a05c697ea78705
vis2k <info@noobtuts.com>
2021-08-28 20:51:07 +0800

    CompiledAssemblyFromFile simplified

--- Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/CompiledAssemblyFromFile.cs
+++ Assets/Mirror/Editor/Weaver/EntryPointILPostProcessor/CompiledAssemblyFromFile.cs
@@ -13,30 +13,19 @@ namespace Mirror.Weaver
     public class CompiledAssemblyFromFile : ICompiledAssembly
     {
         readonly string assemblyPath;
-        InMemoryAssembly inMemoryAssembly;
 
         public string Name => Path.GetFileNameWithoutExtension(assemblyPath);
         public string[] References { get; set; }
         public string[] Defines { get; set; }
+        public InMemoryAssembly InMemoryAssembly { get; }
 
         public CompiledAssemblyFromFile(string assemblyPath)
         {
             this.assemblyPath = assemblyPath;
-        }
-
-        public InMemoryAssembly InMemoryAssembly
-        {
-            get
-            {
-                if (inMemoryAssembly == null)
-                {
-                    byte[] peData = File.ReadAllBytes(assemblyPath);
-                    string pdbFileName = Path.GetFileNameWithoutExtension(assemblyPath) + ".pdb";
-                    byte[] pdbData = File.ReadAllBytes(Path.Combine(Path.GetDirectoryName(assemblyPath), pdbFileName));
-                    inMemoryAssembly = new InMemoryAssembly(peData, pdbData);
-                }
-                return inMemoryAssembly;
-            }
+            byte[] peData = File.ReadAllBytes(assemblyPath);
+            string pdbFileName = Path.GetFileNameWithoutExtension(assemblyPath) + ".pdb";
+            byte[] pdbData = File.ReadAllBytes(Path.Combine(Path.GetDirectoryName(assemblyPath), pdbFileName));
+            InMemoryAssembly = new InMemoryAssembly(peData, pdbData);
         }
     }
 }
