5128c4f987184cf161c664409d01e174bdbc7311
vis2k <info@noobtuts.com>
2021-09-03 14:05:26 +0800

    MIRROR_46_0_OR_NEWER

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -35,7 +35,8 @@ namespace Mirror
                 "MIRROR_41_0_OR_NEWER",
                 "MIRROR_42_0_OR_NEWER",
                 "MIRROR_43_0_OR_NEWER",
-                "MIRROR_44_0_OR_NEWER"
+                "MIRROR_44_0_OR_NEWER",
+                "MIRROR_46_0_OR_NEWER"
             };
 
             // only touch PlayerSettings if we actually modified it.
