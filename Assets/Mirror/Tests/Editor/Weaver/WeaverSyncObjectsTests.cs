6ebcbd8311f7275ca68b5929854dfd8c314949d3
vis2k <info@noobtuts.com>
2021-09-16 21:39:39 +0800

    Tests: Weaver: SyncObjectsExactlyMax

--- Assets/Mirror/Tests/Editor/Weaver/WeaverSyncObjectsTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverSyncObjectsTests.cs
@@ -5,6 +5,12 @@ namespace Mirror.Weaver.Tests
     public class WeaverSyncObjectsTests : WeaverTestsBuildFromTestName
     {
         [Test]
+        public void SyncObjectsExactlyMax()
+        {
+            IsSuccess();
+        }
+
+        [Test]
         public void SyncObjectsMoreThanMax()
         {
             HasError("SyncObjectsMoreThanMax has > 64 SyncObjects (SyncLists etc). Consider refactoring your class into multiple components",
---
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverSyncObjectsTests~/SyncObjectsExactlyMax.cs
@@ -0,0 +1,80 @@
+using Mirror;
+
+namespace WeaverSyncObjectsTest.SyncObjectsExactlyMax
+{
+    // SyncObjects mask is 64 bit. exactly 64 SyncObjects needs to work.
+    public class SyncObjectsExactlyMax : NetworkBehaviour
+    {
+        // from 1..64
+        public SyncList<int> list1 = new SyncList<int>();
+        public SyncList<int> list2 = new SyncList<int>();
+        public SyncList<int> list3 = new SyncList<int>();
+        public SyncList<int> list4 = new SyncList<int>();
+        public SyncList<int> list5 = new SyncList<int>();
+        public SyncList<int> list6 = new SyncList<int>();
+        public SyncList<int> list7 = new SyncList<int>();
+        public SyncList<int> list8 = new SyncList<int>();
+        public SyncList<int> list9 = new SyncList<int>();
+
+        public SyncList<int> list10 = new SyncList<int>();
+        public SyncList<int> list11 = new SyncList<int>();
+        public SyncList<int> list12 = new SyncList<int>();
+        public SyncList<int> list13 = new SyncList<int>();
+        public SyncList<int> list14 = new SyncList<int>();
+        public SyncList<int> list15 = new SyncList<int>();
+        public SyncList<int> list16 = new SyncList<int>();
+        public SyncList<int> list17 = new SyncList<int>();
+        public SyncList<int> list18 = new SyncList<int>();
+        public SyncList<int> list19 = new SyncList<int>();
+
+        public SyncList<int> list20 = new SyncList<int>();
+        public SyncList<int> list21 = new SyncList<int>();
+        public SyncList<int> list22 = new SyncList<int>();
+        public SyncList<int> list23 = new SyncList<int>();
+        public SyncList<int> list24 = new SyncList<int>();
+        public SyncList<int> list25 = new SyncList<int>();
+        public SyncList<int> list26 = new SyncList<int>();
+        public SyncList<int> list27 = new SyncList<int>();
+        public SyncList<int> list28 = new SyncList<int>();
+        public SyncList<int> list29 = new SyncList<int>();
+
+        public SyncList<int> list30 = new SyncList<int>();
+        public SyncList<int> list31 = new SyncList<int>();
+        public SyncList<int> list32 = new SyncList<int>();
+        public SyncList<int> list33 = new SyncList<int>();
+        public SyncList<int> list34 = new SyncList<int>();
+        public SyncList<int> list35 = new SyncList<int>();
+        public SyncList<int> list36 = new SyncList<int>();
+        public SyncList<int> list37 = new SyncList<int>();
+        public SyncList<int> list38 = new SyncList<int>();
+        public SyncList<int> list39 = new SyncList<int>();
+
+        public SyncList<int> list40 = new SyncList<int>();
+        public SyncList<int> list41 = new SyncList<int>();
+        public SyncList<int> list42 = new SyncList<int>();
+        public SyncList<int> list43 = new SyncList<int>();
+        public SyncList<int> list44 = new SyncList<int>();
+        public SyncList<int> list45 = new SyncList<int>();
+        public SyncList<int> list46 = new SyncList<int>();
+        public SyncList<int> list47 = new SyncList<int>();
+        public SyncList<int> list48 = new SyncList<int>();
+        public SyncList<int> list49 = new SyncList<int>();
+
+        public SyncList<int> list50 = new SyncList<int>();
+        public SyncList<int> list51 = new SyncList<int>();
+        public SyncList<int> list52 = new SyncList<int>();
+        public SyncList<int> list53 = new SyncList<int>();
+        public SyncList<int> list54 = new SyncList<int>();
+        public SyncList<int> list55 = new SyncList<int>();
+        public SyncList<int> list56 = new SyncList<int>();
+        public SyncList<int> list57 = new SyncList<int>();
+        public SyncList<int> list58 = new SyncList<int>();
+        public SyncList<int> list59 = new SyncList<int>();
+
+        public SyncList<int> list60 = new SyncList<int>();
+        public SyncList<int> list61 = new SyncList<int>();
+        public SyncList<int> list62 = new SyncList<int>();
+        public SyncList<int> list63 = new SyncList<int>();
+        public SyncList<int> list64 = new SyncList<int>();
+    }
+}
