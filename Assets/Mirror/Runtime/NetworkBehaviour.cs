79f492aaf86edbf19a86b1b6355cb46dc012da89
vis2k <info@noobtuts.com>
2021-09-17 12:48:55 +0800

    TODO

--- Assets/Mirror/Runtime/NetworkBehaviour.cs
+++ Assets/Mirror/Runtime/NetworkBehaviour.cs
@@ -118,6 +118,7 @@ namespace Mirror
         }
 
         // creates a 64 bit dirty mask for Sync Collections (aka SyncObjects)
+        // TODO 64 SyncLists are too much. consider smaller mask later.
         internal ulong DirtyObjectBits()
         {
             ulong dirtyObjects = 0;
