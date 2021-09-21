1ed5b47e5fdb4aec8a2fc72a8f525a9245cb1210
vis2k <info@noobtuts.com>
2021-09-21 15:37:41 +0800

    overwrite Value

--- Assets/Mirror/Runtime/SyncFieldNetworkIdentity.cs
+++ Assets/Mirror/Runtime/SyncFieldNetworkIdentity.cs
@@ -8,6 +8,31 @@ namespace Mirror
 {
     public class SyncFieldNetworkIdentity : SyncField<NetworkIdentity>
     {
+        // persistent netId field of the Value's NetworkIdentity
+        uint netId;
+
+        // overwrite .Value to get/set NetworkIdentity with our stored netId
+        public override NetworkIdentity Value
+        {
+            get
+            {
+                // ORIGINAL WEAVER:
+                //return GetSyncVarNetworkIdentity(netId, ref target);
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+                /* ORIGINAL WEAVER:
+                if (!SyncVarNetworkIdentityEqual(value, netId))
+                {
+                    NetworkIdentity networktarget = Networktarget;
+                    SetSyncVarNetworkIdentity(value, ref target, 1uL, ref netId);
+                }
+                */
+            }
+        }
+
         // ctor
         public SyncFieldNetworkIdentity(NetworkIdentity value, Action<NetworkIdentity, NetworkIdentity> hook = null)
             : base(value, hook) {}
