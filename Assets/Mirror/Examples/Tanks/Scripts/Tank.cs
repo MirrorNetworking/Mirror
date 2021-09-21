0636b077f853e633d32fb07b91a1be885a8481cc
vis2k <info@noobtuts.com>
2021-09-21 11:39:49 +0800

    Tank: not readonly for now, so it shows in inspector

--- Assets/Mirror/Examples/Tanks/Scripts/Tank.cs
+++ Assets/Mirror/Examples/Tanks/Scripts/Tank.cs
@@ -19,7 +19,8 @@ namespace Mirror.Examples.Tanks
         public Transform projectileMount;
 
         [Header("Stats")]
-        public readonly SyncField<int> health = 4;
+        public SyncField<int> health = 4;
+        public readonly SyncField<int> mana = 4; // TODO doesn't show in Inspector yet
 
         void Update()
         {
