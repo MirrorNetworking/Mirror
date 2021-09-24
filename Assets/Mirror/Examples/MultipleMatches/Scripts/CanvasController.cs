23f1b2b0a5ca949572a012b76501b6c8ab485d6b
MrGadget1024 <github@clevertech.net>
2021-09-24 07:21:02 -0400

    Remove unnecesary pragmas

--- Assets/Mirror/Examples/MultipleMatches/Scripts/CanvasController.cs
+++ Assets/Mirror/Examples/MultipleMatches/Scripts/CanvasController.cs
@@ -485,9 +485,7 @@ namespace Mirror.Examples.MultipleMatch
             if (playerMatches.TryGetValue(conn, out matchId))
             {
                 GameObject matchControllerObject = Instantiate(matchControllerPrefab);
-#pragma warning disable 618
                 matchControllerObject.GetComponent<NetworkMatch>().matchId = matchId;
-#pragma warning restore 618
                 NetworkServer.Spawn(matchControllerObject);
 
                 MatchController matchController = matchControllerObject.GetComponent<MatchController>();
@@ -497,9 +495,7 @@ namespace Mirror.Examples.MultipleMatch
                     playerConn.Send(new ClientMatchMessage { clientMatchOperation = ClientMatchOperation.Started });
 
                     GameObject player = Instantiate(NetworkManager.singleton.playerPrefab);
-#pragma warning disable 618
                     player.GetComponent<NetworkMatch>().matchId = matchId;
-#pragma warning restore 618
                     NetworkServer.AddPlayerForConnection(playerConn, player);
 
                     if (matchController.player1 == null)
