b02183f865a51e2fbabb73abad998df910572778
MrGadget1024 <github@clevertech.net>
2021-10-28 15:21:41 -0400

    feat: Team Interest Management

---
+++ Assets/Mirror/Components/InterestManagement/Team/NetworkTeam.cs
@@ -0,0 +1,15 @@
+﻿// simple component that holds team information
+using System;
+using UnityEngine;
+
+namespace Mirror
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Network/NetworkTeam")]
+    [HelpURL("https://mirror-networking.gitbook.io/docs/guides/interest-management")]
+    public class NetworkTeam : NetworkBehaviour
+    {
+        ///<summary>Set this to the same value on all networked objects that belong to a given team</summary>
+        public string teamId = string.Empty;
+    }
+}
