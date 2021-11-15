a3637ad7a55659a3fd729b072e87d01a2a8f7900
MrGadget1024 <github@clevertech.net>
2021-11-15 12:50:23 -0500

    feat: Added Additive Levels example Demonstrates Additive scenes with custom transition, portals, respawning players

---
+++ Assets/Mirror/Examples/AdditiveLevels/Scripts/FadeInOut.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mirror.Examples.AdditiveLevels
+{
+    public class FadeInOut : MonoBehaviour
+    {
+        // set these in the inspector
+        [Range(1, 100), Tooltip("Speed of fade in / out: lower is slower")]
+        public byte speed = 1;
+
+        [Tooltip("Reference to Image component on child panel")]
+        public Image fadeImage;
+
+        [Tooltip("Color to use during scene transition")]
+        public Color fadeColor = Color.black;
+
+        WaitForSeconds waitForSeconds;
+
+        void Awake()
+        {
+            waitForSeconds = new WaitForSeconds(speed * 0.01f);
+        }
+
+        public IEnumerator FadeIn()
+        {
+            //Debug.Log($"FadeIn - fading image in");
+
+            float alpha = fadeImage.color.a;
+
+            while (alpha < 1)
+            {
+                yield return waitForSeconds;
+                alpha += 0.01f;
+                fadeColor.a = alpha;
+                fadeImage.color = fadeColor;
+            }
+
+            //Debug.Log($"FadeIn - done fading");
+        }
+
+        public IEnumerator FadeOut()
+        {
+            //Debug.Log($"FadeOut - fading image out");
+
+            float alpha = fadeImage.color.a;
+
+            while (alpha > 0)
+            {
+                yield return waitForSeconds;
+                alpha -= 0.01f;
+                fadeColor.a = alpha;
+                fadeImage.color = fadeColor;
+            }
+
+            //Debug.Log($"FadeOut - done fading");
+        }
+    }
+}
