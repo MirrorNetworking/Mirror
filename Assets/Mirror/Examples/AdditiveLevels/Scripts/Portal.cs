749c4b7e75fbf0fd72864c64cbddc6d2d961a051
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2021-11-16 02:25:06 -0500

    feat: Added Additive Levels example (#2994)
    * feat: Added Additive Levels example
    Demonstrates Additive scenes with custom transition, portals, respawning players
    
    * Portal waitForSeconds calculated
    
    * added ReadMe

---
+++ Assets/Mirror/Examples/AdditiveLevels/Scripts/Portal.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Mirror.Examples.AdditiveLevels
+{
+    public class Portal : NetworkBehaviour
+    {
+        [Scene, Tooltip("Which scene to send player from here")]
+        public string destinationScene;
+
+        [Tooltip("Where to spawn player in Destination Scene")]
+        public Vector3 startPosition;
+
+        [Tooltip("Reference to child TMP label")]
+        public TMPro.TextMeshPro label;
+
+        WaitForSeconds waitForSeconds;
+
+        public override void OnStartServer()
+        {
+            // This is aproximately the fade time
+            waitForSeconds = new WaitForSeconds(((AdditiveLevelsNetworkManager)NetworkManager.singleton).fadeInOut.speed + 0.3f);
+        }
+
+        /// <summary>
+        /// Called on every NetworkBehaviour when it is activated on a client.
+        /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
+        /// </summary>
+        public override void OnStartClient()
+        {
+            label.text = SceneManager.GetSceneByPath(destinationScene).name;
+        }
+
+        // Note that I have created layers called Player(8) and Portal(9) and set them
+        // up in the Physics collision matrix so only Player collides with Portal.
+        void OnTriggerEnter(Collider other)
+        {
+            //Debug.Log($"Portal::OnTriggerEnter {gameObject.name} in {gameObject.scene.name}");
+
+            // tag check in case you didn't set up the layers and matrix as noted above
+            if (!other.CompareTag("Player")) return;
+
+            // applies to host client on server or remote client
+            if (other.TryGetComponent<PlayerController>(out PlayerController playerController))
+                playerController.enabled = false;
+
+            if (isServer)
+                StartCoroutine(SendPlayerToNewScene(other.gameObject));
+        }
+
+        [ServerCallback]
+        IEnumerator SendPlayerToNewScene(GameObject player)
+        {
+            if (player.TryGetComponent<NetworkIdentity>(out NetworkIdentity identity))
+            {
+                NetworkConnectionToClient conn = identity.connectionToClient;
+                if (conn == null) yield break;
+
+                // Tell client to unload previous subscene. No custom handling for this.
+                conn.Send(new SceneMessage { sceneName = gameObject.scene.path, sceneOperation = SceneOperation.UnloadAdditive, customHandling = true });
+
+                yield return waitForSeconds;
+
+                NetworkServer.RemovePlayerForConnection(conn, false);
+                yield return null;
+
+                // Move player to new subscene.
+                SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByPath(destinationScene));
+
+                // Tell client to load the new subscene with custom handling (see NetworkManager::OnClientChangeScene).
+                conn.Send(new SceneMessage { sceneName = destinationScene, sceneOperation = SceneOperation.LoadAdditive, customHandling = true });
+
+                // reposition player on server and client
+                player.transform.position = startPosition;
+                player.transform.LookAt(Vector3.up);
+
+                NetworkServer.AddPlayerForConnection(conn, player);
+
+                // host client would have been disabled by OnTriggerEnter above
+                PlayerController playerController = null;
+                if (NetworkClient.localPlayer != null && NetworkClient.localPlayer.TryGetComponent<PlayerController>(out playerController))
+                    playerController.enabled = true;
+            }
+        }
+    }
+}
