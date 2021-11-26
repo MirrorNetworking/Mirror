6a85da7667cd1d1625dc983fa105af4b6090a992
MrGadget1024 <github@clevertech.net>
2021-11-26 10:43:38 -0500

    Removed obsolete PersistNetworkManagerToOfflineScene - This was added exclusively for BigBox and they've sold to FB and are version locked now.

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -23,14 +23,6 @@ namespace Mirror
         [Tooltip("Should the Network Manager object be persisted through scene changes?")]
         public bool dontDestroyOnLoad = true;
 
-        // Deprecated 2021-03-10
-        // Temporary bool to allow Network Manager to persist to offline scene
-        // Based on Discord convo, BigBox is invoking StopHost in startup sequence, bouncing the server and clients back to offline scene, which resets Network Manager.
-        // Request is for a checkbox to persist Network Manager to offline scene, despite the collision and warning.
-        [Obsolete("This was added temporarily and will be removed in a future release.")]
-        [Tooltip("Should the Network Manager object be persisted through scene change to the offline scene?")]
-        public bool PersistNetworkManagerToOfflineScene;
-
      /// <summary>Multiplayer games should always run in the background so the network doesn't time out.</summary>
         [FormerlySerializedAs("m_RunInBackground")]
         [Tooltip("Multiplayer games should always run in the background so the network doesn't time out.")]
@@ -547,13 +539,11 @@ namespace Mirror
             // to avoid collision and let a fresh Network Manager be created.
             // IMPORTANT: .gameObject can be null if StopClient is called from
             //            OnApplicationQuit or from tests!
-#pragma warning disable 618
-            if (gameObject != null && !PersistNetworkManagerToOfflineScene &&
-                gameObject.scene.name == "DontDestroyOnLoad"
+            if (gameObject != null
+                && gameObject.scene.name == "DontDestroyOnLoad"
                 && !string.IsNullOrEmpty(offlineScene)
                 && SceneManager.GetActiveScene().path != offlineScene)
-                SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
-#pragma warning restore 618
+                    SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
 
             OnStopServer();
 
@@ -590,13 +580,11 @@ namespace Mirror
             // to avoid collision and let a fresh Network Manager be created.
             // IMPORTANT: .gameObject can be null if StopClient is called from
             //            OnApplicationQuit or from tests!
-#pragma warning disable 618
-            if (gameObject != null && !PersistNetworkManagerToOfflineScene &&
-                gameObject.scene.name == "DontDestroyOnLoad"
+            if (gameObject != null
+                && gameObject.scene.name == "DontDestroyOnLoad"
                 && !string.IsNullOrEmpty(offlineScene)
                 && SceneManager.GetActiveScene().path != offlineScene)
-                SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
-#pragma warning restore 618
+                    SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
 
             OnStopClient();
 
