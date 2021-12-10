c206bb02f987e680eace93ac6c350171a48510f5
MrGadget1024 <github@clevertech.net>
2021-12-10 11:37:08 -0500

    AdditiveLevels - simplified UI LookAt

--- Assets/Mirror/Examples/AdditiveLevels/Scripts/LookAtMainCamera.cs
+++ Assets/Mirror/Examples/AdditiveLevels/Scripts/LookAtMainCamera.cs
@@ -5,21 +5,10 @@ namespace Mirror.Examples.AdditiveLevels
     // This script is attached to portal labels to keep them facing the camera
     public class LookAtMainCamera : MonoBehaviour
     {
-        Transform mainCamTransform;
-
-        private void Awake()
-        {
-            // flip scale so it's not bass-ackwards
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-
+        // LateUpdate so that all camera updates are finished.
         void LateUpdate()
         {
-            if (mainCamTransform == null)
-                mainCamTransform = Camera.main.transform;
-
-            if (mainCamTransform != null)
-                transform.LookAt(mainCamTransform);
+            transform.forward = Camera.main.transform.forward;
         }
     }
 }
