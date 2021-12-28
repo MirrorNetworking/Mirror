502d0913d47ed14258feb5de4bf4af8b902b370d
MrGadget1024 <github@clevertech.net>
2021-12-28 15:56:18 -0500

    fix: fixed warnings

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -188,7 +188,7 @@ namespace Mirror
             // Don't allow collision-destroyed second instance to continue.
             if (!InitializeSingleton()) return;
 
-            Debug.LogWarning("Mirror | mirror-networking.com | discord.gg/N9QVxbM");
+            Debug.Log("Mirror | mirror-networking.com | discord.gg/N9QVxbM");
 
             // Set the networkSceneName to prevent a scene reload
             // if client connection to server fails.
