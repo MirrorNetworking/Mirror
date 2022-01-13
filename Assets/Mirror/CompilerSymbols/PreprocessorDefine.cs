3c26e928067d228a8d73b89ea6b6ec108a499c37
vis2k <info@noobtuts.com>
2022-01-13 12:20:02 +0800

    MIRROR_57_0_OR_NEWER

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -39,7 +39,8 @@ namespace Mirror
                 "MIRROR_46_0_OR_NEWER",
                 "MIRROR_47_0_OR_NEWER",
                 "MIRROR_53_0_OR_NEWER",
-                "MIRROR_55_0_OR_NEWER"
+                "MIRROR_55_0_OR_NEWER",
+                "MIRROR_57_0_OR_NEWER"
             };
 
             // only touch PlayerSettings if we actually modified it.
