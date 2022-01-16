b54d0869014a7783b41baa1c7413478220ae9b7e
vis2k <info@noobtuts.com>
2022-01-16 12:42:51 +0800

    perf: NetworkWriter/Reader Write/ReadBlittable<T> for 4-6x performance improvement! (based on #2441, #3036). This time with Android fix. (#3047)
    * NetworkWriter.WriteBlittable with Android support
    
    * NetworkReader.ReadBlittable with Android support
    
    * link issue
    
    * credits
    
    * don't need thos eanymore

--- Assets/Mirror/Runtime/Utils.cs
+++ Assets/Mirror/Runtime/Utils.cs
@@ -32,40 +32,6 @@ namespace Mirror
         public const int Unreliable = 1; // unordered
     }
 
-    // -- helpers for float conversion without allocations --
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct UIntFloat
-    {
-        [FieldOffset(0)]
-        public float floatValue;
-
-        [FieldOffset(0)]
-        public uint intValue;
-    }
-
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct UIntDouble
-    {
-        [FieldOffset(0)]
-        public double doubleValue;
-
-        [FieldOffset(0)]
-        public ulong longValue;
-    }
-
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct UIntDecimal
-    {
-        [FieldOffset(0)]
-        public ulong longValue1;
-
-        [FieldOffset(8)]
-        public ulong longValue2;
-
-        [FieldOffset(0)]
-        public decimal decimalValue;
-    }
-
     public static class Utils
     {
         public static uint GetTrueRandomUInt()
