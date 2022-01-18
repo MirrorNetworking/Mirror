c463865122547f1b5989bebb6fb654ef01084e56
vis2k <info@noobtuts.com>
2022-01-18 12:18:57 +0800

    breaking: obsolete old experimental NetworkTransform components

--- Assets/Mirror/Components/Experimental/NetworkTransform.cs
+++ Assets/Mirror/Components/Experimental/NetworkTransform.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Mirror.Experimental
 {
     [DisallowMultipleComponent]
     [AddComponentMenu("Network/Experimental/NetworkTransformExperimental")]
     [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-transform")]
+    [Obsolete("Use the default NetworkTransform instead, it has proper snapshot interpolation.")]
     public class NetworkTransform : NetworkTransformBase
     {
         protected override Transform targetTransform => transform;
--- Assets/Mirror/Components/Experimental/NetworkTransformChild.cs
+++ Assets/Mirror/Components/Experimental/NetworkTransformChild.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Mirror.Experimental
 {
@@ -8,6 +9,7 @@ namespace Mirror.Experimental
     /// </summary>
     [AddComponentMenu("Network/Experimental/NetworkTransformChildExperimentalExperimental")]
     [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-transform-child")]
+    [Obsolete("Use the default NetworkTransform instead, it has proper snapshot interpolation.")]
     public class NetworkTransformChild : NetworkTransformBase
     {
         [Header("Target")]
