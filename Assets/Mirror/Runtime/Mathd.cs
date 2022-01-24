f1967bca10ebbb9bc5d92c9e3cfb0278d02fba2c
vis2k <info@noobtuts.com>
2022-01-24 11:48:53 +0800

    perf: Mathd inlining

--- Assets/Mirror/Runtime/Mathd.cs
+++ Assets/Mirror/Runtime/Mathd.cs
@@ -1,13 +1,18 @@
 // 'double' precision variants for some of Unity's Mathf functions.
+
+using System.Runtime.CompilerServices;
+
 namespace Mirror
 {
     public static class Mathd
     {
         /// <summary>Linearly interpolates between a and b by t with no limit to t.</summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double LerpUnclamped(double a, double b, double t) =>
             a + (b - a) * t;
 
         /// <summary>Clamps value between 0 and 1 and returns value.</summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Clamp01(double value)
         {
             if (value < 0.0)
@@ -16,6 +21,7 @@ namespace Mirror
         }
 
         /// <summary>Calculates the linear parameter t that produces the interpolant value within the range [a, b].</summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double InverseLerp(double a, double b, double value) =>
             a != b ? Clamp01((value - a) / (b - a)) : 0;
     }
