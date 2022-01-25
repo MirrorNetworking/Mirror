afc8cab7baae66147ddd52d0a73c869fe493a42d
vis2k <info@noobtuts.com>
2022-01-25 11:58:53 +0800

    pragma

--- Assets/Mirror/Tests/Editor/Weaver/WeaverMonoBehaviourTests_IsSuccess/MonoBehaviourValid.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverMonoBehaviourTests_IsSuccess/MonoBehaviourValid.cs
@@ -4,6 +4,8 @@ namespace WeaverMonoBehaviourTests.MonoBehaviourValid
 {
     class MonoBehaviourValid : MonoBehaviour
     {
+#pragma warning disable CS0414
         int monkeys = 12;
+#pragma warning restore CS0414
     }
 }
