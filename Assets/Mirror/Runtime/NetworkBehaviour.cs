fa8de90495c7d52c4782f2f5dad32ca66e96656d
vis2k <info@noobtuts.com>
2022-01-25 15:14:35 +0800

    no ref

--- Assets/Mirror/Runtime/NetworkBehaviour.cs
+++ Assets/Mirror/Runtime/NetworkBehaviour.cs
@@ -352,7 +352,7 @@ namespace Mirror
         //           }
         //       }
         //   }
-        internal void WeaverSyncVarSetter<T>(ref T value, ref T field, ulong dirtyBit, Action<T, T> OnChanged = null)
+        internal void WeaverSyncVarSetter<T>(T value, ref T field, ulong dirtyBit, Action<T, T> OnChanged = null)
         {
             if (!SyncVarEqual(value, ref field))
             {
