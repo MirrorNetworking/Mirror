d26cd8d34a5ccba8df6fd561c9d3e3098f961bc1
vis2k <info@noobtuts.com>
2022-01-25 11:33:08 +0800

    CommandTests

--- Assets/Mirror/Tests/Editor/Weaver/WeaverCommandTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverCommandTests.cs
@@ -5,40 +5,10 @@ namespace Mirror.Weaver.Tests
     public class WeaverCommandTests : WeaverTestsBuildFromTestName
     {
         [Test]
-        public void CommandValid()
-        {
-            IsSuccess();
-        }
-
-        [Test]
         public void CommandCantBeStatic()
         {
             HasError("CmdCantBeStatic must not be static",
                 "System.Void WeaverCommandTests.CommandCantBeStatic.CommandCantBeStatic::CmdCantBeStatic()");
-        }
-
-        [Test]
-        public void CommandThatIgnoresAuthority()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void CommandWithArguments()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void CommandThatIgnoresAuthorityWithSenderConnection()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void CommandWithSenderConnectionAndOtherArgs()
-        {
-            IsSuccess();
         }
 
         [Test]
@@ -53,36 +23,6 @@ namespace Mirror.Weaver.Tests
         {
             HasError("CmdFunction has invalid parameter connection, Cannot pass NetworkConnections. Instead use 'NetworkConnectionToClient conn = null' to get the sender's connection on the server",
                 "System.Void WeaverCommandTests.ErrorForNetworkConnectionThatIsNotSenderConnection.ErrorForNetworkConnectionThatIsNotSenderConnection::CmdFunction(Mirror.NetworkConnection)");
-        }
-
-        [Test]
-        public void VirtualCommand()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void OverrideVirtualCommand()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void OverrideVirtualCallBaseCommand()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void OverrideVirtualCallsBaseCommandWithMultipleBaseClasses()
-        {
-            IsSuccess();
-        }
-
-        [Test]
-        public void OverrideVirtualCallsBaseCommandWithOverride()
-        {
-            IsSuccess();
         }
 
         [Test]
