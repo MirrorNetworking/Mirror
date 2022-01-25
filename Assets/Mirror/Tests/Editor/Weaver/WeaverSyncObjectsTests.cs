3fdc5eb5d71321786d1ec807b63ba483fd3e3b43
vis2k <info@noobtuts.com>
2022-01-25 12:05:46 +0800

    WeaverSyncObjectsTests

--- Assets/Mirror/Tests/Editor/Weaver/WeaverSyncObjectsTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverSyncObjectsTests.cs
@@ -5,12 +5,6 @@ namespace Mirror.Weaver.Tests
     public class WeaverSyncObjectsTests : WeaverTestsBuildFromTestName
     {
         [Test]
-        public void SyncObjectsExactlyMax()
-        {
-            IsSuccess();
-        }
-
-        [Test]
         public void SyncObjectsMoreThanMax()
         {
             HasError("SyncObjectsMoreThanMax has > 64 SyncObjects (SyncLists etc). Consider refactoring your class into multiple components",
