907dbef79eb85f98134b645df568542ce86a3f91
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2022-01-31 02:24:31 -0500

    Added Logging

--- Assets/Mirror/Authenticators/DeviceAuthenticator.cs
+++ Assets/Mirror/Authenticators/DeviceAuthenticator.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 namespace Mirror.Authenticators
 {
-    /// <summary>
-    /// An authenicator that identifies the user by their device.
-    /// <para>A GUID is used as a fallback when the platform doesn't support SystemInfo.deviceUniqueIdentifier.</para>
-    /// <para>Note: deviceUniqueIdentifier can be spoofed, so security is not guaranteed.</para>
-    /// <para>See https://docs.unity3d.com/ScriptReference/SystemInfo-deviceUniqueIdentifier.html for details.</para>
-    /// </summary>
     [AddComponentMenu("Network/ Authenticators/Device Authenticator")]
     [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-authenticators/device-authenticator")]
     public class DeviceAuthenticator : NetworkAuthenticator
@@ -24,40 +18,46 @@ namespace Mirror.Authenticators
 
         #endregion
 
+        #region Logging
+
+        void Log(string message, ConsoleColor consoleColor)
+        {
+#if UNITY_SERVER || UNITY_WEBGL
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+#else
+            Debug.Log(message);
+#endif
+        }
+
+        #endregion
+
         #region Server
 
-        /// <summary>
-        /// Called on server from StartServer to initialize the Authenticator
-        /// <para>Server message handlers should be registered in this method.</para>
-        /// </summary>
         public override void OnStartServer()
         {
+            Log($"DeviceAuthenticator::OnStartServer", ConsoleColor.DarkGreen);
             // register a handler for the authentication request we expect from client
             NetworkServer.RegisterHandler<AuthRequestMessage>(OnAuthRequestMessage, false);
         }
 
-        /// <summary>
-        /// Called on server from StopServer to reset the Authenticator
-        /// <para>Server message handlers should be registered in this method.</para>
-        /// </summary>
         public override void OnStopServer()
         {
+            Log($"DeviceAuthenticator::OnStopServer", ConsoleColor.DarkYellow);
             // unregister the handler for the authentication request
             NetworkServer.UnregisterHandler<AuthRequestMessage>();
         }
 
-        /// <summary>
-        /// Called on server from OnServerAuthenticateInternal when a client needs to authenticate
-        /// </summary>
-        /// <param name="conn">Connection to client.</param>
         public override void OnServerAuthenticate(NetworkConnection conn)
         {
+            Log($"DeviceAuthenticator::OnServerAuthenticate {conn}", ConsoleColor.DarkGreen);
             // do nothing, wait for client to send his id
         }
 
         void OnAuthRequestMessage(NetworkConnection conn, AuthRequestMessage msg)
         {
-            Debug.Log($"connection {conn.connectionId} authenticated with id {msg.clientDeviceID}");
+            Log($"DeviceAuthenticator::OnAuthRequestMessage connection {conn.connectionId} authenticated with id {msg.clientDeviceID}", ConsoleColor.Green);
 
             // Store the device id for later reference, e.g. when spawning the player
             conn.authenticationData = msg.clientDeviceID;
@@ -73,31 +73,23 @@ namespace Mirror.Authenticators
 
         #region Client
 
-        /// <summary>
-        /// Called on client from StartClient to initialize the Authenticator
-        /// <para>Client message handlers should be registered in this method.</para>
-        /// </summary>
         public override void OnStartClient()
         {
+            Log($"DeviceAuthenticator::OnStartClient", ConsoleColor.DarkGreen);
             // register a handler for the authentication response we expect from server
             NetworkClient.RegisterHandler<AuthResponseMessage>((Action<AuthResponseMessage>)OnAuthResponseMessage, false);
         }
 
-        /// <summary>
-        /// Called on client from StopClient to reset the Authenticator
-        /// <para>Client message handlers should be unregistered in this method.</para>
-        /// </summary>
         public override void OnStopClient()
         {
+            Log($"DeviceAuthenticator::OnStopClient", ConsoleColor.DarkYellow);
             // unregister the handler for the authentication response
             NetworkClient.UnregisterHandler<AuthResponseMessage>();
         }
 
-        /// <summary>
-        /// Called on client from OnClientAuthenticateInternal when a client needs to authenticate
-        /// </summary>
         public override void OnClientAuthenticate()
         {
+            Log($"DeviceAuthenticator::OnClientAuthenticate", ConsoleColor.DarkGreen);
             string deviceUniqueIdentifier = SystemInfo.deviceUniqueIdentifier;
 
             // Not all platforms support this, so we use a GUID instead
@@ -111,16 +103,12 @@ namespace Mirror.Authenticators
             }
 
             // send the deviceUniqueIdentifier to the server
-            NetworkClient.connection.Send(new AuthRequestMessage { clientDeviceID = deviceUniqueIdentifier } );
+            NetworkClient.connection.Send(new AuthRequestMessage { clientDeviceID = deviceUniqueIdentifier });
         }
 
-        /// <summary>
-        /// Called on client when the server's AuthResponseMessage arrives
-        /// </summary>
-        /// <param name="msg">The message payload</param>
         public void OnAuthResponseMessage(AuthResponseMessage msg)
         {
-            Debug.Log("Authentication Success");
+            Log($"DeviceAuthenticator::OnAuthResponseMessage Authentication Success", ConsoleColor.DarkGreen);
             ClientAccept();
         }
 
--- Assets/Mirror/Examples/Basic/Scripts/BasicNetManager.cs
+++ Assets/Mirror/Examples/Basic/Scripts/BasicNetManager.cs
@@ -1,35 +1,100 @@
+using System;
 using UnityEngine;
-
-/*
-	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
-	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
-*/
 
 namespace Mirror.Examples.Basic
 {
     [AddComponentMenu("")]
     public class BasicNetManager : NetworkManager
     {
-        /// <summary>
-        /// Called on the server when a client adds a new player with NetworkClient.AddPlayer.
-        /// <para>The default implementation for this function creates a new player object from the playerPrefab.</para>
-        /// </summary>
-        /// <param name="conn">Connection from client.</param>
+        #region Logging
+
+        void Log(string message, ConsoleColor consoleColor)
+        {
+#if UNITY_SERVER || UNITY_WEBGL
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+#else
+            Debug.Log(message);
+#endif
+        }
+
+        #endregion
+
+        #region Server
+
+        public override void OnStartServer()
+        {
+            Log($"BasicNetManager::OnStartServer", ConsoleColor.Green);
+            base.OnStartServer();
+        }
+
+        public override void OnServerConnect(NetworkConnection conn)
+        {
+            Log($"BasicNetManager::OnServerConnect {conn}", ConsoleColor.Green);
+            base.OnServerConnect(conn);
+        }
+
+        public override void OnServerReady(NetworkConnection conn)
+        {
+            Log($"BasicNetManager::OnServerReady {conn}", ConsoleColor.Green);
+            base.OnServerReady(conn);
+        }
+
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
+            Log($"BasicNetManager::OnServerAddPlayer {conn}", ConsoleColor.Green);
             base.OnServerAddPlayer(conn);
             Player.ResetPlayerNumbers();
         }
 
-        /// <summary>
-        /// Called on the server when a client disconnects.
-        /// <para>This is called on the Server when a Client disconnects from the Server. Use an override to decide what should happen when a disconnection is detected.</para>
-        /// </summary>
-        /// <param name="conn">Connection from client.</param>
         public override void OnServerDisconnect(NetworkConnection conn)
         {
+            Log($"BasicNetManager::OnServerDisconnect {conn}", ConsoleColor.Yellow);
             base.OnServerDisconnect(conn);
             Player.ResetPlayerNumbers();
         }
+
+        public override void OnStopServer()
+        {
+            Log($"BasicNetManager::OnStopServer", ConsoleColor.Red);
+            base.OnStopServer();
+        }
+
+        #endregion
+
+        #region Client
+
+        public override void OnStartClient()
+        {
+            Log($"BasicNetManager::OnStartClient", ConsoleColor.Green);
+            base.OnStartClient();
+        }
+
+        public override void OnStopClient()
+        {
+            Log($"BasicNetManager::OnStopClient", ConsoleColor.Yellow);
+            base.OnStopClient();
+        }
+
+        public override void OnClientConnect()
+        {
+            Log($"BasicNetManager::OnClientConnect", ConsoleColor.Green);
+            base.OnClientConnect();
+        }
+
+        public override void OnClientDisconnect()
+        {
+            Log($"BasicNetManager::OnClientDisconnect", ConsoleColor.Yellow);
+            base.OnClientDisconnect();
+        }
+
+        public override void OnClientNotReady()
+        {
+            Log($"BasicNetManager::OnClientNotReady", ConsoleColor.Red);
+            base.OnClientNotReady();
+        }
+
+        #endregion
     }
 }
