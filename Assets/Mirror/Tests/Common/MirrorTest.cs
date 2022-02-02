23455570e670a73b90a9dad43c4d980c5b304fdf
vis2k <info@noobtuts.com>
2022-02-02 12:44:37 +0800

    MirrorTest: fix CreateNetworkedAndSpawnPlayer not properly spawning because the GO wasn't considered for spawning because it wasn't inactive

--- Assets/Mirror/Tests/Common/MirrorTest.cs
+++ Assets/Mirror/Tests/Common/MirrorTest.cs
@@ -472,11 +472,18 @@ namespace Mirror.Tests
             clientIdentity.sceneId = serverIdentity.sceneId = (ulong)serverGO.GetHashCode();
             NetworkClient.spawnableObjects[clientIdentity.sceneId] = clientIdentity;
 
+            // IMPORTANT: OnSpawn finds 'sceneId' in .spawnableObjects.
+            // only those who are ConsiderForSpawn() are in there.
+            // for scene objects to be considered, they need to be disabled.
+            // (it'll be active by the time we return here)
+            clientGO.SetActive(false);
+
             // add as player & process spawn message on client.
             NetworkServer.AddPlayerForConnection(ownerConnection, serverGO);
             ProcessMessages();
 
             // make sure the client really spawned it.
+            Assert.That(clientGO.activeSelf, Is.True);
             Assert.That(NetworkClient.spawned.ContainsKey(serverIdentity.netId));
         }
 
