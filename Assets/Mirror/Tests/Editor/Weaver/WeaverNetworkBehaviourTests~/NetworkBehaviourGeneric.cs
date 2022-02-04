7a635680368fa95da40e830ba8f2c5a714d462fd
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2022-02-04 07:32:48 -0500

    Removed Examples & Tests

--- Assets/Mirror/Tests/Editor/Weaver/WeaverNetworkBehaviourTests~/NetworkBehaviourGeneric.cs
+++
@@ -1,9 +0,0 @@
-using Mirror;
-
-namespace WeaverNetworkBehaviourTests.NetworkBehaviourGeneric
-{
-    class NetworkBehaviourGeneric<T> : NetworkBehaviour
-    {
-        T genericsNotAllowed;
-    }
-}
