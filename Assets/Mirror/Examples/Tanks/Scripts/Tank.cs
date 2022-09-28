d4b1239dc427859391630d27b095ea444cdabb95
vis2k <info@noobtuts.com>
2022-09-28 11:57:02 +0700

    syntax

--- Assets/Mirror/Examples/Tanks/Scripts/Tank.cs
+++ Assets/Mirror/Examples/Tanks/Scripts/Tank.cs
@@ -7,8 +7,8 @@ namespace Mirror.Examples.Tanks
     {
         [Header("Components")]
         public NavMeshAgent agent;
-        public Animator animator;
-        public TextMesh healthBar;
+        public Animator  animator;
+        public TextMesh  healthBar;
         public Transform turret;
 
         [Header("Movement")]
@@ -17,7 +17,7 @@ namespace Mirror.Examples.Tanks
         [Header("Firing")]
         public KeyCode shootKey = KeyCode.Space;
         public GameObject projectilePrefab;
-        public Transform projectileMount;
+        public Transform  projectileMount;
 
         [Header("Stats")]
         [SyncVar] public int health = 4;
