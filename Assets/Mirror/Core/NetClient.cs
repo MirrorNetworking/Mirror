c2fb60e27e59c7a00e7790062812c9909253bada
vis2k <info@noobtuts.com>
2022-09-30 14:54:14 +0700

    NetClient

---
+++ Assets/Mirror/Core/NetClient.cs
@@ -0,0 +1,10 @@
+// NetworkClient as component.
+// named "NetClient" as long as static "NetworkClient" still exists.
+using UnityEngine;
+
+namespace Mirror
+{
+    public partial class NetClient : MonoBehaviour
+    {
+    }
+}
