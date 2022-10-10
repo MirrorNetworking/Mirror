e7110d30152e0e6173c0c2f499c5631f6b2c7dc7
vis2k <info@noobtuts.com>
2022-10-10 14:52:19 +0200

    TODO

--- Assets/Mirror/Core/NetworkIdentity.cs
+++ Assets/Mirror/Core/NetworkIdentity.cs
@@ -932,6 +932,7 @@ namespace Mirror
             //   7 components fit into 1 byte.  (previously  7 bytes)
             //  11 components fit into 2 bytes. (previously 11 bytes)
             //  16 components fit into 3 bytes. (previously 16 bytes)
+            // TODO imer: client knows amount of comps, write N bytes instead
             Compression.CompressVarUInt(ownerWriter,     ownerMask);
             Compression.CompressVarUInt(observersWriter, observerMask);
 
