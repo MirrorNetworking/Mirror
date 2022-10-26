e473d154d9e7b2c6ecec29fd61d34b880c8c089d
vis2k <info@noobtuts.com>
2022-10-26 12:28:06 +0200

    fix: NetworkRigidbody now uses double time to keep precision over multiple days

--- Assets/Mirror/Components/Experimental/NetworkRigidbody.cs
+++ Assets/Mirror/Components/Experimental/NetworkRigidbody.cs
@@ -204,7 +204,7 @@ namespace Mirror.Experimental
         [Client]
         void SendVelocity()
         {
-            float now = Time.time;
+            double now = NetworkTime.localTime; // Unity 2019 doesn't have Time.timeAsDouble yet
             if (now < previousValue.nextSyncTime)
                 return;
 
@@ -349,7 +349,7 @@ namespace Mirror.Experimental
             /// <summary>
             /// Next sync time that velocity will be synced, based on syncInterval.
             /// </summary>
-            public float nextSyncTime;
+            public double nextSyncTime;
             public Vector3 velocity;
             public Vector3 angularVelocity;
             public bool isKinematic;
