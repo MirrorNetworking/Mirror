e6a65bc5b2750b8db4578c53f3a5e65e11d4d41f
vis2k <info@noobtuts.com>
2022-11-03 16:09:36 +0100

    breaking: remove SyncVar<T> which was meant to replace [SyncVar]s implementation behind the scenes. this hasn't happened, so we don't actually need it.

--- Assets/Mirror/Tests/Editor/Weaver/WeaverNetworkBehaviourTests_IsValid/NetworkBehaviourGeneric.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverNetworkBehaviourTests_IsValid/NetworkBehaviourGeneric.cs
@@ -5,7 +5,6 @@ namespace WeaverNetworkBehaviourTests.NetworkBehaviourGeneric
     class NetworkBehaviourGeneric<T> : NetworkBehaviour
     {
         public T param;
-        public readonly SyncVar<T> syncVar = new SyncVar<T>(default);
         public readonly SyncList<T> syncList = new SyncList<T>();
     }
 
