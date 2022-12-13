d5d5235b7e43eacf322c84a502929696591eddb5
vis2k <info@noobtuts.com>
2022-12-13 12:13:32 +0100

    Welcome message moved out of NetworkManager into standalone Editor script, per-session instead of per-play.

---
+++ Assets/Mirror/Editor/Welcome.cs
@@ -0,0 +1,23 @@
+﻿// Shows either a welcome message, only once per session.
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Mirror
+{
+    static class Welcome
+    {
+        [InitializeOnLoadMethod]
+        static void OnInitializeOnLoad()
+        {
+            // InitializeOnLoad is called on start and after each rebuild,
+            // but we only want to show this once per editor session.
+            if (!SessionState.GetBool("MIRROR_WELCOME", false))
+            {
+                SessionState.SetBool("MIRROR_WELCOME", true);
+                Debug.Log("Mirror | mirror-networking.com | discord.gg/N9QVxbM");
+            }
+        }
+    }
+}
+#endif
