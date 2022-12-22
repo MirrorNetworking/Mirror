6c101c5c8573b1073de9969f8fb51db2b1dcfd3d
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2022-12-22 02:44:52 -0500

    NetworkLerpRigidbody - syntax

--- Assets/Mirror/Components/Experimental/NetworkLerpRigidbody.cs
+++ Assets/Mirror/Components/Experimental/NetworkLerpRigidbody.cs
@@ -8,8 +8,10 @@ namespace Mirror.Experimental
     {
         [Header("Settings")]
         [SerializeField] internal Rigidbody target = null;
+
         [Tooltip("How quickly current velocity approaches target velocity")]
         [SerializeField] float lerpVelocityAmount = 0.5f;
+
         [Tooltip("How quickly current position approaches target position")]
         [SerializeField] float lerpPositionAmount = 0.5f;
 
@@ -17,7 +19,6 @@ namespace Mirror.Experimental
         [SerializeField] bool clientAuthority = false;
 
         double nextSyncTime;
-
 
         [SyncVar()]
         Vector3 targetVelocity;
@@ -28,7 +29,6 @@ namespace Mirror.Experimental
         /// <summary>
         /// Ignore value if is host or client with Authority
         /// </summary>
-        /// <returns></returns>
         bool IgnoreSync => isServer || ClientWithAuthority;
 
         bool ClientWithAuthority => clientAuthority && isOwned;
@@ -36,21 +36,15 @@ namespace Mirror.Experimental
         void OnValidate()
         {
             if (target == null)
-            {
                 target = GetComponent<Rigidbody>();
-            }
         }
 
         void Update()
         {
             if (isServer)
-            {
                 SyncToClients();
-            }
             else if (ClientWithAuthority)
-            {
                 SendToServer();
-            }
         }
 
         void SyncToClients()
