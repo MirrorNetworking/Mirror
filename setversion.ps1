6a7c15e664af55ae9cca69026ef6fbe57c9f61b0
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-01-19 04:42:27 -0500

    Removed unneeded github files

--- setversion.ps1
+++
@@ -1,8 +0,0 @@
-semantic-release --dry-run |  %{ 
-    echo $_
-    $fields = -split $_;
-    if ($fields[0] -eq "#" -or $fields[0] -eq "##") {
-        echo "Mirror version " $fields[1]
-        echo $fields[1] > Assets/Mirror/version.txt
-    }
-}
