5f5eb8eed72499c4ff298caffec7da66bbc7f1d0
vis2k <info@noobtuts.com>
2023-01-29 12:46:29 +0900

    naming

--- Assets/Mirror/Core/NetworkClient.cs
+++ Assets/Mirror/Core/NetworkClient.cs
@@ -1366,7 +1366,7 @@ namespace Mirror
         // set up NetworkIdentity flags on the client.
         // needs to be separate from invoking callbacks.
         // cleaner, and some places need to set flags first.
-        static void SetIdentityFlags(NetworkIdentity identity)
+        static void InitializeIdentityFlags(NetworkIdentity identity)
         {
             // initialize flags before invoking callbacks.
             // this way isClient/isLocalPlayer is correct during callbacks.
@@ -1400,7 +1400,7 @@ namespace Mirror
         // configure flags & invoke callbacks
         static void BootstrapIdentity(NetworkIdentity identity)
         {
-            SetIdentityFlags(identity);
+            InitializeIdentityFlags(identity);
             InvokeIdentityCallbacks(identity);
         }
 
