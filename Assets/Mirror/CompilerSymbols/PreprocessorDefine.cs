09391dcba26cb74e848a06a2324ecd14c2f9172e
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-02-08 03:02:30 -0500

    Revert "breaking: MIRROR_73_OR_NEWER"
    This reverts commit 8596f1d827452c03ffd81f337f5a8474a22bdc12.

--- Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
+++ Assets/Mirror/CompilerSymbols/PreprocessorDefine.cs
@@ -15,6 +15,7 @@ namespace Mirror
             HashSet<string> defines = new HashSet<string>(currentDefines.Split(';'))
             {
                 "MIRROR",
+                "MIRROR_55_0_OR_NEWER",
                 "MIRROR_57_0_OR_NEWER",
                 "MIRROR_58_0_OR_NEWER",
                 "MIRROR_65_0_OR_NEWER",
@@ -23,7 +24,6 @@ namespace Mirror
                 "MIRROR_2022_10_OR_NEWER",
                 "MIRROR_70_0_OR_NEWER",
                 "MIRROR_71_0_OR_NEWER"
-                "MIRROR_73_OR_NEWER"
                 // Remove oldest when adding next month's symbol.
                 // Keep a rolling 12 months of symbols.
             };
