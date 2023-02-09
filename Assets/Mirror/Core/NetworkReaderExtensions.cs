e260d1eb2a07a839fc926a4d422dc62deb2fec74
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2023-02-09 09:24:03 -0500

    fix: NetworkWriter - Change MaxStringLength to ushort.MaxValue (#3374)
    * fix: NetworkWriter - Change MaxStringLength to ushort.MaxValue
    - local `realsize` in NetworkReaderExtensions.ReadString changed to ushort with proper casting
    - Comments added to NetworkWriterExtensions
    - Comments added to NetworkReaderExtensions
    - Log output updated in both for consistency
    
    * Added comment
    
    * Changes per request

--- Assets/Mirror/Core/NetworkReaderExtensions.cs
+++ Assets/Mirror/Core/NetworkReaderExtensions.cs
@@ -64,13 +64,11 @@ namespace Mirror
             if (size == 0)
                 return null;
 
-            int realSize = size - 1;
+            ushort realSize = (ushort)(size - 1);
 
             // make sure it's within limits to avoid allocation attacks etc.
-            if (realSize >= NetworkWriter.MaxStringLength)
-            {
-                throw new EndOfStreamException($"ReadString too long: {realSize}. Limit is: {NetworkWriter.MaxStringLength}");
-            }
+            if (realSize > NetworkWriter.MaxStringLength)
+                throw new EndOfStreamException($"NetworkReader.ReadString - Value too long: {realSize} bytes. Limit is: {NetworkWriter.MaxStringLength} bytes");
 
             ArraySegment<byte> data = reader.ReadBytesSegment(realSize);
 
