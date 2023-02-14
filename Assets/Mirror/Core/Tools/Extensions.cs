59dc88c981b1612cbf62a2a4f739d6475e1bb338
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2023-02-13 20:45:36 -0500

    perf: Cache Stable Hashes (#3377)
    * perf: Cache Stable Hashes
    - Static dictionary of message hashes
    - ResetStatics
    
    * Added debug log for future debugging.
    
    * Update Extensions.cs
    
    ---------
    
    Co-authored-by: mischa <16416509+vis2k@users.noreply.github.com>

--- Assets/Mirror/Core/Tools/Extensions.cs
+++ Assets/Mirror/Core/Tools/Extensions.cs
@@ -9,15 +9,33 @@ namespace Mirror
         public static string ToHexString(this ArraySegment<byte> segment) =>
             BitConverter.ToString(segment.Array, segment.Offset, segment.Count);
 
+        // GetStableHashCode is O(N).
+        // the longer the string, the more it needs to compute:
+        // https://github.com/MirrorNetworking/Mirror/pull/3377
+        // cache results for O(1) lookups.
+        static readonly Dictionary<string, int> StableHashes = new Dictionary<string, int>();
+
+        [UnityEngine.RuntimeInitializeOnLoadMethod]
+        public static void ResetStatics()
+        {
+            StableHashes.Clear();
+        }
+
         // string.GetHashCode is not guaranteed to be the same on all machines, but
         // we need one that is the same on all machines. simple and stupid:
         public static int GetStableHashCode(this string text)
         {
+            if (StableHashes.TryGetValue(text, out int cachedHash))
+                return cachedHash;
+
             unchecked
             {
                 int hash = 23;
                 foreach (char c in text)
                     hash = hash * 31 + c;
+
+                //UnityEngine.Debug.Log($"Caching stable hash {(ushort)hash} for {text}");
+                StableHashes[text] = hash;
                 return hash;
             }
         }
