dc1c45a7b8441d8b7faf3c6a75bec3f7449ea907
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-02-20 02:15:23 -0500

    set multipler default back to 3

--- Assets/Mirror/Examples/TestNT/Scripts/TestNTNetworkAuthenticator.cs
+++ Assets/Mirror/Examples/TestNT/Scripts/TestNTNetworkAuthenticator.cs
@@ -14,7 +14,7 @@ namespace TestNT
         public string playerName;
         public bool useNinja;
         public bool isBot;
-        public uint multiplier = 6;
+        public uint multiplier = 3;
 
         #region Messages
 
