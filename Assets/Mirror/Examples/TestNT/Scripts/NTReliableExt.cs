a4745bcc315d48f5f244e5d79a58cfecc221797f
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-03-14 08:52:26 -0400

    Code cleanup

--- Assets/Mirror/Examples/TestNT/Scripts/NTReliableExt.cs
+++ Assets/Mirror/Examples/TestNT/Scripts/NTReliableExt.cs
@@ -9,54 +9,8 @@ namespace TestNT
         public Action<Vector3, Vector3> VelRotChangedAction;
 
         [Header("Snapshot Interpolation")]
-        //public double t;
-        //public int fromIndex;
-        //public int toIndex;
         public Vector3 velocity;
         public Vector3 angVelocity;
-
-        #region Unity Callbacks
-
-        protected override void OnValidate()
-        {
-            base.OnValidate();
-        }
-
-        protected override void Awake()
-        {
-            base.Awake();
-        }
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-        }
-
-        #endregion
-
-        #region NetworkTransformBase Methods
-
-        protected override bool Changed(TransformSnapshot current)
-        {
-            return base.Changed(current);
-        }
-
-        protected override void OnServerToClientSync(Vector3? position, Quaternion? rotation, Vector3? scale)
-        {
-            base.OnServerToClientSync(position, rotation, scale);
-        }
-
-        protected override void OnClientToServerSync(Vector3? position, Quaternion? rotation, Vector3? scale)
-        {
-            base.OnClientToServerSync(position, rotation, scale);
-        }
-
-        #endregion
 
         #region NetworkTransformReliable Methods
 
@@ -70,16 +24,6 @@ namespace TestNT
                 angVelocity = (transform.rotation.eulerAngles - interpolated.rotation.eulerAngles) / Time.deltaTime;
                 VelRotChangedAction?.Invoke(velocity, angVelocity);
             }
-        }
-
-        protected override TransformSnapshot Construct()
-        {
-            return base.Construct();
-        }
-
-        public override void Reset()
-        {
-            base.Reset();
         }
 
         #endregion
