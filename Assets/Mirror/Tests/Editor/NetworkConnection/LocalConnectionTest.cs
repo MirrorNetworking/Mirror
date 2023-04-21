8b0020f10ceda6034e7d293d16a6ea5ce7229a5b
mischa <info@noobtuts.com>
2023-04-21 13:34:47 +0800

    Tests: NetworkConnections namespace for grouping in test runner

--- Assets/Mirror/Tests/Editor/NetworkConnection/LocalConnectionTest.cs
+++ Assets/Mirror/Tests/Editor/NetworkConnection/LocalConnectionTest.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Mirror.Tests
+namespace Mirror.Tests.NetworkConnections
 {
     public class LocalConnectionTest : MirrorTest
     {
--- Assets/Mirror/Tests/Editor/NetworkConnection/NetworkConnectionToClientTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkConnection/NetworkConnectionToClientTests.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-namespace Mirror.Tests
+namespace Mirror.Tests.NetworkConnections
 {
     public class NetworkConnectionToClientTests : MirrorEditModeTest
     {
