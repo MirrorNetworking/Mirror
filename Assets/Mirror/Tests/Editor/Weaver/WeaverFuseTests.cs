da74c17d7b105186d16aacdd9b89c96b72589e4f
mischa <info@noobtuts.com>
2023-06-11 08:30:02 +0900

    update comment

--- Assets/Mirror/Tests/Editor/Weaver/WeaverFuseTests.cs
+++ Assets/Mirror/Tests/Editor/Weaver/WeaverFuseTests.cs
@@ -7,7 +7,7 @@ namespace Mirror.Weaver.Tests
         [Test]
         public void WeavingSucceded()
         {
-            // .State returns false by default.
+            // the fuse returns false by default.
             // Weaver overwrites this to true.
             Assert.That(WeaverFuse.Weaved(), Is.True);
         }
