6ac67b9b5fd09cc450be9251298045500b8275e4
mischa <info@noobtuts.com>
2023-06-16 12:26:59 +0800

    feature: NetworkRigidbody Benchmark demo

---
+++ Assets/Mirror/Examples/RigidbodyBenchmark/Scripts/AddForce.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Mirror.Examples.RigidbodyBenchmark
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class AddForce : NetworkBehaviour
+    {
+        public Rigidbody rigidbody3d;
+        public float force = 500f;
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            rigidbody3d = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            // do we have authority over this?
+            if (!rigidbody3d.isKinematic)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                    rigidbody3d.AddForce(Vector3.up * force);
+            }
+        }
+    }
+}
