ef08e61c2cf6ab3fb2d3cd8b093f9e041625b030
JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
2023-08-05 21:01:13 +0100

    Couch Co-Op Part 1
    All works, however part 1 only has controls for "Jump", needs the others doing.
    Jump keys are numbers 1, 2,3,4, depending on which player you are. (all can be customised on the CouchPlayerManager script. (everyones joint uses wasd and qe for movement)

---
+++ Assets/Mirror/Examples/CouchCoop/Scripts/CouchNetworkManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Mirror.Examples.CouchCoop
+{
+    public class CouchNetworkManager : NetworkManager
+    {
+        // custom network manager not currently needed
+    }
+}
---
+++ Assets/Mirror/Examples/CouchCoop/Scripts/CouchPlayerManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Mirror.Examples.CouchCoop
+{
+    public class CouchPlayerManager : NetworkBehaviour
+    {
+        public CanvasScript canvasScript;
+        // for multiple player prefabs, currently not implemented
+        public GameObject[] playerPrefabs;
+        public int totalCouchPlayers = 0;
+
+        // ignore key controls 0, we will always start at 1
+        public KeyCode[] playerKeyJump;
+
+        // store a list of players so we know which to remove later
+        // can be non sync-list, but may be useful for future features
+        public readonly SyncList<GameObject> couchPlayersList = new SyncList<GameObject>();
+
+        private void Awake()
+        {
+            canvasScript = GameObject.FindObjectOfType<CanvasScript>();
+            canvasScript.couchPlayerManager = this;
+        }
+
+        [Command]
+        public void CmdAddPlayer()
+        {
+            if (totalCouchPlayers >= playerKeyJump.Length-1)
+            {
+                Debug.Log(name + " - No controls setup for further players.");
+                return;
+            }
+
+            totalCouchPlayers += 1;
+            Transform spawnObj = NetworkManager.startPositions[Random.Range(0, NetworkManager.startPositions.Count)];
+            GameObject playerObj = Instantiate(playerPrefabs[0], spawnObj.position, spawnObj.rotation);
+            CouchPlayer couchPlayer = playerObj.GetComponent<CouchPlayer>();
+            couchPlayer.playerNumber = totalCouchPlayers;
+            NetworkServer.Spawn(playerObj, connectionToClient);
+            couchPlayersList.Add(playerObj);
+        }
+
+        [Command]
+        public void CmdRemovePlayer()
+        {
+            if (totalCouchPlayers <= 0)
+            {
+                Debug.Log(name + " - No players to remove for that connection.");
+                return;
+            }
+
+            totalCouchPlayers -= 1;
+            NetworkServer.Destroy(couchPlayersList[couchPlayersList.Count - 1]);
+            couchPlayersList.RemoveAt(couchPlayersList.Count - 1);
+     
+        }
+    }
+
+}
