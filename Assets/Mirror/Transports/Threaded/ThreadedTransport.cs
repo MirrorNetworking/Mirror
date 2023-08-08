e3f01b00fef7a44f819523a316a0798e44f1c25a
mischa <info@noobtuts.com>
2023-08-08 18:14:46 +0800

    ThreadedTransport empty

--- Assets/Mirror/Transports/Threaded/ThreadedTransport.cs
+++ Assets/Mirror/Transports/Threaded/ThreadedTransport.cs
@@ -73,8 +73,63 @@ namespace Mirror
         }
     }
 
-    public class ThreadedTransport : MonoBehaviour
+    public class ThreadedTransport : Transport
     {
-
+        public override bool Available()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool ClientConnected()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ClientConnect(string address)
+        {
+            throw new NotImplementedException();
+        }
+        public override void ClientSend(ArraySegment<byte> segment, int channelId = Channels.Reliable)
+        {
+            throw new NotImplementedException();
+        }
+        public override void ClientDisconnect()
+        {
+            throw new NotImplementedException();
+        }
+        public override Uri ServerUri()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool ServerActive()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ServerStart()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ServerSend(int connectionId, ArraySegment<byte> segment, int channelId = Channels.Reliable)
+        {
+            throw new NotImplementedException();
+        }
+        public override void ServerDisconnect(int connectionId)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ServerGetClientAddress(int connectionId)
+        {
+            throw new NotImplementedException();
+        }
+        public override void ServerStop()
+        {
+            throw new NotImplementedException();
+        }
+        public override int GetMaxPacketSize(int channelId = Channels.Reliable)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
