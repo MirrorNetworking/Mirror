274da42a7bb29224e03ea421ee706ac6ea6ba8a9
mischa <info@noobtuts.com>
2023-08-08 16:46:17 +0800

    events

---
+++ Assets/Mirror/Transports/Threaded/ThreadedTransport.cs
@@ -0,0 +1,80 @@
+// threaded transport to handle all the magic.
+// implementations are automatically elevated to the worker thread
+// by simply overwriting all the thread functions
+//
+// note that ThreadLog.cs is required for Debug.Log from threads to work in builds.
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
+
+namespace Mirror
+{
+    // buffered events for main thread
+    enum MainEventType
+    {
+        OnConnect,
+        OnSend,
+        OnReceive,
+        OnError,
+        OnDisconnect,
+    }
+
+    // buffered events for worker thread
+    enum ThreadEventType
+    {
+        DoListen,
+        DoSend,
+        DoDisconnect,
+        DoShutdown
+    }
+
+    struct MainEvent
+    {
+        public MainEventType type;
+        public object        param;
+
+        // some events have value type parameters: connectionId, error.
+        // store them explicitly to avoid boxing allocations to 'object param'.
+        public int             connectionId;
+        public TransportError? error;
+
+        public MainEvent(
+            MainEventType type,
+            object param,
+            int connectionId,
+            TransportError? error = null)
+        {
+            this.type = type;
+            this.connectionId = connectionId;
+            this.error = error;
+            this.param = param;
+        }
+    }
+
+    struct ThreadEvent
+    {
+        public ThreadEventType type;
+        public object          param;
+
+        // some events have value type parameters: connectionId.
+        // store them explicitly to avoid boxing allocations to 'object param'.
+        public int? connectionId;
+
+        public ThreadEvent(
+            ThreadEventType type,
+            object param,
+            int? connectionId = null)
+        {
+            this.type = type;
+            this.connectionId = connectionId;
+            this.param = param;
+        }
+    }
+
+    public class ThreadedTransport : MonoBehaviour
+    {
+
+    }
+}
