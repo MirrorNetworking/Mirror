422fec820462ed486ca135881f730175065b1623
mischa <info@noobtuts.com>
2023-08-31 18:15:10 +0200

    custom timestep WIP

--- Assets/Mirror/Examples/BilliardsPredicted/PredictionNetworkManager.cs
+++ Assets/Mirror/Examples/BilliardsPredicted/PredictionNetworkManager.cs
@@ -1,12 +1,17 @@
 // prediction needs some custom setup.
 // for example, we need to ensure Physics 'Auto Simulation' is disabled
 // because we want to call Physics.Simulate() manually.
+using System;
 using UnityEngine;
 
 namespace Mirror.Examples.BilliardsPredicted
 {
     public class PredictionNetworkManager : NetworkManager
     {
+        // physics simulation isn't called automatically anymore.
+        // count the update time and call it every fixedDeltaTime manually.
+        double timeAccumulator = 0;
+
         public override void Awake()
         {
             // Ensure Physics 'Auto Simulation' is disabled when starting this scene
@@ -14,6 +19,48 @@ namespace Mirror.Examples.BilliardsPredicted
             Debug.Log($"Prediction: disabled Physics Auto Simulation to prepare for manual Simulation steps.");
 
             base.Awake();
+        }
+
+        // 'fix your timestep' version with manual accumulator in update.
+        // TODO this is still jittery.
+        // TODO try to call this from NetworkEarly/LateUpdate. which is better?
+        /*
+        public override void Update()
+        {
+            base.Update();
+
+            // 'Fix your Timestep' article
+            // https://www.gafferongames.com/post/fix_your_timestep/
+            // timeAccumulator += Time.deltaTime;
+            // while (timeAccumulator >= Time.fixedDeltaTime)
+            // {
+            //     Physics.Simulate(Time.fixedDeltaTime);
+            //     // subtract from accumulator.
+            //     // note that -= fixedDeltaTime is jittery.
+            //     // glenn fiedler's -= deltaTime is smooth.
+            //     timeAccumulator -= Time.deltaTime;
+            //     physicsTime += Time.fixedDeltaTime;
+            // }
+
+            // increase accumulator with deadlock protection.
+            // only every step through up to 'maximumDeltaTime'.
+            timeAccumulator += Math.Min(Time.deltaTime, Time.maximumDeltaTime);
+            while (timeAccumulator >= Time.fixedDeltaTime)
+            {
+                Physics.Simulate(Time.fixedDeltaTime);
+                timeAccumulator -= Time.fixedDeltaTime;
+            }
+        }
+        */
+
+        // simulating physics from FixedUpdate is the smoothest solution.
+        // calling this from Update or LateUpdate causes noticeable jitter.
+        // TODO try to call this from NetworkEarly/LateUpdate. which is better?
+        protected void FixedUpdate()
+        {
+            // this is the smoothest solution.
+            // automatically does the 'fixed timestep' calculations for us.
+            Physics.Simulate(Time.fixedDeltaTime);
         }
     }
 }
