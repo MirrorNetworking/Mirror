da1c247a73836f12cee64bdec3ea8050af981211
mischa <info@noobtuts.com>
2023-09-01 16:24:12 +0200

    hard prediction every 500ms

--- Assets/Mirror/Components/PredictedRigidbody/PredictedRigidbody.cs
+++ Assets/Mirror/Components/PredictedRigidbody/PredictedRigidbody.cs
@@ -1,3 +1,5 @@
+// make sure to use a reasonable sync interval.
+// for example, correcting every 100ms seems reasonable.
 using UnityEngine;
 
 namespace Mirror.PredictedRigidbody
@@ -6,10 +8,31 @@ namespace Mirror.PredictedRigidbody
     public class PredictedRigidbody : NetworkBehaviour
     {
         Rigidbody rb;
+        Vector3 lastPosition;
+
+        [Tooltip("Broadcast changes if position changed by more than ... meters.")]
+        public float positionSensitivity = 0.01f;
 
         void Awake()
         {
             rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (Vector3.Distance(transform.position, lastPosition) >= positionSensitivity)
+            {
+                lastPosition = transform.position;
+                SetDirty();
+            }
+        }
+
+        protected override void OnValidate()
+        {
+            // make sure syncInterval isn't 0.
+            // it's enough to correct every 100ms or so.
+            syncInterval = Mathf.Max(syncInterval, 0.1f);
+            base.OnValidate();
         }
 
         // send state to clients every sendInterval.
@@ -24,10 +47,14 @@ namespace Mirror.PredictedRigidbody
         // this is where reconciliation happens if necessary.
         public override void OnDeserialize(NetworkReader reader, bool initialState)
         {
-            double timestamp = connectionToServer.remoteTimeStamp;
+            double timestamp = NetworkClient.connection.remoteTimeStamp;
             Vector3 position = reader.ReadVector3();
             Vector3 velocity = reader.ReadVector3();
-            Debug.Log($"OnDeserialize: {timestamp} {position} {velocity}");
+
+            // hard force for now.
+            // TODO compare past position at timestamp, and only correct if needed
+            rb.position = position;
+            rb.velocity = velocity;
         }
     }
 }
