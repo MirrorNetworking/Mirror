2879348413b3474c5b3cd859f89055d7717f6d3c
mischa <info@noobtuts.com>
2023-09-01 15:28:50 +0200

    inputhistory

--- Assets/Mirror/Examples/BilliardsPredicted/Player/PlayerPredicted.cs
+++ Assets/Mirror/Examples/BilliardsPredicted/Player/PlayerPredicted.cs
@@ -1,11 +1,31 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror.Examples.BilliardsPredicted
 {
+    // example input for this exact demo.
+    // not general purpose yet.
+    public struct PlayerInput
+    {
+        public double timestamp;
+        public Vector3 force;
+
+        public PlayerInput(double timestamp, Vector3 force)
+        {
+            this.timestamp = timestamp;
+            this.force = force;
+        }
+    }
+
     public class PlayerPredicted : NetworkBehaviour
     {
         // white ball component
         WhiteBallPredicted whiteBall;
+
+        // keep a history of inputs with timestamp
+        public int inputHistorySize = 64;
+        readonly SortedList<double, PlayerInput> inputs = new SortedList<double, PlayerInput>();
 
         void Awake()
         {
@@ -17,6 +37,11 @@ namespace Mirror.Examples.BilliardsPredicted
         // we reuse the white ball's OnMouseDrag and forward the event to here.
         public void OnDraggedBall(Vector3 force)
         {
+            // record the input for reconciliation if needed
+            if (inputs.Count >= inputHistorySize) inputs.RemoveAt(0);
+            inputs.Add(NetworkTime.time, new PlayerInput(NetworkTime.time, force));
+            Debug.Log($"Inputs.Count={inputs.Count}");
+
             // apply locally immediately
             whiteBall.GetComponent<Rigidbody>().AddForce(force);
 
