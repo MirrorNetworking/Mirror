3302f4f0c77cb9de8f9a6e76ee1ec87fcb2e681a
Justin Nolan <justin@nolan.de>
2023-09-28 15:55:34 +0200

    Fix test

--- Assets/Mirror/Tests/Editor/Tools/ExtensionsTest.cs
+++ Assets/Mirror/Tests/Editor/Tools/ExtensionsTest.cs
@@ -12,7 +12,7 @@ namespace Mirror.Tests.Tools
         public void GetStableHashHode()
         {
             Assert.That("".GetStableHashCode(), Is.EqualTo(-2128831035));
-            Assert.That("Test".GetStableHashCode(), Is.EqualTo(23844169));
+            Assert.That("Test".GetStableHashCode(), Is.EqualTo(805092869));
         }
 
         [Test]
