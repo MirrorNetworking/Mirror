6e71d54e5ef3b935ac4d806164c785848f69cbeb
mischa <info@noobtuts.com>
2023-11-06 10:17:44 +0100

    make it a bit wider

--- Assets/Mirror/Core/NetworkManagerHUD.cs
+++ Assets/Mirror/Core/NetworkManagerHUD.cs
@@ -21,7 +21,7 @@ namespace Mirror
 
         void OnGUI()
         {
-            GUILayout.BeginArea(new Rect(10 + offsetX, 40 + offsetY, 250, 9999));
+            GUILayout.BeginArea(new Rect(10 + offsetX, 40 + offsetY, 300, 9999));
             if (!NetworkClient.isConnected && !NetworkServer.active)
             {
                 StartButtons();
