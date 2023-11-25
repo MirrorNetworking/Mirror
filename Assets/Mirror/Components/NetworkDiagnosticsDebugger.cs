21bc42f38802026bd6a71b9e3be875fcc803b9fc
Andrew Allbright <aallbrig@gmail.com>
2023-11-25 17:44:44 -0500

    feat: Added NetworkDiagnosticsDebugger to log messages sent and received (#3665)

---
+++ Assets/Mirror/Components/NetworkDiagnosticsDebugger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Mirror
+{
+    public class NetworkDiagnosticsDebugger : MonoBehaviour
+    {
+        public bool logInMessages = true;
+        public bool logOutMessages = true;
+        void OnInMessage(NetworkDiagnostics.MessageInfo msgInfo)
+        {
+            if (logInMessages)
+                Debug.Log(msgInfo);
+        }
+        void OnOutMessage(NetworkDiagnostics.MessageInfo msgInfo)
+        {
+            if (logOutMessages)
+                Debug.Log(msgInfo);
+        }
+        void OnEnable()
+        {
+            NetworkDiagnostics.InMessageEvent += OnInMessage;
+            NetworkDiagnostics.OutMessageEvent += OnOutMessage;
+        }
+        void OnDisable()
+        {
+            NetworkDiagnostics.InMessageEvent -= OnInMessage;
+            NetworkDiagnostics.OutMessageEvent -= OnOutMessage;
+        }
+    }
+}
