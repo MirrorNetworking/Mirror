7986a9dd2b317869699773d0f0a89921ee231c72
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-12-03 14:34:46 -0500

    fix(Utils): Add IsWebGL and IsDebug

--- Assets/Mirror/Core/Tools/Utils.cs
+++ Assets/Mirror/Core/Tools/Utils.cs
@@ -50,6 +50,22 @@ namespace Mirror
             SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
 #endif
 
+        // detect WebGL mode
+        public static bool IsWebGL() =>
+#if UNITY_WEBGL
+            true;
+#else
+            false;
+#endif
+
+        // detect Debug mode
+        public static bool IsDebug() =>
+#if DEBUG
+            true;
+#else
+            false;
+#endif
+
         public static uint GetTrueRandomUInt()
         {
             // use Crypto RNG to avoid having time based duplicates
