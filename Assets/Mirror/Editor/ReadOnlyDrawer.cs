ccef7595170d22078bbb229a0087834a56ddf881
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2024-01-02 09:37:00 -0500

    ReadOnlyDrawer: cache and restore GUI.Enabled state

--- Assets/Mirror/Editor/ReadOnlyDrawer.cs
+++ Assets/Mirror/Editor/ReadOnlyDrawer.cs
@@ -8,9 +8,12 @@ namespace Mirror
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            // Cache the current GUI enabled state
+            bool prevGuiEnabledState = GUI.enabled;
+
             GUI.enabled = false;
             EditorGUI.PropertyField(position, property, label, true);
-            GUI.enabled = true;
+            GUI.enabled = prevGuiEnabledState;
         }
     }
 }
