66b38852d53c11c18c9ea6b8c8c03313b6c90e67
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-01-02 20:21:00 -0500

    perf(Match Interest Mgmt): Complete Overhaul (#3707)
    * perf(Match Interest Mgmt) Use Dictionary instead of spawned
    This is better than iterating all spawned objects and doing TryGetComponent in Update.
    
    * undo rename
    
    * Simplified OnRebuildObservers
    
    * Overhauled Match Interest Mgmt
    - no more looping dictionaries
    - no more TryGetComponent in hot path
    - NetworkMatch component drives the updates
    
    * Cleanup
    
    * syntax
    
    * Merged UpdateDirtyMatches & UpdateMatchObjects into OnMatchChanged
    
    * NetworkMatch - Added isServer check
    
    * MatchInterestManagement: Better matchObjects cleanup
    - moved match removal to LateUpdate
    - added matchCount for debugging
    
    * Added comment
    
    Co-authored-by: Robin Rolf <imer@imer.cc>
    
    * Formatting
    
    Co-authored-by: Robin Rolf <imer@imer.cc>
    
    * Updated OnMatchChanged / matchCount -> ushort
    - Depends on ReadOnlyAttribute
    
    * matchCount -> internal
    
    * matchCount Header
    
    ---------
    
    Co-authored-by: Robin Rolf <imer@imer.cc>

--- Assets/Mirror/Components/InterestManagement/Match/NetworkMatch.cs
+++ Assets/Mirror/Components/InterestManagement/Match/NetworkMatch.cs
@@ -9,7 +9,27 @@ namespace Mirror
     [HelpURL("https://mirror-networking.gitbook.io/docs/guides/interest-management")]
     public class NetworkMatch : NetworkBehaviour
     {
+        private Guid _matchId;
+
         ///<summary>Set this to the same value on all networked objects that belong to a given match</summary>
-        public Guid matchId;
+        public Guid matchId
+        {
+            get => _matchId;
+            set
+            {
+                if (!NetworkServer.active)
+                    throw new InvalidOperationException("matchId can only be set at runtime on active server");
+
+                if (_matchId == value)
+                    return;
+
+                Guid oldMatch = _matchId;
+                _matchId = value;
+
+                // Only inform the AOI if this netIdentity has been spawned (isServer) and only if using a MatchInterestManagement
+                if (isServer && NetworkServer.aoi is MatchInterestManagement matchInterestManagement)
+                    matchInterestManagement.OnMatchChanged(this, oldMatch);
+            }
+        }
     }
 }
