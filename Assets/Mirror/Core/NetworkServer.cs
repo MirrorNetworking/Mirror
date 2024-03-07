0909858e10eaea190fa0a3be710af3791afc321f
mischa <info@noobtuts.com>
2024-03-07 11:47:51 +0800

    fix: NetworkServer.OnCommandMessage NullReferenceException due to log message accessing a guaranteed-to-be-null value

--- Assets/Mirror/Core/NetworkServer.cs
+++ Assets/Mirror/Core/NetworkServer.cs
@@ -327,7 +327,7 @@ namespace Mirror
                 // for example, NetworkTransform.
                 // let's not spam the console for unreliable out of order messages.
                 if (channelId == Channels.Reliable)
-                    Debug.LogWarning($"Spawned object not found when handling Command message {identity.name} netId={msg.netId}");
+                    Debug.LogWarning($"Spawned object not found when handling Command message netId={msg.netId}");
                 return;
             }
 
