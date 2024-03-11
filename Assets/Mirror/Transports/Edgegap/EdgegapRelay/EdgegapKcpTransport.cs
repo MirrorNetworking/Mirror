dfafa7a5b11bcb7efc86187f2ffb2827eb4bf9b0
Robin Rolf <imer@imer.cc>
2024-03-11 15:44:42 +0000

    fix: EdgegapKcp StopHost() null reference exception
    see https://github.com/MirrorNetworking/Mirror/issues/3708

--- Assets/Mirror/Transports/Edgegap/EdgegapRelay/EdgegapKcpTransport.cs
+++ Assets/Mirror/Transports/Edgegap/EdgegapRelay/EdgegapKcpTransport.cs
@@ -53,7 +53,7 @@ namespace Edgegap
             client = new EdgegapKcpClient(
                 () => OnClientConnected.Invoke(),
                 (message, channel) => OnClientDataReceived.Invoke(message, FromKcpChannel(channel)),
-                () => OnClientDisconnected.Invoke(),
+                () => OnClientDisconnected?.Invoke(), // may be null in StopHost(): https://github.com/MirrorNetworking/Mirror/issues/3708
                 (error, reason) => OnClientError.Invoke(ToTransportError(error), reason),
                 config
             );
