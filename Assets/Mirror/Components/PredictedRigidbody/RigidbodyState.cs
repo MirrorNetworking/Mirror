2ecafaa6342bf6cc6353e705aa7251638339d291
mischa <info@noobtuts.com>
2024-03-14 15:05:06 +0800

    perf: PredictedRigidbody state properties inlined

--- Assets/Mirror/Components/PredictedRigidbody/RigidbodyState.cs
+++ Assets/Mirror/Components/PredictedRigidbody/RigidbodyState.cs
@@ -1,25 +1,27 @@
 // PredictedRigidbody stores a history of its rigidbody states.
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace Mirror
 {
+    // inline everything because this is performance critical!
     public struct RigidbodyState : PredictedState
     {
-        public double timestamp { get; private set; }
+        public double timestamp { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] private set; }
 
         // we want to store position delta (last + delta = current), and current.
         // this way we can apply deltas on top of corrected positions to get the corrected final position.
-        public Vector3    positionDelta { get; set; } // delta to get from last to this position
-        public Vector3    position { get; set; }
+        public Vector3    positionDelta { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; } // delta to get from last to this position
+        public Vector3    position { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; }
 
-        public Quaternion rotationDelta { get; set; } // delta to get from last to this rotation
-        public Quaternion rotation { get; set; }
+        public Quaternion rotationDelta { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; } // delta to get from last to this rotation
+        public Quaternion rotation { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; }
 
-        public Vector3 velocityDelta { get; set; } // delta to get from last to this velocity
-        public Vector3 velocity { get; set; }
+        public Vector3 velocityDelta { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; } // delta to get from last to this velocity
+        public Vector3 velocity { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; }
 
-        public Vector3 angularVelocityDelta { get; set; } // delta to get from last to this velocity
-        public Vector3 angularVelocity { get; set; }
+        public Vector3 angularVelocityDelta { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; } // delta to get from last to this velocity
+        public Vector3 angularVelocity { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] set; }
 
         public RigidbodyState(
             double timestamp,
