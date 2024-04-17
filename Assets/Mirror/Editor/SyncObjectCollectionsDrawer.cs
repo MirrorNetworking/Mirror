88a1ca49b3b0582a49a31d9b48cd08c21647b30d
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-04-17 02:04:42 -0400

    Use RegEx.Replace

--- Assets/Mirror/Editor/SyncObjectCollectionsDrawer.cs
+++ Assets/Mirror/Editor/SyncObjectCollectionsDrawer.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using UnityEditor;
 
 namespace Mirror
@@ -23,7 +24,7 @@ namespace Mirror
             // so we split it and only take the first part so it looks nicer.
             // e.g. SyncList`1 -> SyncList
             // Better to do it one time here than in hot path in OnInspectorGUI
-            label = $"{field.Name} [{field.FieldType.Name.Split(new string[] { @"`" }, System.StringSplitOptions.RemoveEmptyEntries)[0]}]";
+            label = $"{field.Name} [{Regex.Replace(field.FieldType.Name, @"`\d+", "")}]";
         }
     }
 
