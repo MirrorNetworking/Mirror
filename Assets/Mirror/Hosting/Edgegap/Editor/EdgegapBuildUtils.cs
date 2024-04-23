d7921ebc8d5688fc1089076404705eb0dc3a100a
mischa <info@noobtuts.com>
2024-04-23 13:25:06 +0800

    fix(Edgegap Hosting): ssl updates (https://github.com/edgegap/edgegap-unity-plugin/commit/b721722b5189b20d41ad4103fd3226fa974e7812)

--- Assets/Mirror/Hosting/Edgegap/Editor/EdgegapBuildUtils.cs
+++ Assets/Mirror/Hosting/Edgegap/Editor/EdgegapBuildUtils.cs
@@ -233,6 +233,11 @@ WORKDIR /root/
 
 RUN chmod +x /root/build/ServerBuild
 
+RUN apt-get update && \
+    apt-get install -y ca-certificates && \
+    apt-get clean && \
+    update-ca-certificates
+
 ENTRYPOINT [ ""/root/build/ServerBuild"", ""-batchmode"", ""-nographics""]
 ";
 
