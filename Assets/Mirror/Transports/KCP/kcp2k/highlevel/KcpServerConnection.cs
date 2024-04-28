a94718f404eeeebf3c2e8c0e8ceb7a08e57f94d6
mischa <info@noobtuts.com>
2024-04-28 12:09:32 +0800

    fix(Kcp): V1.41 [2024-04-28] - fix: KcpHeader is now parsed safely, handling attackers potentially sending values out of enum range - fix: KcpClient RawSend may throw ConnectionRefused SocketException when OnDisconnected calls SendDisconnect(), which is fine - fix: less scary cookie message and better explanation

--- Assets/Mirror/Transports/KCP/kcp2k/highlevel/KcpServerConnection.cs
+++ Assets/Mirror/Transports/KCP/kcp2k/highlevel/KcpServerConnection.cs
@@ -82,15 +82,17 @@ namespace kcp2k
             // parse the cookie and make sure it matches (except for initial hello).
             Utils.Decode32U(segment.Array, segment.Offset + 1, out uint messageCookie);
 
-            // compare cookie to protect against UDP spoofing.
-            // messages won't have a cookie until after handshake.
-            // so only compare if we are authenticated.
+            // security: messages after authentication are expected to contain the cookie.
+            // this protects against UDP spoofing.
             // simply drop the message if the cookie doesn't match.
             if (state == KcpState.Authenticated)
             {
                 if (messageCookie != cookie)
                 {
-                    Log.Warning($"[KCP] ServerConnection: dropped message with invalid cookie: {messageCookie} expected: {cookie} state: {state}");
+                    // Info is enough, don't scare users.
+                    // => this can happen for malicious messages
+                    // => it can also happen if client's Hello message was retransmitted multiple times, which is totally normal.
+                    Log.Info($"[KCP] ServerConnection: dropped message with invalid cookie: {messageCookie} from {remoteEndPoint} expected: {cookie} state: {state}. This can happen if the client's Hello message was transmitted multiple times, or if an attacker attempted UDP spoofing.");
                     return;
                 }
             }
