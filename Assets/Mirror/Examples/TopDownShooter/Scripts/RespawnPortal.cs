7ebc960c950d4e5e3ee57e547c2f9392a107e047
JesusLuvsYooh <mail@stephenallengames.co.uk>
2024-06-25 08:27:38 +0100

    Squashed commit of the following:
    commit c1fdf504000cd0536cbadcdbbc158b3db001d049
    Author: JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
    Date:   Thu Jun 20 09:47:55 2024 +0100
    
        feat: Example, TopDown Shooter. (#3836)
    
        * Shooter topdown checkpoint 1
    
        * Topdown shooter, checkpoint 2
    
        * Topdown checkpoint 3
    
        * Topdown Checkpoint 4
    
        * Topdown shooter checkpoint 5
    
        * Port added to UI Canvas Prefab. (#3835)
    
        * Topdown Shooter, checkpoint 6, Alpha.
    
        Almost finished, as of now, whats left is:
        Tidy up
        Comment code
        Add server defines
        Test server-only
    
        * Topdown shooter, server defines and server only tested. Checkpoint 7.
    
        Remaining: Tidy up and Comment code
    
        * TopDown Shooter finished?
    
        * Topdown shooter, fixed PNG size.
    
        * Topdown Shooter tidyup.
    
        Removed unused materials, that remained from 3D version of game.
        3D sound ranges and volumes adjusted.
    
        * Topdown Shooter, audio conversion and compression.
    
        * TopDownShooter, note about OnStartServer player list.
    
        Added line space to separate some code for easier reading.
    
    commit 6d7c808d6ad553c4dab616a5784e2ea9844f2b72
    Author: Robin Rolf <imer@imer.cc>
    Date:   Wed Jun 19 14:35:40 2024 +0000
    
        fix: new GUID for bouncycastle dll meta file (#3844)
    
    commit 736762f20f4a68c014289537e377b73cff53722b
    Author: Robin Rolf <imer@imer.cc>
    Date:   Wed Jun 19 13:53:02 2024 +0000
    
        fix: Namespace BouncyCastle lib to avoid conflicts (#3843)
    
        This uses our fork https://github.com/MirrorNetworking/bc-csharp with the Mirror. prefix
        dll is generated by CI: https://github.com/MirrorNetworking/bc-csharp/actions/runs/9582807523 so updating in the future should be as "easy" as rebasing on bouncycastle release branches, fixing any naming via search&replace and making a release to trigger CI

---
+++ Assets/Mirror/Examples/TopDownShooter/Scripts/RespawnPortal.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Mirror.Examples.TopDownShooter
+{
+    public class RespawnPortal : MonoBehaviour
+    {
+        public float rotationSpeed = 360f; // Degrees per second
+        public float shrinkDuration = 1f;  // Time in seconds to shrink to zero
+        public AudioSource soundEffect;
+
+        private Vector3 originalScale;
+        private float shrinkTimer;
+
+#if !UNITY_SERVER
+        void Awake()
+        {
+            // Store the original setup
+            originalScale = transform.localScale;
+            shrinkTimer = shrinkDuration;
+        }
+
+        void OnEnable()
+        {
+            // By using OnEnable, it shortcuts the function to be called automatically when gameobject is SetActive false/true.
+            // Here we reset variables, and then call the Portal respawn effect
+            transform.localScale = originalScale;
+            shrinkTimer = shrinkDuration;
+
+            StartCoroutine(StartEffect());
+        }
+
+        IEnumerator StartEffect()
+        {
+            soundEffect.Play();
+            while (shrinkTimer > 0)
+            {
+                transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+                if (shrinkTimer > 0)
+                {
+                    shrinkTimer -= Time.deltaTime;
+                    float scale = Mathf.Clamp01(shrinkTimer / shrinkDuration);
+                    transform.localScale = originalScale * scale;
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+#endif
+    }
+}
