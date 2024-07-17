02e9b4fc0b04faa0c087caf12527b76ae05f3b3a
JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
2024-07-17 08:58:44 +0100

    New Example, auto LAN connect + client acting as controller for server. (#3863)

---
+++ Assets/Mirror/Examples/AutoLANClientController/Scripts/NetworkSceneScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+namespace Mirror.Examples.AutoLANClientController
+{
+    public class NetworkSceneScript : NetworkBehaviour
+    {
+
+        public Button clientButton;
+        public Text textResult;
+        public GameObject panelClient, panelServer;
+
+        void Start()
+        {
+            clientButton.onClick.AddListener(ClientButton);
+
+            panelServer.SetActive(false);
+            panelClient.SetActive(false);
+
+            if (isServer)
+            {
+                panelServer.SetActive(true);
+
+            }
+            if (isClient)
+            {
+                panelClient.SetActive(true);
+            }
+        }
+
+        void ClientButton()
+        {
+            if (isClient)
+            {
+                CmdSendText("Text: " + Random.Range(0, 999));
+            }
+        }
+
+        [Command(requiresAuthority = false)]
+        public void CmdSendText(string _value)
+        {
+            textResult.text = _value;
+        }
+    }
+}
