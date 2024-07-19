da986bc1b3d8f5e36fca04b79c150aa252870a46
JesusLuvsYooh <mail@stephenallengames.co.uk>
2024-07-19 12:44:25 +0100

    Pickup Party Physics, part 12! Floppy Physics

---
+++ Assets/Mirror/Examples/PhysicsPickupParty/Scripts/JointForce.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class JointForce : MonoBehaviour
+{
+    public CharacterController characterController;
+    public Rigidbody jointRigidbody;
+    public float forceMultiplier = 100f;
+
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = characterController.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementDelta = characterController.transform.position - lastPosition;
+        Vector3 force = -movementDelta * forceMultiplier;
+        jointRigidbody.AddForce(force);
+        lastPosition = characterController.transform.position;
+    }
+}
