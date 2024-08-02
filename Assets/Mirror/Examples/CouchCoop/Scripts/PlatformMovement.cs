5b7da43d5cafb4a8618610226355ab5b60ddbfc8
JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
2024-08-02 11:57:56 +0100

    fix: Examples colliders and missing namespace. (#3881)
    * fix: Example scene and prefab colliders fixed.
    
    No idea what happened, but some box colliders seem to be double the default size.
    
    * Namespace to couch coop scripts.

--- Assets/Mirror/Examples/CouchCoop/Scripts/PlatformMovement.cs
+++ Assets/Mirror/Examples/CouchCoop/Scripts/PlatformMovement.cs
@@ -1,46 +1,49 @@
 using UnityEngine;
 using Mirror;
 
-public class PlatformMovement : NetworkBehaviour
+namespace Mirror.Examples.CouchCoop
 {
-    // A separate script to handle platform behaviour, see its partner script, MovingPlatform.cs
-    private bool onPlatform;
-    private Transform platformTransform;
-    private Vector3 lastPlatformPosition;
-
-    public override void OnStartAuthority()
+    public class PlatformMovement : NetworkBehaviour
     {
-        this.enabled = true;
-    }
+        // A separate script to handle platform behaviour, see its partner script, MovingPlatform.cs
+        private bool onPlatform;
+        private Transform platformTransform;
+        private Vector3 lastPlatformPosition;
 
-    void FixedUpdate()
-    {
-        if (onPlatform)
+        public override void OnStartAuthority()
         {
-            Vector3 deltaPosition = platformTransform.position - lastPlatformPosition;
-            transform.position += deltaPosition;
-            lastPlatformPosition = platformTransform.position;
+            this.enabled = true;
         }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-        if (collision.gameObject.tag == "Finish")
+        void FixedUpdate()
         {
-            platformTransform = collision.gameObject.GetComponent<Transform>();
-            lastPlatformPosition = platformTransform.position;
-            onPlatform = true;
+            if (onPlatform)
+            {
+                Vector3 deltaPosition = platformTransform.position - lastPlatformPosition;
+                transform.position += deltaPosition;
+                lastPlatformPosition = platformTransform.position;
+            }
         }
-    }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        // ideally set a Platform tag, but we'l just use a Unity Pre-set.
-        if (collision.gameObject.tag == "Finish")
+        private void OnCollisionEnter(Collision collision)
         {
-            onPlatform = false;
-            platformTransform = null;
+
+            if (collision.gameObject.tag == "Finish")
+            {
+                platformTransform = collision.gameObject.GetComponent<Transform>();
+                lastPlatformPosition = platformTransform.position;
+                onPlatform = true;
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            // ideally set a Platform tag, but we'l just use a Unity Pre-set.
+            if (collision.gameObject.tag == "Finish")
+            {
+                onPlatform = false;
+                platformTransform = null;
+            }
         }
     }
 }
