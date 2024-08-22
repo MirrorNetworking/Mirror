715789b1f3b3e2a48551e7858453904702e95890
mischa <16416509+miwarnec@users.noreply.github.com>
2024-08-22 09:09:58 +0200

    Edgegap Plugin: update to latest version to remove more MIRROR CHANGEs; also supports moving the folder better (#3891)
    * Edgegap Plugin: non-breaking updates first
    
    * Edgegap Plugin: update to latest version to remove more MIRROR CHANGEs; also supports moving the folder better
    
    * 2021+
    
    ---------
    
    Co-authored-by: mischa <info@noobtuts.com>
    Co-authored-by: mischa <16416509+vis2k@users.noreply.github.com>

--- Assets/Mirror/Hosting/Edgegap/Editor/EdgegapServerDataManager.cs
+++ Assets/Mirror/Hosting/Edgegap/Editor/EdgegapServerDataManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.IO;
 
 namespace Edgegap
 {
@@ -88,10 +89,15 @@ namespace Edgegap
 
         public static Status GetServerStatus() => _serverData;
 
+#if UNITY_EDITOR
+        internal static string StylesheetPath =>
+            Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets($"t:Script {nameof(EdgegapServerDataManager)}")[0]));
+#endif
+
         static EdgegapServerDataManager()
         {
 #if UNITY_EDITOR
-            _serverDataStylesheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Edgegap/Editor/EdgegapServerData.uss");
+            _serverDataStylesheet = AssetDatabase.LoadAssetAtPath<StyleSheet>($"{StylesheetPath}{Path.DirectorySeparatorChar}EdgegapServerData.uss");
 #endif
         }
         public static void RegisterServerDataContainer(VisualElement serverDataContainer)
---
+++ Assets/Mirror/Hosting/Edgegap/_MIRROR_README.md
@@ -0,0 +1,6 @@
+Notes for Mirror integration:
+
+-> Plugin needs to be zip drop in instead of package manager dependency:
+   because Asset Store doesn't allow Github dependencies.
+
+-> We have a few MIRROR CHANGES in the code for fixes / compatibility.
