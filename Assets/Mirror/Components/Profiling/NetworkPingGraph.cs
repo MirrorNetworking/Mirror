7b9d96eec5d6538805f8219b9d4c19f4a8b3588f
Robin Rolf <imer@imer.cc>
2024-09-09 18:58:09 +0000

    ping graph

---
+++ Assets/Mirror/Components/Profiling/NetworkPingGraph.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+namespace Mirror
+{
+    public class NetworkPingGraph : BaseUIGraph
+    {
+        protected override void CollectData(int category, out float value, out GraphAggregationMode mode)
+        {
+            mode = GraphAggregationMode.Average;
+            switch (category)
+            {
+                case 0:
+                    value = (float)NetworkTime.rtt * 1000f;
+                    break;
+                case 1:
+                    value = (float)NetworkTime.rttVariance * 1000f;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"{category} is not valid.");
+            }
+
+        }
+
+        protected override string FormatValue(float value)
+        {
+            return $"{value:N0}ms";
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            if (CategoryColors.Length != 2)
+            {
+                CategoryColors = new[]
+                {
+                    Color.cyan,
+                    Color.yellow,
+                };
+            }
+            IsStacked = false;
+        }
+    }
+}
---
+++ Assets/Mirror/Components/Profiling/ToggleHotkey.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+namespace Mirror
+{
+    public class ToggleHotkey : MonoBehaviour
+    {
+        public KeyCode Key = KeyCode.F10;
+        public GameObject ToToggle;
+        private void Update()
+        {
+            if (Input.GetKeyDown(Key))
+            {
+                ToToggle.SetActive(!ToToggle.activeSelf);
+            }
+        }
+    }
+}
