738a283e150900533d19a274d7dcd8e80bc06951
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-09-20 12:38:32 -0400

    PlayerTest with PlayerControllerHybrid

---
+++ Assets/Mirror/Examples/_Common/Controllers/PlayerController/PlayerControllerHybrid.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Mirror.Examples.Common.Controllers.Player
+{
+    [AddComponentMenu("Network/Player Controller (Hybrid)")]
+    [RequireComponent(typeof(NetworkTransformUnreliableCompressed))]
+    public class PlayerControllerHybrid : PlayerControllerBase { }
+}
