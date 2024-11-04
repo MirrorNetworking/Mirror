c2e117a0c2e42744804bdcb617aba4c4b635d20d
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-11-04 09:58:54 -0500

    Test RB Hybrid

---
+++ Assets/Mirror/Examples/_Common/Controllers/PlayerControllerRB/PlayerControllerRBHybrid.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Mirror.Examples.Common.Controllers.Player
+{
+    [AddComponentMenu("Network/Player Controller RB (Hybrid)")]
+    [RequireComponent(typeof(NetworkTransformHybrid2022))]
+    public class PlayerControllerRBHybrid : PlayerControllerRBBase { }
+}
