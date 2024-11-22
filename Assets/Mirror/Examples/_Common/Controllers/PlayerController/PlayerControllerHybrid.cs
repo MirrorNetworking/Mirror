8cfbccd9d705bfce3baa596cf555037a863e40d4
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-11-22 03:09:13 -0500

    feat: Added PlayerControllerHybrid - Added PlayerHybrid prefab to PlayerTest example

---
+++ Assets/Mirror/Examples/_Common/Controllers/PlayerController/PlayerControllerHybrid.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Mirror.Examples.Common.Controllers.Player
+{
+    [AddComponentMenu("Network/Player Controller (Hybrid)")]
+    [RequireComponent(typeof(NetworkTransformHybrid))]
+    public class PlayerControllerHybrid : PlayerControllerBase { }
+}
