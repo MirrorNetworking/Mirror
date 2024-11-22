5a5e3baf668011d79b1bab3e2eb11b83a6b05d64
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-11-22 03:36:39 -0500

    fix(NetworkTransformHybrid) Default SyncDir to ClientToServer Same as NT Base and NetworkAnimator

--- Assets/Mirror/Components/NetworkTransform/NetworkTransformHybrid.cs
+++ Assets/Mirror/Components/NetworkTransform/NetworkTransformHybrid.cs
@@ -1325,6 +1325,9 @@ namespace Mirror
 
         public virtual void Reset()
         {
+            // default to ClientToServer so this works immediately for users
+            syncDirection = SyncDirection.ClientToServer;
+
             // disabled objects aren't updated anymore.
             // so let's clear the buffers.
             serverSnapshots.Clear();
