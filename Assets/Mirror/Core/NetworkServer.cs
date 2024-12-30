6c2df9e630274eb9598b47252fc7851b6d84a39b
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2024-12-29 23:20:48 -0500

    fix(NetworkServer): Call NetworkClient.InvokeUnSpawnHandler from UnSpawnInternal - Fixes #3962 There may be a custom unspawn handler for the prefab and NetworkClient::OnHostClientObjectDestroy doesn't invoke it and can't because it's already out of spawned dictionary.

--- Assets/Mirror/Core/NetworkServer.cs
+++ Assets/Mirror/Core/NetworkServer.cs
@@ -1720,6 +1720,9 @@ namespace Mirror
             // in host mode, call OnStopClient/OnStopLocalPlayer manually
             if (NetworkClient.active && activeHost)
             {
+                // custom unspawn handler for this prefab (for prefab pools etc.)
+                NetworkClient.InvokeUnSpawnHandler(identity.assetId, identity.gameObject);
+
                 if (identity.isLocalPlayer)
                     identity.OnStopLocalPlayer();
 
