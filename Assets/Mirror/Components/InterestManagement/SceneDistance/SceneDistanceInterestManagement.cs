82b3f3cde4767ffc9bf3de1d389cfac8f11fb3c9
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2025-02-11 03:30:49 -0500

    fix(SceneDistanceInterestManagement): Use LateUpdate

--- Assets/Mirror/Components/InterestManagement/SceneDistance/SceneDistanceInterestManagement.cs
+++ Assets/Mirror/Components/InterestManagement/SceneDistance/SceneDistanceInterestManagement.cs
@@ -64,10 +64,10 @@ namespace Mirror
         {
             CustomRanges.Remove(identity);
 
-            // Don't RebuildSceneObservers here - that will happen in Update.
+            // Don't RebuildSceneObservers here - that will happen in LateUpdate.
             // Multiple objects could be destroyed in same frame and we don't
-            // want to rebuild for each one...let Update do it once.
-            // We must add the current scene to dirtyScenes for Update to rebuild it.
+            // want to rebuild for each one...let LateUpdate do it once.
+            // We must add the current scene to dirtyScenes for LateUpdate to rebuild it.
             if (lastObjectScene.TryGetValue(identity, out Scene currentScene))
             {
                 lastObjectScene.Remove(identity);
@@ -76,9 +76,8 @@ namespace Mirror
             }
         }
 
-        // internal so we can update from tests
         [ServerCallback]
-        internal void Update()
+        void LateUpdate()
         {
             // for each spawned:
             //   if scene changed:
