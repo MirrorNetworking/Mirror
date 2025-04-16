cac934ce1aea014e72460be9118569ffdfc756f7
JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
2025-04-16 09:09:18 +0100

    Check if counter is more than. Allows for runtime changing of SIM. (#3882)

--- Assets/Mirror/Components/NetworkTransform/NetworkTransformReliable.cs
+++ Assets/Mirror/Components/NetworkTransform/NetworkTransformReliable.cs
@@ -85,7 +85,7 @@ namespace Mirror
             // instead.
             if (isServer || (IsClientWithAuthority && NetworkClient.ready))
             {
-                if (sendIntervalCounter == sendIntervalMultiplier && (!onlySyncOnChange || Changed(Construct())))
+                if (sendIntervalCounter >= sendIntervalMultiplier && (!onlySyncOnChange || Changed(Construct())))
                     SetDirty();
 
                 CheckLastSendTime();
@@ -170,7 +170,7 @@ namespace Mirror
             // timeAsDouble not available in older Unity versions.
             if (AccurateInterval.Elapsed(NetworkTime.localTime, NetworkServer.sendInterval, ref lastSendIntervalTime))
             {
-                if (sendIntervalCounter == sendIntervalMultiplier)
+                if (sendIntervalCounter >= sendIntervalMultiplier)
                     sendIntervalCounter = 0;
                 sendIntervalCounter++;
             }
