5b69d0616af976075b32b8705dbbe78311b2fd6f
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2025-09-16 12:39:25 +0200

    feat: bar with lit part colored, unlit part grey

--- Assets/Mirror/Components/NetworkPingDisplay.cs
+++ Assets/Mirror/Components/NetworkPingDisplay.cs
@@ -11,16 +11,14 @@ namespace Mirror
     [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-ping-display")]
     public class NetworkPingDisplay : MonoBehaviour
     {
-        [Header("UI")]
         public Color color = Color.white;
         public int padding = 2;
-        public int width = 250;
+        public int width = 150;
         public int height = 25;
         
-        [Header("Bar Characters")]
+        [Header("Bar Character")]
         [Tooltip("Character to represent filled segments of the connection quality bar")]
-        public char filledChar = '■';
-        public char emptyChar  = '·';
+        public char barChar = '■';
 
 #if !UNITY_SERVER || UNITY_EDITOR
         void OnGUI()
@@ -34,18 +32,25 @@ namespace Mirror
             GUILayout.BeginArea(rect);
             GUIStyle style = GUI.skin.GetStyle("Label");
             style.alignment = TextAnchor.MiddleRight;
+            style.richText = true; // enable colored substrings
+            
+            GUILayout.BeginHorizontal(style);
             
             double rttMs = Math.Round(NetworkTime.rtt * 1000);
             var quality = NetworkClient.connectionQuality;
             int totalSegments = Enum.GetValues(typeof(ConnectionQuality)).Length - 1;
             int filled = Mathf.Clamp((int)quality, 0, totalSegments);
-            string bar = new string(filledChar, filled) + new string(emptyChar, totalSegments - filled);
 
-            GUILayout.BeginHorizontal(style);
+            string lit   = new string(barChar, filled);
+            string unlit = new string(barChar, totalSegments - filled);
+            string litColor  = ColorUtility.ToHtmlStringRGB(quality.ColorCode());
+            string unlitColor = "666666";
+            
+            // Q: [■■■■] (lit part colored, unlit part grey)
+            string bar = $"[<color=#{litColor}>{lit}</color><color=#{unlitColor}>{unlit}</color>]";
             GUILayout.Label($"RTT: {rttMs} ms");
-            GUI.color = quality.ColorCode();
-            GUILayout.Label($"Q: {quality} [{bar}]");
-                
+            GUILayout.Label($"Q: {bar}");
+            
             GUILayout.EndHorizontal();
             GUILayout.EndArea();
             GUI.color = Color.white;
