633aa8c97b05f0db822255489b0370a05d7b4e29
miwarnec <16416509+miwarnec@users.noreply.github.com>
2025-11-05 12:19:45 +0100

    fix: #4058 [Command] on host now populates NetworkConnectionToClient automatically

--- Assets/Mirror/Editor/Weaver/WeaverTypes.cs
+++ Assets/Mirror/Editor/Weaver/WeaverTypes.cs
@@ -13,6 +13,7 @@ namespace Mirror.Weaver
         public MethodReference NetworkBehaviourIsClientReference;
         public MethodReference NetworkBehaviourIsServerReference;
         public FieldReference NetworkBehaviourDirtyBitsReference;
+        public MethodReference NetworkBehaviourConnectionToClientReference;
         public MethodReference GetWriterReference;
         public MethodReference ReturnWriterReference;
 
@@ -96,6 +97,8 @@ namespace Mirror.Weaver
             NetworkBehaviourIsServerReference = Resolvers.ResolveMethod(NetworkBehaviourType, assembly, Log, "get_isServer", ref WeavingFailed);
 
             NetworkBehaviourDirtyBitsReference = Resolvers.ResolveField(NetworkBehaviourType, assembly, Log, "syncVarDirtyBits", ref WeavingFailed);
+
+            NetworkBehaviourConnectionToClientReference = Resolvers.ResolveMethod(NetworkBehaviourType, assembly, Log, "get_connectionToClient", ref WeavingFailed);
 
             generatedSyncVarSetter = Resolvers.ResolveMethod(NetworkBehaviourType, assembly, Log, "GeneratedSyncVarSetter", ref WeavingFailed);
             generatedSyncVarSetter_GameObject = Resolvers.ResolveMethod(NetworkBehaviourType, assembly, Log, "GeneratedSyncVarSetter_GameObject", ref WeavingFailed);
