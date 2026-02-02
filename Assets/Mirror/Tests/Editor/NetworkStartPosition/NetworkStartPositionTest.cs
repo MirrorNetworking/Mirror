c0b6129ec04977d3c94fe10854719e195fe3daf7
Nietod <nico-berndt@gmx.de>
2026-02-02 18:53:25 +0100

    Changed Test to be a LifecycleTest

--- Assets/Mirror/Tests/Editor/NetworkStartPosition/NetworkStartPositionTest.cs
+++ Assets/Mirror/Tests/Editor/NetworkStartPosition/NetworkStartPositionTest.cs
@@ -9,42 +9,33 @@ namespace Mirror.Tests.Editor
         public override void SetUp()
         {
             NetworkManager.startPositions.Clear();
-
             base.SetUp();
         }
 
-        [Test]
-        public void NetworkStartPositionOnAwakeTest()
+        [TearDown]
+        public override void TearDown()
         {
-            Assert.That(NetworkManager.startPositions.Count, Is.Zero);
-
-            CreateGameObject(out GameObject startPositionGameObject, out NetworkStartPosition networkStartPosition);
-
-            networkStartPosition.Awake();
-            Assert.That(NetworkManager.startPositions.Count, Is.EqualTo(1));
-            Assert.That(NetworkManager.startPositions, Has.Member(startPositionGameObject.transform));
-
-            Object.DestroyImmediate(startPositionGameObject);
             NetworkManager.startPositions.Clear();
+            base.TearDown();
         }
 
         [Test]
-        public void NetworkStartPositionOnDestroyTest()
+        public void NetworkStartPositionLifecycleTest()
         {
             Assert.That(NetworkManager.startPositions.Count, Is.Zero);
 
             CreateGameObject(out GameObject startPositionGameObject, out NetworkStartPosition networkStartPosition);
 
+            // Must call Unity lifecycle methods manually in edit mode tests
             networkStartPosition.Awake();
             Assert.That(NetworkManager.startPositions.Count, Is.EqualTo(1));
             Assert.That(NetworkManager.startPositions, Has.Member(startPositionGameObject.transform));
 
+            // Must call Unity lifecycle methods manually in edit mode tests
             networkStartPosition.OnDestroy();
+            Object.DestroyImmediate(startPositionGameObject);
 
             Assert.That(NetworkManager.startPositions.Count, Is.Zero);
-
-            Object.DestroyImmediate(startPositionGameObject);
-            NetworkManager.startPositions.Clear();
         }
     }
 }
