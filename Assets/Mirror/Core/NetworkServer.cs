71b89d63754dc57fd883760c9b9d862a10fdb223
MrGadget <9826063+MrGadget1024@users.noreply.github.com>
2026-02-27 13:21:49 -0500

    feat: Quiet Mode for certain game types Network Manager now has a checkbox to enable Quiet Mode - Disables TimeSnapshotMessage entirely - Forces PingMessage frequency to 0.5Hz (2 sec interval) - Only for games NOT using NT or PredictedRB (warnings included) - Unit tests updated accordingly

--- Assets/Mirror/Core/NetworkServer.cs
+++ Assets/Mirror/Core/NetworkServer.cs
@@ -2272,7 +2272,7 @@ namespace Mirror
                 //   pull in UpdateVarsMessage for each entity it observes
                 if (connection.isReady)
                 {
-                    // send time for snapshot interpolation every sendInterval.
+                    // send time for snapshot interpolation every sendInterval unless quietMode is enabled.
                     // BroadcastToConnection() may not send if nothing is new.
                     //
                     // sent over unreliable.
@@ -2281,7 +2281,8 @@ namespace Mirror
                     // make sure Broadcast() is only called every sendInterval,
                     // even if targetFrameRate isn't set in host mode (!)
                     // (done via AccurateInterval)
-                    connection.Send(new TimeSnapshotMessage(), Channels.Unreliable);
+                    if (!NetworkTime.quietMode)
+                        connection.Send(new TimeSnapshotMessage(), Channels.Unreliable);
 
                     // broadcast world state to this connection
                     BroadcastToConnection(connection, unreliableBaselineElapsed);
